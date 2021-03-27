d71d56c6195fd67d0b8b73c9f28b6a8325ebac47
archibate <1931127624@qq.com>
2021-03-27 13:12:27 +0800

    standardize memory sync

--- zenbase/ViewMesh.cpp
+++ zenbase/ViewMesh.cpp
@@ -11,15 +11,17 @@ struct ViewMesh : zen::INode {
   std::vector<float> vertex_data;
 
   virtual void apply() override {
+    /**************/
     auto mesh = get_input("mesh")->as<zenbase::MeshObject>();
-
     size_t vertex_count = mesh->vertices.size();
-
     size_t memsize = vertex_count * 8 * sizeof(float);
-    const char *mempath = "/tmp/zenipc/mesh01";
-    SharedMemory shm(mempath, memsize);
+    /**************/
+
+    Socket sock("/tmp/zenipc/command");
+    SharedMemory shm("/tmp/zenipc/memory", memsize);
     float *memdata = (float *)shm.data();
 
+    /**************/
     int memi = 0;
     for (int i = 0; i < vertex_count; i++) {
       memdata[memi++] = mesh->vertices[i].x;
@@ -31,11 +33,11 @@ struct ViewMesh : zen::INode {
       memdata[memi++] = mesh->normals[i].y;
       memdata[memi++] = mesh->normals[i].z;
     }
+    /**************/
 
     shm.release();
 
-    Socket sock("/tmp/zenipc/command");
-    dprintf(sock.filedesc(), "@MESH %zd %s", vertex_count, mempath);
+    dprintf(sock.filedesc(), "@MESH %zd\n", memsize);
   }
 };
 
--- zenvis/main.cpp
+++ zenvis/main.cpp
@@ -1,8 +1,28 @@
 #include <Hg/IPC/Socket.hpp>
+#include <Hg/IPC/SharedMemory.hpp>
+#include <cstring>
+#include <vector>
+#include <memory>
+#include <string>
+#include <map>
 
 
-int main(int argc, char **argv)
-{
+struct ObjectData {
+  std::unique_ptr<std::vector<char>> serial;
+  std::string type;
+};
+
+
+struct FrameData {
+  std::vector<std::unique_ptr<ObjectData>> objects;
+};
+
+
+std::map<int, std::unique_ptr<FrameData>> frames;
+int frameid = 1;
+
+
+void server_loop() {
   Socket::Server serv("/tmp/zenipc/command");
 
   while (true) {
@@ -11,13 +31,29 @@ int main(int argc, char **argv)
     char buf[1024];
     size_t num = sock.read(buf, sizeof(buf));
     buf[num] = 0;
+    printf("COMMAND: %s\n", buf);
 
+    size_t memsize = 0;
     char type[32] = {0};
-    char mempath[1024] = {0};
-    size_t vertex_count;
-    sscanf(buf, "@%s %zd %s", type, &vertex_count, mempath);
-    printf("recv: %s, %zd, %s\n", type, vertex_count, mempath);
+    sscanf(buf, "@%s%zd", type, &memsize);
+    SharedMemory shm("/tmp/zenipc/memory", memsize);
+
+    if (frames.find(frameid) == frames.end()) {
+      frames[frameid] = std::make_unique<FrameData>();
+    }
+    auto const &frm = frames.at(frameid);
+
+    auto obj = std::make_unique<ObjectData>();
+    obj->serial = std::make_unique<std::vector<char>>(shm.size());
+    std::memcpy(obj->serial->data(), shm.data(), shm.size());
+    obj->type = std::string(type);
+    frm->objects.push_back(std::move(obj));
   }
- 
+}
+
+
+int main(int argc, char **argv)
+{
+  server_loop();
   return 0;
 }
