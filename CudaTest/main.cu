57bf4544281fa772e07f0a82c7610cc74ff93ebc
archibate <1931127624@qq.com>
2021-04-05 15:48:23 +0800

    works managed array

--- CudaTest/main.cu
+++ CudaTest/main.cu
@@ -10,29 +10,33 @@ __global__ void test() { printf("FuCK U NVIDIA!\n"); } int main(void) { test<<<1
 #include <cstdio>
 #include <cmath>
 
-template <size_t Nx>
-__global__ void blur(Array<float, Nx> *arr)
+__global__ void blur(MemoryArray<float> *parr)
 {
+  auto &arr = *parr;
   size_t ix = blockIdx.x * blockDim.x + threadIdx.x;
-  if (ix < Nx) {
-    (*arr)(ix) = 2 * (*arr)(ix);
+  printf("gpu:%d\n", (int)arr.size());
+  if (ix < arr.size()) {
+    arr(ix) = 2 * arr(ix);
   }
 }
 
 int main(void)
 {
-  const size_t Nx = 16384 * 1024;
-  auto arr = new Array<float, Nx>();
+  const size_t nx = 1 * 1;
+  auto parr = new MemoryArray<float>(nx);
+  auto &arr = *parr;
 
-  for (size_t x = 0; x < Nx; x++) {
-    (*arr)(x) = drand48();
+  for (size_t ix = 0; ix < nx; ix++) {
+    arr(ix) = drand48();
   }
+  printf("cpu:%d\n", (int)arr.size());
 
   for (size_t i = 0; i < 256; i++) {
-    blur<Nx><<<16384, 1024>>>(arr);
+    blur<<<(nx + 1023) / 1024, (nx < 1024 ? nx : 1024)>>>(parr);
   }
   checkCudaErrors(cudaDeviceSynchronize());
 
+  delete parr;
   return 0;
 }
 #endif
--- CudaTest/managed.cuh
+++ CudaTest/managed.cuh
@@ -13,8 +13,22 @@ public:
   }
 };
 
+template <typename T>
+class ManagedSlot : public Managed {
+  T core;
+
+public:
+  __host__ __device__ T &get() {
+    return core;
+  }
+
+  __host__ __device__ T const &get() const {
+    return core;
+  }
+};
+
 template <typename T, size_t N>
-class Array : public Managed {
+class ManagedArray : public Managed {
   T core[N];
 
 public:
@@ -24,5 +38,58 @@ public:
 
   __host__ __device__ T const &operator()(int i) const {
     return core[i];
+  }
+};
+
+class Memory : public Managed {
+  void *ptr{nullptr};
+  size_t len{0};
+
+  __host__ __device__ Memory(Memory const &) = delete;
+  __host__ __device__ Memory(Memory &&) = delete;
+
+public:
+  __host__ Memory(size_t len) : len(len) {
+    printf("Memory(%d)\n", (int)len);
+    cudaMallocManaged(&ptr, len);
+  }
+
+  __host__ __device__ void *data() const {
+    return ptr;
+  }
+
+  __host__ __device__ size_t size() const {
+    return len;
+  }
+
+  __host__ ~Memory() {
+    printf("~Memory(%d)\n", (int)len);
+    cudaFree(ptr);
+  }
+};
+
+template <typename T>
+class MemoryArray : public Managed {
+  Memory mem;
+  size_t len;
+
+  __host__ __device__ MemoryArray(MemoryArray const &) = delete;
+  __host__ __device__ MemoryArray(MemoryArray &&) = delete;
+
+public:
+  __host__ MemoryArray(size_t len)
+    : len(len), mem(len * sizeof(T)) {
+  }
+
+  __host__ __device__ T *data() const {
+    return (T *)mem.data();
+  }
+
+  __host__ __device__ size_t size() const {
+    return len;
+  }
+
+  __host__ __device__ T &operator()(int i) const {
+    return data()[i];
   }
 };
