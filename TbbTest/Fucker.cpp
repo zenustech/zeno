d7cc8234414c1f618054bf40427da51ff92aad01
zhxx1987 <zhangshinshin@gmail.com>
2021-04-05 14:37:05 +0800

    cannot reproduce

--- TbbTest/Fucker.cpp
+++ TbbTest/Fucker.cpp
@@ -2,12 +2,10 @@
 #include "tbb/scalable_allocator.h"
 #include <iostream>
 
-static int fucker_main()
+int fucker_main()
 {
   auto msg = new std::string("Fucker load");
   std::cout << *msg << std::endl;
   delete msg;
   return 0;
 }
-
-static int fucker = fucker_main();
