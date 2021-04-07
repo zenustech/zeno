b416361bf4586bbd75178e67ae701433bfa8f7f1
archibate <1931127624@qq.com>
2021-04-07 14:48:31 +0800

    add NDView

--- CudaTest/main.cu
+++ CudaTest/main.cu
@@ -5,36 +5,80 @@ __global__ void test() { printf("FuCK U NVIDIA!\n"); } int main(void) { test<<<1
 
 #include "helper_cuda.h"
 #include "helper_math.h"
-#include "managed.cuh"
 #include <cassert>
 #include <cstdio>
 #include <cmath>
 
-const size_t Nx = 128;
+template <class T, size_t N>
+class Array {
+  T m_data[N];
 
-template <class T>
-__global__ void blur(T arr)
-{
-  size_t ix = blockIdx.x * blockDim.x + threadIdx.x;
-  if (ix < Nx) {
-    arr[ix] = ix + 1;
+public:
+  Array() = default;
+  Array(Array const &) = default;
+  Array(Array &&) = default;
+
+  __host__ __device__ Array(std::initializer_list<T> const &args) {
+    int i = 0;
+    for (auto const &value: args) {
+      m_data[i++] = value;
+    }
+    for (; i < N; i++) {
+      m_data[i] = T(0);
+    }
   }
+
+  __host__ __device__ T &operator[](long i) {
+    return m_data[i];
+  }
+
+  __host__ __device__ T const &operator[](long i) const {
+    return m_data[i];
+  }
+};
+
+struct NDView {
+  static constexpr long Dims = 1;
+  Array<long, Dims> stride;
+  Array<long, Dims> shape;
+  void *base{nullptr};
+
+  NDView() = default;
+  NDView(NDView const &) = default;
+  NDView(NDView &&) = default;
+
+  __host__ __device__ void *operator()(Array<long, Dims> const &indices) {
+    long offset = 0;
+    for (int i = 0; i < Dims; i++) {
+      offset += stride[i] * indices[i];
+    }
+    return (char *)base + offset;
+  }
+};
+
+__global__ void blur(NDView arr)
+{
+  long ix = blockIdx.x * blockDim.x + threadIdx.x;
+  *(int *)arr({ix}) = ix + 1;
 }
 
 int main(void)
 {
-  Field<Dense<Pointer<Dense<Place<int>, 4, 0>>, Nx / 4, 0>> arr_; auto arr = arr_.copy();
-
-  for (size_t ix = 0; ix < Nx; ix++) {
-    arr.activate(ix);
-    arr[ix] = 3;
+  NDView arr;
+  arr.shape[0] = 16;
+  arr.stride[0] = sizeof(int);
+  cudaMallocManaged(&arr.base, arr.shape[0] * arr.stride[0]);
+  for (long ix = 0; ix < 16; ix++) {
+    *(int *)arr({ix}) = 3;
   }
-  blur<<<(Nx + 1023) / 1024, (Nx < 1024 ? Nx : 1024)>>>(arr);
+
+  blur<<<4, 4>>>(arr);
   checkCudaErrors(cudaDeviceSynchronize());
-  for (size_t ix = 0; ix < Nx; ix++) {
-    printf("%d\n", arr[ix]);
+  for (long ix = 0; ix < 16; ix++) {
+    printf("%d\n", *(int *)arr({ix}));
   }
 
+  cudaFree(arr.base);
   return 0;
 }
 #endif
