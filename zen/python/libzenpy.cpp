2312a5c30b172ba994b4d834b0df96ca563ba6b8
archibate <1931127624@qq.com>
2021-04-08 16:53:42 +0800

    catch zen::Exception to print exception.what() in python

--- zen/python/libzenpy.cpp
+++ zen/python/libzenpy.cpp
@@ -4,9 +4,18 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(libzenpy, m) {
-    m.def("addNode", zen::addNode);
-    m.def("setNodeParam", zen::setNodeParam);
-    m.def("setNodeInput", zen::setNodeInput);
-    m.def("applyNode", zen::applyNode);
-    m.def("dumpDescriptors", zen::dumpDescriptors);
+  m.def("addNode", zen::addNode);
+  m.def("setNodeParam", zen::setNodeParam);
+  m.def("setNodeInput", zen::setNodeInput);
+  m.def("applyNode", zen::applyNode);
+  m.def("dumpDescriptors", zen::dumpDescriptors);
+
+  py::register_exception_translator([](std::exception_ptr p) {
+    try {
+      if (p)
+        std::rethrow_exception(p);
+    } catch (zen::Exception const &e) {
+      PyErr_SetString(PyExc_RuntimeError, e.what());
+    }
+  });
 }
