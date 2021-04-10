14b6dbc933484938bffc980d216b012e2c474e90
archibate <1931127624@qq.com>
2021-04-10 09:53:17 +0800

    better ndarray

--- CudaTest/main.cu
+++ CudaTest/main.cu
@@ -10,18 +10,37 @@ __global__ void test() { printf("FuCK U NVIDIA!\n"); } int main(void) { test<<<1
 #include <cstdio>
 #include <cmath>
 
+__global__ void blur(NDTypedView<int> arr)
+{
+  size_t ix = blockIdx.x * blockDim.x + threadIdx.x;
+  size_t iy = blockIdx.y * blockDim.y + threadIdx.y;
+
+  arr({ix, iy}) = ix + 1;
+
+  __shared__ char tmpData[16 * sizeof(int)];
+  NDTypedView<int> tmp({16}, {sizeof(int)}, tmpData);
+}
+
 int main(void)
 {
-  NDArray arr({16}, {sizeof(int)});
+  size_t nx = 32, ny = 32;
+  NDArray arr({nx, ny}, {sizeof(int), sizeof(int) * nx});
 
-  for (ssize_t ix = 0; ix < 16; ix++) {
-    *(int *)arr({ix}) = 3;
+  for (size_t iy = 0; iy < ny; iy++) {
+    for (size_t ix = 0; ix < nx; ix++) {
+      *(int *)arr({ix, iy}) = 233;
+    }
   }
 
-  blur<<<4, 4>>>(arr);
+  dim3 griddim{(unsigned)nx / 8, (unsigned)ny / 8, 1};
+  dim3 blockdim{8, 8, 1};
+  blur<<<griddim, blockdim>>>(arr);
   checkCudaErrors(cudaDeviceSynchronize());
-  for (ssize_t ix = 0; ix < 16; ix++) {
-    printf("%d\n", *(int *)arr({ix}));
+
+  for (size_t iy = 0; iy < ny; iy++) {
+    for (size_t ix = 0; ix < nx; ix++) {
+      printf("%d\n", *(int *)arr({ix, iy}));
+    }
   }
 
   return 0;
