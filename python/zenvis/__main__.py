b89cbccf2041f21afb437d823b6b864d83ffd047
archibate <1931127624@qq.com>
2021-04-15 09:39:45 +0800

    use QtOpenGL for zenvis

--- python/zenvis/__main__.py
+++ python/zenvis/__main__.py
@@ -1,13 +1,45 @@
 from . import core
+
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtOpenGL import QGLWidget
 from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
-import time
 
 
-class App:
-    def __init__(self, nx=960, ny=800):
-        self.nx, self.ny = nx, ny
+class MainWindow(QMainWindow):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+ 
+		self.setWindowTitle("zenvis")
+		self.resize(1100, 650)
+		screen = QDesktopWidget().geometry()
+		self_size = self.geometry()
+		self.move(
+                (screen.width() - self_size.width()) // 2,
+                (screen.height() - self_size.height()) // 2)
+ 
+		splitter = QSplitter(Qt.Vertical)
+		widget = OpenGLWidget()
+		splitter.addWidget(widget)
+		testedit = QTextEdit()
+		splitter.addWidget(testedit)
+		splitter.setStretchFactor(0, 3)
+		splitter.setStretchFactor(1, 2)
+		splitter_main = QSplitter(Qt.Horizontal)
+		textedit_main = QTextEdit()
+		splitter_main.addWidget(textedit_main)
+		splitter_main.addWidget(splitter)
+		splitter_main.setStretchFactor(0, 1)
+		splitter_main.setStretchFactor(1, 4)
+		self.setCentralWidget(splitter_main)
+ 
+
+class OpenGLWidget(QGLWidget):
+    def initializeGL(self):
+        core.initialize()
 
     @property
     def curr_frameid(self):
@@ -17,24 +49,16 @@ class App:
     def curr_frameid(self, value):
         return core.set_curr_frameid(value)
 
-    def draw(self):
-        self.nx = glutGet(GLUT_WINDOW_WIDTH)
-        self.ny = glutGet(GLUT_WINDOW_HEIGHT)
-        core.set_window_size(self.nx, self.ny)
+    def resizeGL(self, nx, ny):
+        print('resize', nx, ny)
+        core.set_window_size(nx, ny)
 
+    def paintGL(self):
         core.new_frame()
-        glFlush()
-        time.sleep(1 / 60)
-
-    def mainloop(self):
-        glutInit()
-        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA)
-        glutInitWindowSize(self.nx, self.ny)
-        glutCreateWindow('zenvis')
-        glutDisplayFunc(self.draw)
-        glutIdleFunc(self.draw)
-        core.initialize()
-        glutMainLoop()
-        core.finalize()
-
-App().mainloop()
+ 
+ 
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	win = MainWindow()
+	win.show()
+	sys.exit(app.exec_())
