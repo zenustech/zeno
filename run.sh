a3ad2fc2bb2a3846c500001eb3a0281a37bea891
archibate <1931127624@qq.com>
2021-05-02 17:43:06 +0800

    add build docs and build on new machine

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 export PYTHONPATH=`pwd`/python
-kill `lsof -i tcp:8000 | awk '{print $2}' | grep -v PID | uniq` 2> /dev/null
-python -m zenserver &
-sleep 0.1
 python -m zenclient
-kill %1
-wait
