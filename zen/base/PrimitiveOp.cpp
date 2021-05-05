74e07700e67814dff132e6ccb0db1251721c4f97
archibate <1931127624@qq.com>
2021-05-05 18:01:23 +0800

    add PrimaryBinaryOp

--- zen/base/PrimitiveOp.cpp
+++ zen/base/PrimitiveOp.cpp
@@ -77,15 +77,15 @@ struct UnaryOperator {
     UnaryOperator(FuncT const &func) : func(func) {}
 
     template <class TOut, class TA>
-    void operator()(std::vector<TOut> &arrOut, std::vector<TA> arrA) {
+    void operator()(std::vector<TOut> &arrOut, std::vector<TA> const &arrA) {
+        size_t n = std::min(arrOut.size(), arrA.size());
         #pragma omp parallel for
-        for (int i = 0; i < arrA.size(); i++) {
+        for (int i = 0; i < n; i++) {
             auto val = func(arrA[i]);
             arrOut[i] = decltype(arrOut[i])(val);
         }
     }
 };
-
 
 struct PrimitiveUnaryOp : zen::INode {
   virtual void apply() override {
@@ -94,8 +94,8 @@ struct PrimitiveUnaryOp : zen::INode {
     auto attrA = std::get<std::string>(get_param("attrA"));
     auto attrOut = std::get<std::string>(get_param("attrOut"));
     auto op = std::get<std::string>(get_param("op"));
-    auto &arrOut = primOut->attr(attrOut);
     auto const &arrA = primA->attr(attrA);
+    auto &arrOut = primOut->attr(attrOut);
     std::visit([op](auto &arrOut, auto const &arrA) {
         if (0) {
 #define _PER_OP(opname, expr) \
@@ -112,12 +112,31 @@ struct PrimitiveUnaryOp : zen::INode {
         _PER_OP("atan", glm::atan(a))
         _PER_OP("exp", glm::exp(a))
         _PER_OP("log", glm::log(a))
+#undef _PER_OP
         } else {
             printf("%s\n", op.c_str());
             assert(0 && "Bad operator type");
         }
     }, arrOut, arrA);
   }
+};
+
+
+template <class FuncT>
+struct BinaryOperator {
+    FuncT func;
+    BinaryOperator(FuncT const &func) : func(func) {}
+
+    template <class TOut, class TA, class TB>
+    void operator()(std::vector<TOut> &arrOut,
+        std::vector<TA> const &arrA, std::vector<TB> const &arrB) {
+        size_t n = std::min(arrOut.size(), std::min(arrA.size(), arrB.size()));
+        #pragma omp parallel for
+        for (int i = 0; i < n; i++) {
+            auto val = func(arrA[i], arrB[i]);
+            arrOut[i] = decltype(arrOut[i])(val);
+        }
+    }
 };
 
 static int defPrimitiveUnaryOp = zen::defNodeClass<PrimitiveUnaryOp>("PrimitiveUnaryOp",
@@ -127,6 +146,61 @@ static int defPrimitiveUnaryOp = zen::defNodeClass<PrimitiveUnaryOp>("PrimitiveU
     }, /* outputs: */ {
     }, /* params: */ {
     {"string", "attrA", "pos"},
+    {"string", "attrOut", "pos"},
+    {"string", "op", "copy"},
+    }, /* category: */ {
+    "primitive",
+    }});
+
+
+struct PrimitiveBinaryOp : zen::INode {
+  virtual void apply() override {
+    auto primA = get_input("primA")->as<PrimitiveObject>();
+    auto primB = get_input("primB")->as<PrimitiveObject>();
+    auto primOut = get_input("primOut")->as<PrimitiveObject>();
+    auto attrA = std::get<std::string>(get_param("attrA"));
+    auto attrB = std::get<std::string>(get_param("attrB"));
+    auto attrOut = std::get<std::string>(get_param("attrOut"));
+    auto op = std::get<std::string>(get_param("op"));
+    auto const &arrA = primA->attr(attrA);
+    auto const &arrB = primB->attr(attrB);
+    auto &arrOut = primOut->attr(attrOut);
+    std::visit([op](auto &arrOut, auto const &arrA, auto const &arrB) {
+        if (0) {
+#define _PER_OP(opname, expr) \
+        } else if (op == opname) { \
+            BinaryOperator([](auto const &a_, auto const &b_) { \
+                using PromotedType = decltype(a_ + b_); \
+                auto a = PromotedType(a_); \
+                auto b = PromotedType(b_); \
+                return expr; \
+            })(arrOut, arrA, arrB);
+        _PER_OP("copyA", a)
+        _PER_OP("copyB", b)
+        _PER_OP("add", a + b)
+        _PER_OP("sub", a - b)
+        _PER_OP("mul", a * b)
+        _PER_OP("div", a / b)
+        _PER_OP("pow", glm::pow(a, b))
+        _PER_OP("atan2", glm::atan(a, b))
+#undef _PER_OP
+        } else {
+            printf("%s\n", op.c_str());
+            assert(0 && "Bad operator type");
+        }
+    }, arrOut, arrA, arrB);
+  }
+};
+
+static int defPrimitiveBinaryOp = zen::defNodeClass<PrimitiveBinaryOp>("PrimitiveBinaryOp",
+    { /* inputs: */ {
+    "primA",
+    "primB",
+    "primOut",
+    }, /* outputs: */ {
+    }, /* params: */ {
+    {"string", "attrA", "pos"},
+    {"string", "attrN", "pos"},
     {"string", "attrOut", "pos"},
     {"string", "op", "copy"},
     }, /* category: */ {
