7e5c3fac48d74f16a689e71cf6320b10557fdcdb
littlemine <wxlwxl1993@zju.edu.cn>
2021-05-14 21:43:14 +0800

    use wrapped version of partition_for_particles

--- MPM/nosys/PartitionForParticles.cpp
+++ MPM/nosys/PartitionForParticles.cpp
@@ -2,7 +2,7 @@
 #include "../ZensimGeometry.h"
 //#include "zensim/cuda/execution/ExecutionPolicy.cuh"
 #include "zensim/geometry/VdbLevelSet.h"
-#include "zensim/simulation/sparsity/SparsityOp.hpp"
+#include "zensim/simulation/sparsity/SparsityCompute.hpp"
 #include <zen/VDBGrid.h>
 
 namespace zenbase {
@@ -16,24 +16,8 @@ struct SpatialPartitionForParticles : zen::INode {
     // pass in FloatGrid::Ptr
     auto &particles = get_input("ZSParticles")->as<ZenoParticles>()->get();
 
-#if 0
-    zs::HashTable<int, 3, zs::i64> ret{zs::match([](auto &particles) {
-                                         return particles.size() / blocklen /
-                                                blocklen / blocklen;
-                                       })(particles),
-                                       zs::memsrc_e::um, 0};
-    auto cudaPol = zs::cuda_exec().device(0);
-    cudaPol({ret._tableSize},
-            zs::CleanSparsity{zs::wrapv<zs::execspace_e::cuda>{}, ret});
+    partition->get() = zs::partition_for_particles(particles, dx, blocklen);
 
-    zs::match([&ret, dx, blocklen](auto &particles) {
-      cudaPol({particles.size()},
-              zs::ComputeSparsity{zs::wrapv<zs::execspace_e::cuda>{}, dx,
-                                  blocklen, ret, particles.X});
-    })(particles);
-
-    partition->get() = std::move(ret);
-#endif
     set_output("ZSPartition", partition);
   }
 };
