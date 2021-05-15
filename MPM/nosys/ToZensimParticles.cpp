3f74a1bb7f4b6555f2086d11e7e8c1b62b7b8c70
littlemine <wxlwxl1993@zju.edu.cn>
2021-05-16 00:19:23 +0800

    model dependent particle initialization

--- MPM/nosys/ToZensimParticles.cpp
+++ MPM/nosys/ToZensimParticles.cpp
@@ -2,38 +2,75 @@
 #include <zen/zen.h>
 
 #include "../ZensimGeometry.h"
+#include "../ZensimModel.h"
 #include "zensim/simulation/mpm/Simulator.hpp"
 
 namespace zenbase {
 
 struct ToZensimParticles : zen::INode {
   void apply() override {
+    auto &model = get_input("ZSModel")->as<ZenoConstitutiveModel>()->get();
     auto inParticles = get_input("ParticleObject")->as<ParticlesObject>();
     auto outParticles = zen::IObject::make<ZenoParticles>();
     const auto size = inParticles->size();
     outParticles->get() = zs::Particles<zs::f32, 3>{};
+
+    const bool hasPlasticity =
+        model.index() ==
+            magic_enum::enum_integer(zs::constitutive_model_e::DruckerPrager) ||
+        model.index() ==
+            magic_enum::enum_integer(zs::constitutive_model_e::NACC);
+    const bool hasF =
+        model.index() !=
+        magic_enum::enum_integer(zs::constitutive_model_e::EquationOfState);
     {
-      zs::match([&inParticles, size](auto &res) {
-        using PT = zs::remove_cvref_t<decltype(res)>;
-        using TV = typename PT::TV;
-        using TM = typename PT::TM;
-        res.X = zs::Vector<TV>{size, zs::memsrc_e::um, 0};
-        res.V = zs::Vector<TV>{size, zs::memsrc_e::um, 0};
-        for (auto &&[dst, src] : zs::zip(res.X, inParticles->pos))
-          dst = TV{src[0], src[1], src[2]};
-        for (auto &&[dst, src] : zs::zip(res.V, inParticles->vel))
-          dst = TV{src[0], src[1], src[2]};
-        res.C = zs::Vector<TM>{size, zs::memsrc_e::um, 0};
-        for (auto &&c : res.C)
-          c = TM{1, 0, 0, 0, 1, 0, 0, 0, 1};
-      })(outParticles->get());
+      zs::match(
+          [&inParticles, size, hasF, hasPlasticity](auto &model, auto &res) {
+            using PT = zs::remove_cvref_t<decltype(res)>;
+            using T = typename PT::T;
+            using TV = typename PT::TV;
+            using TM = typename PT::TM;
+            using ModelT = zs::remove_cvref_t<decltype(model)>;
+
+            res.M = zs::Vector<T>{size, zs::memsrc_e::um, 0};
+            res.X = zs::Vector<TV>{size, zs::memsrc_e::um, 0};
+            res.V = zs::Vector<TV>{size, zs::memsrc_e::um, 0};
+            if (hasF)
+              res.F = zs::Vector<TM>{size, zs::memsrc_e::um, 0};
+            else
+              res.J = zs::Vector<T>{size, zs::memsrc_e::um, 0};
+            res.C = zs::Vector<TM>{size, zs::memsrc_e::um, 0};
+            if (hasPlasticity)
+              res.logJp = zs::Vector<T>{size, zs::memsrc_e::um, 0};
+
+            auto mass = model.volume * model.rho;
+            for (auto &&m : res.M)
+              m = mass;
+            for (auto &&[dst, src] : zs::zip(res.X, inParticles->pos))
+              dst = TV{src[0], src[1], src[2]};
+            for (auto &&[dst, src] : zs::zip(res.V, inParticles->vel))
+              dst = TV{src[0], src[1], src[2]};
+            for (auto &&c : res.C)
+              c = TM{0, 0, 0, 0, 0, 0, 0, 0, 0};
+            if (hasF)
+              for (auto &&f : res.F)
+                f = TM{1, 0, 0, 0, 1, 0, 0, 0, 1};
+            else
+              for (auto &&j : res.J)
+                j = 1.f;
+            if constexpr (std::is_same_v<ModelT, zs::NACCConfig> ||
+                          std::is_same_v<ModelT, zs::DruckerPragerConfig>) {
+              for (auto &&logjp : res.logJp)
+                logjp = model.logJp0;
+            }
+          })(model, outParticles->get());
     }
     set_output("ZensimParticles", outParticles);
   }
 };
 
 static int defToZensimParticles = zen::defNodeClass<ToZensimParticles>(
-    "ToZensimParticles", {/* inputs: */ {"ParticleObject"},
+    "ToZensimParticles", {/* inputs: */ {"ZSModel", "ParticleObject"},
                           /* outputs: */
                           {"ZensimParticles"},
                           /* params: */
