716ddb5479ee3fdaf871605e5046e3d6f19b236f
archibate <1931127624@qq.com>
2021-05-17 11:14:40 +0800

    support line shading and PrimitiveSimpleLines

--- zenvis/GraphicPrimitive.cpp
+++ zenvis/GraphicPrimitive.cpp
@@ -10,9 +10,13 @@
 namespace zenvis {
 
 struct GraphicPrimitive : IGraphic {
-  Program *prog;
+  Program *points_prog;
+  Program *lines_prog;
+
   size_t vertex_count;
   std::unique_ptr<Buffer> vbo;
+  std::unique_ptr<Buffer> ebo;
+  size_t lines_count;
 
   GraphicPrimitive
     ( zenbase::PrimitiveObject *prim
@@ -20,7 +24,8 @@ struct GraphicPrimitive : IGraphic {
     ) {
     auto const &pos = prim->add_attr<zen::vec3f>("pos");
     auto const &clr = prim->add_attr<zen::vec3f>("clr");
-    vertex_count = pos.size();
+    vertex_count = prim->size();
+
     vbo = std::make_unique<Buffer>(GL_ARRAY_BUFFER);
     std::vector<zen::vec3f> mem(vertex_count * 2);
     for (int i = 0; i < vertex_count; i++) {
@@ -29,6 +34,54 @@ struct GraphicPrimitive : IGraphic {
     }
     vbo->bind_data(mem.data(), mem.size() * sizeof(mem[0]));
 
+    lines_count = prim->lines.size();
+    ebo = std::make_unique<Buffer>(GL_ELEMENT_ARRAY_BUFFER);
+    ebo->bind_data(prim->lines.data(), lines_count * sizeof(prim->lines[0]));
+
+    points_prog = get_points_program(path);
+    lines_prog = get_lines_program(path);
+  }
+
+  virtual void draw() override {
+    points_prog->use();
+    set_program_uniforms(points_prog);
+
+    vbo->bind();
+    vbo->attribute(/*index=*/0,
+        /*offset=*/sizeof(float) * 0, /*stride=*/sizeof(float) * 6,
+        GL_FLOAT, /*count=*/3);
+    vbo->attribute(/*index=*/1,
+        /*offset=*/sizeof(float) * 3, /*stride=*/sizeof(float) * 6,
+        GL_FLOAT, /*count=*/3);
+
+    CHECK_GL(glDrawArrays(GL_POINTS, /*first=*/0, /*count=*/vertex_count));
+
+    vbo->disable_attribute(0);
+    vbo->disable_attribute(1);
+    vbo->unbind();
+
+    lines_prog->use();
+    set_program_uniforms(lines_prog);
+
+    vbo->bind();
+    vbo->attribute(/*index=*/0,
+        /*offset=*/sizeof(float) * 0, /*stride=*/sizeof(float) * 6,
+        GL_FLOAT, /*count=*/3);
+    vbo->attribute(/*index=*/1,
+        /*offset=*/sizeof(float) * 3, /*stride=*/sizeof(float) * 6,
+        GL_FLOAT, /*count=*/3);
+
+    ebo->bind();
+    CHECK_GL(glDrawElements(GL_LINES, /*count=*/lines_count,
+          GL_UNSIGNED_INT, /*first=*/0));
+    ebo->unbind();
+
+    vbo->disable_attribute(0);
+    vbo->disable_attribute(1);
+    vbo->unbind();
+  }
+
+  Program *get_points_program(std::string const &path) {
     auto vert = hg::file_get_content(path + ".points.vert");
     auto frag = hg::file_get_content(path + ".points.frag");
 
@@ -76,26 +129,59 @@ struct GraphicPrimitive : IGraphic {
 "}\n";
     }
 
-    prog = compile_program(vert, frag);
+    return compile_program(vert, frag);
   }
 
-  virtual void draw() override {
-    prog->use();
-    set_program_uniforms(prog);
+  Program *get_lines_program(std::string const &path) {
+    auto vert = hg::file_get_content(path + ".lines.vert");
+    auto frag = hg::file_get_content(path + ".lines.frag");
 
-    vbo->bind();
-    vbo->attribute(/*index=*/0,
-        /*offset=*/sizeof(float) * 0, /*stride=*/sizeof(float) * 6,
-        GL_FLOAT, /*count=*/3);
-    vbo->attribute(/*index=*/1,
-        /*offset=*/sizeof(float) * 3, /*stride=*/sizeof(float) * 6,
-        GL_FLOAT, /*count=*/3);
+    if (vert.size() == 0) {
+      vert =
+"#version 120\n"
+"\n"
+"uniform mat4 mVP;\n"
+"uniform mat4 mInvVP;\n"
+"uniform mat4 mView;\n"
+"uniform mat4 mProj;\n"
+"uniform mat4 mInvView;\n"
+"uniform mat4 mInvProj;\n"
+"\n"
+"attribute vec3 vPosition;\n"
+"attribute vec3 vVercolor;\n"
+"\n"
+"varying vec3 position;\n"
+"varying vec3 vercolor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"  position = vPosition;\n"
+"  vercolor = vVercolor;\n"
+"\n"
+"  gl_Position = mVP * vec4(position, 1.0);\n"
+"}\n";
+    }
+    if (frag.size() == 0) {
+      frag =
+"#version 120\n"
+"\n"
+"uniform mat4 mVP;\n"
+"uniform mat4 mInvVP;\n"
+"uniform mat4 mView;\n"
+"uniform mat4 mProj;\n"
+"uniform mat4 mInvView;\n"
+"uniform mat4 mInvProj;\n"
+"\n"
+"varying vec3 position;\n"
+"varying vec3 vercolor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"  gl_FragColor = vec4(vercolor, 1.0);\n"
+"}\n";
+    }
 
-    CHECK_GL(glDrawArrays(GL_POINTS, /*first=*/0, /*count=*/vertex_count));
-
-    vbo->disable_attribute(0);
-    vbo->disable_attribute(1);
-    vbo->unbind();
+    return compile_program(vert, frag);
   }
 };
 
