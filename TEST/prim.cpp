392179f21d4594edf38c9cd5fb7ceee0a943e251
archibate <1931127624@qq.com>
2021-05-22 16:53:09 +0800

    AOAOS

--- TEST/prim.cpp
+++ TEST/prim.cpp
@@ -1,5 +1,13 @@
 #include <vector>
 #include <string>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+#define show(x) (std::cout << #x "=" << (x) << std::endl)
+
+using std::cout;
+using std::endl;
 
 
 struct AOSOA
@@ -10,34 +18,37 @@ struct AOSOA
     std::vector<size_t> m_offsets;
     size_t m_stride;
 
-    struct ChunkPtr {
-        char *m_ptr;
+    std::vector<void *> m_chunks;
 
-        ChunkPtr() {
-            m_ptr = new char[CHK_SIZE];
+    explicit AOSOA(std::vector<size_t> const &elmsizes) {
+        size_t accum = 0;
+        m_offsets.resize(elmsizes.size());
+        for (size_t i = 0; i < elmsizes.size(); i++) {
+            m_offsets[i] = accum;
+            accum += elmsizes[i];
         }
-
-        ~ChunkPtr() {
-            delete m_ptr;
-            m_ptr = nullptr;
-        }
-
-        void *get() const {
-            return (void *)m_ptr;
-        }
-    };
-
-    std::vector<ChunkPtr> m_chunks;
+        m_stride = accum;
+    }
 
     void *address(size_t a, size_t p) const {
         size_t chkoff = m_stride * (p % CHK_SIZE) + m_offsets[a];
         size_t chkidx = p / CHK_SIZE;
-        return (void *)((char *)m_chunks[chkidx].get() + chkoff);
+        return (void *)((char *)m_chunks[chkidx] + chkoff);
     }
 
     void resize(size_t n) {
+        size_t oldchknr = m_chunks.size();
         size_t chknr = (n + CHK_SIZE - 1) / CHK_SIZE;
+        for (size_t i = chknr; i < oldchknr; i++) {
+            std::free(m_chunks[i]);
+            m_chunks[i] = nullptr;
+        }
         m_chunks.resize(chknr);
+        for (size_t i = oldchknr; i < chknr; i++) {
+            size_t size = CHK_SIZE * m_stride;
+            void *p = std::malloc(size);
+            m_chunks[i] = p;
+        }
         m_count = n;
     }
 
@@ -49,5 +60,23 @@ struct AOSOA
 
 int main(void)
 {
-    AOSOA a;
+    AOSOA a({sizeof(int), sizeof(short), sizeof(char), sizeof(char)});
+    a.resize(32);
+    a.resize(1000);
+    for (int i = 0; i < 1000; i++) {
+        *(int *)a.address(0, i) = i + 1;
+    }
+    for (int i = 0; i < 1000; i++) {
+        cout << i << "=" << *(int *)a.address(0, i) << endl;
+    }
+    a.resize(8);
+    a.resize(1024);
+    for (int i = 0; i < 14; i++) {
+        cout << i << "=" << *(int *)a.address(0, i) << endl;
+    }
+    show(a.address(0, 0));
+    show(a.address(1, 0));
+    show(a.address(2, 0));
+    show(a.address(3, 0));
+    return 0;
 }
