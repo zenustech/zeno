b3771a5e4a13db6dbafc3aa93baabf401b37da7e
archibate <1931127624@qq.com>
2021-06-03 11:33:32 +0800

    Findzen.cmake instead

--- zen/share/cmake/Findzen.cmake
+++ zen/share/cmake/Findzen.cmake
@@ -1,4 +1,4 @@
-message("Finding ZENO...")
+message("Finding package zen...")
 
 if (PYTHON_EXECUTABLE)
     message("Using ${PYTHON_EXECUTABLE} as python executable.")
@@ -15,19 +15,21 @@ execute_process(COMMAND ${PYTHON_EXECUTABLE} --version)
 
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
         "import zen"
-        RESULT_VARIABLE ZENO_IMPORT_RET)
-if (ZENO_IMPORT_RET)
+        RESULT_VARIABLE zen_IMPORT_RET)
+if (zen_IMPORT_RET)
     # returns zero if success
     message(FATAL_ERROR "Cannot import zen. Have you installed it or add it to PYTHONPATH?")
 endif ()
 
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
         "import sys; import zen; sys.stdout.write(zen.getIncludeDir())"
-        OUTPUT_VARIABLE ZENO_INCLUDE_DIR)
+        OUTPUT_VARIABLE zen_INCLUDE_DIR)
 
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
         "import sys; import zen; sys.stdout.write(zen.getLibraryDir())"
-        OUTPUT_VARIABLE ZENO_LIBRARY_DIR)
+        OUTPUT_VARIABLE zen_LIBRARY_DIR)
 
-link_directories(${ZENO_LIBRARY_DIR})
-include_directories(${ZENO_INCLUDE_DIR})
+message("zen_INCLUDE_DIR=${zen_INCLUDE_DIR}")
+message("zen_LIBRARY_DIR=${zen_LIBRARY_DIR}")
+link_directories(${zen_LIBRARY_DIR})
+include_directories(${zen_INCLUDE_DIR})
