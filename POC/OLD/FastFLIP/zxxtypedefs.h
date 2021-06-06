eb8e71a8dda46cfd31560dbe98903f9133a92b10
archibate <1931127624@qq.com>
2021-06-06 09:56:32 +0800

    rm POC/OLD

--- POC/OLD/FastFLIP/zxxtypedefs.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-typedef float		FLOAT32;		// 32 bit floating point
-typedef double		FLOAT64;		// 64 bit floating point
-typedef long double	FLOAT80;		// 80 bit floating point
-typedef unsigned int uint;			// Generic unsigned integer
-typedef unsigned char uint8;		// 8 bit unsigned char
-typedef unsigned long long uint64;	// 64 bit integer
-typedef long int int64;
