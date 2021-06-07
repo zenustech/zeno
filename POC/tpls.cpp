b30e20ebdc990b4d479f551349aba44df7564d8e
archibate <1931127624@qq.com>
2021-06-07 08:08:46 +0800

    c++ nb

--- POC/tpls.cpp
+++ POC/tpls.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 
-struct MC {};
-
-template <class, class = void>
-struct has_add_one : false_type {
+struct MC {
+    int operator+(int x) const {
+        return x + 1;
+    }
 };
 
-template <class T>
-struct has_add_one<T, std::void_t<decltype(declval<T>() + 1)>> : true_type {
+struct op_add {
+    template <class T1, class T2, decltype(
+        std::declval<T1>() + std::declval<T2>()
+        , true) = true>
+    static auto apply(T1 const &x, T2 const &y) {
+        return x + y;
+    }
+};
+
+template <class T, class ...>
+using _left_t = T;
+
+template <class Op, class ...Ts>
+struct is_op_valid : std::false_type {
+    void apply(Ts const &...ts) {
+    }
+};
+
+template <class Op, class ...Ts>
+struct is_op_valid<_left_t<Op, decltype(
+    Op::apply(std::declval<Ts>()...))>, Ts...> : std::true_type {
+
+    auto apply(Ts const &...ts) {
+        return Op::apply(std::forward<Ts>(ts)...);
+    }
 };
 
 int main()
 {
-    cout << has_add_one<MC>::value << endl;
+    cout << is_op_valid<op_add, MC, MC>::value << endl;
+    cout << is_op_valid<op_add, MC, int>::value << endl;
+    cout << is_op_valid<op_add, int, int>::value << endl;
+    cout << is_op_valid<op_add, int>::value << endl;
 }
