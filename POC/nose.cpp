367414654915d60ec6d5a454de3539cccee70b6f
archibate <1931127624@qq.com>
2021-06-09 11:12:15 +0800

    add set_output

--- POC/nose.cpp
+++ POC/nose.cpp
@@ -12,15 +12,32 @@ struct IObject {
 std::map<Id, std::unique_ptr<IObject>> objects;
 
 struct INode {
+  Id myname;
   std::map<Id, Id> inputs;
   std::map<Id, Id> outputs;
 
   virtual void apply() = 0;
+
+  IObject *get_input(Id const &id) {
+    return objects.at(inputs.at(id)).get();
+  }
+
+  template <class T>
+  T *get_input(Id const &id) {
+    return dynamic_cast<T *>(get_input(id));
+  }
+
+  void set_output(Id const &id, std::unique_ptr<IObject> &&obj) {
+    auto objid = myname + "::" + id;
+    objects[objid] = std::move(obj);
+    outputs[id] = objid;
+  }
 };
 
 std::map<Id, std::unique_ptr<INode>> nodes;
 
 void addNode(Id const &id, std::unique_ptr<INode> &&node) {
+  node->myname = id;
   nodes[id] = std::move(node);
 }
 
@@ -39,18 +56,16 @@ struct MyObject : IObject {
 struct MyNodeA : INode {
   virtual void apply() override {
     auto obj = std::make_unique<MyObject>();
-    objects["A::TestOut"] = std::move(obj);
-    outputs["Out0"] = "A::TestOut";
+    set_output("Out0", std::move(obj));
   }
 };
 
 struct MyNodeB : INode {
   virtual void apply() override {
-    auto obj = dynamic_cast<MyObject *>(objects.at(inputs.at("In0")).get());
+    auto obj = get_input<MyObject>("In0");
     auto newobj = std::make_unique<MyObject>();
     newobj->i = obj->i + 1;
-    objects["B::TestOut"] = std::move(newobj);
-    outputs["Out0"] = "B::TestOut";
+    set_output("Out0", std::move(newobj));
   }
 };
 
@@ -61,7 +76,7 @@ int main()
   applyNode("A");
   setNodeInput("B", "In0", "A", "Out0");
   applyNode("B");
-  auto objid = nodes.at("B")->outputs["Out0"];
+  auto objid = nodes.at("B")->outputs.at("Out0");
   cout << "objid=" << objid << endl;
   auto obj = dynamic_cast<MyObject *>(objects.at(objid).get());
   cout << "obj->i=" << obj->i << endl;
