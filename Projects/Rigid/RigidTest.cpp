a6682b53e46c1da0882a7cb6ad394ee118a25f5f
archibate <1931127624@qq.com>
2021-06-14 06:58:02 +0800

    ooish bullet world

--- Projects/Rigid/RigidTest.cpp
+++ Projects/Rigid/RigidTest.cpp
@@ -1,45 +1,50 @@
 #include <stdio.h>
 #include <btBulletDynamicsCommon.h>
+#include <memory>
+#include <vector>
 
-/// This is a Hello World program for running a basic Bullet physics simulation
+struct BulletWorld {
 
-int main(int argc, char** argv)
-{
-    ///-----includes_end-----
-
-    int i;
-    ///-----initialization_start-----
-
-    ///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
-    btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+    std::unique_ptr<btDefaultCollisionConfiguration> collisionConfiguration = std::make_unique<btDefaultCollisionConfiguration>();
 
     ///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
-    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    std::unique_ptr<btCollisionDispatcher> dispatcher = std::make_unique<btCollisionDispatcher>(collisionConfiguration.get());
 
     ///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
-    btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase();
+    std::unique_ptr<btBroadphaseInterface> overlappingPairCache = std::make_unique<btDbvtBroadphase>();
 
     ///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
-    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+    std::unique_ptr<btSequentialImpulseConstraintSolver> solver = std::make_unique<btSequentialImpulseConstraintSolver>();
 
-    btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+    std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), overlappingPairCache.get(), solver.get(), collisionConfiguration.get());
 
-    dynamicsWorld->setGravity(btVector3(0, -10, 0));
+    std::vector<std::unique_ptr<btCollisionShape>> collisionShapes;
 
-    ///-----initialization_end-----
+    BulletWorld() {
+        dynamicsWorld->setGravity(btVector3(0, -10, 0));
+    }
 
-    //keep track of the shapes, we release memory at exit.
-    //make sure to re-use collision shapes among rigid bodies whenever possible!
-    btAlignedObjectArray<btCollisionShape*> collisionShapes;
+    ~BulletWorld() {
+        for (int i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
+        {
+            btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
+            btRigidBody* body = btRigidBody::upcast(obj);
+            if (body && body->getMotionState())
+            {
+                delete body->getMotionState();
+            }
+            dynamicsWorld->removeCollisionObject(obj);
+            delete obj;
+        }
 
-    ///create a few basic rigid bodies
+        //next line is optional: it will be cleared by the destructor when the array goes out of scope
+        collisionShapes.clear();
+    }
 
     //the ground is a cube of side 100 at position y = -56.
     //the sphere will hit it at y = -6, with center at -5
-    {
-        btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
-
-        collisionShapes.push_back(groundShape);
+    void addGround() {
+        auto groundShape = std::make_unique<btBoxShape>(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
 
         btTransform groundTransform;
         groundTransform.setIdentity();
@@ -56,19 +61,18 @@ int main(int argc, char** argv)
 
         //using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
         btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
-        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
+        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape.get(), localInertia);
         btRigidBody* body = new btRigidBody(rbInfo);
 
         //add the body to the dynamics world
         dynamicsWorld->addRigidBody(body);
+        collisionShapes.push_back(std::move(groundShape));
     }
 
-    {
+    void addBall() {
         //create a dynamic rigidbody
 
-        //btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
-        btCollisionShape* colShape = new btSphereShape(btScalar(1.));
-        collisionShapes.push_back(colShape);
+        auto colShape = std::make_unique<btSphereShape>(btScalar(1.));
 
         /// Create Dynamic Objects
         btTransform startTransform;
@@ -87,17 +91,14 @@ int main(int argc, char** argv)
 
         //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
         btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
-        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+        btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape.get(), localInertia);
         btRigidBody* body = new btRigidBody(rbInfo);
 
         dynamicsWorld->addRigidBody(body);
+        collisionShapes.push_back(std::move(colShape));
     }
 
-    /// Do some simulation
-
-    ///-----stepsimulation_start-----
-    for (i = 0; i < 150; i++)
-    {
+    void step() {
         dynamicsWorld->stepSimulation(1.f / 60.f, 10);
 
         //print positions of all objects
@@ -117,6 +118,33 @@ int main(int argc, char** argv)
             printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
         }
     }
+};
+
+
+/// This is a Hello World program for running a basic Bullet physics simulation
+
+int main(int argc, char** argv)
+{
+    ///-----initialization_start-----
+
+    ///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
+    BulletWorld w;
+
+    ///-----initialization_end-----
+
+    //keep track of the shapes, we release memory at exit.
+    //make sure to re-use collision shapes among rigid bodies whenever possible!
+    w.addGround();
+    w.addBall();
+
+    ///create a few basic rigid bodies
+
+    /// Do some simulation
+
+    ///-----stepsimulation_start-----
+    for (int i = 0; i < 120; i++) {
+        w.step();
+    }
 
     ///-----stepsimulation_end-----
 
@@ -125,42 +153,7 @@ int main(int argc, char** argv)
     ///-----cleanup_start-----
 
     //remove the rigidbodies from the dynamics world and delete them
-    for (i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
-    {
-        btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
-        btRigidBody* body = btRigidBody::upcast(obj);
-        if (body && body->getMotionState())
-        {
-            delete body->getMotionState();
-        }
-        dynamicsWorld->removeCollisionObject(obj);
-        delete obj;
-    }
 
-    //delete collision shapes
-    for (int j = 0; j < collisionShapes.size(); j++)
-    {
-        btCollisionShape* shape = collisionShapes[j];
-        collisionShapes[j] = 0;
-        delete shape;
-    }
-
-    //delete dynamics world
-    delete dynamicsWorld;
-
-    //delete solver
-    delete solver;
-
-    //delete broadphase
-    delete overlappingPairCache;
-
-    //delete dispatcher
-    delete dispatcher;
-
-    delete collisionConfiguration;
-
-    //next line is optional: it will be cleared by the destructor when the array goes out of scope
-    collisionShapes.clear();
 
     return 0;
 }
