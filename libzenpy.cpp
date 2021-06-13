532a92f52037a66fb5daa74964e2675ac873f4ce
archibate <1931127624@qq.com>
2021-06-13 18:24:20 +0800

    buildable

--- libzenpy.cpp
+++ libzenpy.cpp
@@ -1,155 +1,15 @@
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <zen/ArrayObject.h>
-#include <zen/NumericObject.h>
-#include <zen/StringObject.h>
 #include <zen/zen.h>
 namespace py = pybind11;
 
-static std::string getCppObjectType(std::string name) {
-  auto obj = zen::getObject(name);
-  if (obj->as<zen::ArrayObject>()) {
-    return "array";
-  } else if (obj->as<zen::BooleanObject>()) {
-    return "boolean";
-  } else if (obj->as<zen::StringObject>()) {
-    return "string";
-  } else if (obj->as<zen::NumericObject>()) {
-    return "numeric";
-  } else {
-    return "other";
-  }
-}
-
-static void setBooleanObject(std::string name, bool value) {
-  auto obj = std::make_unique<zen::BooleanObject>();
-  obj->value = value;
-  zen::setObject(name, std::move(obj));
-}
-
-static bool getBooleanObject(std::string name) {
-  auto obj = zen::getObject(name)->as<zen::BooleanObject>();
-  return obj->value;
-}
-
-static void setNumericObject(std::string name, zen::FixedNumericValue value) {
-  auto obj = std::make_unique<zen::NumericObject>();
-  std::visit([&](auto const &x) { obj->value = (decltype(obj->value))x; },
-             value);
-  zen::setObject(name, std::move(obj));
-}
-
-static zen::FixedNumericValue getNumericObject(std::string name) {
-  auto obj = zen::getObject(name)->as<zen::NumericObject>();
-  zen::FixedNumericValue value;
-  std::visit([&](auto const &x) { value = (decltype(value))x; }, obj->value);
-  return value;
-}
-
-static void setStringObject(std::string name, std::string value) {
-  auto obj = std::make_unique<zen::StringObject>();
-  obj->value = value;
-  zen::setObject(name, std::move(obj));
-}
-
-static std::string getStringObject(std::string name) {
-  auto obj = zen::getObject(name)->as<zen::StringObject>();
-  return obj->value;
-}
-
-static std::map<std::string, std::unique_ptr<std::vector<char>>> savedNumpys;
-
-template <class T>
-static void setArrayObject(std::string name,
-                    py::array_t<T, py::array::c_style> const &data) {
-  py::buffer_info buf = data.request();
-
-  auto obj = zen::IObject::make<zen::ArrayObject>();
-  obj->ptr = buf.ptr;
-  obj->itemsize = buf.itemsize;
-  obj->format = buf.format;
-  obj->ndim = buf.ndim;
-  obj->shape = buf.shape;
-  obj->strides = buf.strides;
-
-  // make a deep-copy to prevent pointer out-of-date / dirty data:
-  ssize_t size = 1;
-  for (ssize_t i = 0; i < obj->ndim; i++)
-    size += obj->shape[i];
-  size *= obj->itemsize;
-  auto saved = std::make_unique<std::vector<char>>(size);
-  std::memcpy(saved->data(), obj->ptr, size);
-  obj->ptr = saved->data();
-  savedNumpys[name] = std::move(saved);
-
-  zen::setObject(name, std::move(obj));
-  // by the way, will we provide zen::deleteObject as well?
-}
-
-static std::tuple<uintptr_t, ssize_t, std::string, ssize_t, std::vector<ssize_t>,
-           std::vector<ssize_t>>
-getArrayObjectMeta(std::string name) {
-  auto obj = zen::getObject(name)->as<zen::ArrayObject>();
-  return std::tuple<uintptr_t, ssize_t, std::string, ssize_t,
-                    std::vector<ssize_t>, std::vector<ssize_t>>(
-      (uintptr_t)obj->ptr, (ssize_t)obj->itemsize, (std::string)obj->format,
-      (ssize_t)obj->ndim, (std::vector<ssize_t>)obj->shape,
-      (std::vector<ssize_t>)obj->strides);
-};
-
-template <class T>
-static py::array_t<T, py::array::c_style> getArrayObject(std::string name) {
-  auto obj = zen::getObject(name)->as<zen::ArrayObject>();
-
-  ssize_t size = 1;
-  for (ssize_t i = 0; i < obj->ndim; i++)
-    size *= obj->shape[i];
-  py::array_t<T, py::array::c_style> arr(size);
-  auto acc = arr.mutable_unchecked();
-  for (ssize_t i = 0; i < size; i++) {
-    acc(i) = *(T *)((uint8_t *)obj->ptr + obj->itemsize * i);
-  }
-  arr.resize(obj->shape);
-  return arr;
-}
 
 PYBIND11_MODULE(libzenpy, m) {
-  m.def("addNode", zen::addNode);
-  m.def("setNodeParam", zen::setNodeParam);
-  m.def("setNodeInput", zen::setNodeInput);
-  m.def("initNode", zen::initNode);
-  m.def("applyNode", zen::applyNode);
   m.def("dumpDescriptors", zen::dumpDescriptors);
-
-  m.def("isCppObject", zen::hasObject);
-  m.def("getNodeRequirements", zen::getNodeRequirements);
-  m.def("getCppObjectType", getCppObjectType);
-
-  m.def("setBooleanObject", setBooleanObject);
-  m.def("getBooleanObject", getBooleanObject);
-  m.def("setStringObject", setStringObject);
-  m.def("getStringObject", getStringObject);
-  m.def("setNumericObject", setNumericObject);
-  m.def("getNumericObject", getNumericObject);
-  m.def("setReference", zen::setReference);
-  m.def("getReference", zen::getReference);
-
-  m.def("getArrayObjectMeta", getArrayObjectMeta);
-#define _DEF_TYPE(T)                                                           \
-  m.def("setArrayObject", setArrayObject<T>);                                  \
-  m.def("getArrayObject_" #T, getArrayObject<T>);
-  _DEF_TYPE(uint8_t)
-  _DEF_TYPE(uint16_t)
-  _DEF_TYPE(uint32_t)
-  _DEF_TYPE(uint64_t)
-  _DEF_TYPE(int8_t)
-  _DEF_TYPE(int16_t)
-  _DEF_TYPE(int32_t)
-  _DEF_TYPE(int64_t)
-  _DEF_TYPE(float)
-  _DEF_TYPE(double)
-#undef _DEF_TYPE
+  m.def("bindNodeInput", zen::bindNodeInput);
+  m.def("applyNode", zen::applyNode);
+  m.def("addNode", zen::addNode);
 
   py::register_exception_translator([](std::exception_ptr p) {
     try {
--- zensession.cpp
+++ zensession.cpp
@@ -14,18 +14,6 @@ ZENAPI char const *Exception::what() const noexcept {
     return msg.c_str();
 }
 
-template <class S, class T>
-static std::string join_str(std::vector<T> const &elms, S const &delim) {
-  std::stringstream ss;
-  auto p = elms.begin(), end = elms.end();
-  if (p != end)
-    ss << *p++;
-  for (; p != end; ++p) {
-    ss << delim << *p;
-  }
-  return ss.str();
-}
-
 template <class T>
 T *safe_at(std::map<std::string, std::unique_ptr<T>> const &m,
            std::string const &key, std::string const &msg) {
@@ -55,199 +43,88 @@ T safe_at(std::map<S, T> const &m, S const &key, std::string const &msg) {
   return it->second;
 }
 
-static std::unique_ptr<zen::Session> sess;
 
-ZENAPI Session &getSession() {
-  if (!sess) {
-    sess = std::make_unique<zen::Session>();
-  }
-  return *sess;
-}
 
 ZENAPI INode::INode() = default;
 ZENAPI INode::~INode() = default;
 
-ZENAPI void INode::init() {}
-
-ZENAPI std::vector<std::string> INode::requirements() {
-  std::vector<std::string> ret;
-  for (auto const &[key, name] : inputs) {
-    ret.push_back(name);
-  }
-  return ret;
-}
-
-ZENAPI void INode::on_init() { init(); }
-
-ZENAPI void INode::set_param(std::string const &name, IValue const &value) {
-  params[name] = value;
-}
-
-ZENAPI void INode::set_input_ref(std::string const &name,
-                                 std::string const &srcname) {
-  inputs[name] = srcname;
-}
-
-ZENAPI std::string INode::get_node_name() { return getNodeName(this); }
-
-ZENAPI IValue INode::get_param(std::string const &name) {
-  return safe_at(params, name, "param");
-}
-
-ZENAPI std::string INode::get_input_ref(std::string const &name) {
-  return safe_at(inputs, name, "input");
-}
-
-ZENAPI IObject *INode::get_input(std::string const &name) {
-  auto ref = get_input_ref(name);
-  return getObject(ref);
-}
-
-ZENAPI bool INode::has_input(std::string const &name) {
-  return inputs.find(name) != inputs.end();
-}
-
-ZENAPI std::string INode::get_output_ref(std::string const &name) {
-  auto myname = get_node_name();
-  return myname + "::" + name;
-}
-
-ZENAPI IObject *INode::get_output(std::string const &name) {
-  auto ref = get_output_ref(name);
-  return getObject(ref);
-}
-
-ZENAPI void INode::set_output(std::string const &name, IObject::Ptr object) {
-  auto ref = get_output_ref(name);
-  setObject(ref, std::move(object));
-}
-
-ZENAPI void INode::set_output_ref(std::string const &name,
-                                  std::string const &srcname) {
-  auto ref = get_output_ref(name);
-  setReference(ref, srcname);
-}
-
-ZENAPI void INode::on_apply() {
-  bool ok = true;
-
-  // get dummy boolean to see if this node should be executed
-  if (has_input("COND")) {
-    auto cond = get_input("COND")->as<BooleanObject>();
-    ok = cond->value;
-  }
-
-  if (ok)
+ZENAPI void INode::doApply(Context *ctx) {
+    for (auto [ds, bound]: inputBounds) {
+        auto [sn, ss] = bound;
+        sess->requestNode(sn, ctx);
+        inputs[ds] = sess->getNodeOutput(sn, ss);
+    }
     apply();
-
-  // set dummy output sockets for connection order
-  set_output("DST", IObject::make<BooleanObject>());
 }
 
-ZENAPI INodeClass::INodeClass() = default;
-ZENAPI INodeClass::~INodeClass() = default;
-
-ZENAPI ParamDescriptor::ParamDescriptor(std::string const &type,
-                                        std::string const &name,
-                                        std::string const &defl)
-    : type(type), name(name), defl(defl) {}
-
-ZENAPI ParamDescriptor::~ParamDescriptor() = default;
-
-ZENAPI Descriptor::Descriptor() = default;
-
-ZENAPI Descriptor::Descriptor(std::vector<std::string> const &inputs,
-                              std::vector<std::string> const &outputs,
-                              std::vector<ParamDescriptor> const &params,
-                              std::vector<std::string> const &categories)
-    : inputs(inputs), outputs(outputs), params(params), categories(categories) {
-  // append dummy sockets for perserving exec orders
-  this->inputs.push_back("SRC");
-  this->inputs.push_back("COND");
-  this->outputs.push_back("DST");
+ZENAPI bool INode::has_input(std::string const &id) const {
+    return inputs.find(id) != inputs.end();
 }
 
-ZENAPI Descriptor::~Descriptor() = default;
+ZENAPI IObject *INode::get_input(std::string const &id) const {
+    return sess->getObject(inputs.at(id));
+}
 
-ZENAPI std::string Descriptor::serialize() const {
+ZENAPI void INode::set_output(std::string const &id, std::unique_ptr<IObject> &&obj) {
+    auto objid = myname + "::" + id;
+    sess->objects[objid] = std::move(obj);
+    outputs[id] = objid;
+}
+
+
+ZENAPI void Session::_defNodeClass(std::string const &id, std::unique_ptr<INodeClass> &&cls) {
+    nodeClasses[id] = std::move(cls);
+}
+
+ZENAPI std::string Session::getNodeOutput(std::string const &sn, std::string const &ss) const {
+    return nodes.at(sn)->outputs.at(ss);
+}
+
+ZENAPI IObject *Session::getObject(std::string const &id) const {
+    return objects.at(id).get();
+}
+
+ZENAPI void Session::addNode(std::string const &cls, std::string const &id) {
+    auto node = nodeClasses.at(cls)->new_instance();
+    node->sess = this;
+    node->myname = id;
+    nodes[id] = std::move(node);
+}
+
+ZENAPI void Session::requestNode(std::string const &id, Context *ctx) {
+    if (ctx->visited.find(id) != ctx->visited.end()) {
+        return;
+    }
+    ctx->visited.insert(id);
+    nodes.at(id)->doApply(ctx);
+}
+
+ZENAPI void Session::applyNode(std::string const &id) {
+    auto ctx = std::make_unique<Context>();
+    requestNode(id, ctx.get());
+}
+
+ZENAPI void Session::bindNodeInput(std::string const &dn, std::string const &ds,
+        std::string const &sn, std::string const &ss) {
+    nodes.at(dn)->inputBounds[ds] = std::pair(sn, ss);
+}
+
+ZENAPI std::string Session::dumpDescriptors() const {
   std::string res = "";
-  res += "(" + join_str(inputs, ",") + ")";
-  res += "(" + join_str(outputs, ",") + ")";
-  std::vector<std::string> paramStrs;
-  for (auto const &[type, name, defl] : params) {
-    paramStrs.push_back(type + ":" + name + ":" + defl);
+  for (auto const &[key, cls] : nodeClasses) {
+    res += key + ":" + cls->desc->serialize() + "\n";
   }
-  res += "(" + join_str(paramStrs, ",") + ")";
-  res += "(" + join_str(categories, ",") + ")";
   return res;
 }
 
-ZENAPI void Session::addNode(std::string const &type, std::string const &name) {
-  if (nodes.find(name) != nodes.end())
-    return;
-  auto node = safe_at(nodeClasses, type, "node class")->new_instance();
-  nodesRev[node.get()] = name;
-  nodes[name] = std::move(node);
+
+ZENAPI Session &getSession() {
+    std::unique_ptr<Session> ptr;
+    if (!ptr) {
+        ptr = std::make_unique<Session>();
+    }
+    return *ptr;
 }
-ZENAPI std::vector<std::string>
-Session::getNodeRequirements(std::string const &name) {
-  return safe_at(nodes, name, "node")->requirements();
-}
-ZENAPI void Session::setNodeParam(std::string const &name,
-                                  std::string const &key, IValue const &value) {
-  safe_at(nodes, name, "node")->set_param(key, value);
-}
-ZENAPI void Session::setNodeInput(std::string const &name,
-                                  std::string const &key,
-                                  std::string const &srcname) {
-  safe_at(nodes, name, "node")->set_input_ref(key, srcname);
-}
-ZENAPI void Session::initNode(std::string const &name) {
-  safe_at(nodes, name, "node")->on_init();
-}
-ZENAPI void Session::applyNode(std::string const &name) {
-  safe_at(nodes, name, "node")->on_apply();
-}
-ZENAPI void Session::setObject(std::string const &name, IObject::Ptr object) {
-  objects[name] = std::move(object);
-}
-ZENAPI bool Session::hasObject(std::string const &name) {
-  auto refname = getReference(name).value_or(name);
-  return objects.find(refname) != objects.end();
-}
-ZENAPI IObject *Session::getObject(std::string const &name) {
-  auto refname = getReference(name).value_or(name);
-  return safe_at(objects, refname, "object");
-}
-ZENAPI void Session::setReference(std::string const &name,
-                                  std::string const &srcname) {
-  auto refname = getReference(srcname).value_or(srcname);
-  references[name] = refname;
-}
-ZENAPI std::optional<std::string>
-Session::getReference(std::string const &name) {
-  auto it = references.find(name);
-  if (it == references.end()) {
-    return std::nullopt;
-  }
-  return it->second;
-}
-ZENAPI std::string Session::getNodeName(INode *node) {
-  return safe_at(nodesRev, node, "node pointer");
-}
-ZENAPI std::string Session::dumpDescriptors() {
-  // dump the node descriptors (for node editor),
-  // according to the defNodeClass'es in this DLL.
-  std::string res = "";
-  for (auto const &[key, desc] : nodeDescriptors) {
-    res += key + ":" + desc.serialize() + "\n";
-  }
-  return res;
-}
-ZENAPI void Session::doDefNodeClass(std::unique_ptr<INodeClass> cls,
-                                    std::string const &name,
-                                    Descriptor const &desc) {
-  nodeClasses[name] = std::move(cls);
-  nodeDescriptors[name] = desc;
-}
+
+
 } // namespace zen
