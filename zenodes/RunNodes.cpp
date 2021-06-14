cf5cdd1e44420bf9c7744b5c248bcad4211416a7
archibate <1931127624@qq.com>
2021-06-14 16:02:58 +0800

    fix IntegrateFrameTime nodes

--- zenodes/RunNodes.cpp
+++ zenodes/RunNodes.cpp
@@ -1,4 +1,5 @@
 #include <zen/zen.h>
+#include <zen/NumericObject.h>
 #include <zen/ConditionObject.h>
 #include <zen/GlobalState.h>
 
@@ -15,5 +16,81 @@ ZENDEFNODE(RunOnce, {
     {},
     {"cond"},
     {},
+    {"substep"},
+});
+
+struct RunAfterFrame : zen::INode {
+    virtual void apply() override {
+        bool yes = zen::state.has_frame_completed || !zen::state.time_step_integrated;
+        auto obj = std::make_unique<zen::ConditionObject>();
+        obj->set(yes);
+        set_output("cond", std::move(obj));
+    }
+};
+
+ZENDEFNODE(RunAfterFrame, {
+    {},
+    {"cond"},
+    {},
+    {"substep"},
+});
+
+struct RunBeforeFrame : zen::INode {
+    virtual void apply() override {
+        bool yes = !zen::state.has_substep_executed;
+        auto obj = std::make_unique<zen::ConditionObject>();
+        obj->set(yes);
+        set_output("cond", std::move(obj));
+    }
+};
+
+ZENDEFNODE(RunBeforeFrame, {
+    {},
+    {"cond"},
+    {},
+    {"substep"},
+});
+
+
+struct SetFrameTime : zen::INode {
+    virtual void apply() override {
+        auto time = get_input<zen::NumericObject>("time")->get<float>();
+        zen::state.frame_time = time;
+    }
+};
+
+ZENDEFNODE(SetFrameTime, {
+    {"time"},
+    {},
+    {},
+    {"substep"},
+});
+
+struct IntegrateFrameTime : zen::INode {
+    virtual void apply() override {
+        float dt = zen::state.frame_time;
+        if (has_input("desired_dt")) {
+            dt = get_input<zen::NumericObject>("desired_dt")->get<float>();
+            auto min_scale = get_param<float>("min_scale");
+            dt = std::max(std::fabs(dt), min_scale * zen::state.frame_time);
+        }
+        if (zen::state.frame_time_elapsed + dt >= zen::state.frame_time) {
+            dt = zen::state.frame_time - zen::state.frame_time_elapsed;
+            zen::state.frame_time_elapsed = zen::state.frame_time;
+            zen::state.has_frame_completed = true;
+        } else {
+            zen::state.frame_time_elapsed += dt;
+        }
+        zen::state.time_step_integrated = true;
+        auto ret = std::make_unique<zen::NumericObject>();
+        ret->set(dt);
+        set_output("actual_dt", ret);
+    }
+};
+
+ZENDEFNODE(IntegrateFrameTime, {
+    {"desired_dt"},
+    {"actual_dt"},
+    {{"float", "min_scale", "0.0001"}},
     {"substep"},
 });
