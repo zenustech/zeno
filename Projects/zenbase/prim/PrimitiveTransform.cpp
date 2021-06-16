f150aceeb467a1421ec7accd0fc59785834c7f31
archibate <1931127624@qq.com>
2021-06-16 10:28:47 +0800

    fix glm quat

--- Projects/zenbase/prim/PrimitiveTransform.cpp
+++ Projects/zenbase/prim/PrimitiveTransform.cpp
@@ -10,21 +10,20 @@
 namespace zen {
 
 
-static glm::vec3 mapplypos(glm::mat4 const &matrix, glm::vec3 const &vector) {
-  auto vector4 = matrix * glm::vec4(vector, 1.0f);
-  
-  return glm::vec3(vector4) / vector4.w;
-}
-
-
-static glm::vec3 mapplydir(glm::mat4 const &matrix, glm::vec3 const &vector) {
-  glm::mat3 normMatrix(matrix);
-  normMatrix = glm::transpose(glm::inverse(normMatrix));
-  auto vector3 = normMatrix * vector;
-  return vector3;
-}
-
 struct TransformPrimitive : zen::INode {
+    static glm::vec3 mapplypos(glm::mat4 const &matrix, glm::vec3 const &vector) {
+        auto vector4 = matrix * glm::vec4(vector, 1.0f);
+        return glm::vec3(vector4) / vector4.w;
+    }
+
+
+    static glm::vec3 mapplynrm(glm::mat4 const &matrix, glm::vec3 const &vector) {
+        glm::mat3 normMatrix(matrix);
+        normMatrix = glm::transpose(glm::inverse(normMatrix));
+        auto vector3 = normMatrix * vector;
+        return glm::normalize(vector3);
+    }
+
     virtual void apply() override {
         zen::vec3f translate = {0,0,0};
         zen::vec4f rotation = {0,0,0,1};
@@ -42,10 +41,11 @@ struct TransformPrimitive : zen::INode {
         glm::mat4 matRotx  = glm::rotate( eulerXYZ[0], glm::vec3(1,0,0) );
         glm::mat4 matRoty  = glm::rotate( eulerXYZ[1], glm::vec3(0,1,0) );
         glm::mat4 matRotz  = glm::rotate( eulerXYZ[2], glm::vec3(0,0,1) );
-        glm::quat myQuat(rotation[0], rotation[1], rotation[2], rotation[3]);
+        glm::quat myQuat(rotation[3], rotation[0], rotation[1], rotation[2]);
         glm::mat4 matQuat  = glm::toMat4(myQuat);
         glm::mat4 matScal  = glm::scale( glm::vec3(scaling[0], scaling[1], scaling[2] ));
-        auto matrix = matTrans*matRotz*matRoty*matRotx*matQuat*matScal;
+        //auto matrix = matTrans*matRotz*matRoty*matRotx*matQuat*matScal;
+        auto matrix = matTrans*(matQuat);
 
         auto prim = get_input<PrimitiveObject>("prim");
         auto outprim = std::make_unique<PrimitiveObject>(*prim);
@@ -65,7 +65,7 @@ struct TransformPrimitive : zen::INode {
             //#pragma omp parallel for
             for (int i = 0; i < nrm.size(); i++) {
                 auto n = zen::vec_to_other<glm::vec3>(nrm[i]);
-                n = glm::normalize(mapplydir(matrix, n));  // TODO: inverse-transpose??
+                n = mapplynrm(matrix, n);
                 nrm[i] = zen::other_to_vec<3>(n);
             }
         }
