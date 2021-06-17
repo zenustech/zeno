e5c5d8f955bb4df9ba89f1a133f2e1707fe28185
archibate <1931127624@qq.com>
2021-06-17 09:06:59 +0800

    prevent 1 copy by PtrObject

--- POC/vcopy.cpp
+++ POC/vcopy.cpp
@@ -1,35 +1,67 @@
 #include <cstdio>
 #include <memory>
 
-struct Base {
-    Base() = default;
-    virtual ~Base() = default;
 
-    virtual Base *clone() const = 0;
+struct AnyObject {
+    virtual ~AnyObject() = default;
 
-    virtual void show() const {
-        printf("Base\n");
-    }
+    virtual AnyObject *clone() const = 0;
+    virtual void show() const = 0;
 };
 
-struct Derived : Base {
-    Derived() = default;
+template <class T>
+struct PtrObject : AnyObject {
+    std::unique_ptr<T> tp;
 
-    virtual Base *clone() const override {
-        return new Derived(static_cast<Derived const &>(*this));
+    PtrObject(std::unique_ptr<T> &&tp) : tp(std::move(tp)) {}
+
+    virtual AnyObject *clone() const override {
+        return new PtrObject<T>(std::make_unique<T>(static_cast<T const &>(*tp)));
     }
 
     virtual void show() const override {
-        printf("Derived\n");
+        printf("PtrObject: %s\n", typeid(T).name());
+    }
+};
+
+template <class T>
+struct ImplObject : AnyObject {
+    T t;
+
+    ImplObject(T const &t) : t(t) {}
+
+    virtual AnyObject *clone() const override {
+        return new ImplObject<T>(t);
+    }
+
+    virtual void show() const override {
+        printf("ImplObject: %s\n", typeid(T).name());
+    }
+};
+
+
+struct VDBGrid {
+    virtual void help() const = 0;
+};
+
+struct VDBFloatGrid : VDBGrid {
+    VDBFloatGrid() = default;
+
+    VDBFloatGrid(VDBFloatGrid const &) {
+        printf("VDBFloatGrid copy!\n");
+    }
+
+    virtual void help() const override {
+        printf("VDBFloatGrid!\n");
     }
 };
 
 
 int main(void)
 {
-    std::unique_ptr<Base> x = std::make_unique<Derived>();
-    std::unique_ptr<Base> y(x->clone());
-    y->show();
-    x->show();
+    AnyObject *p = new PtrObject<VDBFloatGrid>( std::make_unique<VDBFloatGrid>() );
+    p->show();
+    auto q = p->clone();
+    q->show();
     return 0;
 }
