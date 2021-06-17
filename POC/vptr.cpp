5a4091567da33aeacb33723ea2d1c91db47d1978
archibate <1931127624@qq.com>
2021-06-17 14:19:25 +0800

    test vptr poc

---
+++ POC/vptr.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using std::cout;
+using std::endl;
+
+template <class T>
+struct vector_ptr {
+    std::vector<char> m_data;
+    size_t m_count;
+};
+
+int main()
+{
+    return 0;
+}
