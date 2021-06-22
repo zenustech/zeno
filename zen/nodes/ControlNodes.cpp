e8be099ec5c5b5b6a2e88e346742067ba9aae752
archibate <1931127624@qq.com>
2021-06-22 14:56:40 +0800

    worked forloop

--- zen/nodes/ControlNodes.cpp
+++ zen/nodes/ControlNodes.cpp
@@ -4,43 +4,15 @@
 #include <cassert>
 
 
-struct BeginFor : zen::INode {
-    int m_index;
-    int m_count;
-
-    void initState() {
-        m_index = 0;
-        m_count = 1;
-    }
-
-    bool isContinue() const {
-        return m_index < m_count;
-    }
-
-    virtual void apply() override {
-        auto count = get_input<zen::NumericObject>("count")->get<int>();
-
-        auto fore = std::make_shared<zen::ConditionObject>();
-        set_output("FOR", std::move(fore));
-
-        auto ret = std::make_shared<zen::NumericObject>();
-        ret->set(m_index);
-        set_output("index", std::move(ret));
-
-        m_count = count;
-        m_index++;
-    }
+template <class F>
+struct RAII {
+    F dtor;
+    RAII(F &&dtor) : dtor(dtor) {}
+    ~RAII() { dtor(); }
 };
 
-ZENDEFNODE(BeginFor, {
-    {"count"},
-    {"index", "FOR"},
-    {},
-    {"list"},
-});
 
-
-struct EndFor : zen::INode {
+struct ContextManagedNode : zen::INode {
     std::unique_ptr<zen::Context> m_ctx = nullptr;
 
     void push_context() {
@@ -54,6 +26,45 @@ struct EndFor : zen::INode {
         sess->ctx = std::move(m_ctx);
     }
 
+    auto scoped_push_context() {
+        push_context();
+        return RAII([this]() {
+            pop_context();
+        });
+    }
+};
+
+
+struct BeginFor : zen::INode {
+    int m_index;
+    int m_count;
+
+    bool isContinue() const {
+        return m_index < m_count;
+    }
+
+    virtual void apply() override {
+        m_index = 0;
+        m_count = get_input<zen::NumericObject>("count")->get<int>();
+        set_output("FOR", std::make_shared<zen::ConditionObject>());
+    }
+
+    void update() {
+        auto ret = std::make_shared<zen::NumericObject>();
+        ret->set(m_index++);
+        set_output("index", std::move(ret));
+    }
+};
+
+ZENDEFNODE(BeginFor, {
+    {"count"},
+    {"index", "FOR"},
+    {},
+    {"list"},
+});
+
+
+struct EndFor : ContextManagedNode {
     virtual void doApply() override {
         auto [sn, ss] = inputBounds.at("FOR");
         auto fore = dynamic_cast<BeginFor *>(sess->nodes.at(sn).get());
@@ -61,8 +72,9 @@ struct EndFor : zen::INode {
             printf("EndFor::FOR must be conn to BeginFor::FOR!\n");
             abort();
         }
-        fore->initState();
+        sess->applyNode(sn);
         while (fore->isContinue()) {
+            fore->update();
             push_context();
             zen::INode::doApply();
             pop_context();
