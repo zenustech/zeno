9234039fc50017406bd182dbbac53ea6d7166639
archibate <1931127624@qq.com>
2021-06-27 13:11:48 +0800

    zen/Visualization.h

---
+++ zen/Visualization.cpp
@@ -0,0 +1,36 @@
+#include <zen/zen.h>
+#include <zen/Visualization.h>
+#include <zen/GlobalState.h>
+
+namespace zen::Visualization {
+
+static int objid = 0;
+
+ZENAPI std::string exportPath(std::string const &ext) {
+    char buf[100];
+    sprintf(buf, "%06d", zen::state.frameid);
+    auto path = fs::path(zen::state.iopath) / buf;
+    if (!fs::is_directory(path)) {
+        fs::create_directory(path);
+    }
+    sprintf(buf, "%06d.%s", objid++, ext.c_str());
+    path /= buf;
+    auto ret = std::make_unique<zen::StringObject>();
+    //printf("EXPORTPATH: %s\n", path.c_str());
+    return path.string();
+}
+
+ZENAPI void endFrame() {
+    char buf[100];
+    sprintf(buf, "%06d", zen::state.frameid);
+    auto path = fs::path(zen::state.iopath) / buf;
+    if (!fs::is_directory(path)) {
+        fs::create_directory(path);
+    }
+    path /= "done.lock";
+    std::ofstream ofs(path.string());
+    ofs.write("DONE", 4);
+    objid = 0;
+}
+
+}
