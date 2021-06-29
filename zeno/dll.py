cb0ac31108240964d8f32aa50d502e750960f200
archibate <1931127624@qq.com>
2021-06-29 13:58:20 +0800

    use os.add_dll_directory instead of pywin32

--- zeno/dll.py
+++ zeno/dll.py
@@ -3,11 +3,7 @@ import ctypes
 from zenutils import rel2abs, os_name
 
 if os_name == 'win32':
-    try:
-        import win32api
-    except ImportError as e:
-        raise ImportError('Please run: python -m pip install pywin32') from e
-    win32api.SetDllDirectory(rel2abs(__file__, 'lib'))
+    os.add_dll_directory(rel2abs(__file__, 'lib'))
     ctypes.cdll.LoadLibrary('zeno.dll')
 else:
     ctypes.cdll.LoadLibrary(rel2abs(__file__, 'lib', 'libzeno.so'))
