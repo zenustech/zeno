a1725aa9abf860c1041a9b94e68c358ea0a94c83
zhouhang95 <765229842@qq.com>
2021-06-30 10:30:14 +0800

    win32: bug-fix

--- zeno/dll.py
+++ zeno/dll.py
@@ -1,9 +1,9 @@
-import ctypes
+import ctypes, os
 
 from zenutils import rel2abs, os_name
 
 if os_name == 'win32':
-    os.add_dll_directory(rel2abs(__file__, 'lib'))
+    os.environ['PATH'] += os.pathsep + rel2abs(__file__, 'lib')
     ctypes.cdll.LoadLibrary('zeno.dll')
 else:
     ctypes.cdll.LoadLibrary(rel2abs(__file__, 'lib', 'libzeno.so'))
