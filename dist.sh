546ba7e8cbc1f49b11c8d13bedbbd369493eb516
archibate <1931127624@qq.com>
2021-07-01 19:26:05 +0800

    update dist

--- dist.sh
+++ dist.sh
@@ -4,9 +4,13 @@ set -e
 rm -rf /tmp/zenv
 mkdir -p /tmp/zenv/{bin,lib/python3.9}
 
-dlls=`scripts/tracedll.sh python3 -m zenapi arts/FLIPSolver.zsg | awk -F.so '{print $1".so*";}'`
-echo $dlls
-cp -d $dlls /tmp/zenv/lib
+for x in `scripts/tracedll.sh python3 -m zenapi arts/FLIPSolver.zsg`; do
+    y="`realpath $x`"
+    echo "$x => $y"
+    x="$(echo "$x" | awk -F.so '{print $1".so*"}')"
+    cp -d $x /tmp/zenv/lib
+    cp "$y" /tmp/zenv/lib
+done
 cp -d /usr/lib/ld-linux-x86-64.so.2 /usr/lib/ld-2.33.so /tmp/zenv/lib
 cp -rd `ls -d /usr/lib/python3.9/* | grep -v site-packages` /tmp/zenv/lib/python3.9
 cp -d /usr/bin/python{,3,3.9}{,-config} /tmp/zenv/bin
