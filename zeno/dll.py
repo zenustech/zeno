096757c295a38246c16951ad2468e8b629851b69
archibate <1931127624@qq.com>
2021-07-01 17:17:26 +0800

    make zhouhang happy

--- zeno/dll.py
+++ zeno/dll.py
@@ -3,7 +3,11 @@ import ctypes, os
 from zenutils import rel2abs, os_name
 
 if os_name == 'win32':
-    os.environ['PATH'] += os.pathsep + rel2abs(__file__, 'lib')
+    dllpath = rel2abs(__file__, 'lib')
+    if hasattr(os, 'add_dll_directory'):
+        os.add_dll_directory(dllpath)
+    else:
+        os.environ['PATH'] += os.pathsep + dllpath
     ctypes.cdll.LoadLibrary('zeno.dll')
 else:
     ctypes.cdll.LoadLibrary(rel2abs(__file__, 'lib', 'libzeno.so'))
