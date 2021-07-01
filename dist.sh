80484a9c0578fc5e66307bf8c12e8b632622011b
archibate <1931127624@qq.com>
2021-07-01 18:06:31 +0800

    add release note

--- dist.sh
+++ dist.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 set -e
 
-rm -rf /tmp/zenv
-mkdir -p /tmp/zenv/{bin,lib/python3.9}
+rm -rf /tmp/build
+mkdir -p /tmp/build/{bin,lib/python3.9}
 
-cp -d /tmp/tmp-install/lib/*.so* /tmp/zenv/lib
+cp -d /tmp/tmp-install/lib/*.so* /tmp/build/lib
 for x in `ZEN_SPROC=1 ZEN_DOEXEC=2 ZEN_OPEN=arts/FLIPSolver.zsg scripts/tracedll.sh python3 -m zenqt`; do
     y="`realpath $x`"
     echo "$x => $y"
     x="$(echo "$x" | sed 's/\.so/@dot@so/g' | awk -F @dot@so '{print $1".so*"}')"
-    cp -d $x /tmp/zenv/lib
-    cp "$y" /tmp/zenv/lib
+    cp -d $x /tmp/build/lib
+    cp "$y" /tmp/build/lib
 done
-cp -d /usr/lib/ld-linux-x86-64.so.2 /usr/lib/ld-2.33.so /tmp/zenv/lib
-cp -rd `ls -d /usr/lib/python3.9/* | grep -v site-packages` /tmp/zenv/lib/python3.9
-cp -d /usr/bin/python{,3,3.9}{,-config} /tmp/zenv/bin
-/tmp/zenv/bin/python3.9 -m ensurepip
-https_proxy= python3.9 -m pip install -t /tmp/zenv/lib/python3.9 PyQt5 numpy
-/tmp/zenv/bin/python3.9 setup.py install
+cp -d /usr/lib/ld-linux-x86-64.so.2 /usr/lib/ld-2.33.so /tmp/build/lib
+cp -rd `ls -d /usr/lib/python3.9/* | grep -v site-packages` /tmp/build/lib/python3.9
+cp -d /usr/bin/python{,3,3.9}{,-config} /tmp/build/bin
+/tmp/build/bin/python3.9 -m ensurepip
+https_proxy= python3.9 -m pip install -t /tmp/build/lib/python3.9 PyQt5 numpy
+/tmp/build/bin/python3.9 setup.py install
 
-mv /tmp/zenv/{bin,.bin}
-mkdir -p /tmp/zenv/bin
+mv /tmp/build/{bin,.bin}
+mkdir -p /tmp/build/bin
 
-cp scripts/ldmock /tmp/zenv/.ldmock
-for x in `ls /tmp/zenv/.bin`; do
-    ln -sf ../.ldmock /tmp/zenv/bin/$x
+cp scripts/ldmock /tmp/build/.ldmock
+for x in `ls /tmp/build/.bin`; do
+    ln -sf ../.ldmock /tmp/build/bin/$x
 done
 
-cat > /tmp/zenv/start.sh <<EOF
+cat > /tmp/build/start.sh <<EOF
 #!/bin/bash
 
 oldwd="\$(pwd)"
@@ -36,16 +36,20 @@ newwd="\$(pwd)"
 cd -- "\$oldwd"
 exec -- "\$newwd/bin/python3.9" -m zenqt "\$@"
 EOF
-chmod +x /tmp/zenv/start.sh
+chmod +x /tmp/build/start.sh
 
-echo 'docker run -v /tmp/zenv:/tmp/zenv -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -it ubuntu:18.04 /tmp/zenv/start.sh'
+echo 'docker run -v /tmp/build:/tmp/build -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -it ubuntu:18.04 /tmp/build/start.sh'
 
 
 version=`cat setup.py | grep "version =" | awk -F "version = '" '{print $2}' | cut -d\' -f1`
 fname=zeno-linux-$version
 echo $fname
 
+cp scripts/release_note.md /tmp/build/README.md
+cp assets /tmp/build/
+cp arts /tmp/build/
+
 cd /tmp
 rm -rf $fname $fname.tar.gz
-mv zenv $fname
+mv build $fname
 tar zcvf $fname.tar.gz $fname/
---
+++ scripts/release_note.md
@@ -0,0 +1,5 @@
+Welcome to use the binary release of ZENO! To play with it, simply extract the `.tar.gz` and run `./start.sh` in it, the node editor window will shows up if everything works.
+
+There are some example graphs in the `./arts/` folder, you may open them in the editor and have fun!
+
+If you find the binary release didn't worked properly or some error message has been thrown on your machine, please let me know by opening an [issue](https://github.com/zensim-dev/zeno/issues) on the GitHub, thanks for you support!
