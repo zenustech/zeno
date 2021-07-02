d5beaecabf4a786d6f8360ea732d9b9c2a9cf710
archibate <1931127624@qq.com>
2021-07-02 10:05:10 +0800

    fix using type traits

--- projects/zenvdb/SampleVDBToPrimitive.cpp
+++ projects/zenvdb/SampleVDBToPrimitive.cpp
@@ -1,17 +1,47 @@
-#if 0
 #include <zeno/zeno.h>
 #include <zeno/VDBGrid.h>
 #include <zeno/NumericObject.h>
 #include <zeno/PrimitiveObject.h>
-#include <openvdb/openvdb.h>
-#include <openvdb/tree/LeafManager.h>
 
 namespace zeno {
 
-template <class T, class S>
-void sampleVDBAttribute(std::vector<zen::vec3f> const &pos, std::vector<T> &arr,
+template <class T>
+struct attr_to_vdb_type {
+};
+
+template <>
+struct attr_to_vdb_type<float> {
+    using type = VDBFloatGrid;
+};
+
+template <>
+struct attr_to_vdb_type<vec3f> {
+    using type = VDBFloat3Grid;
+};
+
+template <>
+struct attr_to_vdb_type<int> {
+    using type = VDBIntGrid;
+};
+
+template <>
+struct attr_to_vdb_type<vec3i> {
+    using type = VDBInt3Grid;
+};
+
+template <class T>
+using attr_to_vdb_type_t = typename attr_to_vdb_type<T>::type;
+
+template <class T>
+void sampleVDBAttribute(std::vector<vec3f> const &pos, std::vector<T> &arr,
     VDBGrid *ggrid, std::string const &attr) {
-    //dynamic_cast<VDBFloatGrid>(ggrid);
+    using VDBType = attr_to_vdb_type_t<T>;
+    auto ptr = dynamic_cast<VDBType *>(ggrid);
+    if (!ptr) {
+        printf("ERROR: vdb attribute type mismatch!\n");
+        return;
+    }
+    auto grid = ptr->m_grid;
 }
 
 struct SampleVDBToPrimitive : INode {
@@ -20,7 +50,7 @@ struct SampleVDBToPrimitive : INode {
         auto grid = get_input<VDBGrid>("grid");
         auto attrPrim = get_param<std::string>("attrPrim");
         auto attrGrid = get_param<std::string>("attrGrid");
-        auto &pos = prim->attr<zen::vec3f>("pos");
+        auto &pos = prim->attr<vec3f>("pos");
 
         std::visit([&] (auto &vel) {
             sampleVDBAttribute(pos, vel, grid.get(), attrGrid);
@@ -39,5 +69,3 @@ ZENDEFNODE(SampleVDBToPrimitive, {
 
 
 }
-
-#endif
