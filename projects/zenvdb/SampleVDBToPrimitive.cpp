92f7337d65547173012a853f2ebe90e4a598cdf5
zhxx1987 <zhangshinshin@gmail.com>
2021-07-03 03:49:28 +0800

    volume slice upgraded

--- projects/zenvdb/SampleVDBToPrimitive.cpp
+++ projects/zenvdb/SampleVDBToPrimitive.cpp
@@ -1,84 +1,194 @@
-#include <zeno/zeno.h>
-#include <zeno/VDBGrid.h>
 #include <zeno/NumericObject.h>
 #include <zeno/PrimitiveObject.h>
-
+#include <zeno/StringObject.h>
+#include <zeno/VDBGrid.h>
+#include <zeno/vec.h>
+#include <zeno/zeno.h>
 namespace zeno {
 
-template <class T>
-struct attr_to_vdb_type {
+template <class T> struct attr_to_vdb_type {};
+
+template <> struct attr_to_vdb_type<float> {
+  static constexpr bool is_scalar = true;
+  using type = VDBFloatGrid;
 };
 
-template <>
-struct attr_to_vdb_type<float> {
-    static constexpr bool is_scalar = true;
-    using type = VDBFloatGrid;
+template <> struct attr_to_vdb_type<vec3f> {
+  static constexpr bool is_scalar = false;
+  using type = VDBFloat3Grid;
 };
 
-template <>
-struct attr_to_vdb_type<vec3f> {
-    static constexpr bool is_scalar = false;
-    using type = VDBFloat3Grid;
+template <> struct attr_to_vdb_type<int> {
+  static constexpr bool is_scalar = true;
+  using type = VDBIntGrid;
 };
 
-template <>
-struct attr_to_vdb_type<int> {
-    static constexpr bool is_scalar = true;
-    using type = VDBIntGrid;
-};
-
-template <>
-struct attr_to_vdb_type<vec3i> {
-    static constexpr bool is_scalar = false;
-    using type = VDBInt3Grid;
+template <> struct attr_to_vdb_type<vec3i> {
+  static constexpr bool is_scalar = false;
+  using type = VDBInt3Grid;
 };
 
 template <class T>
 void sampleVDBAttribute(std::vector<vec3f> const &pos, std::vector<T> &arr,
-    VDBGrid *ggrid) {
-    using VDBType = typename attr_to_vdb_type<T>::type;
-    auto ptr = dynamic_cast<VDBType *>(ggrid);
-    if (!ptr) {
-        printf("ERROR: vdb attribute type mismatch!\n");
-        return;
-    }
-    auto grid = ptr->m_grid;
+                        VDBGrid *ggrid) {
+  using VDBType = typename attr_to_vdb_type<T>::type;
+  auto ptr = dynamic_cast<VDBType *>(ggrid);
+  if (!ptr) {
+    printf("ERROR: vdb attribute type mismatch!\n");
+    return;
+  }
+  auto grid = ptr->m_grid;
 
-    #pragma omp parallel for
-    for (int i = 0; i < pos.size(); i++) {
-        auto p0 = pos[i];
-        auto p1 = vec_to_other<openvdb::Vec3R>(p0);
-        auto p2 = grid->worldToIndex(p1);
-        auto val = openvdb::tools::PointSampler::sample(grid->tree(), p2);
-        if constexpr (attr_to_vdb_type<T>::is_scalar) {
-            arr[i] = val;
-        } else {
-            arr[i] = other_to_vec<3>(val);
-        }
+#pragma omp parallel for
+  for (int i = 0; i < pos.size(); i++) {
+    auto p0 = pos[i];
+    auto p1 = vec_to_other<openvdb::Vec3R>(p0);
+    auto p2 = grid->worldToIndex(p1);
+    auto val = openvdb::tools::BoxSampler::sample(grid->tree(), p2);
+    if constexpr (attr_to_vdb_type<T>::is_scalar) {
+      arr[i] = val;
+    } else {
+      arr[i] = other_to_vec<3>(val);
     }
+  }
 }
+struct GetVDBBound : INode {
+  virtual void apply() override {
+    auto grid = get_input<VDBGrid>("vdbGrid");
+    auto bbmin = zeno::IObject::make<zeno::NumericObject>();
+    auto bbmax = zeno::IObject::make<zeno::NumericObject>();
 
-struct SampleVDBToPrimitive : INode {
-    virtual void apply() override {
-        auto prim = get_input<PrimitiveObject>("prim");
-        auto grid = get_input<VDBGrid>("vdbGrid");
-        auto attr = get_param<std::string>("primAttr");
-        auto &pos = prim->attr<vec3f>("pos");
+    zeno::vec3f bmin, bmax;
+    openvdb::CoordBBox box = grid->evalActiveVoxelBoundingBox();
+    auto corner = box.min();
+    auto length = box.max() - box.min();
+    auto world_min = grid->indexToWorld(box.min());
+    auto world_max = grid->indexToWorld(box.max());
 
-        std::visit([&] (auto &vel) {
-            sampleVDBAttribute(pos, vel, grid.get());
-        }, prim->attr(attr));
-
-        set_output("prim", std::move(prim));
+    for (size_t d = 0; d < 3; d++) {
+      bmin[d] = world_min[d];
+      bmax[d] = world_max[d];
     }
+
+    for (int dx = 0; dx < 2; dx++)
+      for (int dy = 0; dy < 2; dy++)
+        for (int dz = 0; dz < 2; dz++) {
+          auto coord =
+              corner + decltype(length){dx ? length[0] : 0, dy ? length[1] : 0,
+                                        dz ? length[2] : 0};
+
+          auto pos = grid->indexToWorld(coord);
+
+          for (int d = 0; d < 3; d++) {
+            bmin[d] = pos[d] < bmin[d] ? pos[d] : bmin[d];
+            bmax[d] = pos[d] > bmax[d] ? pos[d] : bmax[d];
+          }
+        }
+
+    bbmin->set<zeno::vec3f>(bmin);
+    bbmax->set<zeno::vec3f>(bmax);
+    set_output("bmin", bbmin);
+    set_output("bmax", bbmax);
+  }
+};
+
+ZENDEFNODE(GetVDBBound, {
+                            {"vdbGrid"},
+                            {"bmin", "bmax"},
+                            {},
+                            {"openvdb"},
+                        });
+struct SampleVDBToPrimitive : INode {
+  virtual void apply() override {
+    auto prim = get_input<PrimitiveObject>("prim");
+    auto grid = get_input<VDBGrid>("vdbGrid");
+    std::string attrs = get_input<StringObject>("primdAttr")->get();
+    auto attr = get_param<std::string>("primAttr");
+    auto &pos = prim->attr<vec3f>("pos");
+    if (grid->getType() == std::string("FloatGrid"))
+      prim->add_attr<float>(attrs);
+    if (grid->getType() == std::string("Float3Grid")) {
+      prim->add_attr<zeno::vec3f>(attrs);
+    }
+    std::visit([&](auto &vel) { sampleVDBAttribute(pos, vel, grid.get()); },
+               prim->attr(attrs));
+
+    set_output("prim", std::move(prim));
+  }
 };
 
 ZENDEFNODE(SampleVDBToPrimitive, {
-    {"prim", "vdbGrid"},
-    {"prim"},
-    {{"string", "primAttr", "vel"}},
-    {"openvdb"},
-});
+                                     {"prim", "vdbGrid", "primdAttr"},
+                                     {"prim"},
+                                     {{"string", "primAttr", "vel"}},
+                                     {"visualize"},
+                                 });
 
+// this is to be deprecated
+struct HeatMap {
+  std::vector<zeno::vec3f> colorBar;
 
+  HeatMap() {
+    colorBar.resize(1024);
+    for (int i = 0; i < 512; i++) {
+      colorBar[i] = (1.0f - (float)i / 512.0f) * zeno::vec3f(0, 0, 1) +
+                    (float)i / 512.0f * zeno::vec3f(0, 1, 0);
+      colorBar[i + 512] = (1.0f - (float)i / 512.0f) * zeno::vec3f(0, 1, 0) +
+                          (float)i / 512.0f * zeno::vec3f(1, 0, 0);
+    }
+  }
+  zeno::vec3f sample(float c) {
+    int i = c * 1024.0f;
+    int j = i + 1;
+    zeno::clamp(i, 0, 1023);
+    zeno::clamp(j, 0, 1023);
+    float dx = c * 1024.0f - (float)i;
+    zeno::clamp(dx, 0.0f, 1.0f);
+    return colorBar[i] * (1.0f - dx) + dx * colorBar[j];
+  }
+};
+static HeatMap tempHeatMap;
+
+template <class T>
+void colorFromAttr(std::vector<T> &arr, std::vector<zeno::vec3f> &clr,
+                   float minval, float maxval, HeatMap *hm) {
+
+#pragma omp parallel for
+  for (int i = 0; i < arr.size(); i++) {
+    float d;
+    if (!std::is_same<T, float>::value)
+      d = std::sqrt(zeno::dot(arr[i], arr[i]));
+    else if constexpr (std::is_same<T, float>::value)
+      d = arr[i];
+    auto c =
+        (std::max(std::min(d, maxval), minval) - minval) / (maxval - minval);
+    clr[i] = hm->sample(c);
+  }
 }
+
+struct HeatMapPrimitive : INode {
+  virtual void apply() override {
+    auto prim = get_input<PrimitiveObject>("prim");
+    std::string attrs = get_input<StringObject>("primdAttr")->get();
+    auto minval = get_input<NumericObject>("min")->get<float>();
+    auto maxval = get_input<NumericObject>("max")->get<float>();
+    prim->add_attr<zeno::vec3f>("clr");
+
+    std::visit(
+        [&](auto &vel) {
+          colorFromAttr(vel, prim->attr<zeno::vec3f>("clr"), minval, maxval,
+                        &tempHeatMap);
+        },
+        prim->attr(attrs));
+    set_output("prim", std::move(prim));
+  }
+};
+
+ZENDEFNODE(HeatMapPrimitive, {
+                                 {"prim", "primdAttr", "min", "max"},
+                                 {"prim"},
+                                 {},
+                                 {"visualize"},
+                             });
+
+} // namespace zeno
--- zeno/nodes/prim/MakeGridPrimitive.cpp
+++ zeno/nodes/prim/MakeGridPrimitive.cpp
@@ -1,55 +1,86 @@
-#include <zeno/zeno.h>
-#include <zeno/PrimitiveObject.h>
-#include <zeno/NumericObject.h>
-#include <zeno/vec.h>
-#include <cstring>
-#include <cstdlib>
 #include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <zeno/NumericObject.h>
+#include <zeno/PrimitiveObject.h>
+#include <zeno/vec.h>
+#include <zeno/zeno.h>
 
 namespace zeno {
 
-
 struct Make2DGridPrimitive : INode {
-    virtual void apply() override {
-        size_t nx = get_input<NumericObject>("nx")->get<int>();
-        size_t ny = has_input("ny") ? get_input<NumericObject>("ny")->get<int>() : nx;
-        float dx = has_input("dx") ? get_input<NumericObject>("dx")->get<float>()
-            : 1.f / std::max(nx - 1, (size_t)1);
-        float dy = has_input("dy") ? get_input<NumericObject>("dy")->get<float>()
-            : (has_input("dx") ? dx : 1.f / std::max(ny - 1, (size_t)1));
-        vec3f ax = get_input<NumericObject>("sizeX")->get<vec3f>() * dx;
-        vec3f ay = get_input<NumericObject>("sizeY")->get<vec3f>() * dy;
-        vec3f o = has_input("origin") ? get_input<NumericObject>("origin")->get<vec3f>()
-            : vec3f(0);
+  virtual void apply() override {
+    size_t nx = (size_t)(get_input<NumericObject>("nx")->get<float>());
+    size_t ny = has_input("ny")
+                    ? (size_t)(get_input<NumericObject>("ny")->get<float>())
+                    : nx;
+    float dx = has_input("dx") ? get_input<NumericObject>("dx")->get<float>()
+                               : 1.f / std::max(nx - 1, (size_t)1);
+    float dy = has_input("dy")
+                   ? get_input<NumericObject>("dy")->get<float>()
+                   : (has_input("dx") ? dx : 1.f / std::max(ny - 1, (size_t)1));
+    vec3f ax = vec3f(1, 0, 0) *
+               dx; // get_input<NumericObject>("sizeX")->get<vec3f>() * dx;
+    vec3f ay = vec3f(0, 1, 0) *
+               dy; // get_input<NumericObject>("sizeY")->get<vec3f>() * dy;
+    vec3f o = has_input("origin")
+                  ? get_input<NumericObject>("origin")->get<vec3f>()
+                  : vec3f(0);
 
-        if (get_param<int>("isCentered"))
-            o -= (ax * (nx - 1) + ay * (ny - 1)) / 2;
+    if (get_param<int>("isCentered"))
+      o -= (ax * (nx - 1) + ay * (ny - 1)) / 2;
 
-        auto prim = std::make_shared<PrimitiveObject>();
-        prim->resize(nx * ny);
-        auto &pos = prim->add_attr<vec3f>("pos");
-        for (size_t y = 0; y < ny; y++) {
-            for (size_t x = 0; x < nx; x++) {
-                vec3f p = o + x * ax + y * ay;
-                size_t i = x + y * nx;
-                pos[i] = p;
-            }
-        }
-
-        set_output("prim", std::move(prim));
+    auto prim = std::make_shared<PrimitiveObject>();
+    prim->resize(nx * ny);
+    auto &pos = prim->add_attr<vec3f>("pos");
+#pragma omp parallel for
+    // for (size_t y = 0; y < ny; y++) {
+    //     for (size_t x = 0; x < nx; x++) {
+    for (int index = 0; index < nx * ny; index++) {
+      int x = index % nx;
+      int y = index / nx;
+      vec3f p = o + x * ax + y * ay;
+      size_t i = x + y * nx;
+      pos[i] = p;
+      // }
     }
+    prim->tris.resize((nx - 1) * (ny - 1) * 2);
+#pragma omp parallel for
+    for (int index = 0; index < (nx - 1) * (ny - 1); index++) {
+      int x = index % (nx - 1);
+      int y = index / (nx - 1);
+      prim->tris[index * 2][0] = y * nx + x;
+      prim->tris[index * 2][1] = y * nx + x + 1;
+      prim->tris[index * 2][2] = (y + 1) * nx + x + 1;
+      prim->tris[index * 2 + 1][0] = (y + 1) * nx + x + 1;
+      prim->tris[index * 2 + 1][1] = (y + 1) * nx + x;
+      prim->tris[index * 2 + 1][2] = y * nx + x;
+    }
+
+    set_output("prim", std::move(prim));
+  }
 };
 
-ZENDEFNODE(Make2DGridPrimitive,
-        { /* inputs: */ {
-        "nx", "ny", "dx", "dy", "sizeX", "sizeY", "origin",
-        }, /* outputs: */ {
-        "prim",
-        }, /* params: */ {
-        {"int", "isCentered", "0"},
-        }, /* category: */ {
-        "primitive",
-        }});
+ZENDEFNODE(Make2DGridPrimitive, {/* inputs: */ {
+                                     "nx",
+                                     "ny",
+                                     "dx",
+                                     "dy",
+                                     "sizeX",
+                                     "sizeY",
+                                     "origin",
+                                 },
+                                 /* outputs: */
+                                 {
+                                     "prim",
+                                 },
+                                 /* params: */
+                                 {
+                                     {"int", "isCentered", "0"},
+                                 },
+                                 /* category: */
+                                 {
+                                     "primitive",
+                                 }});
 
-
-}
+} // namespace zeno
