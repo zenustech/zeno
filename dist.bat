fba1eb71c2f1a3258bd6112e6a4ab09355050c62
archibate <1931127624@qq.com>
2021-07-02 15:33:17 +0800

    copy arts and assets to win release too

--- dist.bat
+++ dist.bat
@@ -3,5 +3,7 @@
 zenv\PCBuild\amd64\python -m ensurepip
 python -m pip install PyQt5 numpy -t zenv\Lib\site-packages -i https://mirrors.aliyun.com/pypi/simple
 zenv\PCBuild\amd64\python setup.py install
+copy /y arts zenv
+copy /y assets zenv
 copy /y scripts\start.bat zenv
 copy /y scripts\start_nocons.bat zenv
