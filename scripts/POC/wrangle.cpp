85f10c9ae693d351d31a361fd28274904bdfff64
archibate <1931127624@qq.com>
2021-07-03 16:57:44 +0800

    fix isvoid

--- scripts/POC/wrangle.cpp
+++ scripts/POC/wrangle.cpp
@@ -4,12 +4,15 @@
 #include <iostream>
 
 template <int First, int Last, typename Lambda>
-inline void static_for(Lambda const& f) {
+inline constexpr bool static_for(Lambda const &f) {
     if constexpr (First < Last) {
         if (f(std::integral_constant<int, First>{})) {
-            static_for<First + 1, Last>(f);
+            return true;
+        } else {
+            return static_for<First + 1, Last>(f);
         }
     }
+    return false;
 }
 
 using std::cout;
@@ -18,8 +21,6 @@ using std::endl;
 enum class dtype : int {
     none, i32, f32,
 };
-
-inline constexpr size_t dtype_count = magic_enum::enum_values<dtype>().size();
 
 template <dtype dt>
 struct dtype_traits {
@@ -40,30 +41,52 @@ struct dtype_traits<dtype::f32> {
     using type = float;
 };
 
-template <dtype dt>
-using dtype_t = typename dtype_traits<dt>::type;
+constexpr auto dtype_name(dtype dt) {
+    return magic_enum::enum_name(dt);
+}
+
+constexpr size_t dtype_size(dtype dt) {
+    size_t ret = 0;
+    static_for<0, magic_enum::enum_values<dtype>().size()>([&](auto i) {
+        constexpr auto t = magic_enum::enum_cast<dtype>(i).value();
+        if (dt == t) {
+            using T = typename dtype_traits<t>::type;
+            if constexpr (std::is_void_v<T>)
+                ret = 0;
+            else
+                ret = sizeof(T);
+            return true;
+        }
+        return false;
+    });
+    return ret;
+}
 
 struct array {
     std::vector<char> m_data;
     dtype m_type;
+    size_t m_size;
 
     constexpr dtype type() const { return m_type; }
     void *data() const { return (void *)m_data.data(); }
+    size_t size() const { return m_size; }
 };
 
 template <class T>
-void do_apply(void *p) {
+void do_apply(T *p, size_t n) {
     printf("%s\n", typeid(T).name());
 }
 
 void apply(array &a) {
+    auto dt = a.type();
     static_for<0, magic_enum::enum_values<dtype>().size()>([&](auto i) {
-        constexpr dtype dt = magic_enum::enum_cast<dtype>(i).value();
-        if (a.type() == dt) {
-            do_apply<dtype_t<dt>>(a.data());
-            return false;
+        constexpr auto t = magic_enum::enum_cast<dtype>(i).value();
+        if (dt == t) {
+            using T = typename dtype_traits<t>::type;
+            do_apply<T>((T *)a.data(), a.size());
+            return true;
         }
-        return true;
+        return false;
     });
 }
 
