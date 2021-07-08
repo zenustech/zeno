2faa66dac2781ade27e29f0359ad6a41854adc34
archibate <1931127624@qq.com>
2021-07-08 23:32:31 +0800

    code.zfx

--- scripts/POC/speed.cpp
+++ scripts/POC/speed.cpp
@@ -1,15 +1,36 @@
 #define ZFX_IMPLEMENTATION
-#include <cstdio>
 #include <zeno/zfx.h>
+#include <Hg/IOUtils.h>
+#include <iostream>
+#include <chrono>
+
+#define N (128*1024*1024)
 
 int main() {
-    auto prog = zfx::compile_program("define f1 @x\n@x = 3 * @x");
+    auto prog = zfx::compile_program(hg::file_get_content("code.zfx"));
 
-    float *x = new float[233];
+    volatile float *x = new volatile float[N];
+    memset((float *)x, 0, sizeof(float) * N);
 
-    for (int i = 0; i < 32; i++) {
+    auto t0 = std::chrono::steady_clock::now();
+
+#if 1
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {
         zfx::Context ctx;
-        ctx.memtable[0] = x + i;
+        ctx.memtable[0] = (float *)x + i;
         prog->execute(&ctx);
     }
+#else
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {
+        x[i] = x[i] * x[i] + x[i] * 3.14f + 2.718f / x[i];
+    }
+#endif
+
+    auto t1 = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>
+        (t1 - t0).count() << " ms" << std::endl;
+
+    return 0;
 }
