9d924a9f655bc63b71991380d0c78f4252cb34bc
archibate <1931127624@qq.com>
2021-07-09 15:54:23 +0800

    dummy parse

--- scripts/POC/zfxllvm/parse.cpp
+++ scripts/POC/zfxllvm/parse.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstring>
 #include <iostream>
+#include <memory>
 
 using std::cout;
 using std::endl;
@@ -43,11 +44,53 @@ std::vector<std::string> tokenize(const char *cp) {
     return tokens;
 }
 
+struct AST {
+    std::string token;
+    std::vector<AST *> args;
+
+    explicit AST(std::string const &token, std::vector<AST *> const &args = {})
+        : token(token), args(args) {}
+};
+
+struct Parser {
+    std::vector<std::string> tokens;
+    typename std::vector<std::string>::iterator token;
+    std::vector<std::unique_ptr<AST>> asts;
+
+    Parser(std::vector<std::string> const &tokens_) : tokens(tokens_) {
+        token = tokens.begin();
+    }
+
+    void parse_atom() {
+        auto ast = std::make_unique<AST>(*token++);
+        asts.push_back(std::move(ast));
+    }
+
+    void parse() {
+        parse_atom();
+    }
+};
+
+void print(AST *ast) {
+    if (ast->args.size())
+        cout << '(';
+    cout << ast->token;
+    for (auto const &a: ast->args) {
+        cout << ' ';
+        print(a);
+    }
+    if (ast->args.size())
+        cout << ')';
+}
+
 int main() {
     std::string code("pos = 1 * 3");
     auto tokens = tokenize(code.c_str());
-    for (auto t: tokens) {
-        cout << t << endl;
+    Parser p(tokens);
+    p.parse();
+    for (auto const &a: p.asts) {
+        print(a.get());
     }
+    cout << endl;
     return 0;
 }
