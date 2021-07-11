021e842c60297d9e13722cb7be6761ea8b0ed7af
archibate <1931127624@qq.com>
2021-07-11 15:36:36 +0800

    do type check pass

--- ZFX/Visitors.h
+++ ZFX/Visitors.h
@@ -6,6 +6,7 @@
 namespace zfx {
 
 std::unique_ptr<IR> apply_clone(IR *ir);
+std::unique_ptr<IR> apply_type_check(IR *ir);
 std::unique_ptr<IR> apply_lower_math(IR *ir);
 std::unique_ptr<IR> apply_lower_access(IR *ir);
 std::string apply_emit_assembly(IR *ir);
--- ZFX/visitors/TypeCheck.cpp
+++ ZFX/visitors/TypeCheck.cpp
@@ -3,20 +3,25 @@
 
 namespace zfx {
 
-struct LowerMath : Visitor<LowerMath> {
+struct TypeCheck : Visitor<TypeCheck> {
     using visit_stmt_types = std::tuple
-        < Statement
+        < SymbolStmt
+        , Statement
         >;
 
     std::unique_ptr<IR> ir = std::make_unique<IR>();
+
+    void visit(SymbolStmt *stmt) {
+        ir->push_clone_back(stmt);
+    }
 
     void visit(Statement *stmt) {
         ir->push_clone_back(stmt);
     }
 };
 
-std::unique_ptr<IR> apply_lower_math(IR *ir) {
-    LowerMath visitor;
+std::unique_ptr<IR> apply_type_check(IR *ir) {
+    TypeCheck visitor;
     visitor.apply(ir);
     return std::move(visitor.ir);
 }
