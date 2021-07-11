cd3721db31eff0a2adae90d2cbcdf9ac237edb6d
archibate <1931127624@qq.com>
2021-07-11 22:17:15 +0800

    try StoreRAII

--- ZFX/visitors/KillLocalStore.cpp
+++ ZFX/visitors/KillLocalStore.cpp
@@ -10,30 +10,56 @@ struct GatherLocalLoad : Visitor<GatherLocalLoad> {
         < AsmLocalLoadStmt
         >;
 
-    std::map<int, int> usage;
+    std::map<int, int> last_load;
 
     void visit(AsmLocalLoadStmt *stmt) {
-        usage[stmt->mem] = std::max(usage[stmt->mem], stmt->id);
+        last_load[stmt->mem] = std::max(last_load[stmt->mem], stmt->id);
     }
 };
 
 struct KillLocalStore : Visitor<KillLocalStore> {
     using visit_stmt_types = std::tuple
-        < AsmLocalStoreStmt
+        < AsmLocalLoadStmt
+        , AsmLocalStoreStmt
         , Statement
         >;
 
     std::unique_ptr<IR> ir = std::make_unique<IR>();
 
-    std::map<int, int> usage;
+    struct StoreRAII {
+        int reg = -1;
+        std::function<void()> dtor;
+
+        StoreRAII() = default;
+        ~StoreRAII() { dtor(); }
+        StoreRAII &operator=(StoreRAII const &) = default;
+
+        template <class F>
+        StoreRAII(int reg, F const &dtor)
+            : reg(reg), dtor(dtor) {}
+    };
+
+    std::map<int, int> last_load;
+    std::map<int, StoreRAII> storer;
+
+    void visit(AsmLocalLoadStmt *stmt) {
+        if (auto it = storer.find(stmt->mem); it != storer.end()) {
+            storer.erase(it);
+            return;
+        }
+        visit((Statement *)stmt);
+    }
 
     void visit(AsmLocalStoreStmt *stmt) {
-        auto it = usage.find(stmt->mem);
-        if (it == usage.end())
+        auto it = last_load.find(stmt->mem);
+        if (it == last_load.end())
             return;
         if (stmt->id > it->second)
             return;
-        ir->push_clone_back(stmt);
+        auto hole = ir->make_hole_back();
+        storer[stmt->mem] = StoreRAII(stmt->val, [hole, stmt]() {
+            hole.place<AsmLocalStoreStmt>(stmt->mem, stmt->val);
+        });
     }
 
     void visit(Statement *stmt) {
@@ -45,7 +71,7 @@ std::unique_ptr<IR> apply_kill_local_store(IR *ir) {
     GatherLocalLoad gather;
     gather.apply(ir);
     KillLocalStore visitor;
-    visitor.usage = gather.usage;
+    visitor.last_load = gather.last_load;
     visitor.apply(ir);
     return std::move(visitor.ir);
 }
