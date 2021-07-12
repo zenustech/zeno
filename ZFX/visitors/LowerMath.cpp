3fe24e0b274b55d767583880123c9c5eae97eadc
archibate <1931127624@qq.com>
2021-07-12 10:48:56 +0800

    fixed LowerMath

--- ZFX/visitors/LowerMath.cpp
+++ ZFX/visitors/LowerMath.cpp
@@ -7,12 +7,21 @@ struct LowerMath : Visitor<LowerMath> {
     using visit_stmt_types = std::tuple
         < TempSymbolStmt
         , SymbolStmt
+        , LiterialStmt
+        , UnaryOpStmt
+        , BinaryOpStmt
+        , AssignStmt
         , Statement
         >;
 
     std::unique_ptr<IR> ir = std::make_unique<IR>();
 
     std::map<Statement *, std::vector<Statement *>> replaces;
+
+    Statement *replace(Statement *stmt, int i) {
+        auto const &rep = replaces.at(stmt);
+        return rep[i % rep.size()];
+    }
 
     void visit(TempSymbolStmt *stmt) {
         auto &rep = replaces[stmt];
@@ -33,38 +42,42 @@ struct LowerMath : Visitor<LowerMath> {
         }
     }
 
+    void visit(UnaryOpStmt *stmt) {
+        auto &rep = replaces[stmt];
+        rep.clear();
+        for (int i = 0; i < stmt->dim; i++) {
+            rep.push_back(ir->emplace_back<UnaryOpStmt>(
+                stmt->op, replace(stmt->src, i)));
+        }
+    }
+
+    void visit(BinaryOpStmt *stmt) {
+        auto &rep = replaces[stmt];
+        rep.clear();
+        for (int i = 0; i < stmt->dim; i++) {
+            rep.push_back(ir->emplace_back<BinaryOpStmt>(
+                stmt->op, replace(stmt->lhs, i), replace(stmt->rhs, i)));
+        }
+    }
+
+    void visit(AssignStmt *stmt) {
+        auto &rep = replaces[stmt];
+        rep.clear();
+        for (int i = 0; i < stmt->dim; i++) {
+            rep.push_back(ir->emplace_back<AssignStmt>(
+                replace(stmt->dst, i), replace(stmt->src, i)));
+        }
+    }
+
+    void visit(LiterialStmt *stmt) {
+        auto &rep = replaces[stmt];
+        rep.clear();
+        rep.push_back(ir->push_clone_back(stmt));
+    }
+
     void visit(Statement *stmt) {
-        int repdim = -1;
-        for (auto const &field: stmt->fields()) {
-            if (auto it = replaces.find(stmt); it != replaces.end()) {
-                auto newdim = it->second.size();
-                if (repdim == -1) {
-                    repdim = newdim;
-                } else {
-                    if (repdim == 1) {
-                        repdim = newdim;
-                    } else if (repdim != newdim) {
-                        error("`%s`: vector dimension mismatch %d != %d",
-                            typeid(stmt).name(), repdim, newdim);
-                    }
-                }
-            }
-        }
-        if (repdim == -1) {
-            ir->push_clone_back(stmt);
-        } else {
-            for (int i = 0; i < repdim; i++) {
-                auto new_stmt = ir->push_clone_back(stmt);
-                int f = 0;
-                for (auto &field: stmt->fields()) {
-                    if (auto it = replaces.find(stmt); it != replaces.end()) {
-                        auto const &rep = replaces.at(stmt);
-                        field.get() = rep[f % rep.size()];
-                    }
-                    f++;
-                }
-            }
-        }
+        error("unexpected statement type to LowerMath: `%s`",
+            typeid(*stmt).name());
     }
 };
 
