9b3762aec12787531c60730804d51d8c16d3d2d8
archibate <1931127624@qq.com>
2021-07-13 11:26:03 +0800

    insert into interval

--- scripts/POC/ucla.cpp
+++ scripts/POC/ucla.cpp
@@ -4,6 +4,8 @@
 #include <array>
 #include <vector>
 
+#define NREGS 4
+
 struct Stmt {
     std::set<int> regs;
 };
@@ -11,6 +13,7 @@ struct Stmt {
 std::map<int, Stmt> stmts;
 
 struct Reg {
+    int regid;
     std::set<int> stmts;
 
     int startpoint() const {
@@ -39,6 +42,49 @@ struct inc_by_end {
 std::set<Reg *, inc_by_start> interval;
 std::set<Reg *, inc_by_end> active;
 
+void free_register(Reg *i) {
+    printf("free %d\n", i->regid);
+}
+
+void alloc_register(Reg *i) {
+    printf("alloc %d\n", i->regid);
+}
+
+void transit_register(Reg *i, Reg *spill) {
+    printf("transit %d <- %d\n", i->regid, spill->regid);
+}
+
+void alloc_stack(Reg *i) {
+    printf("alloc_stack %d\n", i->regid);
+}
+
+void linscan() {
+    active.clear();
+    for (auto const &i: interval) {
+        for (auto const &j: active) {
+            if (j->endpoint() >= i->startpoint()) {
+                break;
+            }
+            active.erase(j);
+            free_register(j);
+        }
+        if (active.size() == NREGS) {
+            auto spill = *active.begin();
+            if (spill->endpoint() > i->endpoint()) {
+                transit_register(i, spill);
+                alloc_stack(spill);
+                active.erase(spill);
+                active.insert(i);
+            } else {
+                alloc_stack(i);
+            }
+        } else {
+            alloc_register(i);
+            active.insert(i);
+        }
+    }
+}
+
 int main() {
     stmts[0].regs = {0};
     stmts[1].regs = {1};
@@ -46,6 +92,7 @@ int main() {
 
     for (auto const &[stmtid, stmt]: stmts) {
         for (auto const &regid: stmt.regs) {
+            regs[regid].regid = regid;
             regs[regid].stmts.insert(stmtid);
         }
     }
@@ -57,4 +104,11 @@ int main() {
         }
         printf("\n");
     }
+
+    for (auto &[regid, reg]: regs) {
+        interval.insert(&reg);
+    }
+
+    linscan();
+    return 0;
 }
