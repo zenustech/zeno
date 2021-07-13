edeb9d9402b80ad7485b3e87dfe23545eba59a11
archibate <1931127624@qq.com>
2021-07-13 11:02:49 +0800

    add inc_beg/end

--- scripts/POC/ucla.cpp
+++ scripts/POC/ucla.cpp
@@ -12,12 +12,37 @@ std::map<int, Stmt> stmts;
 
 struct Reg {
     std::set<int> stmts;
+
+    int startpoint() const {
+        return *stmts.begin();
+    }
+
+    int endpoint() const {
+        return *stmts.rbegin();
+    }
 };
 
 std::map<int, Reg> regs;
 
+struct inc_by_start {
+    bool operator()(Reg *l, Reg *r) const {
+        return l->startpoint() < r->startpoint();
+    }
+};
+
+struct inc_by_end {
+    bool operator()(Reg *l, Reg *r) const {
+        return l->endpoint() < r->endpoint();
+    }
+};
+
+std::set<Reg *, inc_by_start> interval;
+std::set<Reg *, inc_by_end> active;
+
 int main() {
-    stmts[0].regs = {0, 1, 2};
+    stmts[0].regs = {0};
+    stmts[1].regs = {1};
+    stmts[2].regs = {2, 0, 1};
 
     for (auto const &[stmtid, stmt]: stmts) {
         for (auto const &regid: stmt.regs) {
