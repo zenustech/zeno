0d366e92071731b24187ad39bef6fd32b8daafe3
archibate <1931127624@qq.com>
2021-07-13 15:34:41 +0800

    pars to prim

--- projects/ZenoFX/conv.cpp
+++ projects/ZenoFX/conv.cpp
@@ -31,3 +31,34 @@ ZENDEFNODE(ParticlesFromPrimitive, {
     {},
     {"zenofx"},
 });
+
+struct ParticlesToPrimitive : zeno::INode {
+    virtual void apply() override {
+        auto prim = std::make_shared<zeno::PrimitiveObject>();
+        auto pars = get_input<Particles>("prim");
+
+        pars->foreach_attr([&] (auto const &key, auto &arr) {
+            if constexpr (arr.Dimension == 3) {
+                auto &outarr = prim->add_attr<zeno::vec3f>(key);
+                for (int i = 0; i < arr.size(); i++) {
+                    outarr[i] = zeno::vec3f(
+                        arr.at(i, 0), arr.at(i, 1), arr.at(i, 2));
+                }
+            } else if constexpr (arr.Dimension == 1) {
+                auto &outarr = prim->add_attr<float>(key);
+                for (int i = 0; i < arr.size(); i++) {
+                    outarr[i] = arr.at(i, 0);
+                }
+            }
+        });
+
+        set_output("prim", std::move(prim));
+    }
+};
+
+ZENDEFNODE(ParticlesToPrimitive, {
+    {"pars"},
+    {"prim"},
+    {},
+    {"zenofx"},
+});
