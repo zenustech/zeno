104311760a86d0b6c0ba99a2c8ec4e09c7b089a9
archibate <1931127624@qq.com>
2021-07-16 12:08:45 +0800

    add a.out to .gitignore

--- scripts/POC/spgrid/main.cpp
+++ scripts/POC/spgrid/main.cpp
@@ -37,10 +37,10 @@ inline bool operator==(T const &other, null_iterator) {
 
 inline constexpr null_iterator npos{};
 
-template <class D, class T>
+template <class D>
 struct iterator_base {
     inline D const &begin() const {
-        return *static_cast<D const *>(this);
+        return *reinterpret_cast<D const *>(this);
     }
 
     inline auto end() const {
@@ -63,45 +63,39 @@ struct iterator_base {
     }
 
     template <class _ = void>
-    [[noreturn]] T const &get() const {
+    [[noreturn]] auto const &get() const {
         static_assert(sizeof(_), "get() not implemented");
     }
 
-    template <class = void>
-    T &get() {
-        return const_cast<D const *>(static_cast<D *>(this))->get();
+    inline auto &get() {
+        return const_cast<D const *>(reinterpret_cast<D *>(this))->get();
     }
 
-    template <class = void>
-    D &operator++() {
-        static_cast<D *>(this)->next();
-        return *static_cast<D *>(this);
+    inline D &operator++() {
+        reinterpret_cast<D *>(this)->next();
+        return *reinterpret_cast<D *>(this);
     }
 
-    template <class = void>
-    D &operator--() {
-        static_cast<D *>(this)->prev();
-        return *static_cast<D *>(this);
+    inline D &operator--() {
+        reinterpret_cast<D *>(this)->prev();
+        return *reinterpret_cast<D *>(this);
     }
 
-    template <class = void>
-    operator bool() const {
-        return static_cast<D const *>(this)->eof();
+    inline operator bool() const {
+        return reinterpret_cast<D const *>(this)->eof();
     }
 
-    template <class = void>
-    decltype(auto) operator*() const {
-        return static_cast<D const *>(this)->get();
+    inline decltype(auto) operator*() const {
+        return reinterpret_cast<D const *>(this)->get();
     }
 
-    template <class = void>
-    decltype(auto) operator*() {
-        return static_cast<D *>(this)->get();
+    inline decltype(auto) operator*() {
+        return reinterpret_cast<D *>(this)->get();
     }
 };
 
 template <class T>
-struct range : iterator_base<range<T>, T> {
+struct range : iterator_base<range<T>> {
     T m_now;
     T m_end;
 
@@ -135,6 +129,18 @@ struct slice {
         )
         : t(t)
         {}
+
+    void next() {
+        t++;
+    }
+
+    bool eof() const {
+        return t;
+    }
+
+    T const &get() const {
+        return *t;
+    }
 };
 
 
