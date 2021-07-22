d0359fcbadc497af0e972a6427d3d9dd5656b560
archibate <1931127624@qq.com>
2021-07-22 10:18:27 +0800

    refactor zeno to not used deprecated API

--- zeno/nodes/prim/PrimitiveConnection.cpp
+++ zeno/nodes/prim/PrimitiveConnection.cpp
@@ -11,14 +11,14 @@ namespace zeno {
 
 struct PrimitiveSimplePoints : zeno::INode {
   virtual void apply() override {
-    auto prim = get_input("prim")->as<PrimitiveObject>();
+    auto prim = get_input<PrimitiveObject>("prim");
     size_t points_count = prim->size();
     prim->points.resize(points_count);
     for (int i = 0; i < points_count; i++) {
       prim->points[i] = i;
     }
 
-    set_output_ref("prim", get_input_ref("prim"));
+    set_output("prim", get_input("prim"));
   }
 };
 
@@ -35,14 +35,14 @@ static int defPrimitiveSimplePoints = zeno::defNodeClass<PrimitiveSimplePoints>(
 
 struct PrimitiveSimpleLines : zeno::INode {
   virtual void apply() override {
-    auto prim = get_input("prim")->as<PrimitiveObject>();
+    auto prim = get_input<PrimitiveObject>("prim");
     size_t lines_count = prim->size() / 2;
     prim->lines.resize(lines_count);
     for (int i = 0; i < lines_count; i++) {
       prim->lines[i] = zeno::vec2i(2 * i, 2 * i + 1);
     }
 
-    set_output_ref("prim", get_input_ref("prim"));
+    set_output("prim", get_input("prim"));
   }
 };
 
@@ -59,14 +59,14 @@ static int defPrimitiveSimpleLines = zeno::defNodeClass<PrimitiveSimpleLines>("P
 
 struct PrimitiveSimpleTris : zeno::INode {
   virtual void apply() override {
-    auto prim = get_input("prim")->as<PrimitiveObject>();
+    auto prim = get_input<PrimitiveObject>("prim");
     size_t tris_count = prim->size() / 3;
     prim->tris.resize(tris_count);
     for (int i = 0; i < tris_count; i++) {
       prim->tris[i] = zeno::vec3i(3 * i, 3 * i + 1, 3 * i + 2);
     }
 
-    set_output_ref("prim", get_input_ref("prim"));
+    set_output("prim", get_input("prim"));
   }
 };
 
@@ -83,14 +83,14 @@ static int defPrimitiveSimpleTris = zeno::defNodeClass<PrimitiveSimpleTris>("Pri
 
 struct PrimitiveSimpleQuads : zeno::INode {
   virtual void apply() override {
-    auto prim = get_input("prim")->as<PrimitiveObject>();
+    auto prim = get_input<PrimitiveObject>("prim");
     size_t quads_count = prim->size() / 4;
     prim->quads.resize(quads_count);
     for (int i = 0; i < quads_count; i++) {
       prim->quads[i] = zeno::vec4i(4 * i, 4 * i + 1, 4 * i + 2, 4 * i + 3);
     }
 
-    set_output_ref("prim", get_input_ref("prim"));
+    set_output("prim", get_input("prim"));
   }
 };
 
@@ -107,13 +107,13 @@ static int defPrimitiveSimpleQuads = zeno::defNodeClass<PrimitiveSimpleQuads>("P
 
 struct PrimitiveClearConnect : zeno::INode {
   virtual void apply() override {
-    auto prim = get_input("prim")->as<PrimitiveObject>();
+    auto prim = get_input<PrimitiveObject>("prim");
     prim->points.clear();
     prim->lines.clear();
     prim->tris.clear();
     prim->quads.clear();
 
-    set_output_ref("prim", get_input_ref("prim"));
+    set_output("prim", get_input("prim"));
   }
 };
 
