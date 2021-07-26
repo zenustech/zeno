cecf1e7261770d708f6287a380ea1d67d1d5d86d
archibate <1931127624@qq.com>
2021-07-26 14:47:27 +0800

    fixed one

--- scripts/POC/myvdb.cpp
+++ scripts/POC/myvdb.cpp
@@ -362,6 +362,7 @@ struct HashGrid {
         }
         auto *entry = new HashEntry;
         auto *leaf = new LeafNodeType;
+        entry->m_coord = coord;
         entry->m_next = m_entries[i];
         entry->m_leaf = leaf;
         m_entries[i] = entry;
@@ -393,32 +394,35 @@ struct HashGrid {
 int main() {
     HashGrid<Points<int, 32>> grid, new_grid;
 
-    const int N = 1024;
+    const int N = 1;
     std::vector<glm::vec3> pos(N);
     std::vector<glm::vec3> new_pos(N);
     std::vector<glm::vec3> vel(N);
     for (int i = 0; i < N; i++) {
         pos[i] = (glm::vec3)glm::ivec3(
-                rand(), rand(), rand()) / (float)RAND_MAX * 2.f - 1.f;
+                rand(), rand(), rand()) / (float)RAND_MAX;
         vel[i] = (glm::vec3)glm::ivec3(
                 rand(), rand(), rand()) / (float)RAND_MAX * 2.f - 1.f;
     }
 
     float leaf_size = 0.1f;
-    float dt = 0.1f;
+    float dt = 0.01f;
 
     // p2g
     for (int i = 0; i < N; i++) {
-        auto ipos = glm::ivec3(pos[i] / leaf_size);
-        auto jpos = glm::ivec3(glm::mod(pos[i], leaf_size) / float(1 << 3));
+        auto iipos = pos[i] / leaf_size;
+        auto ipos = glm::ivec3(iipos);
+        auto jpos = glm::ivec3(glm::mod(iipos, 1.f) * float(1 << 3));
         Coord leafCoord{ipos.x, ipos.y, ipos.z};
         auto *leaf = grid.leafAt(leafCoord);
         Coord subCoord{jpos.x, jpos.y, jpos.z};
         leaf->insertElement(subCoord, i);
+        printf("? %d %d %d\n", leafCoord.x, leafCoord.y, leafCoord.z);
+            printf("! %d %d %d\n", subCoord.x, subCoord.y, subCoord.z);
     }
 
     // advect
-    grid.foreachLeaf([&] (auto *leaf, Coord const &leafCoord) {
+    /*grid.foreachLeaf([&] (auto *leaf, Coord const &leafCoord) {
         leaf->foreachElement([&] (auto &value, int index) {
             Coord subCoord = leaf->indexToCoord(index);
             auto vel_dt = vel[value] * dt;
@@ -437,12 +441,14 @@ int main() {
 
             new_grid.leafAt(newLeafCoord)->insertElement(subCoord, value);
         });
-    });
+    });*/
 
     // g2p
-    new_grid.foreachLeaf([&] (auto *leaf, Coord const &leafCoord) {
+    grid.foreachLeaf([&] (auto *leaf, Coord const &leafCoord) {
+        printf("? %d %d %d\n", leafCoord.x, leafCoord.y, leafCoord.z);
         leaf->foreachElement([&] (auto &value, int index) {
             Coord subCoord = leaf->indexToCoord(index);
+            printf("! %d %d %d\n", subCoord.x, subCoord.y, subCoord.z);
             float fx = (leafCoord.x + subCoord.x / float(1 << 3)) * leaf_size;
             float fy = (leafCoord.y + subCoord.y / float(1 << 3)) * leaf_size;
             float fz = (leafCoord.z + subCoord.z / float(1 << 3)) * leaf_size;
