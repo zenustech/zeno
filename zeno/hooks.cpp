2e199283e30a93b5e0cd75f34999a46d09a52973
archibate <1931127624@qq.com>
2021-07-28 13:17:35 +0800

    ZEN_AUTOGDB envvar

--- zeno/hooks.cpp
+++ zeno/hooks.cpp
@@ -5,21 +5,33 @@
 #include <signal.h>
 #include <unistd.h>
 
+namespace {
+
 static void handler(int signo) {
     printf("ZENO process recieved signal %d: %s\n", signo, strsignal(signo));
+    printf("Launching emergency GDB for debugging...\n");
     char cmd[1024];
     sprintf(cmd, "sudo gdb -p %d", getpid());
     system(cmd);
     exit(-signo);
 }
 
-static auto _ = (
-        signal(SIGSEGV, handler),
-        signal(SIGFPE, handler),
-        signal(SIGABRT, handler),
-        signal(SIGILL, handler),
-        signal(SIGBUS, handler),
-        signal(SIGPIPE, handler),
-        0);
+static int registerMyHandlers() {
+    if (!getenv("ZEN_AUTOGDB")) {
+        return 0;
+    }
+    printf("ZENO is hooking signal handlers to auto-launch GDB\n");
+    signal(SIGSEGV, handler);
+    signal(SIGFPE, handler);
+    signal(SIGABRT, handler);
+    signal(SIGILL, handler);
+    signal(SIGBUS, handler);
+    signal(SIGPIPE, handler);
+    return 1;
+}
+
+static int doRegisterMyHandlers = registerMyHandlers();
+
+}
 
 #endif
