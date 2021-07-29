0dc19a2a5f856cd9b2db4e6829435e10f56d95d2
archibate <1931127624@qq.com>
2021-07-29 23:16:48 +0800

    add test file to .gitignore

--- zenvis/GraphicPrimitive.cpp
+++ zenvis/GraphicPrimitive.cpp
@@ -210,7 +210,7 @@ void main()
   if (radius != 0)
     gl_PointSize = max(1, radius * mPointScale / dist);
   else
-    gl_PointSize = max(min(3.0, radius * mPointScale / dist),1.0);
+    gl_PointSize = max(1, min(3, radius * mPointScale / dist));
   gl_Position = mVP * vec4(position, 1.0);
 }
 )";
