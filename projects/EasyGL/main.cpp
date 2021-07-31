81c4a3c4fd5e74a69c8cdc68ffd4ca7f66b31c65
archibate <1931127624@qq.com>
2021-07-31 11:00:38 +0800

    why suboutput dst..

--- projects/EasyGL/main.cpp
+++ projects/EasyGL/main.cpp
@@ -19,7 +19,7 @@ void initFunc() {
 
 void displayFunc() {
     glViewport(0, 0, 512, 512);
-    glClearColor(0.375f, 0.75f, 1.0f, 0.0f);
+    glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     drawScene();
     glFlush();
