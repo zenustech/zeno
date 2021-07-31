1b124a1b0b8c913c16c69cd4ffe4a32547c70bf4
archibate <1931127624@qq.com>
2021-07-31 09:03:34 +0800

    add more nodes

--- projects/EasyGL/main.cpp
+++ projects/EasyGL/main.cpp
@@ -7,15 +7,33 @@ namespace zeno::easygl {
 struct GLPrimitiveObject : zeno::IObjectClone<GLPrimitiveObject,
     zeno::PrimitiveObject> {
     std::vector<std::string> boundAttrs;
+
+    void add_bound_attr(std::string const &name) {
+        boundAttrs.push_back(name);
+    }
 };
 
-struct GLDrawPoints : zeno::INode {
-    inline static const GLfloat vVertices[] = {
-        0.0f,  0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
+struct GLShaderObject : zeno::IObjectClone<GLShaderObject> {
+    struct Impl {
     };
 
+    std::shared_ptr<Impl> impl;
+};
+
+struct GLCreateShader : zeno::INode {
+    virtual void apply() override {
+        auto source = get_input<GLPrimitiveObject>("source");
+    }
+};
+
+ZENDEFNODE(GLCreateShader, {
+        {"source"},
+        {},
+        {},
+        {"EasyGL"},
+});
+
+struct GLDrawArrayTriangles : zeno::INode {
     virtual void apply() override {
         auto prim = get_input<GLPrimitiveObject>("prim");
         for (int i = 0; i < prim->boundAttrs.size(); i++) {
@@ -23,20 +41,47 @@ struct GLDrawPoints : zeno::INode {
             std::visit([] (auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 using S = zeno::decay_vec_t<T>;
-                if constexpr (zeno::is_vec_v<T>) {
-                    glVertexAttribPointer
-                            ( /*index=*/0
-                            , /*size=*/zeno::is_vec_n<T>
-                            , GL_FLOAT
-                            , GL_FALSE
-                            , /*stride=*/0
-                            , vVertices
-                            );
-                }
+                glVertexAttribPointer
+                        ( /*index=*/0
+                        , /*size=*/zeno::is_vec_n<T>
+                        , GL_FLOAT
+                        , GL_FALSE
+                        , /*stride=*/0
+                        , (void *)arr.data()
+                        );
             }, prim->attr(name));
         }
-        glDrawArrays(GL_TRIANGLES, 0, /*count=*/3);
+        glDrawArrays(GL_TRIANGLES, 0, /*count=*/prim->size());
     }
 };
+
+ZENDEFNODE(GLDrawArrayTriangles, {
+        {"prim"},
+        {},
+        {},
+        {"EasyGL"},
+});
+
+struct MakeSimpleTriangle : zeno::INode {
+    inline static const GLfloat vVertices[] = {
+        0.0f,  0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+    };
+
+    virtual void apply() override {
+        auto prim = std::make_shared<GLPrimitiveObject>();
+        prim->add_attr<zeno::vec3f>("pos");
+        prim->add_bound_attr("pos");
+        set_output("prim", std::move(prim));
+    }
+};
+
+ZENDEFNODE(MakeSimpleTriangle, {
+        {},
+        {"prim"},
+        {},
+        {"EasyGL"},
+});
 
 }
