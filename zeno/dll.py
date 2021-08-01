652c721276a578fb7849fdf30dc5feace442ec5c
zhouhang95 <765229842@qq.com>
2021-08-01 15:45:28 +0800

    remove-py38-new-api

--- zeno/dll.py
+++ zeno/dll.py
@@ -4,10 +4,7 @@ from zenutils import rel2abs, os_name
 
 if os_name == 'win32':
     dllpath = rel2abs(__file__, 'lib')
-    if hasattr(os, 'add_dll_directory'):
-        os.add_dll_directory(dllpath)
-    else:
-        os.environ['PATH'] += os.pathsep + dllpath
+    os.environ['PATH'] += os.pathsep + dllpath
     ctypes.cdll.LoadLibrary('zeno.dll')
 elif os_name == 'darwin':
     ctypes.cdll.LoadLibrary(rel2abs(__file__, 'lib', 'libzeno.dylib'))
