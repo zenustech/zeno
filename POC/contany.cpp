41b891f69b4bf2790d9a0321c5e43d350211722a
archibate <1931127624@qq.com>
2021-08-05 13:11:15 +0800

    finally to variant

--- POC/contany.cpp
+++ POC/contany.cpp
@@ -26,18 +26,27 @@ template <class T>
 struct any_traits<T, std::void_t<decltype(
         std::declval<scalar_type_variant &>() = std::declval<T>()
         )>> {
-    using underlying_type = T;
-    //using underlying_type = scalar_type_variant;
+    using underlying_type = scalar_type_variant;
 };
 
 struct any : std::any {
-    using std::any::any;
-    using std::any::operator=;
+    any() = default;
+    any(any &&a) = default;
+    any(any const &a) = default;
 
     template <class T>
     any(T const &t)
-    : std::any(static_cast<typename any_traits<T>::underlying_type const &>(t))
-    {}
+    : std::any(static_cast<typename any_traits<T>::underlying_type>(t))
+    {
+        printf("!!!%s\n", typeid(t).name());
+    }
+
+    template <class T>
+    any &operator=(T const &t) {
+        std::any::operator=(
+                static_cast<typename any_traits<T>::underlying_type>(t));
+        return *this;
+    }
 };
 
 template <class T>
@@ -49,8 +58,8 @@ struct is_variant<std::variant<Ts...>> : std::true_type {
 };
 
 template <class T>
-T &&any_cast(any &&a) {
-    auto &&v = std::any_cast<typename any_traits<T>::underlying_type &&>(a);
+auto our_any_cast(any const &a) {
+    auto &&v = std::any_cast<typename any_traits<T>::underlying_type>(a);
     if constexpr (is_variant<std::decay_t<decltype(v)>>::value) {
         return std::get<T>(v);
     } else {
@@ -59,7 +68,7 @@ T &&any_cast(any &&a) {
 }
 
 int main() {
-    any a = 1;
-    std::cout << any_cast<int>(a) << std::endl;
+    any a = 3.14f;
+    std::cout << our_any_cast<int>(a) << std::endl;
     return 0;
 }
