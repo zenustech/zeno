786767c2c8945d64a3ec498ac18101fe138e7c2a
Arno <bahdaxxzm@gmail.com>
2021-08-06 00:18:32 +0800

    MakeDict node can automatically create and delete sockets

--- zenqt/editor/makedict.py
+++ zenqt/editor/makedict.py
@@ -31,7 +31,7 @@ class QDMGraphicsSocketEdiable(QDMGraphicsSocket):
 
 class QDMGraphicsNode_MakeDict(QDMGraphicsNode):
     def __init__(self, parent=None):
-        self.socket_keys = []
+        self.socket_keys = ['obj0']
         super().__init__(parent)
 
     def initSockets(self):
@@ -39,43 +39,20 @@ class QDMGraphicsNode_MakeDict(QDMGraphicsNode):
         self.height -= TEXT_HEIGHT * 0.75
 
         for key in self.socket_keys:
-            """param = QDMGraphicsParam_string(self)
-            rect = QRectF(HORI_MARGIN, self.height,
-                self.width - HORI_MARGIN * 2, 0)
-            param.setGeometry(rect)
-            param.setName('')
-            param.setDefault('this is {}'.format(index))
-            self.params['name{}'.format(index)] = param"""
-
             socket = QDMGraphicsSocketEdiable(self)
             socket.setPos(0, self.height + TEXT_HEIGHT * 0.5)
             socket.setName(key)
             socket.setIsOutput(False)
             self.inputs[socket.name] = socket
-
             self.height += TEXT_HEIGHT
-
-        self.height += TEXT_HEIGHT * 0.4
-
-        if not hasattr(self, 'add_button'):
-            self.add_button = QDMGraphicsButton(self)
-        W = self.width / 4
-        rect = QRectF(HORI_MARGIN, self.height,
-            W - HORI_MARGIN, TEXT_HEIGHT)
-        self.add_button.setGeometry(rect)
-        self.add_button.setText('+')
-        self.add_button.on_click = self.add_new_key
-
-        if not hasattr(self, 'del_button'):
-            self.del_button = QDMGraphicsButton(self)
-        rect = QRectF(HORI_MARGIN + W, self.height,
-            W - HORI_MARGIN, TEXT_HEIGHT)
-        self.del_button.setGeometry(rect)
-        self.del_button.setText('-')
-        self.del_button.on_click = self.del_last_key
-        self.height += TEXT_HEIGHT
-
         self.height += TEXT_HEIGHT * 1.5
+
+    def onInputChanged(self):
+        if len(self.inputs[self.socket_keys[-1]].edges) > 0:
+            self.add_new_key()
+        else:
+            while len(self.socket_keys) > 1 and len(self.inputs[self.socket_keys[-2]].edges) == 0:
+                self.del_last_key()
 
     def add_new_key(self):
         self.socket_keys.append('obj{}'.format(len(self.socket_keys)))
