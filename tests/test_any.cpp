0d2a00d825dd12c834f7b66bdc16fc02a2521dc5
archibate <1931127624@qq.com>
2021-08-06 16:57:00 +0800

    add static tests

--- tests/test_any.cpp
+++ tests/test_any.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
 #include <zeno/zeno.h>
+#include <zeno/types/PrimitiveObject.h>
 #include <zeno/utils/any.h>
 
 TEST_CASE("cast any of int to float", "[any]") {
@@ -40,14 +41,23 @@ TEST_CASE("cast any of vec3f to vec3i", "[any]") {
     REQUIRE(allTrue(i == zeno::vec3i(42, 985, 211)));
 }
 
+TEST_CASE("cast any of vec3f to vec3i", "[any]") {
+    zeno::vec3f f(42.0f, 985.99f, 211.3f);
+    zeno::any a = f;
+    zeno::vec3i i = zeno::smart_any_cast<zeno::vec3i>(a);
+    REQUIRE(allTrue(i == zeno::vec3i(42, 985, 211)));
+}
+
 TEST_CASE("static assertion of underlying type", "[any]") {
     REQUIRE(std::is_same_v<
-            typename zeno::any_traits<zeno::vec3f>::underlying_type,
-            zeno::vector_type_variant<3>>);
+            zeno::any_underlying_type_t<float>,
+            zeno::scalar_type_variant>);
     REQUIRE(std::is_same_v<
-            typename zeno::any_traits<zeno::vec2f>::underlying_type,
-            zeno::vector_type_variant<2>>);
-    REQUIRE(!std::is_same_v<
-            typename zeno::any_traits<zeno::vec3f>::underlying_type,
-            zeno::vec3f>);
+            zeno::any_underlying_type_t<std::shared_ptr<zeno::PrimitiveObject>>,
+            std::shared_ptr<zeno::IObject>>);
+    REQUIRE(std::is_same_v<
+            zeno::any_underlying_type_t<zeno::vec4i>,
+            zeno::vector_type_variant<4>>);
 }
+
+// TODO: add shared_ptr tests too
