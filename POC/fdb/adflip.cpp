77876b8aeefc557a13e20486018de55a5bc09b9a
archibate <1931127624@qq.com>
2021-08-09 15:33:06 +0800

    fix xyz order

--- POC/fdb/adflip.cpp
+++ POC/fdb/adflip.cpp
@@ -1,4 +1,5 @@
 #include <functional>
+#include "vec.h"
 
 struct LeafNode {
     float m[8 * 8 * 8];
@@ -15,17 +16,17 @@ struct RootNode {
 
 void foreachLeaf
     ( RootNode* root
-    , std::function<void(LeafNode*, int, int, int)> cb
+    , std::function<void(LeafNode*, fdb::vec3i)> cb
     ) {
     for (int i = 0; i < 32 * 32 * 32; i++) {
         int ix = i % 32 * 16, iy = i / 32 % 32 * 16, iz = i / 32 / 32 * 16;
         auto intern = root->m[i];
         if (!intern) continue;
-        for (int x = ix; x < 16; x++) {
+        for (int z = iz; z < 16; z++) {
             for (int y = iy; y < 16; y++) {
-                for (int z = iz; z < 16; z++) {
+                for (int x = ix; x < 16; x++) {
                     auto leaf = intern->m[i];
-                    cb(leaf, x, y, z);
+                    cb(leaf, fdb::vec3i(x, y, z));
                 }
             }
         }
