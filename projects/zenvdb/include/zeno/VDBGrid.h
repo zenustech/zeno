b6a219ddb457a88fbab79b946928c10fc894d343
archibate <1931127624@qq.com>
2021-08-09 11:39:02 +0800

    fix missing PointCount

--- projects/zenvdb/include/zeno/VDBGrid.h
+++ projects/zenvdb/include/zeno/VDBGrid.h
@@ -3,13 +3,12 @@
 #include <vector>
 #include <zeno/zeno.h>
 
-#include <openvdb/points/PointCount.h>
+//#include <openvdb/points/PointCount.h>
 #include <openvdb/tree/LeafManager.h>
 #include <openvdb/points/PointAdvect.h>
 #include <openvdb/tools/Morphology.h>
 #include <openvdb/tools/MeshToVolume.h>
 #include <openvdb/openvdb.h>
-#include <openvdb/points/PointCount.h>
 #include <string.h>
 namespace zeno {
 
