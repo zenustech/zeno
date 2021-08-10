df87098a3216358a0fb7bc86f98b1edd99908e07
archibate <1931127624@qq.com>
2021-08-10 15:33:45 +0800

    sounding vdb

--- POC/fdb/vdbdense.cpp
+++ POC/fdb/vdbdense.cpp
@@ -1,16 +1,29 @@
+#include <openvdb/openvdb.h>
 #include <openvdb/tools/Dense.h>
 #include <string_view>
 #include "vec.h"
 
 namespace zinc {
 
-int writevdb
+void writevdb
         ( std::string_view path
-        , vec3i size
+        , std::function<float(vec3I)> sampler
+        , vec3I size
         )
 {
-    openvdb::CoordBBox bbox = openvdb::CoordBBox(size);
-    openvdb::tools::Dense dens(bbox);
+    openvdb::tools::Dense<float> dens(openvdb::Coord(size[0], size[1], size[2]));
+    for (uint32_t z = 0; z < size[2]; z++) {
+        for (uint32_t y = 0; y < size[1]; y++) {
+            for (uint32_t x = 0; x < size[0]; x++) {
+                auto val = sampler({x, y, z});
+                dens.setValue(x, y, z, val);
+            }
+        }
+    }
+    auto grid = openvdb::FloatGrid::create();
+    openvdb::FloatGrid::ValueType tolerance{0};
+    openvdb::tools::copyFromDense(dens, grid->tree(), tolerance);
+    openvdb::io::File((std::string)path).write({grid});
 }
 
 }
