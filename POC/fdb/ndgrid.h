b753434b14eb5ba6ad1c178c81182c26b038641f
archibate <1931127624@qq.com>
2021-08-10 12:55:30 +0800

    fdb add zinc::NDGrid

---
+++ POC/fdb/ndgrid.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <cstdint>
+#include "vec.h"
+
+namespace zinc {
+
+template <size_t N>
+struct NDGrid {
+    float *m_data = new float[N * N * N];
+    uint8_t *m_mask = new uint8_t[N * N * N / 8];
+
+    NDGrid() = default;
+    ~NDGrid() {
+        delete[] m_data;
+        delete[] m_mask;
+    }
+
+    NDGrid(NDGrid const &) = delete;
+    NDGrid &operator=(NDGrid const &) = delete;
+
+    [[nodiscard]] static uintptr_t linearize(vec3I coor) {
+        return dot(clamp(coor, 0, N-1), vec3L(1, N, N * N));
+    }
+
+    [[nodiscard]] bool is_active(vec3I coor) const {
+        uintptr_t i = linearize(coor);
+        return m_mask[i >> 3] & (1 << (i & 7));
+    }
+
+    [[nodiscard]] bool is_active(uint32_t x, uint32_t y, uint32_t z) {
+        return is_active({x, y, z});
+    }
+
+    void activate(vec3I coor) {
+        uintptr_t i = linearize(coor);
+        m_mask[i >> 3] |= 1 << (i & 7);
+    }
+
+    void activate(uint32_t x, uint32_t y, uint32_t z) {
+        return activate({x, y, z});
+    }
+
+    void deactivate(vec3I coor) {
+        uintptr_t i = linearize(coor);
+        m_mask[i >> 3] &= ~(1 << (i & 7));
+    }
+
+    void deactivate(uint32_t x, uint32_t y, uint32_t z) {
+        return deactivate({x, y, z});
+    }
+
+    [[nodiscard]] auto const &at(vec3I coor) const {
+        uintptr_t i = linearize(coor);
+        return m_data[i];
+    }
+
+    [[nodiscard]] auto &at(vec3I coor) {
+        uintptr_t i = linearize(coor);
+        return m_data[i];
+    }
+
+    [[nodiscard]] decltype(auto) at(uint32_t x, uint32_t y, uint32_t z) {
+        return at({x, y, z});
+    }
+
+    [[nodiscard]] decltype(auto) at(uint32_t x, uint32_t y, uint32_t z) const {
+        return at({x, y, z});
+    }
+
+    [[nodiscard]] auto &aat(vec3I coor) {
+        uintptr_t i = linearize(coor);
+        m_mask[i >> 3] |= 1 << (i & 7);
+        return m_data[i];
+    }
+
+    [[nodiscard]] decltype(auto) aat(uint32_t x, uint32_t y, uint32_t z) {
+        return aat({x, y, z});
+    }
+
+    [[nodiscard]] decltype(auto) operator()(vec3I &&coor) {
+        return at(std::forward<vec3I>(coor));
+    }
+
+    [[nodiscard]] decltype(auto) operator()(vec3I &&coor) const {
+        return at(std::forward<vec3I>(coor));
+    }
+
+    [[nodiscard]] decltype(auto) operator[](vec3I &&coor) {
+        return aat(std::forward<vec3I>(coor));
+    }
+
+    [[nodiscard]] decltype(auto) operator()(uint32_t x, uint32_t y, uint32_t z) {
+        return operator()({x, y, z});
+    }
+
+    [[nodiscard]] decltype(auto) operator()(uint32_t x, uint32_t y, uint32_t z) const {
+        return operator()({x, y, z});
+    }
+};
+
+}
