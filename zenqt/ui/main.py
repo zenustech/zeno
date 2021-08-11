535f006ceb03f49a6ac735faa6f38e7db078d666
zhouhang95 <765229842@qq.com>
2021-08-12 00:16:02 +0800

    fix-win-scaling

--- zenqt/ui/main.py
+++ zenqt/ui/main.py
@@ -3,6 +3,7 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 
 import sys
+import os
 
 from .window import MainWindow
 from .utils import asset_path
@@ -14,7 +15,10 @@ def main():
         return _main()
 
     QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-    app = QApplication(sys.argv)
+
+    os.environ['QT_AUTO_SCREEN_SCALE_FACTOR'] = '1'
+    app = QApplication(['run.py', '-platform', 'windows:dpiawareness=0'])
+
     font_file_path = asset_path('SourceSansPro-Regular.ttf')
     QFontDatabase().addApplicationFont(font_file_path)
     app.setFont(QFont('Source Sans Pro'))
