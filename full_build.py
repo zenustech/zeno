4789301192f82275b98151dd540e0f94f9372819
archibate <1931127624@qq.com>
2021-08-14 22:19:25 +0800

    full_build.py

---
+++ full_build.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+args = [
+'-DCMAKE_BUILD_TYPE=RelWithDebInfo',
+'-DZENOFX_ENABLE_OPENVDB:BOOL=ON',
+'-DEXTENSION_oldzenbase:BOOL=ON',
+'-DEXTENSION_ZenoFX:BOOL=ON',
+'-DEXTENSION_Rigid:BOOL=ON',
+'-DEXTENSION_FastFLIP:BOOL=ON',
+'-DEXTENSION_zenvdb:BOOL=ON',
+]
+
+subprocess.check_call(['cmake', '-B', 'build'] + args)
+subprocess.check_call(['cmake', '--build', 'build', '--parallel'])
