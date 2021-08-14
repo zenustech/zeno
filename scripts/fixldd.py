a4e4656538146ff2ed1aaaacb3e890aef6f6b283
archibate <1931127624@qq.com>
2021-08-14 22:01:05 +0800

    work fixldd

--- scripts/fixldd.py
+++ scripts/fixldd.py
@@ -1,114 +1,141 @@
 import subprocess as sp
+import shutil
+import glob
+import os
 
-# pacman -Ql gcc-libs glibc | cut -f2 -d\  | grep '^\/usr\/lib\/lib.*\.so' | cut -d/ -f4
-whitelist = '''
-libasan.so
-libasan.so.6
-libasan.so.6.0.0
-libatomic.so
-libatomic.so.1
-libatomic.so.1.2.0
-libgcc_s.so
-libgcc_s.so.1
-libgdruntime.so
-libgdruntime.so.2
-libgdruntime.so.2.0.0
-libgfortran.so
-libgfortran.so.5
-libgfortran.so.5.0.0
-libgo.so
-libgo.so.19
-libgo.so.19.0.0
-libgomp.so
-libgomp.so.1
-libgomp.so.1.0.0
-libgphobos.so
-libgphobos.so.2
-libgphobos.so.2.0.0
-libitm.so
-libitm.so.1
-libitm.so.1.0.0
-liblsan.so
-liblsan.so.0
-liblsan.so.0.0.0
-libobjc.so
-libobjc.so.4
-libobjc.so.4.0.0
-libquadmath.so
-libquadmath.so.0
-libquadmath.so.0.0.0
-libstdc++.so
-libstdc++.so.6
-libstdc++.so.6.0.29
-libtsan.so
-libtsan.so.0
-libtsan.so.0.0.0
-libubsan.so
-libubsan.so.1
-libubsan.so.1.0.0
-libBrokenLocale-2.33.so
-libBrokenLocale.so
-libBrokenLocale.so.1
-libSegFault.so
-libanl-2.33.so
-libanl.so
-libanl.so.1
-libc-2.33.so
-libc.so
-libc.so.6
-libcrypt-2.33.so
-libcrypt.so.1
-libdl-2.33.so
-libdl.so
-libdl.so.2
-libm-2.33.so
-libm.so
-libm.so.6
-libmemusage.so
-libmvec-2.33.so
-libmvec.so
-libmvec.so.1
-libnsl-2.33.so
-libnsl.so.1
-libnss_compat-2.33.so
-libnss_compat.so
-libnss_compat.so.2
-libnss_db-2.33.so
-libnss_db.so
-libnss_db.so.2
-libnss_dns-2.33.so
-libnss_dns.so
-libnss_dns.so.2
-libnss_files-2.33.so
-libnss_files.so
-libnss_files.so.2
-libnss_hesiod-2.33.so
-libnss_hesiod.so
-libnss_hesiod.so.2
-libpcprofile.so
-libpthread-2.33.so
-libpthread.so
-libpthread.so.0
-libresolv-2.33.so
-libresolv.so
-libresolv.so.2
-librt-2.33.so
-librt.so
-librt.so.1
-libthread_db-1.0.so
-libthread_db.so
-libthread_db.so.1
-libutil-2.33.so
-libutil.so
-libutil.so.1
-'''.strip().splitlines()
+# pacman -Ql gcc-libs glibc | cut -f2 -d\  | grep '^\/usr\/lib\/lib.*\.so' #| cut -d/ -f4
+whitelist = set(x.strip() for x in '''
+/usr/lib/libasan.so
+/usr/lib/libasan.so.6
+/usr/lib/libasan.so.6.0.0
+/usr/lib/libatomic.so
+/usr/lib/libatomic.so.1
+/usr/lib/libatomic.so.1.2.0
+/usr/lib/libgcc_s.so
+/usr/lib/libgcc_s.so.1
+/usr/lib/libgdruntime.so
+/usr/lib/libgdruntime.so.2
+/usr/lib/libgdruntime.so.2.0.0
+/usr/lib/libgfortran.so
+/usr/lib/libgfortran.so.5
+/usr/lib/libgfortran.so.5.0.0
+/usr/lib/libgo.so
+/usr/lib/libgo.so.19
+/usr/lib/libgo.so.19.0.0
+/usr/lib/libgomp.so
+/usr/lib/libgomp.so.1
+/usr/lib/libgomp.so.1.0.0
+/usr/lib/libgphobos.so
+/usr/lib/libgphobos.so.2
+/usr/lib/libgphobos.so.2.0.0
+/usr/lib/libitm.so
+/usr/lib/libitm.so.1
+/usr/lib/libitm.so.1.0.0
+/usr/lib/liblsan.so
+/usr/lib/liblsan.so.0
+/usr/lib/liblsan.so.0.0.0
+/usr/lib/libobjc.so
+/usr/lib/libobjc.so.4
+/usr/lib/libobjc.so.4.0.0
+/usr/lib/libquadmath.so
+/usr/lib/libquadmath.so.0
+/usr/lib/libquadmath.so.0.0.0
+/usr/lib/libstdc++.so
+/usr/lib/libstdc++.so.6
+/usr/lib/libstdc++.so.6.0.29
+/usr/lib/libtsan.so
+/usr/lib/libtsan.so.0
+/usr/lib/libtsan.so.0.0.0
+/usr/lib/libubsan.so
+/usr/lib/libubsan.so.1
+/usr/lib/libubsan.so.1.0.0
+/usr/lib/libBrokenLocale-2.33.so
+/usr/lib/libBrokenLocale.so
+/usr/lib/libBrokenLocale.so.1
+/usr/lib/libSegFault.so
+/usr/lib/libanl-2.33.so
+/usr/lib/libanl.so
+/usr/lib/libanl.so.1
+/usr/lib/libc-2.33.so
+/usr/lib/libc.so
+/usr/lib/libc.so.6
+/usr/lib/libcrypt-2.33.so
+/usr/lib/libcrypt.so.1
+/usr/lib/libdl-2.33.so
+/usr/lib/libdl.so
+/usr/lib/libdl.so.2
+/usr/lib/libm-2.33.so
+/usr/lib/libm.so
+/usr/lib/libm.so.6
+/usr/lib/libmemusage.so
+/usr/lib/libmvec-2.33.so
+/usr/lib/libmvec.so
+/usr/lib/libmvec.so.1
+/usr/lib/libnsl-2.33.so
+/usr/lib/libnsl.so.1
+/usr/lib/libnss_compat-2.33.so
+/usr/lib/libnss_compat.so
+/usr/lib/libnss_compat.so.2
+/usr/lib/libnss_db-2.33.so
+/usr/lib/libnss_db.so
+/usr/lib/libnss_db.so.2
+/usr/lib/libnss_dns-2.33.so
+/usr/lib/libnss_dns.so
+/usr/lib/libnss_dns.so.2
+/usr/lib/libnss_files-2.33.so
+/usr/lib/libnss_files.so
+/usr/lib/libnss_files.so.2
+/usr/lib/libnss_hesiod-2.33.so
+/usr/lib/libnss_hesiod.so
+/usr/lib/libnss_hesiod.so.2
+/usr/lib/libpcprofile.so
+/usr/lib/libpthread-2.33.so
+/usr/lib/libpthread.so
+/usr/lib/libpthread.so.0
+/usr/lib/libresolv-2.33.so
+/usr/lib/libresolv.so
+/usr/lib/libresolv.so.2
+/usr/lib/librt-2.33.so
+/usr/lib/librt.so
+/usr/lib/librt.so.1
+/usr/lib/libthread_db-1.0.so
+/usr/lib/libthread_db.so
+/usr/lib/libthread_db.so.1
+/usr/lib/libutil-2.33.so
+/usr/lib/libutil.so
+/usr/lib/libutil.so.1
+'''.strip().splitlines())
 
-file = '/lib/libopenvdb.so.8.1.0'
-output = sp.check_output(['ldd', file]).decode()
-for line in output.splitlines():
-    if '=>' not in line: continue
-    lhs, rhs = line.split('=>')
-    rhs = rhs.split('(')[0]
-    lhs = lhs.strip()
-    rhs = rhs.strip()
-    print('!{}!{}!'.format(lhs, rhs))
+resolved = {}
+visited = set()
+
+def touch(path):
+    if path in whitelist:
+        return
+    if path in visited:
+        return
+    visited.add(path)
+    output = sp.check_output(['ldd', path]).decode()
+    for line in output.splitlines():
+        if '=>' not in line: continue
+        lhs, rhs = line.split('=>')
+        rhs = rhs.split('(')[0]
+        lhs = lhs.strip()
+        rhs = rhs.strip()
+        #print('{} => {}'.format(lhs, rhs))
+        if lhs in resolved:
+            assert rhs == resolved[lhs]
+        else:
+            resolved[lhs] = rhs
+        touch(rhs)
+
+for path in glob.glob('ZenoBin/*.so'):
+    path = os.path.abspath(path)
+    touch(path)
+
+for path in visited:
+    name = os.path.basename(path)
+    dstpath = os.path.join('ZenoBin', name)
+    if not os.path.exists(dstpath):
+        print('copying', path, '=>', dstpath)
+        shutil.copy(path, dstpath)
