3713468ea5f9b40aeda7a41dee2b720b91a16da2
Arno <bahdaxxzm@gmail.com>
2021-08-22 19:15:14 +0800

    add get/set attribute value nodes

--- zeno/nodes/prim/MakePrimitive.cpp
+++ zeno/nodes/prim/MakePrimitive.cpp
@@ -1,9 +1,6 @@
 #include <zeno/zeno.h>
 #include <zeno/types/PrimitiveObject.h>
 #include <zeno/types/NumericObject.h>
-#include <zeno/utils/vec.h>
-#include <cstring>
-#include <cstdlib>
 #include <cassert>
 
 namespace zeno {
@@ -66,49 +63,4 @@ ZENDEFNODE(PrimitiveResize,
     }, /* category: */ {
     "primitive",
     }});
-
-
-struct PrimitiveAddAttr : zeno::INode {
-  virtual void apply() override {
-    auto prim = get_input<PrimitiveObject>("prim");
-    auto name = std::get<std::string>(get_param("name"));
-    auto type = std::get<std::string>(get_param("type"));
-    if (type == "float") {
-      if(has_input("fillValue")){
-        auto fillvalue = get_input<NumericObject>("fillValue")->get<float>();
-        prim->add_attr<float>(name, fillvalue);
-      }
-      else {
-        prim->add_attr<float>(name);
-      }
-    } else if (type == "float3") {
-        if(has_input("fillValue")){
-          auto fillvalue = get_input<NumericObject>("fillValue")->get<vec3f>();
-          prim->add_attr<vec3f>(name, fillvalue);
-        }
-        else {
-          prim->add_attr<vec3f>(name);
-        }
-    } else {
-        printf("%s\n", type.c_str());
-        assert(0 && "Bad attribute type");
-    }
-
-    set_output("prim", get_input("prim"));
-  }
-};
-
-ZENDEFNODE(PrimitiveAddAttr,
-    { /* inputs: */ {
-    "prim","fillValue",
-    }, /* outputs: */ {
-    "prim",
-    }, /* params: */ {
-    {"string", "name", "pos"},
-    {"string", "type", "float3"},
-    }, /* category: */ {
-    "primitive",
-    }});
-
-
 }
