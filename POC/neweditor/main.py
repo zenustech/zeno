0c08fd8905ec9a7caf42d6b0d899022db7bdcb65
archibate <1931127624@qq.com>
2021-08-24 13:34:18 +0800

    finally

--- POC/neweditor/main.py
+++ POC/neweditor/main.py
@@ -7,21 +7,51 @@ from PySide2.QtGui import *
 class QDMGraphicsScene(QGraphicsScene):
     def __init__(self, parent=None):
         super().__init__(parent)
+        palette = QPalette()
+
+        width, height = 64000, 64000
+        self.setSceneRect(-width // 2, -height // 2, width, height)
+        self.setBackgroundBrush(QColor('#393939'))
+
+        text = self.addText('awesome text')
+        text.setDefaultTextColor(QColor('white'))
+        text.setFlag(QGraphicsItem.ItemIsSelectable)
+        text.setFlag(QGraphicsItem.ItemIsMovable)
 
 
 class QDMGraphicsView(QGraphicsView):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setRenderHints(QPainter.Antialiasing
+                | QPainter.HighQualityAntialiasing
+                | QPainter.SmoothPixmapTransform
+                | QPainter.TextAntialiasing)
+
+        self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+
 
 class NodeEditor(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.layout = QVBoxLayout()
+        layout = QVBoxLayout()
 
-        self.view = QDMGraphicsView()
-        self.layout.addWidget(self.view)
+        self._view = QDMGraphicsView()
+        layout.addWidget(self._view)
+
+        self._scene = QDMGraphicsScene()
+        self._view.setScene(self._scene)
+
+        self.setLayout(layout)
+
+        palette = QPalette()
+        palette.setColor(QPalette.Window, QColor('#393939'))
+        palette.setColor(QPalette.WindowText, QColor('white'))
+        self.setPalette(palette)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
