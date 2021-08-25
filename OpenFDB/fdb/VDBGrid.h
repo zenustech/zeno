e94681a3f677c5d9137597cf762e63e374e45f90
archibate <1931127624@qq.com>
2021-08-25 16:13:47 +0800

    fix sample

--- OpenFDB/fdb/VDBGrid.h
+++ OpenFDB/fdb/VDBGrid.h
@@ -6,82 +6,81 @@
 
 namespace fdb {
 
-namespace VDBGrid_details {
-
-using LeafNode = Dense<Qfloat, 8>;
-
-struct InternalNode {
-    Dense<LeafNode *, 16> m_data;
-
-    InternalNode() = default;
-    ~InternalNode() = default;
-    InternalNode(InternalNode const &) = delete;
-    InternalNode &operator=(InternalNode const &) = delete;
-    InternalNode(InternalNode &&) = default;
-    InternalNode &operator=(InternalNode &&) = default;
-};
-
-struct RootNode {
-    Dense<InternalNode *, 32> m_data;
-    Dense<std::atomic<ushort>, 32> m_counter;
-
-    RootNode() = default;
-    ~RootNode() = default;
-    RootNode(RootNode const &) = delete;
-    RootNode &operator=(RootNode const &) = delete;
-    RootNode(RootNode &&) = default;
-    RootNode &operator=(RootNode &&) = default;
-
-    LeafNode *add(Quint3 coor) {
-        InternalNode *&node = m_data(coor >> 4);
-        if (!node) {
-            node = new InternalNode;
-        }
-        LeafNode *&leaf = node->m_data(coor & 15);
-        if (!leaf) {
-            leaf = new LeafNode;
-            ++m_counter(coor >> 4);
-        }
-        return leaf;
-    }
-
-    LeafNode *get(Quint3 coor) {
-        InternalNode *&node = m_data(coor >> 4);
-        if (!node) {
-            return nullptr;
-        }
-        LeafNode *leaf = node->m_data(coor & 15);
-        return leaf;
-    }
-
-    void del(Quint3 coor) {
-        InternalNode *&node = m_data(coor >> 4);
-        LeafNode *&leaf = node->m_data(coor & 15);
-        if (leaf) {
-            delete leaf;
-            if (--m_counter(coor >> 4) <= 0) {
-                delete node;
-            }
-            leaf = nullptr;
-        }
-    }
-
-    template <class Pol, class F>
-    void foreach(Pol const &pol, F const &func) {
-        m_data.foreach(pol, [&] (Quint3 coor1, InternalNode *node) {
-            if (node) {
-                node->m_data.foreach(policy::Serial{}, [&] (Quint3 coor2, LeafNode *leaf) {
-                    if (leaf) {
-                        Quint3 coor = coor1 << 4 | coor2;
-                        func(coor, leaf);
-                    }
-                });
-            }
-        });
-    }
-};
-
+template <class T>
 struct VDBGrid {
+    using LeafNode = Dense<T, 8>;
+
+    struct InternalNode {
+        Dense<LeafNode *, 16> m_data;
+
+        InternalNode() = default;
+        ~InternalNode() = default;
+        InternalNode(InternalNode const &) = delete;
+        InternalNode &operator=(InternalNode const &) = delete;
+        InternalNode(InternalNode &&) = default;
+        InternalNode &operator=(InternalNode &&) = default;
+    };
+
+    struct RootNode {
+        Dense<InternalNode *, 32> m_data;
+        Dense<std::atomic<ushort>, 32> m_counter;
+
+        RootNode() = default;
+        ~RootNode() = default;
+        RootNode(RootNode const &) = delete;
+        RootNode &operator=(RootNode const &) = delete;
+        RootNode(RootNode &&) = default;
+        RootNode &operator=(RootNode &&) = default;
+
+        LeafNode *add(Quint3 coor) {
+            InternalNode *&node = m_data(coor >> 4);
+            if (!node) {
+                node = new InternalNode;
+            }
+            LeafNode *&leaf = node->m_data(coor & 15);
+            if (!leaf) {
+                leaf = new LeafNode;
+                ++m_counter(coor >> 4);
+            }
+            return leaf;
+        }
+
+        LeafNode *get(Quint3 coor) {
+            InternalNode *&node = m_data(coor >> 4);
+            if (!node) {
+                return nullptr;
+            }
+            LeafNode *leaf = node->m_data(coor & 15);
+            return leaf;
+        }
+
+        void del(Quint3 coor) {
+            InternalNode *&node = m_data(coor >> 4);
+            LeafNode *&leaf = node->m_data(coor & 15);
+            if (leaf) {
+                delete leaf;
+                if (--m_counter(coor >> 4) <= 0) {
+                    delete node;
+                }
+                leaf = nullptr;
+            }
+        }
+
+        template <class Pol, class F>
+        void foreach(Pol const &pol, F const &func) {
+            m_data.foreach(pol, [&] (Quint3 coor1, InternalNode *node) {
+                if (node) {
+                    node->m_data.foreach(policy::Serial{}, [&] (Quint3 coor2, LeafNode *leaf) {
+                        if (leaf) {
+                            Quint3 coor = coor1 << 4 | coor2;
+                            func(coor, leaf);
+                        }
+                    });
+                }
+            });
+        }
+    };
+
     std::unique_ptr<RootNode> m_root = std::make_unique<RootNode>();
 
     LeafNode *add(Quint3 coor) const {
@@ -96,14 +95,15 @@ struct VDBGrid {
         return m_root->del(coor);
     }
 
+    T sample(Quint3 coor) const {
+        auto leaf = get(coor >> 3);
+        return leaf ? leaf->at(coor & 7) : T(0);
+    }
+
     template <class Pol, class F>
     void foreach(Pol const &pol, F const &func) const {
         return m_root->foreach(pol, func);
     }
 };
-
-}
-
-using VDBGrid_details::VDBGrid;
 
 }
--- OpenFDB/main.cpp
+++ OpenFDB/main.cpp
@@ -1,25 +1,33 @@
+#include <cstdio>
 #include <fdb/types.h>
 #include <fdb/policy.h>
 #include <fdb/VDBGrid.h>
 #include <fdb/stencil.h>
 #include <fdb/openvdb.h>
-#include <cstdio>
 
 using namespace fdb;
 
 int main() {
-    VDBGrid grid;
+    VDBGrid<float> grid;
 
-    grid.add({4, 4, 4});
+    grid.add({0, 0, 0});
+    grid.add({1, 0, 0});
+    grid.add({0, 1, 0});
+    grid.add({1, 1, 0});
+    grid.add({0, 0, 1});
+    grid.add({1, 0, 1});
+    grid.add({0, 1, 1});
+    grid.add({1, 1, 1});
 
     fdb::foreach(policy::Serial{}, grid, [&] (auto coor, auto &value) {
-        value = length(coor - 36) - 4;
+        value = length(coor - 8.f) < 8.f ? 1.f : 0.f;
         printf("%d %d %d: %f\n", coor[0], coor[1], coor[2], value);
     });
 
     fdb::write_dense_vdb("/tmp/a.vdb", [&] (Quint3 coor) {
-        return grid.get({4, 4, 4})->at(coor);
-    }, Quint3(8, 8, 8));
+        return grid.sample(coor);
+        //return length(coor - 8.f) < 8.f ? 1.f : 0.f;
+    }, Quint3(16, 16, 16));
 
     /*fdb::foreach_cell(policy::Serial{}, grid, [&] (auto coor
         , auto &value000
