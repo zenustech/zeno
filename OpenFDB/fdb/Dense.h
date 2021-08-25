2db8f77ba358ff32a4360c6e856c4e6f4de3b108
archibate <1931127624@qq.com>
2021-08-25 23:34:06 +0800

    uchar at

--- OpenFDB/fdb/Dense.h
+++ OpenFDB/fdb/Dense.h
@@ -6,7 +6,7 @@
 
 namespace fdb {
 
-template <class T, Quint N>
+template <class T, size_t N>
 struct Dense {
     std::array<T, N * N * N> m_data;
 
@@ -17,36 +17,36 @@ struct Dense {
     Dense(Dense &&) = default;
     Dense &operator=(Dense &&) = default;
 
-    [[nodiscard]] static Quint linearize(Quint3 coor) {
-        return dot((coor + N) % N, Quint3(1, N, N * N));
+    [[nodiscard]] static size_t linearize(vec<size_t, 3> coor) {
+        return dot((coor + N) % N, vec<size_t, 3>(1, N, N * N));
     }
 
-    [[nodiscard]] static Quint3 delinearize(Quint i) {
-        return Quint3(i % N, (i / N) % N, (i / N) / N);
+    [[nodiscard]] static vec<size_t, 3> delinearize(size_t i) {
+        return vec<size_t, 3>(i % N, (i / N) % N, (i / N) / N);
     }
 
-    [[nodiscard]] T &at(Quint3 coor) {
-        Quint i = linearize(coor);
+    [[nodiscard]] T &at(vec<size_t, 3> coor) {
+        size_t i = linearize(coor);
         return m_data[i];
     }
 
-    [[nodiscard]] T const &at(Quint3 coor) const {
-        Quint i = linearize(coor);
+    [[nodiscard]] T const &at(vec<size_t, 3> coor) const {
+        size_t i = linearize(coor);
         return m_data[i];
     }
 
-    [[nodiscard]] decltype(auto) operator()(Quint3 coor) {
+    [[nodiscard]] decltype(auto) operator()(vec<size_t, 3> coor) {
         return at(coor);
     }
 
-    [[nodiscard]] decltype(auto) operator()(Quint3 coor) const {
+    [[nodiscard]] decltype(auto) operator()(vec<size_t, 3> coor) const {
         return at(coor);
     }
 
     template <class Pol, class F>
     void foreach(Pol const &pol, F const &func) {
-        range_for(pol, (Quint)0, N * N * N, [&] (Quint i) {
-            Quint3 coor = delinearize(i);
+        range_for(pol, (size_t)0, N * N * N, [&] (size_t i) {
+            vec<size_t, 3> coor = delinearize(i);
             func(coor, m_data[i]);
         });
     }
--- OpenFDB/main.cpp
+++ OpenFDB/main.cpp
@@ -8,37 +8,5 @@
 using namespace fdb;
 
 int main() {
-    VDBGrid<Qfloat> g_pressure;
-    VDBGrid<Qfloat3> g_velocity;
-
-    ndrange_for(policy::Serial{}, Quint3(0), Quint3(8), [&] (auto coor) {
-        g_pressure.add(coor);
-    });
-
-    VDBStencil(g_pressure).foreach(policy::Parallel{}, [&] (auto leafCoor, auto *leaf, auto callback) {
-        callback([&] (auto coor, auto &value) {
-            value = length(coor - 32.f) < 32.f ? 1.0f : 0.0f;
-        });
-    });
-
-    VDBStencil(g_pressure).foreach_2x2x2_star(policy::Serial{},
-    [&] (auto leafCoor, auto *leaf, auto callback) {
-        auto *vel_leaf = g_velocity.add(leafCoor);
-        callback([&] (auto coor
-            , auto &value000
-            , auto &value100
-            , auto &value010
-            , auto &value001
-            ) {
-            vel_leaf->at(coor) = Qfloat3(
-                    value000 - value100,
-                    value000 - value010,
-                    value000 - value001);
-        });
-    });
-
-    write_dense_vdb("/tmp/a.vdb", [&] (Quint3 coor) {
-        return abs(g_velocity.read_at(coor));
-    }, Quint3(64, 64, 64));
     return 0;
 }
