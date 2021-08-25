d987b6ef3ca68023dbf6c2eaf3474d6e5cfbab4e
archibate <1931127624@qq.com>
2021-08-25 22:20:58 +0800

    worked stencil class, add sycltest

--- OpenFDB/main.cpp
+++ OpenFDB/main.cpp
@@ -2,7 +2,7 @@
 #include <fdb/types.h>
 #include <fdb/policy.h>
 #include <fdb/VDBGrid.h>
-#include <fdb/stencil.h>
+#include <fdb/Stencil.h>
 #include <fdb/openvdb.h>
 
 using namespace fdb;
@@ -10,22 +10,17 @@ using namespace fdb;
 int main() {
     VDBGrid<float> grid, grid2;
 
-    int recount = 0;
     ndrange_for(policy::Serial{}, Quint3(0), Quint3(8), [&] (auto coor) {
-        recount++;
         grid.add(coor);
     });
 
-    int count = 0;
-    fdb::foreach(policy::Serial{}, grid, [&] (auto leafCoor, auto *leaf, auto callback) {
-        count++;
+    fdb::Stencil(grid).foreach(policy::Serial{}, [&] (auto leafCoor, auto *leaf, auto callback) {
         callback([&] (auto coor, auto &value) {
             value = length(coor - 32.f) < 32.f ? 1.0f : 0.0f;
         });
     });
-    printf("%d %d\n", count, recount);
 
-    /*fdb::foreach_cell(policy::Serial{}, grid, [&] (auto leafCoor, auto *leaf, auto callback) {
+    fdb::Stencil(grid).foreach_cell(policy::Serial{}, [&] (auto leafCoor, auto *leaf, auto callback) {
         auto *leaf2 = grid2.add(leafCoor);
         callback([&] (auto coor
             , auto &value000
@@ -37,11 +32,11 @@ int main() {
             , auto &value011
             , auto &value111
             ) {
-            leaf2->at(coor) = value000;//fabsf(value100 - value000);
+            leaf2->at(coor) = fabsf(value100 - value000);
         });
-    });*/
+    });
 
     fdb::write_dense_vdb("/tmp/a.vdb", [&] (Quint3 coor) {
-        return grid.read_at(coor);
+        return grid2.read_at(coor);
     }, Quint3(64, 64, 64));
 }
