014f0e81e76f10409d41643d36addc635174b184
archibate <1931127624@qq.com>
2021-08-25 15:12:41 +0800

    adhoc

--- OpenFDB/fdb/Stencil.h
+++ OpenFDB/fdb/Stencil.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "VDBGrid.h"
+#include "policy.h"
 
 namespace fdb {
 
 template <class Pol, class Grid, class F>
-void foreachVert(Pol const &pol, Grid &grid, F const &func) const {
+void foreach_1x1x1(Pol const &pol, Grid &grid, F const &func) const {
     return grid->foreach(pol, [&] (Quint3 coor1, auto &leaf) {
         leaf->foreach([&] (Quint3 coor2, auto &value) {
             auto coor = coor1 << 3 | coor2;
@@ -15,11 +16,77 @@ void foreachVert(Pol const &pol, Grid &grid, F const &func) const {
 }
 
 template <class Pol, class Grid, class F>
-void foreachCell(Pol const &pol, Grid &grid, F const &func) const {
+void foreach_2x2x2(Pol const &pol, Grid &grid, F const &func) const {
     return grid->foreach(pol, [&] (Quint3 coor1, auto &leaf) {
-        leaf->foreach([&] (Quint3 coor2, auto &value) {
+        ndrange_for(policy::Serial{}, Quint3(0), Quint3(7), [&] (Quint3 coor2, auto &value) {
             auto coor = coor1 << 3 | coor2;
-            func(coor, value);
+            func(coor, value
+            , leaf(coor + Quint3(1, 0, 0))
+            , leaf(coor + Quint3(0, 1, 0))
+            , leaf(coor + Quint3(1, 1, 0))
+            , leaf(coor + Quint3(0, 0, 1))
+            , leaf(coor + Quint3(1, 0, 1))
+            , leaf(coor + Quint3(0, 1, 1))
+            , leaf(coor + Quint3(1, 1, 1))
+            );
+        });
+        auto xleaf = grid.get(coor1 + Quint(1, 0, 0));
+        auto yleaf = grid.get(coor1 + Quint(0, 1, 0));
+        auto zleaf = grid.get(coor1 + Quint(0, 0, 1));
+        ndrange_for(policy::Serial{}, Quint2(0), Quint2(7), [&] (Quint2 coor2, auto &value) {
+            if (xleaf) {
+                auto coor = coor1 << 3 | Quint3(coor2[0], coor2[1], 7);
+                func(coor, value
+                , xleaf(coor + Quint3(-7, 0, 0))
+                , leaf(coor + Quint3(0, 1, 0))
+                , xleaf(coor + Quint3(-7, 1, 0))
+                , leaf(coor + Quint3(0, 0, 1))
+                , xleaf(coor + Quint3(-7, 0, 1))
+                , leaf(coor + Quint3(0, 1, 1))
+                , xleaf(coor + Quint3(-7, 1, 1))
+                );
+            }
+            if (yleaf) {
+                auto coor = coor1 << 3 | Quint3(coor2[0], 7, coor2[1]);
+                func(coor, value
+                , leaf(coor + Quint3(1, 0, 0))
+                , yleaf(coor + Quint3(0, -7, 0))
+                , yleaf(coor + Quint3(1, -7, 0))
+                , leaf(coor + Quint3(0, 0, 1))
+                , leaf(coor + Quint3(1, 0, 1))
+                , yleaf(coor + Quint3(0, -7, 1))
+                , yleaf(coor + Quint3(1, -7, 1))
+                );
+            }
+            if (zleaf) {
+                auto coor = coor1 << 3 | Quint3(7, coor2[0], coor2[1]);
+                func(coor, value
+                , leaf(coor + Quint3(1, 0, 0))
+                , leaf(coor + Quint3(0, 1, 0))
+                , leaf(coor + Quint3(1, 1, 0))
+                , zleaf(coor + Quint3(0, 0, -7))
+                , zleaf(coor + Quint3(1, 0, -7))
+                , zleaf(coor + Quint3(0, 1, -7))
+                , zleaf(coor + Quint3(1, 1, -7))
+                );
+            }
+        });
+        auto xyleaf = grid.get(coor1 + Quint(1, 1, 0));
+        auto yzleaf = grid.get(coor1 + Quint(0, 1, 1));
+        auto zxleaf = grid.get(coor1 + Quint(1, 0, 1));
+        ndrange_for(policy::Serial{}, Quint(0), Quint(7), [&] (Quint coor2, auto &value) {
+            if (xleaf && yleaf) {
+                auto coor = coor1 << 3 | Quint3(coor2, 7, 7);
+                func(coor, value
+                , xleaf(coor + Quint3(-7, 0, 0))
+                , leaf(coor + Quint3(0, 1, 0))
+                , xleaf(coor + Quint3(-7, 1, 0))
+                , leaf(coor + Quint3(0, 0, 1))
+                , xleaf(coor + Quint3(-7, 0, 1))
+                , leaf(coor + Quint3(0, 1, 1))
+                , xleaf(coor + Quint3(-7, 1, 1))
+                );
+            }
         });
     });
 }
