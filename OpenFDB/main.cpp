e1a65ca9f6384d6ff08f315f6248d31c04632b3d
archibate <1931127624@qq.com>
2021-08-26 11:42:15 +0800

    fix it

--- OpenFDB/main.cpp
+++ OpenFDB/main.cpp
@@ -11,13 +11,9 @@ int main() {
 
     ndrange_for(policy::Serial{},
     vec3i(-64), vec3i(64), [&] (auto idx) {
-        float value = length(idx) < 40.f ? 1.f : 0.f;
+        float value = max(0.f, 40.f - length(tofloat(idx)));
         g_pre.set(idx, value);
     });
-
-    printf("%p\n", g_pre.address(0, {-1, 0, 0}));
-    printf("%p\n", g_pre.address(0, {0, 0, 0}));
-    printf("%p\n", g_pre.address(0, {4095, 0, 0}));
 
     /*ndrange_for(policy::Serial{},
     vec3i(1), vec3i(127), [&] (auto idx) {
