4a81d23adda9fa21c4da5aaa45c910c48353e07b
archibate <1931127624@qq.com>
2021-08-28 15:42:58 +0800

    be explicit in fdb filename

--- OpenFDB/fdb/openvdb.cpp
+++ OpenFDB/fdb/openvdb.cpp
@@ -1,55 +1,19 @@
 #include <openvdb/openvdb.h>
-#include <openvdb/tools/Dense.h>
 #include "openvdb.h"
-#include "vec.h"
+#include "VDBGrid.h"
 
-namespace fdb {
+template <class VdbGridT, class OurGridT>
+void vdb_to_our(VdbGridT &vdbGrid, OurGridT &ourGrid) {
+    vdbGrid.foreach([&] (auto leaf, auto coor));
 
-template <class GridT, class ValT>
-void impl_write_dense_vdb
-        ( std::string path
-        , std::function<ValT(vec3i)> sampler
-        , vec3i start
-        , vec3i stop
-        )
-{
-    openvdb::CoordBBox bbox(
-            openvdb::Coord(start[0], start[1], start[2]),
-            openvdb::Coord(stop[0], stop[1], stop[2]));
-    openvdb::tools::Dense<typename GridT::ValueType> dens(bbox);
-    for (int z = start[2]; z < stop[2]; z++) {
-        for (int y = start[1]; y < stop[1]; y++) {
-            for (int x = start[0]; x < stop[0]; x++) {
-                auto val = sampler({x, y, z});
-                dens.setValue(x - start[0], y - start[1], z - start[2],
-                        vec_to_other<typename GridT::ValueType>(val));
-            }
+    auto wrangler = [&](auto &leaf, openvdb::Index leafpos) {
+        for (auto iter = leaf.beginValueOn(); iter != leaf.endValueOn(); ++iter) {
+            auto valpos = iter.getCoord();
+            auto value = iter.getValue();
+            ourGrid.set(ijk, value);
         }
-    }
-    auto grid = GridT::create();
-    typename GridT::ValueType tolerance{0};
-    openvdb::tools::copyFromDense(dens, grid->tree(), tolerance);
-    openvdb::io::File((std::string)path).write({grid});
-}
-
-void write_dense_vdb
-    ( std::string path
-    , std::function<float(vec3i)> sampler
-    , vec3i start
-    , vec3i stop
-    )
-{
-    return impl_write_dense_vdb<openvdb::FloatGrid>(path, sampler, start, stop);
-}
-
-void write_dense_vdb
-    ( std::string path
-    , std::function<vec3f(vec3i)> sampler
-    , vec3i start
-    , vec3i stop
-    )
-{
-    return impl_write_dense_vdb<openvdb::Vec3fGrid>(path, sampler, start, stop);
-}
-
+    };
+    auto leafman = openvdb::tree::LeafManager<std::decay_t<decltype(vdbgrid->tree())>>
+        (vdbgrid->tree());
+    leafman.foreach(wrangler);
 }
