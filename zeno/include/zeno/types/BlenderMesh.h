9688ed876c2b79a10cc2a9f137bb492122064a97
archibate <1931127624@qq.com>
2021-08-30 11:39:01 +0800

    fix blmesh

--- zeno/include/zeno/types/BlenderMesh.h
+++ zeno/include/zeno/types/BlenderMesh.h
@@ -1,14 +1,32 @@
 #pragma once
 
-#ifndef _MSC_VER
-#warning "<zeno/types/BlenderMesh.h> is deprecated, use <zeno/types/PolyMeshObject.h> instead"
-#endif
-
-#include <zeno/types/PolymeshObject.h>
+#include <zeno/core/IObject.h>
+#include <zeno/utils/vec.h>
+#include <array>
+#include <vector>
 
 namespace zeno {
 
-using BlenderMesh = PolyMeshObject;
-using BlenderAxis = TransformObject;
+struct PolyMesh {
+    struct Polygon {
+        int start = 0, len = 0;
 
+        inline Polygon() = default;
+        inline Polygon(int start, int len)
+            : start(start), len(len) {}
+    };
+
+    std::vector<vec3f> vert;
+    std::vector<Polygon> poly;
+    std::vector<int> loop;
 };
+
+struct BlenderAxis : IObjectClone<BlenderAxis> {
+    std::array<std::array<float, 4>, 4> matrix;
+};
+
+struct BlenderMesh : IObjectClone<BlenderMesh, BlenderAxis>, PolyMesh {
+    bool is_smooth = false;
+};
+
+}
