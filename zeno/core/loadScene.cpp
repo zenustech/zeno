e1b3f4a9efdd0d00376a6f8b6689ae7b6ac59d0e
archibate <1931127624@qq.com>
2021-08-30 22:23:20 +0800

    support vectors in loadscene

--- zeno/core/loadScene.cpp
+++ zeno/core/loadScene.cpp
@@ -7,7 +7,7 @@ namespace zeno {
 
 using namespace rapidjson;
 
-template <class T>
+template <class T, bool HasVec = true>
 static T generic_get(Value const &x) {
     if (x.IsString()) {
         return (std::string)x.GetString();
@@ -18,6 +18,31 @@ static T generic_get(Value const &x) {
     } else if (x.IsBool()) {
         return x.GetBool();
     } else {
+        if constexpr (HasVec) {
+            if (x.IsArray()) {
+                auto a = x.GetArray();
+                if (a.Size() == 2) {
+                    if (a[0].IsInt()) {
+                        return zeno::vec2i(a[0].GetInt(), a[1].GetInt());
+                    } else if (a[0].IsFloat()) {
+                        return zeno::vec2f(a[0].GetFloat(), a[1].GetFloat());
+                    }
+                } else if (a.Size() == 3) {
+                    if (a[0].IsInt()) {
+                        return zeno::vec3i(a[0].GetInt(), a[1].GetInt(), a[2].GetInt());
+                    } else if (a[0].IsFloat()) {
+                        return zeno::vec3f(a[0].GetFloat(), a[1].GetFloat(), a[2].GetFloat());
+                    }
+                } else if (a.Size() == 4) {
+                    if (a[0].IsInt()) {
+                        return zeno::vec4i(a[0].GetInt(), a[1].GetInt(), a[2].GetInt(), a[4].GetInt());
+                    } else if (a[0].IsFloat()) {
+                        return zeno::vec4f(a[0].GetFloat(), a[1].GetFloat(), a[2].GetFloat(), a[4].GetFloat());
+                    }
+                }
+            }
+        }
+        spdlog::warn("unknown type encountered in generic_get");
         return 0;
     }
 }
@@ -40,7 +65,7 @@ ZENO_API void Scene::loadScene(const char *json) {
             } else if (cmd == "setNodeInput") {
                 getGraph().setNodeInput(di[1].GetString(), di[2].GetString(), generic_get<zeno::zany>(di[3]));
             } else if (cmd == "setNodeParam") {
-                getGraph().setNodeParam(di[1].GetString(), di[2].GetString(), generic_get<std::variant<int, float, std::string>>(di[3]));
+                getGraph().setNodeParam(di[1].GetString(), di[2].GetString(), generic_get<std::variant<int, float, std::string>, false>(di[3]));
             } else if (cmd == "setNodeOption") {
                 getGraph().setNodeOption(di[1].GetString(), di[2].GetString());
             } else if (cmd == "bindNodeInput") {
