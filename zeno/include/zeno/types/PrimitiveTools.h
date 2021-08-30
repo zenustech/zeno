869fcf2e3f3e520981b738cc4f2b492b595ece4d
archibate <1931127624@qq.com>
2021-08-30 13:23:03 +0800

    triangulate

--- zeno/include/zeno/types/PrimitiveTools.h
+++ zeno/include/zeno/types/PrimitiveTools.h
@@ -5,7 +5,29 @@
 namespace zeno {
 
 static void prim_triangulate(PrimitiveObject *prim) {
+    prim->resize(prim->verts().size());
+    prim->tris.clear();
+
     for (auto [start, len]: prim->polys()) {
+        if (len == 1) {
+            prim->points.emplace_back(
+                prim->loops()[start]);
+        } else if (len == 2) {
+            prim->lines.emplace_back(
+                prim->loops()[start],
+                prim->loops()[start + 1]);
+        } else if (len > 3) {
+            prim->tris.emplace_back(
+                    prim->loops()[start],
+                    prim->loops()[start + 1],
+                    prim->loops()[start + 2]);
+            for (int i = 3; i < len; i++) {
+                prim->tris.emplace_back(
+                        prim->loops()[start],
+                        prim->loops()[start + i - 1],
+                        prim->loops()[start + i]);
+            }
+        }
     }
 }
 
