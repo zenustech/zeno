d1347cc9d42ca5eba5c3a724661a627b544a0556
archibate <1931127624@qq.com>
2021-08-31 14:21:12 +0800

    now worked

--- projects/cgmesh/PrimitiveBooleanOp.cpp
+++ projects/cgmesh/PrimitiveBooleanOp.cpp
@@ -1,5 +1,6 @@
 #include <zeno/zeno.h>
 #include <zeno/utils/vec.h>
+#include <zeno/types/NumericObject.h>
 #include <zeno/types/PrimitiveObject.h>
 #include <igl/copyleft/cgal/mesh_boolean.h>
 #include <Eigen/Core>
@@ -9,33 +10,36 @@ namespace {
 using namespace zeno;
 
 
-void prim_to_eigen(Eigen::MatrixXd &V, Eigen::MatrixXi &F, PrimitiveObject const *prim) {
+auto prim_to_eigen(PrimitiveObject const *prim) {
+    Eigen::MatrixXd V(prim->size(), 3);
+    Eigen::MatrixXi F(prim->tris.size(), 3);
     auto &verts = prim->attr<vec3f>("pos");
     for (int i = 0; i < prim->size(); i++) {
-        auto pos = verts[i];
-        V.push_back({pos[0], pos[1], pos[2]});
+        vec3d pos = verts[i];
+        V.row(i) = Eigen::RowVector3d(pos[0], pos[1], pos[2]);
     }
 
     for (int i = 0; i < prim->tris.size(); i++) {
-        auto ind = prim->tris[i];
-        F.push_back({ind[0], ind[1], ind[2]});
+        auto const &ind = prim->tris[i];
+        F.row(i) = Eigen::RowVector3i(ind[0], ind[1], ind[2]);
     }
+    return std::make_pair(V, F);
 }
 
 
 void eigen_to_prim(Eigen::MatrixXd const &V, Eigen::MatrixXi const &F, PrimitiveObject *prim) {
     auto &verts = prim->attr<vec3f>("pos");
-    prim->verts.clear();
-    for (int i = 0; i < V->size(); i++) {
-        auto const &pos = V[i];
-        verts.emplace_back(V[0], V[1], V[2]);
+    verts.clear();
+    for (int i = 0; i < V.rows(); i++) {
+        auto const &pos = V.row(i);
+        verts.emplace_back(V(0), V(1), V(2));
     }
-    prim->resize(verts);
+    prim->resize(verts.size());
 
     prim->tris.clear();
-    for (int i = 0; i < F.size(); i++) {
-        auto const &ind = F[i];
-        prim->tris.emplace_back(ind[0], ind[1], ind[2]);
+    for (int i = 0; i < F.rows(); i++) {
+        auto const &ind = F.row(i);
+        prim->tris.emplace_back(ind(0), ind(1), ind(2));
     }
 }
 
@@ -59,14 +63,13 @@ struct PrimitiveBooleanOp : INode {
           boolean_type = igl::MESH_BOOLEAN_TYPE_RESOLVE;
         }
 
-        Eigen::MatrixXd VA, VB, VC;
-        Eigen::MatrixXi FA, FB, FC;
+        auto [VA, FA] = prim_to_eigen(primA.get());
+        auto [VB, FB] = prim_to_eigen(primB.get());
+
+        Eigen::MatrixXd VC;
+        Eigen::MatrixXi FC;
         Eigen::VectorXi J;
-
-        prim_to_eigen(VA, FA, primA.get());
-        prim_to_eigen(VB, FB, primB.get());
-
-        igl::copyleft::cgal::mesh_boolean(VA,FA,VB,FB,boolean_type,VC,FC,J);
+        igl::copyleft::cgal::mesh_boolean(VA, FA, VB, FB, boolean_type, VC, FC, J);
 
         auto attrName = get_param<std::string>("attrName");
         if (attrName.size()) {
