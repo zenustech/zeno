8cc4269941de921856b5fe41449ef71672a23590
archibate <1931127624@qq.com>
2021-08-31 14:28:20 +0800

    fuck igl pwn

--- projects/cgmesh/PrimitiveBooleanOp.cpp
+++ projects/cgmesh/PrimitiveBooleanOp.cpp
@@ -15,7 +15,7 @@ auto prim_to_eigen(PrimitiveObject const *prim) {
     Eigen::MatrixXi F(prim->tris.size(), 3);
     auto &verts = prim->attr<vec3f>("pos");
     for (int i = 0; i < prim->size(); i++) {
-        vec3d pos = verts[i];
+        auto const &pos = verts[i];
         V.row(i) = Eigen::RowVector3d(pos[0], pos[1], pos[2]);
     }
 
@@ -28,7 +28,7 @@ auto prim_to_eigen(PrimitiveObject const *prim) {
 
 
 void eigen_to_prim(Eigen::MatrixXd const &V, Eigen::MatrixXi const &F, PrimitiveObject *prim) {
-    auto &verts = prim->attr<vec3f>("pos");
+    auto &verts = prim->add_attr<vec3f>("pos");
     verts.clear();
     for (int i = 0; i < V.rows(); i++) {
         auto const &pos = V.row(i);
@@ -50,6 +50,7 @@ struct PrimitiveBooleanOp : INode {
         auto primB = get_input<PrimitiveObject>("primB");
         auto op_type = get_param<std::string>("op_type");
 
+        printf("0\n");
         igl::MeshBooleanType boolean_type;
         if (op_type == "Union") {
           boolean_type = igl::MESH_BOOLEAN_TYPE_UNION;
@@ -61,17 +62,24 @@ struct PrimitiveBooleanOp : INode {
           boolean_type = igl::MESH_BOOLEAN_TYPE_XOR;
         } else if (op_type == "Resolve") {
           boolean_type = igl::MESH_BOOLEAN_TYPE_RESOLVE;
+        } else {
+          throw Exception("bad boolean op type: " + op_type);
         }
 
+        printf("1\n");
         auto [VA, FA] = prim_to_eigen(primA.get());
+        printf("2\n");
         auto [VB, FB] = prim_to_eigen(primB.get());
+        printf("3\n");
 
         Eigen::MatrixXd VC;
         Eigen::MatrixXi FC;
         Eigen::VectorXi J;
         igl::copyleft::cgal::mesh_boolean(VA, FA, VB, FB, boolean_type, VC, FC, J);
+        printf("4\n");
 
-        auto attrName = get_param<std::string>("attrName");
+        /*auto attrName = get_param<std::string>("attrName");
+        printf("5\n");
         if (attrName.size()) {
             auto attrValA = get_input<NumericObject>("attrValA")->value;
             auto attrValB = get_input<NumericObject>("attrValB")->value;
@@ -79,11 +87,13 @@ struct PrimitiveBooleanOp : INode {
             std::visit([&] (auto valA) {
                 auto valB = std::get<decltype(valA)>(attrValB);
             }, attrValA);
-        }
+        }*/
 
+        printf("6\n");
         auto primC = std::make_shared<PrimitiveObject>();
         eigen_to_prim(VC, FC, primC.get());
 
+        printf("7\n");
         set_output("primC", std::move(primC));
     }
 };
