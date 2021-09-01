5e2a313a871cd4e867a0199e573ec1b1f3242ad3
archibate <1931127624@qq.com>
2021-09-01 12:46:51 +0800

    fix zenvis with apifree

--- zeno/include/zeno/utils/Exception.h
+++ zeno/include/zeno/utils/Exception.h
@@ -10,14 +10,24 @@ class BaseException : public std::exception {
 private:
   std::string msg;
 public:
+#ifndef ZENO_APIFREE
   ZENO_API BaseException(std::string_view msg) noexcept;
   ZENO_API ~BaseException() noexcept;
   ZENO_API char const *what() const noexcept;
+#else
+  BaseException(std::string_view msg) noexcept : msg(msg) {}
+  ~BaseException() noexcept = defalut;
+  char const *what() const noexcept { return msg.c_str(); }
+#endif
 };
 
 class Exception : public BaseException {
 public:
+#ifndef ZENO_APIFREE
   ZENO_API Exception(std::string_view msg) noexcept;
+#else
+  ZENO_API Exception(std::string_view msg) noexcept = default;
+#endif
 };
 
 }
