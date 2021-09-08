8f7cc52ad5293bfe8ee232cc7e941c98864df293
archibate <1931127624@qq.com>
2021-09-08 13:16:56 +0800

    working timer

--- zeno/include/zinc/format.h
+++ zeno/include/zinc/format.h
@@ -9,13 +9,14 @@ namespace zinc {
 template <class ...Ts>
 std::string format(const char *fmt, Ts &&...ts) {
 #if 0
-    int n = sprintf(nullptr, fmt, std::forward<Ts>(ts)...);
+    int n = snprintf(nullptr, 0, fmt, std::forward<Ts>(ts)...);
     if (n < 0) return {};
 #else
     int n = strlen(fmt) * 2 + 4096;
 #endif
-    std::string res(n);
-    sprintf(res.data(), fmt, std::forward<Ts>(ts)...);
+    std::string res(n + 2);
+    sprintf(res.data(), n + 1, fmt, std::forward<Ts>(ts)...);
+    res.resize(n);
     return res;
 }
 
