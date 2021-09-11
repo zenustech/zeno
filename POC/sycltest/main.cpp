95716f6698a45ba1758376138fd4fe1b72f66127
archibate <1931127624@qq.com>
2021-09-11 13:35:35 +0800

    imitate sycl

--- POC/sycltest/main.cpp
+++ POC/sycltest/main.cpp
@@ -6,6 +6,26 @@
 
 namespace sycl = cl::sycl;
 
+
+struct SyclHandler {
+    sycl::handler &m_handler;
+
+    SyclHandler(sycl::handler &handler) : m_handler(handler) {}
+
+    template <auto Mode = sycl::access::mode::read_write, class T>
+    [[nodiscard]] auto remapPointer(T *ptr) {
+        auto axr = m_mapper.get_access<Mode,
+             sycl::access::mode::global_buffer, T>(ptr, m_handler);
+        return axr;
+    }
+
+    template <class Key, class Range, class Kernel>
+    decltype(auto) parallel_for(Range &&range, Kernel &&kernel) {
+        return m_handler.parallel_for(range, kernel);
+    }
+};
+
+
 struct SyclSession {
     sycl::queue m_queue;
     sycl::codeplay::PointerMapper m_mapper;
@@ -15,29 +35,28 @@ struct SyclSession {
         return m_queue.submit(f);
     }
 
-    decltype(auto) submit() {
+    decltype(auto) wait() {
         return m_queue.wait();
     }
 
-    void *allocate(size_t size) {
+    [[nodiscard]] void *syclMalloc(size_t size) {
         return sycl::codeplay::SYCLmalloc(size, m_mapper);
     }
 
-    void deallocate(void *ptr) {
+    void syclFree(void *ptr) {
         sycl::codeplay::SYCLfree(ptr, m_mapper);
     }
 
-    auto &queue() {
-        return m_queue;
-    }
-
-    auto &mapper() {
-        return m_mapper;
+    template <auto Mode = sycl::access::mode::read_write, class T>
+    [[nodiscard]] auto remapPointer(T *ptr) {
+        auto axr = m_mapper.get_access<Mode,
+             sycl::access::mode::host_buffer, T>(ptr);
+        return axr;
     }
 };
 
 
-static SyclSession *syclSession() {
+[[nodiscard]] static SyclSession *syclSession() {
     static std::unique_ptr<SyclSession> g_session = std::make_unique<SyclSession>();
     return g_session.get();
 }
@@ -60,19 +79,19 @@ struct SyclAllocator {
     SyclAllocator() = default;
     ~SyclAllocator() = default;
 
-    pointer allocate(size_type numObjects) {
-        return static_cast<pointer>(syclSession()->allocate(sizeof(T) * numObjects));
+    [[nodiscard]] pointer allocate(size_type numObjects) {
+        return static_cast<pointer>(syclSession()->syclMalloc(sizeof(T) * numObjects));
     }
 
-    pointer allocate(size_type numObjects, const void *hint) {
+    [[nodiscard]] pointer allocate(size_type numObjects, const void *hint) {
         return allocate(numObjects);
     }
 
     void deallocate(pointer p, size_type numObjects) {
-        syclSession()->deallocate(p);
+        syclSession()->syclFree(p);
     }
 
-    size_type max_size() const {
+    [[nodiscard]] size_type max_size() const {
         return std::numeric_limits<size_type>::max();
     }
 
@@ -95,19 +114,15 @@ int main() {
     SyclAllocator<int> svm;
     int *arr = svm.allocate(32);
 
-    sess->queue().submit([&] (sycl::handler &cgh) {
-        auto axr = sess->mapper().get_access<
-            sycl::access::mode::discard_write, sycl::access::target::global_buffer,
-            int>(arr, cgh);
-        cgh.parallel_for<kernel0>(sycl::range<1>(32), [=](sycl::item<1> id) {
+    sess->submit([&] (SyclHandler &hdl) {
+        auto axr = hdl.remapPointer(arr);
+        handler.parallel_for<kernel0>(sycl::range<1>(32), [=](sycl::item<1> id) {
             axr[id[0]] = id[0];
         });
     });
 
     {
-        auto axr = sess->mapper().get_access<
-            sycl::access::mode::read, sycl::access::target::host_buffer,
-            int>(arr);
+        auto axr = sess.remapPointer(arr);
         for (int i = 0; i < 32; i++) {
             printf("%d\n", axr[i]);
         }
