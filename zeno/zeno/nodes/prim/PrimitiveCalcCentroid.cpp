c54e9a5e8518af4145f3c895f8e89a0e652e98c7
archibate <1931127624@qq.com>
2021-09-12 13:08:51 +0800

    fix no tri centroid

--- zeno/zeno/nodes/prim/PrimitiveCalcCentroid.cpp
+++ zeno/zeno/nodes/prim/PrimitiveCalcCentroid.cpp
@@ -14,15 +14,25 @@ struct PrimitiveCalcCentroid : zeno::INode {
         auto prim = get_input<PrimitiveObject>("prim");
         auto &pos = prim->attr<vec3f>("pos");
 
-        vec4f acc = parallel_reduce_array(prim->tris.size(), vec4f(0), [&] (size_t i) {
-            auto ind = prim->tris[i];
-            auto a = pos[ind[0]], b = pos[ind[1]], c = pos[ind[2]];
-            auto weight = length(cross(b - a, c - a));
-            auto center = weight * (a + b + c);
-            return vec4f(center[0], center[1], center[2], weight);
-        }, [&] (auto a, auto b) { return a + b; });
+        vec4f acc;
+        if (prim->tris.size()) {
+            acc = parallel_reduce_array(prim->tris.size(), vec4f(0), [&] (size_t i) {
+                auto ind = prim->tris[i];
+                auto a = pos[ind[0]], b = pos[ind[1]], c = pos[ind[2]];
+                auto weight = length(cross(b - a, c - a));
+                auto center = weight * (a + b + c);
+                return vec4f(center[0], center[1], center[2], weight);
+            }, [&] (auto a, auto b) { return a + b; });
+            acc /= 3.0f;
+        } else {
+            acc = parallel_reduce_array(prim->size(), vec4f(0), [&] (size_t i) {
+                auto pos = prim->verts[i];
+                return vec4f(pos[0], pos[1], pos[2], 1.0f);
+            }, [&] (auto a, auto b) { return a + b; });
+        }
+
         auto centroid = acc[3] == 0 ? vec3f(0) :
-            vec3f(acc[0], acc[1], acc[2]) / (3.f * acc[3]);
+            vec3f(acc[0], acc[1], acc[2]) / acc[3];
 
         set_output2("centroid", centroid);
         set_output2("totalArea", acc[3]);
