685a4402ec3675a931781637ddf2aa9a4147c470
archibate <1931127624@qq.com>
2021-09-14 00:41:03 +0800

    work

--- POC/sycltest/main.cpp
+++ POC/sycltest/main.cpp
@@ -9,21 +9,22 @@ using namespace fdb;
 class kernel0;
 
 int main() {
-    NDArray<float, 2> img({512, 512});
+    NDArray<float, 2> img({16, 16});
 
     fdb::getQueue().submit([&] (fdb::DeviceHandler dev) {
         auto imgAxr = img.accessor<fdb::Access::discard_write>(dev);
-        dev.parallelFor<kernel0>(fdb::vec1S(16), [=] (size_t id) {
-            imgAxr(id, id) = id;
+        dev.parallelFor<kernel0>(img.shape(), [=] (fdb::vec2S id) {
+            imgAxr(id[0], id[1]) = (id[0] + id[1]) % 2;
         });
     });
 
     {
         auto imgAxr = img.accessor<fdb::Access::read>(fdb::host);
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 16; j++) {
-                printf("%f\n", imgAxr(i, j));
+        for (int j = 0; j < 16; j++) {
+            for (int i = 0; i < 16; i++) {
+                printf(" %.3f", imgAxr(i, j));
             }
+            printf("\n");
         }
     }
 }
