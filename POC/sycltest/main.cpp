fb97d4c21e1d5dfc11988a998b49f229aa60bb0e
archibate <1931127624@qq.com>
2021-09-14 13:31:50 +0800

    castit

--- POC/sycltest/main.cpp
+++ POC/sycltest/main.cpp
@@ -3,82 +3,6 @@
 #include "sycl_sink.h"
 
 
-namespace fdb {
-
-template <class T>
-struct ExtensibleArray {
-    NDArray<T> m_arr;
-    size_t m_size = 0;
-
-    ExtensibleArray(size_t n, T *data = nullptr)
-        : m_arr(n, data)
-        , m_size(n)
-    {}
-
-    template <class ArrAxr>
-    struct _Accessor {
-        ArrAxr m_arrAxr;
-
-        _Accessor(ArrAxr &&arrAxr)
-            : m_arrAxr(std::move(arrAxr))
-        {}
-
-        using ReferenceT = typename ArrAxr::ReferenceT;
-
-        inline ReferenceT operator[](size_t index) const {
-            return m_arrAxr[index];
-        }
-
-        inline ReferenceT operator()(size_t index) const {
-            return operator[](index);
-        }
-    };
-
-    template <auto Mode = Access::read_write, class Handler>
-    auto accessor(Handler hand) {
-        auto arrAxr = m_arr.template accessor<Mode>(hand);
-        return _Accessor<std::decay_t<decltype(arrAxr)>>(std::move(arrAxr));
-    }
-
-    size_t size() const {
-        return m_size;
-    }
-
-    size_t capacity() const {
-        return m_arr.shape();
-    }
-
-    void __recapacity(size_t n) {
-        auto old_buffer = std::move(m_arr.m_buffer);
-        m_arr.reshape(n);
-        __partial_memcpy(m_arr.m_buffer, old_buffer, m_size);
-    }
-
-    void reserve(size_t n) {
-        if (n > capacity()) {
-            __recapacity(n);
-        }
-    }
-
-    void shrink_to_fit() {
-        if (capacity() > m_size) {
-            __recapacity(m_size);
-        }
-    }
-
-    void resize(size_t n) {
-        reserve(n);
-        m_size = n;
-    }
-
-    void clear() {
-        m_size = 0;
-    }
-};
-
-}
-
-
 using namespace fdb;
 
 
@@ -88,7 +12,9 @@ class kernel0;
 int main() {
     ExtensibleArray<float> arr(4);
 
+    auto b = arr.clone();
     arr.resize(16);
+    arr = b;
 
     fdb::enqueue([&] (fdb::DeviceHandler dev) {
         auto arrAxr = arr.accessor<fdb::Access::discard_write>(dev);
--- POC/sycltest/sycl_sink.h
+++ POC/sycltest/sycl_sink.h
@@ -52,6 +52,44 @@ enum class Access {
 };
 
 
+template <class T>
+class __partial_memcpy_kernel;
+
+template <class T>
+static void __partial_memcpy
+        ( sycl::buffer<T, 1> &dst
+        , sycl::buffer<T, 1> &src
+        , size_t n
+        ) {
+    enqueue([&] (fdb::DeviceHandler dev) {
+        auto dstAxr = dst.template get_access<sycl::access::mode::write>(*dev.m_cgh);
+        auto srcAxr = src.template get_access<sycl::access::mode::read>(*dev.m_cgh);
+        dev.parallelFor<__partial_memcpy_kernel<T>, 1>(n, [=] (size_t id) {
+            dstAxr[id] = srcAxr[id];
+        });
+    });
+}
+
+template <class T>
+class __fully_memcpy_kernel;
+
+template <class T, size_t Dim>
+static void __fully_memcpy
+        ( sycl::buffer<T, Dim> &dst
+        , sycl::buffer<T, Dim> &src
+        , vec<Dim, size_t> shape
+        ) {
+    enqueue([&] (fdb::DeviceHandler dev) {
+        auto dstAxr = dst.template get_access<sycl::access::mode::discard_write>(*dev.m_cgh);
+        auto srcAxr = src.template get_access<sycl::access::mode::read>(*dev.m_cgh);
+        dev.parallelFor<__fully_memcpy_kernel<T>, Dim>(shape, [=] (vec<Dim, size_t> idx) {
+            auto id = vec_to_other<sycl::id<Dim>>(idx);
+            dstAxr[id] = srcAxr[id];
+        });
+    });
+}
+
+
 template <class T, size_t Dim = 1>
 struct NDArray {
     static_assert(Dim > 0);
@@ -74,6 +112,13 @@ struct NDArray {
         m_buffer = sycl::buffer<T, Dim>(
                 (T *)nullptr, vec_to_other<sycl::range<Dim>>(shape));
         m_shape = shape;
+    }
+
+    NDArray clone() const {
+        NDArray ret(m_shape);
+        __fully_memcpy<T, Dim>(ret.m_buffer, const_cast<
+                sycl::buffer<T, Dim> &>(m_buffer), m_shape);
+        return ret;
     }
 
     template
@@ -116,22 +161,85 @@ struct NDArray {
 
 
 template <class T>
-class __partial_memcpy_kernel;
+struct ExtensibleArray {
+    NDArray<T> m_arr;
+    size_t m_size = 0;
 
-template <class T>
-static void __partial_memcpy
-        ( sycl::buffer<T, 1> &dst
-        , sycl::buffer<T, 1> &src
-        , size_t n
-        ) {
-    enqueue([&] (fdb::DeviceHandler dev) {
-        auto dstAxr = dst.template get_access<sycl::access::mode::write>(*dev.m_cgh);
-        auto srcAxr = src.template get_access<sycl::access::mode::read>(*dev.m_cgh);
-        dev.parallelFor<__partial_memcpy_kernel<T>, 1>(n, [=] (size_t id) {
-            dstAxr[id] = srcAxr[id];
-        });
-    });
-}
+    ExtensibleArray(NDArray<T> &&arr)
+        : m_arr(std::move(arr))
+        , m_size(m_arr.shape())
+    {}
+
+    ExtensibleArray(size_t n, T *data = nullptr)
+        : m_arr(n, data)
+        , m_size(n)
+    {}
+
+    ExtensibleArray clone() const {
+        ExtensibleArray ret(m_arr.clone());
+        return ret;
+    }
+
+    template <class ArrAxr>
+    struct _Accessor {
+        ArrAxr m_arrAxr;
+
+        _Accessor(ArrAxr &&arrAxr)
+            : m_arrAxr(std::move(arrAxr))
+        {}
+
+        using ReferenceT = typename ArrAxr::ReferenceT;
+
+        inline ReferenceT operator[](size_t index) const {
+            return m_arrAxr[index];
+        }
+
+        inline ReferenceT operator()(size_t index) const {
+            return operator[](index);
+        }
+    };
+
+    template <auto Mode = Access::read_write, class Handler>
+    auto accessor(Handler hand) {
+        auto arrAxr = m_arr.template accessor<Mode>(hand);
+        return _Accessor<std::decay_t<decltype(arrAxr)>>(std::move(arrAxr));
+    }
+
+    size_t size() const {
+        return m_size;
+    }
+
+    size_t capacity() const {
+        return m_arr.shape();
+    }
+
+    void __recapacity(size_t n) {
+        auto old_buffer = std::move(m_arr.m_buffer);
+        m_arr.reshape(n);
+        __partial_memcpy(m_arr.m_buffer, old_buffer, m_size);
+    }
+
+    void reserve(size_t n) {
+        if (n > capacity()) {
+            __recapacity(n);
+        }
+    }
+
+    void shrink_to_fit() {
+        if (capacity() > m_size) {
+            __recapacity(m_size);
+        }
+    }
+
+    void resize(size_t n) {
+        reserve(n);
+        m_size = n;
+    }
+
+    void clear() {
+        m_size = 0;
+    }
+};
 
 
 }
