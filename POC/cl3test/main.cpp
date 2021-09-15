3aed8c8ecac4ad40135d694a31caf2138e2c5fc2
archibate <1931127624@qq.com>
2021-09-15 10:21:04 +0800

    defl

--- POC/cl3test/main.cpp
+++ POC/cl3test/main.cpp
@@ -1,17 +1,22 @@
 #if !defined(__OPENCL__)
 #include <cstdio>
+#include <CL/opencl.hpp>
 #endif
 
 #if defined(__OPENCL__)
 __kernel void test() {
-    //printf("hello from kernel!\n");
+    printf("hello from kernel!\n");
 }
 #endif
 
 #if !defined(__OPENCL__)
 int main() {
-    printf("hello from the host!\n");
-    test();
+    cl::Context context = cl::Context::getDefault();
+    std::vector<cl::Device> devices = {cl::Device::getDefault()};
+    cl::Program::Binaries binaries;
+    std::vector<unsigned char> spir;
+    binaries.push_back(spir);
+    cl::Program program(context, devices, binaries);
     return 0;
 }
 #endif
