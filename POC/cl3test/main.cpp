8c3b767bca1e81649c00393ba863c0a2efb2065f
archibate <1931127624@qq.com>
2021-09-15 10:09:23 +0800

    no std hdrs

--- POC/cl3test/main.cpp
+++ POC/cl3test/main.cpp
@@ -1,12 +1,14 @@
+#if !defined(__OPENCL__)
 #include <cstdio>
+#endif
 
-#if defined(__OPENCL_VERSION__)
+#if defined(__OPENCL__)
 __kernel void test() {
-    printf("hello from kernel!\n");
+    //printf("hello from kernel!\n");
 }
 #endif
 
-#if !defined(__OPENCL_VERSION__)
+#if !defined(__OPENCL__)
 int main() {
     printf("hello from the host!\n");
     test();
