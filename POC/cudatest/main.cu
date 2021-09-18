4747760dc4df332624014a86888370c712d41c5b
archibate <1931127624@qq.com>
2021-09-19 07:33:22 +0800

    cudatest

---
+++ POC/cudatest/main.cu
@@ -0,0 +1,37 @@
+// https://www.slideserve.com/lars/3d-simulation-of-particle-motion-in-lid-driven-cavity-flow-by-mrt-lbm
+#include "helper_cuda.h"
+#include "helper_math.h"
+#include <cstdio>
+
+template <typename T>
+struct Allocator {
+    T *allocate(size_t n) {
+        T *p;
+        checkCudaErrors(cudaMallocManaged(&p, n * sizeof(T)));
+    }
+
+    void deallocate(T *p) {
+        checkCudaErrors(cudaFree(p));
+    }
+};
+
+template <class Kernel>
+__global__ void __launch(Kernel kernel) {
+    printf("prep launch\n");
+    kernel();
+}
+
+template <class Kernel>
+void launch(Kernel kernel) {
+    __launch<<<1, 1>>>([=] __device__ () {
+        kernel();
+    });
+};
+
+int main() {
+    launch([=] __device__ () {
+        printf("hello, world!\n");
+    });
+
+    return 0;
+}
