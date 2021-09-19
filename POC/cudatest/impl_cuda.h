c51b49f51caf0db679a2a29c937d781b1385a557
archibate <1931127624@qq.com>
2021-09-19 21:38:28 +0800

    zyx;

--- POC/cudatest/impl_cuda.h
+++ POC/cudatest/impl_cuda.h
@@ -32,6 +32,37 @@ static void parallelFor(vec3S grid_dim, vec3S block_dim, Kernel kernel) {
     });
 }
 
+template <size_t block_width = 1024, size_t grid_dim_scale = 1, class Kernel>
+static void parallelFor(vec3S dim, Kernel kernel) {
+    vec3S block_dim = clamp(dim, 1, max(1, block_width));
+    vec3S grid_dim = (dim + block_dim - 1) / (block_dim * max(1, grid_dim_scale));
+    dim3 gridDim(grid_dim[0], grid_dim[1], grid_dim[2]);
+    dim3 blockDim(block_dim[0], block_dim[1], block_dim[2]);
+    __parallelFor<<<gridDim, blockDim>>>([=] __device__ () {
+        for (size_t z = blockDim.z * blockIdx.z + threadIdx.z; z < dim[2]; z += gridDim.z * blockDim.z) {
+            for (size_t y = blockDim.y * blockIdx.y + threadIdx.y; y < dim[1]; y += gridDim.y * blockDim.y) {
+                for (size_t x = blockDim.x * blockIdx.x + threadIdx.x; x < dim[0]; x += gridDim.x * blockDim.x) {
+                    vec3S idx(x, y, z);
+                    kernel(idx);
+                }
+            }
+        }
+    });
+}
+
+template <size_t block_width = 1024, size_t grid_dim_scale = 1, class Kernel>
+static void parallelFor(size_t dim, Kernel kernel) {
+    size_t block_dim = clamp(dim, 1, max(1, block_width));
+    size_t grid_dim = (dim + block_dim - 1) / (block_dim * max(1, grid_dim_scale));
+    dim3 gridDim(grid_dim, 1, 1);
+    dim3 blockDim(block_dim, 1, 1);
+    __parallelFor<<<gridDim, blockDim>>>([=] __device__ () {
+        for (size_t x = blockDim.x * blockIdx.x + threadIdx.x; x < dim[0]; x += gridDim.x * blockDim.x) {
+            kernel(x);
+        }
+    });
+}
+
 static void synchronize() {
     checkCudaErrors(cudaDeviceSynchronize());
 }
--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -9,6 +9,9 @@ int main() {
     parallelFor(vec3S(1, 1, 1), vec3S(1, 1, 4), [=] FDB_DEVICE (vec3S block_idx, vec3S thread_idx) {
         printf("hello, world! %ld\n", thread_idx[2]);
     });
+    parallelFor(vec3S(2, 2, 4), [=] FDB_DEVICE (vec3S idx) {
+        printf("are you ok? %ld %ld %ld\n", idx[0], idx[1], idx[2]);
+    });
     synchronize();
     return 0;
 }
