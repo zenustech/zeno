68064e6ba46ec4bc9220e4b2471218019bb9a911
archibate <1931127624@qq.com>
2021-09-19 21:21:23 +0800

    static

--- POC/cudatest/impl_cuda.h
+++ POC/cudatest/impl_cuda.h
@@ -6,23 +6,23 @@
 
 namespace fdb {
 
-void *allocate(size_t n) {
+static void *allocate(size_t n) {
     void *p = nullptr;
     checkCudaErrors(cudaMallocManaged(&p, n));
     return p;
 }
 
-void deallocate(void *p) {
+static void deallocate(void *p) {
     checkCudaErrors(cudaFree(p));
 }
 
 template <class Kernel>
-__global__ void __parallelFor(Kernel kernel) {
+static __global__ void __parallelFor(Kernel kernel) {
     kernel();
 }
 
 template <class Kernel>
-void parallelFor(vec3S grid_dim, vec3S block_dim, Kernel kernel) {
+static void parallelFor(vec3S grid_dim, vec3S block_dim, Kernel kernel) {
     dim3 gridDim(grid_dim[0], grid_dim[1], grid_dim[2]);
     dim3 blockDim(block_dim[0], block_dim[1], block_dim[2]);
     __parallelFor<<<gridDim, blockDim>>>([=] __device__ () {
@@ -30,6 +30,10 @@ void parallelFor(vec3S grid_dim, vec3S block_dim, Kernel kernel) {
         vec3S thread_idx(threadIdx.x, threadIdx.y, threadIdx.z);
         kernel(block_idx, thread_idx);
     });
+}
+
+static void synchronize() {
+    checkCudaErrors(cudaDeviceSynchronize());
 }
 
 #define FDB_DEVICE __device__
--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -7,9 +7,8 @@ __global__ void a() { printf("a\n"); }
 
 int main() {
     parallelFor(vec3S(1, 1, 1), vec3S(1, 1, 4), [=] FDB_DEVICE (vec3S block_idx, vec3S thread_idx) {
-        printf("hello, world! %d\n", thread_idx[2]);
+        printf("hello, world! %ld\n", thread_idx[2]);
     });
-
-    checkCudaErrors(cudaDeviceSynchronize());
+    synchronize();
     return 0;
 }
