63df3d9b832aa11889b98689fc8a6a103583e96a
archibate <1931127624@qq.com>
2021-09-20 06:46:48 +0800

    hashgrid u64_3x21?

--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -6,6 +6,55 @@
 
 using namespace fdb;
 
+template <class T>
+struct HashGrid {
+    struct u64_3x21 {
+        uint64_t value;
+
+        u64_3x21() : value((uint64_t)-1l) {}
+
+        FDB_CONSTEXPR u64_3x21(vec3S const &a) {
+            uint64_t x = a[0] & 0x1ffffful;
+            uint64_t y = a[1] & 0x1ffffful;
+            uint64_t z = a[2] & 0x1ffffful;
+            value = x | y << 21 | z << 42;
+        }
+
+        FDB_CONSTEXPR bool has_value() const {
+            return (int64_t)value >= 0l;
+        }
+
+        FDB_CONSTEXPR operator vec3S() const {
+            uint64_t x = value & 0x1ffffful;
+            uint64_t y = (value >> 21) & 0x1ffffful;
+            uint64_t z = (value >> 42) & 0x1ffffful;
+            return vec3S(x, y, z);
+        }
+    };
+
+    HashMap<u64_3x21, T> m_table;
+
+    struct View {
+        HashMap<u64_3x21, T> m_view;
+
+        template <class Kernel>
+        inline void parallel_foreach(Kernel kernel, ParallelConfig cfg = {512, 2}) const {
+            m_view.parallel_foreach([=] FDB_DEVICE (u64_3x21 key, T &value) {
+                vec3S coord(key);
+                kernel(std::as_const(coord), value);
+            }, cfg);
+        }
+
+        View(HashGrid const &parent)
+            : m_view(parent.view())
+        {}
+    };
+
+    inline View view() const {
+        return *this;
+    }
+};
+
 int main() {
 #if 1
     HashMap<int, int> a;
