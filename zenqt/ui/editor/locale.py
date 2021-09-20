ba23c16cf82c45ee28716208e80f951b3bad632b
archibate <1931127624@qq.com>
2021-09-20 17:26:10 +0800

    add translations

--- zenqt/ui/editor/locale.py
+++ zenqt/ui/editor/locale.py
@@ -1,6 +1,17 @@
-zh = {
-    'prim': 'Primitive',
-}
+from . import *
+
+translation = {}
+
+@eval('lambda x: x()')
+def load_translation():
+    translation.clear()
+    with open(asset_path('zh-cn.txt'), 'r') as f:
+        for line in f.readlines():
+            try:
+                en, zh = line.split(' ', maxsplit=1)
+            except ValueError:
+                pass
+            translation[en.lower()] = zh
 
 def translate(x):
-    return zh.get(x, x)
+    return translation.get(x.lower(), x)
