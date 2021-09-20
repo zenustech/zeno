60c147c28bbba478fde27e7c825ef26730fd8153
archibate <1931127624@qq.com>
2021-09-20 21:26:00 +0800

    worked

--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -1,9 +1,11 @@
 #include <cstdio>
-//#include "impl_cuda.h"
-#include "impl_host.h"
+#include "impl_cuda.h"
+//#include "impl_host.h"
 #include "HashTiledListGrid.h"
 
 using namespace fdb;
+
+__managed__ int count = 0;
 
 int main() {
     HashTiledListGrid<int> a;
@@ -16,8 +18,10 @@ int main() {
 
     av.parallel_foreach([=] FDB_DEVICE (vec3i c, int &val) {
         printf("%d %d %d = %d\n", c[0], c[1], c[2], val);
+        atomic_add(&count, val);
     });
 
     synchronize();
+    printf("%d\n", count);
     return 0;
 }
