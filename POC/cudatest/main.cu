ee3b2b5d32368da3715d86c016aab23964dd688a
archibate <1931127624@qq.com>
2021-09-20 16:11:12 +0800

    bt

--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -8,17 +8,17 @@ using namespace fdb;
 
 int main() {
 #if 1
-    H21B3_Grid<float> a;
-    a.reserve_blocks(4095);
+    HashMap<int, int> a;
+    a.reserve(16);
     {
         auto av = a.view();
-        parallel_for(vec3i(16, 8, 2), [=] FDB_DEVICE (vec3i c) {
-            av[c] = length(vcast<float>(c));
+        parallel_for(32, [=] FDB_DEVICE (size_t i) {
+            av[i / 2] = i;
         });
 
-        /*av.parallel_foreach([=] FDB_DEVICE (vec3i c, float &v) {
-            printf("%d %d %d %f %f\n", c[0], c[1], c[2], v, length(vcast<float>(c)));
-        });*/
+        av.parallel_foreach([=] FDB_DEVICE (size_t i, int &val) {
+            printf("%ld %d\n", i, val);
+        });
     }
 
 #else
