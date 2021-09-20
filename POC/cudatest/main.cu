f507f7503ecf3ab18f54275c5788bdfeaa751bad
archibate <1931127624@qq.com>
2021-09-20 23:34:15 +0800

    reo

--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -20,10 +20,19 @@ int main() {
         av.append(vec3i((c * 114514 + 31415) % 8, 0, 0)) = c;
     });
 
+#if 0
     av.parallel_foreach([=] FDB_DEVICE (vec3i c, int &val) {
         printf("%d = %d\n", c[0], val);
         atomic_add(&count, 1);
     });
+#else
+    av.parallel_foreach_leaf([=] FDB_DEVICE (vec3i coord, auto &leaf) {
+        leaf.foreach([&] (int &val) {
+            printf("%d = %d\n", coord[0], val);
+            atomic_add(&count, 1);
+        });
+    });
+#endif
 
     synchronize();
     printf("%d = %d\n", n, count);
