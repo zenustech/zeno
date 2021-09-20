13b6b2a74f0e123ead4046f9c1d1fcb27340f3a0
archibate <1931127624@qq.com>
2021-09-21 00:00:18 +0800

    stmbrk

--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -13,21 +13,22 @@ __managed__ int count = 0;
 int main() {
     const int n = 8192 * 2;
     HashListGrid<int> a;
-    a.reserve_blocks(n / 4);
+    a.reserve_blocks(16);
 
     auto av = a.view();
-    parallel_for(n, [=] FDB_DEVICE (size_t c) {
-        av.append(vec3i((c * 114514 + 31415) % 8, 0, 0), c);
+    parallel_for(n, [=] FDB_DEVICE (size_t i) {
+        vec3i coord((i * 114514 + 31415) % 8, 0, 0);
+        av.append(coord, i);
     });
 
 #if 0
-    av.parallel_foreach([=] FDB_DEVICE (vec3i c, int &val) {
-        printf("%d = %d\n", c[0], val);
+    av.parallel_foreach([=] FDB_DEVICE (vec3i coord, int &val) {
+        printf("%d = %d\n", coord[0], val);
         atomic_add(&count, 1);
     });
 #else
     av.parallel_foreach_leaf([=] FDB_DEVICE (vec3i coord, auto &leaf) {
-        leaf.foreach([&] (int val) {
+        leaf.foreach_load([&] (int val) {
             printf("%d = %d\n", coord[0], val);
             atomic_add(&count, 1);
         });
