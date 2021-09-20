7a8ab0f6a9dca5519b0ad5258fa1c21eb330d1a6
archibate <1931127624@qq.com>
2021-09-20 21:33:07 +0800

    s

--- POC/cudatest/main.cu
+++ POC/cudatest/main.cu
@@ -1,27 +1,33 @@
 #include <cstdio>
+#if 1
 #include "impl_cuda.h"
-//#include "impl_host.h"
+#else
+#include "impl_host.h"
+#endif
 #include "HashTiledListGrid.h"
 
 using namespace fdb;
 
+__managed__ int total = 0;
 __managed__ int count = 0;
 
 int main() {
-    HashTiledListGrid<int, 8> a;
-    a.reserve_blocks(4096);
+    HashTiledListGrid<int, 32> a;
+    a.reserve_blocks(166444);
 
     auto av = a.view();
-    parallel_for(vec3S(2, 2, 16), [=] FDB_DEVICE (vec3i c) {
-        *av.append(c / 2) = c[0] + c[1] * 2 + c[2] * 4;
+    parallel_for(vec3S(64, 32, 16), [=] FDB_DEVICE (vec3i c) {
+        *av.append(c) = c[0] + c[1] * 2 + c[2] * 4;
     });
 
     av.parallel_foreach([=] FDB_DEVICE (vec3i c, int &val) {
         printf("%d %d %d = %d\n", c[0], c[1], c[2], val);
-        atomic_add(&count, val);
+        atomic_add(&total, val);
+        atomic_add(&count, 1);
     });
 
     synchronize();
-    printf("2016 = %d\n", count);
+    printf("3031040 = %d\n", total);
+    printf("32768 = %d\n", count);
     return 0;
 }
