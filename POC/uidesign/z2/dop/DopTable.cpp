42512e73825c1fd9f7286d04578387c1a320d096
archibate <1931127624@qq.com>
2021-10-02 16:52:31 +0800

    rwbd

--- POC/uidesign/z2/dop/DopTable.cpp
+++ POC/uidesign/z2/dop/DopTable.cpp
@@ -8,22 +8,23 @@ namespace z2::dop {
 DopTable tab;
 
 
-static int def_readvdb = tab.define("readvdb", {
-    {
-        {"path"},
-        {"type"},
-    },
-    {
-        {"grid"},
-    },
-}, [] (auto *node, auto *visited) {
+static int def_readvdb = tab.define("readvdb", {{
+    {"path"},
+    {"type"},
+}, {
+    {"grid"},
+}}, [] (auto *node, auto *visited) {
     printf("readvdb out[0]\n");
     node->set_output(0, 1024);
 });
 
 
-static int def_vdbsmooth = tab.define("vdbsmooth", {
-}, [] (auto *node, auto *visited) {
+static int def_vdbsmooth = tab.define("vdbsmooth", {{
+    {"grid"},
+    {"type"},
+}, {
+    {"grid"},
+}}, [] (auto *node, auto *visited) {
     auto grid = std::any_cast<int>(node->get_input(0, visited));
     auto type = node->get_input(1, visited);
     grid += 1;
@@ -32,8 +33,12 @@ static int def_vdbsmooth = tab.define("vdbsmooth", {
 });
 
 
-static int def_vdberode = tab.define("vdberode", {
-}, [] (auto *node, auto *visited) {
+static int def_vdberode = tab.define("vdberode", {{
+    {"grid"},
+    {"type"},
+}, {
+    {"grid"},
+}}, [] (auto *node, auto *visited) {
     auto grid = std::any_cast<int>(node->get_input(0, visited));
     auto type = node->get_input(1, visited);
     grid -= 3;
@@ -42,8 +47,12 @@ static int def_vdberode = tab.define("vdberode", {
 });
 
 
-static int def_repeat = tab.define("repeat", {
-}, [] (auto *node, auto *visited) {
+static int def_repeat = tab.define("repeat", {{
+    {"grid"},
+    {"type"},
+}, {
+    {"grid"},
+}}, [] (auto *node, auto *visited) {
     printf("repeat out[0]\n");
     for (int i = 0; i < 4; i++) {
         auto saved_visited = *visited;
