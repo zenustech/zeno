dd111ac4f8956c74b748975405fc72abcddbb1df
archibate <1931127624@qq.com>
2021-10-02 12:57:35 +0800

    use anycast

--- POC/uidesign/zui/DopTable.cpp
+++ POC/uidesign/zui/DopTable.cpp
@@ -13,15 +13,18 @@ static int def_readvdb = tab.define("readvdb", [] (auto const &in, auto &out) {
 
 static int def_vdbsmooth = tab.define("vdbsmooth", [] (auto const &in, auto &out) {
     out[0] = [=] () -> std::any {
-        auto grid = in[0]();
-        printf("vdbsmooth out[0]\n");
-        return 1024;
+        auto grid = std::any_cast<int>(in[0]());
+        grid += 1;
+        printf("vdbsmooth out[0] %d\n", grid);
+        return grid;
     };
 });
 
 static int def_vdberode = tab.define("vdberode", [] (auto const &in, auto &out) {
     out[0] = [=] () -> std::any {
-        printf("vdberode out[0]\n");
-        return 1024;
+        auto grid = std::any_cast<int>(in[0]());
+        grid -= 3;
+        printf("vdberode out[0] %d\n", grid);
+        return grid;
     };
 });
