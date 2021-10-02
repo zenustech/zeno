b045839c1208646d347f539f7cefe529e4f41e8d
archibate <1931127624@qq.com>
2021-10-02 11:32:07 +0800

    segf

--- POC/uidesign/zui/DopTable.cpp
+++ POC/uidesign/zui/DopTable.cpp
@@ -5,16 +5,23 @@ DopTable tab;
 
 
 static int def_readvdb = tab.define("readvdb", [] (DopContext *ctx) {
-    printf("readvdb\n");
+    ctx->out[0] = [=] () -> std::any {
+        printf("readvdb out[0]\n");
+        return 1024;
+    };
 });
 
 static int def_vdbsmooth = tab.define("vdbsmooth", [] (DopContext *ctx) {
-    printf("vdbsmooth\n");
     ctx->out[0] = [=] () -> std::any {
+        auto grid = ctx->in[0]();
+        printf("vdbsmooth out[0]\n");
         return 1024;
     };
 });
 
 static int def_vdberode = tab.define("vdberode", [] (DopContext *ctx) {
-    printf("vdberode\n");
+    ctx->out[0] = [=] () -> std::any {
+        printf("vdberode out[0]\n");
+        return 1024;
+    };
 });
