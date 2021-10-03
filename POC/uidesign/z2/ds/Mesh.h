63e26ae5048e4ce7fc5e147cbf5f969c8c395cef
archibate <1931127624@qq.com>
2021-10-03 15:15:59 +0800

    readobj with uv

--- POC/uidesign/z2/ds/Mesh.h
+++ POC/uidesign/z2/ds/Mesh.h
@@ -11,11 +11,10 @@ namespace z2::ds {
 struct Mesh {
     // points
     std::vector<ztd::vec3f> vert;
-    std::vector<ztd::vec2f> uv_vert;
 
     // face corners
     std::vector<int> loop;
-    std::vector<int> uv_loop;
+    std::vector<ztd::vec2f> loop_uv;
 
     // faces
     std::vector<ztd::vec2i> poly;
