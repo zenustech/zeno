f6ab65bbdd91a3723c64d02504fc1aad8600e8ee
archibate <1931127624@qq.com>
2021-10-06 23:08:45 +0800

    204

--- POC/nodedesign/main.cpp
+++ POC/nodedesign/main.cpp
@@ -9,7 +9,7 @@ struct Node {
     std::string kind;
     int xorder = 0;
     std::vector<int> deps;
-    std::any value = (int)0;
+    std::any value = (int)3;
 };
 
 
@@ -44,28 +44,36 @@ void sortexec(std::vector<int> &tolink, std::set<int> &visited) {
     }
 }
 
-std::any resolve(int idx);
+std::any resolve(int idx, std::set<int> &visited);
 
 void touch(int idx, std::vector<int> &tolink, std::set<int> &visited) {
     if (idx == -1) return;
+
     if (nodes[idx].kind == "if") {
-        auto cond = resolve(nodes[idx].deps[0]);
-        if (std::any_cast<int>(cond)) {
+        auto cond = std::any_cast<int>(resolve(nodes[idx].deps[0], visited));
+        if (cond) {
             return touch(nodes[idx].deps[1], tolink, visited);
         } else {
             return touch(nodes[idx].deps[2], tolink, visited);
         }
+
+    } else if (nodes[idx].kind == "for") {
+        auto cond = std::any_cast<int>(resolve(nodes[idx].deps[0], visited));
+        for (int i = 0; i < cond; i++) {
+            auto tmp_visited = visited;
+            resolve(nodes[idx].deps[1], tmp_visited);
+        }
     }
+
     for (auto dep: nodes[idx].deps) {
         touch(dep, tolink, visited);
     }
     tolink.push_back(idx);
 }
 
-std::any resolve(int idx) {
+std::any resolve(int idx, std::set<int> &visited) {
     if (idx == -1) return {};
     std::vector<int> tolink;
-    std::set<int> visited;
     touch(idx, tolink, visited);
     sortexec(tolink, visited);
     return nodes[idx].value;
@@ -76,9 +84,10 @@ int main() {
     nodes[0] = {"float", 100, {}};
     nodes[1] = {"float", 200, {}};
     nodes[2] = {"float", 300, {}};
-    nodes[3] = {"iff", 400, {2, 0, 1}};
+    nodes[3] = {"if", 400, {2, 0, 1}};
     nodes[4] = {"test", 500, {3, 0}};
 
-    resolve(4);
+    std::set<int> visited;
+    resolve(4, visited);
     return 0;
 }
