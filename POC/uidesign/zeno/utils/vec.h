7f691dd0f3f72d4c300415bd00104eed82a21102
archibate <1931127624@qq.com>
2021-10-07 14:46:38 +0800

    add more utils

--- POC/uidesign/zeno/utils/vec.h
+++ POC/uidesign/zeno/utils/vec.h
@@ -1,21 +1,12 @@
 #pragma once
 
-#include <z2/ztd/vec.h>
+#include <z2/ztd/random.h>
 
 
 namespace zeno {
 
-template <size_t N, class T>
-using vec = z2::ztd::vec<N, T>;
-
-using z2::ztd::vec2f;
-using z2::ztd::vec3f;
-using z2::ztd::vec4f;
-using z2::ztd::vec2i;
-using z2::ztd::vec3i;
-using z2::ztd::vec4i;
-using z2::ztd::vec2I;
-using z2::ztd::vec3I;
-using z2::ztd::vec4I;
+    using z2::ztd::irand;
+    using z2::ztd::frand;
+    using z2::ztd::srand;
 
 }
