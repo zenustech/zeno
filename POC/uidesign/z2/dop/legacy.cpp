668190ad59b148c3d181153ab3ac64ebe000d240
archibate <1931127624@qq.com>
2021-10-07 14:11:27 +0800

    cihou 2

---
+++ POC/uidesign/z2/dop/legacy.cpp
@@ -0,0 +1,27 @@
+#include <z2/dop/legacy.h>
+#include <z2/dop/execute.h>
+
+
+namespace z2::dop::legacy {
+
+
+std::any INode::get_input2(std::string const &name) const {
+    for (int i = 0; i < desc->inputs.size(); i++) {
+        if (desc->inputs[i].name == name) {
+            return Node::get_input(i);
+        }
+    }
+    return {};
+}
+
+
+void INode::set_output2(std::string const &name, std::any &&val) {
+    for (int i = 0; i < desc->inputs.size(); i++) {
+        if (desc->outputs[i].name == name) {
+            return Node::set_output(i, std::move(val));
+        }
+    }
+}
+
+
+}
