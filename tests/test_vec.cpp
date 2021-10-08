c6e9dbc4cc2e13f4a0bdd760f057754e3bfe7275
archibate <1931127624@qq.com>
2021-10-08 16:17:27 +0800

    test vall

--- tests/test_vec.cpp
+++ tests/test_vec.cpp
@@ -4,8 +4,9 @@
 namespace zeno2 {
 
 TEST(test_vec, test_clamp) {
-    ztd::vec3f x;
-    x = ztd::clamp(x, 0, 1);
+    ztd::vec3f x(3.14f, -1.2f, 0.2f);
+    x = clamp(x, -0.5f, 1);
+    EXPECT_TRUE(vall(x == ztd::vec3f(1.0f, -0.5f, 0.2f)));
 }
 
 }
