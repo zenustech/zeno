504ea6812cfbae3d46294f20793e063c45fba1bf
archibate <1931127624@qq.com>
2021-10-09 12:00:31 +0800

    spdlog critical

--- core/zeno2/dop/execute.h
+++ core/zeno2/dop/execute.h
@@ -8,15 +8,10 @@ namespace zeno2::dop {
 
 
 ztd::zany getval(Input const &input);
+ztd::zany resolve(Input const &input);
 ztd::zany resolve(Input const &input, std::set<Node *> &visited);
 void touch(Input const &input, std::vector<Node *> &tolink, std::set<Node *> &visited);
 void sortexec(Node *root, std::vector<Node *> &tolink, std::set<Node *> &visited);
-
-
-inline ztd::zany resolve(Input const &input) {
-    std::set<Node *> visited;
-    return resolve(input, visited);
-}
 
 
 }
