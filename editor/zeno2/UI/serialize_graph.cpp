d57eadea59b7cfe072b0aa2ee82878485aea72e6
archibate <1931127624@qq.com>
2021-10-10 08:16:37 +0800

    add deserializer

--- editor/zeno2/UI/serialize_graph.cpp
+++ editor/zeno2/UI/serialize_graph.cpp
@@ -27,6 +27,8 @@ std::string UiDopGraph::serialize_graph() {
         Value v_node(kObjectType);
         v_node.AddMember("name", dump_string(node->name), doc.GetAllocator());
         v_node.AddMember("kind", dump_string(node->kind), doc.GetAllocator());
+        v_node.AddMember("xpos", Value().SetFloat(node->position.x), doc.GetAllocator());
+        v_node.AddMember("ypos", Value().SetFloat(node->position.y), doc.GetAllocator());
 
         Value v_inputs(kArrayType);
         for (int i = 0; i < node->inputs.size(); i++) {
@@ -59,7 +61,7 @@ std::string UiDopGraph::serialize_graph() {
         v_link.AddMember("from_node", dump_string(from_node->name), doc.GetAllocator());
         for (int i = 0; i < from_node->outputs.size(); i++) {
             if (from_node->outputs[i] == link->from_socket) {
-                v_link.AddMember("from_id", Value().SetInt(i), doc.GetAllocator());
+                v_link.AddMember("from_socket", Value().SetInt(i), doc.GetAllocator());
                 break;
             }
         }
@@ -68,7 +70,7 @@ std::string UiDopGraph::serialize_graph() {
         v_link.AddMember("to_node", dump_string(to_node->name), doc.GetAllocator());
         for (int i = 0; i < to_node->inputs.size(); i++) {
             if (to_node->inputs[i] == link->to_socket) {
-                v_link.AddMember("to_id", Value().SetInt(i), doc.GetAllocator());
+                v_link.AddMember("to_socket", Value().SetInt(i), doc.GetAllocator());
                 break;
             }
         }
@@ -98,20 +100,21 @@ void UiDopGraph::deserialize_graph(std::string const &buffer) {
     Document doc;
     doc.Parse(buffer.c_str());
 
+    nodes.clear();
     auto v_nodes = load_member(doc, "nodes").GetArray();
     for (auto const &_: v_nodes) {
         auto v_node = _.GetObject();
 
-        std::string kind = "Route";
-        Point pos = {0, 0};
-        kind = load_member(v_node, "kind").GetString();
-        auto node = add_node(kind, pos);
+        auto node = add_child<UiDopNode>();
+        node->kind = load_member(v_node, "kind").GetString();
         node->name = load_member(v_node, "name").GetString();
+        node->position.x = load_member(v_node, "xpos").GetFloat();
+        node->position.y = load_member(v_node, "xpos").GetFloat();
 
         auto v_inputs = load_member(v_node, "inputs").GetArray();
         for (auto const &_: v_inputs) {
             auto v_socket = _.GetObject();
-            auto socket = node->inputs.emplace_back();
+            auto socket = node->add_input_socket();
             socket->name = load_member(v_socket, "name").GetString();
             socket->value = load_member(v_socket, "value").GetString();
         }
@@ -119,10 +122,32 @@ void UiDopGraph::deserialize_graph(std::string const &buffer) {
         auto v_outputs = load_member(v_node, "outputs").GetArray();
         for (auto const &_: v_outputs) {
             auto v_socket = _.GetObject();
-            auto socket = node->outputs.emplace_back();
+            auto socket = node->add_output_socket();
             socket->name = load_member(v_socket, "name").GetString();
         }
+
+        node->update_sockets();
+        nodes.emplace(node->name, node);
     }
+
+    auto v_links = load_member(doc, "links").GetArray();
+    for (auto const &_: v_links) {
+        auto v_link = _.GetObject();
+
+        auto from_node = load_member(v_link, "from_node").GetString();
+        auto from_socket = load_member(v_link, "from_socket").GetInt();
+        auto to_node = load_member(v_link, "to_node").GetString();
+        auto to_socket = load_member(v_link, "to_socket").GetInt();
+
+        auto f_node = nodes.at(from_node);
+        auto f_sock = f_node->outputs.at(from_socket);
+        auto t_node = nodes.at(to_node);
+        auto t_sock = t_node->inputs.at(to_socket);
+
+        add_link(f_sock, t_sock);
+    }
+
+    SPDLOG_INFO("deserialize_graph done, {} nodes loaded", nodes.size());
 }
 
 
