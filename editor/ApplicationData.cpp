8bca4779d339841e2b58e2232edb1853a52d4cec
archibate <1931127624@qq.com>
2021-10-16 12:48:21 +0800

    recihou

--- editor/ApplicationData.cpp
+++ editor/ApplicationData.cpp
@@ -3,7 +3,9 @@
 
 ApplicationData::ApplicationData(QObject *parent)
     : QObject(parent) {}
+
 Q_INVOKABLE void ApplicationData::load_scene(QString str) const {
     printf("load_scene %s\n", str.toStdString().c_str());
 }
+
 ApplicationData::~ApplicationData() = default;
