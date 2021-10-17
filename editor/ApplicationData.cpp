3476092775eb229978860c16370d33edf94a88ca
archibate <1931127624@qq.com>
2021-10-17 13:52:31 +0800

    work ref wrapper

--- editor/ApplicationData.cpp
+++ editor/ApplicationData.cpp
@@ -19,12 +19,7 @@ static std::unique_ptr<zeno::dop::Graph> parse_graph(rapidjson::Value const &v_g
         return it->value;
     };
 
-    struct NodeLutData {
-        zeno::dop::Node *node_ptr{};
-        std::vector<std::string> input_exprs;
-    };
-
-    std::map<std::string, NodeLutData> nodeslut;
+    std::map<std::string, zeno::dop::Node *> nodeslut;
 
     auto graph = std::make_unique<zeno::dop::Graph>();
 
@@ -39,24 +34,27 @@ static std::unique_ptr<zeno::dop::Graph> parse_graph(rapidjson::Value const &v_g
         node->xpos = load_member(v_node, "x").GetFloat();
         node->ypos = load_member(v_node, "y").GetFloat();
 
-        auto &lutdata = nodeslut[node->name];
-        lutdata.node_ptr = node;
+        nodeslut.emplace(node->name, node);
 
         auto v_inputs = load_member(v_node, "inputs").GetArray();
         ZS_ZTD_ASSERT(v_inputs.Size() == desc.inputs.size());
-        lutdata.input_exprs.reserve(v_inputs.Size());
-        for (auto const &[v_input, desc_input]: ranges::views::zip(
-                v_inputs | ranges::views::transform([&] (auto &&x) { return x.GetObject(); }),
-                desc.inputs)) {
+        for (auto const &[v_input, node_input, desc_input]: ranges::views::zip
+                ( v_inputs | ranges::views::transform([&] (auto &&x) { return x.GetObject(); })
+                , node->inputs | ranges::views::transform([&] (auto &&x) { return std::ref(x); })
+                , desc.inputs
+                )) {
             auto name = load_member(v_input, "name").GetString();
             auto value = load_member(v_input, "value").GetString();
             ZS_ZTD_ASSERT(name == desc_input.name);
-            lutdata.input_exprs.push_back(value);
+            node_input.get() = zeno::dop::Input_Value{.value = value};
         }
 
         auto v_outputs = load_member(v_node, "outputs").GetArray();
         ZS_ZTD_ASSERT(v_outputs.Size() == desc.outputs.size());
-        for (auto const &[v_output, desc_output]: ranges::views::zip(v_outputs, desc.outputs)) {
+        for (auto const &[v_output, desc_output]: ranges::views::zip
+                ( v_outputs | ranges::views::transform([&] (auto &&x) { return x.GetObject(); })
+                , desc.outputs
+            )) {
             auto name = load_member(v_output, "name").GetString();
             ZS_ZTD_ASSERT(name == desc_output.name);
         }
@@ -71,13 +69,16 @@ static std::unique_ptr<zeno::dop::Graph> parse_graph(rapidjson::Value const &v_g
         auto dst_node = load_member(v_link, "dst_node").GetString();
         auto dst_socket = load_member(v_link, "dst_socket").GetString();
 
-        auto s_node = nodeslut.at(src_node).node_ptr;
-        auto d_node = nodeslut.at(dst_node).node_ptr;
-        auto i_input = ZS_ZTD_ASSERT(ranges::find_if(d_node->desc->inputs, [&] (auto const &i) {
-            return i.name == dst_socket;
+        auto s_node = nodeslut.at(src_node);
+        auto d_node = nodeslut.at(dst_node);
+        auto d_idx = ZS_ZTD_ASSERT(ranges::find_if(d_node->desc->inputs, [&] (auto const &_) {
+            return _.name == dst_socket;
         }), != ranges::end(d_node->desc->inputs)) - ranges::begin(d_node->desc->inputs);
+        auto s_idx = ZS_ZTD_ASSERT(ranges::find_if(d_node->desc->outputs, [&] (auto const &_) {
+            return _.name == dst_socket;
+        }), != ranges::end(d_node->desc->outputs)) - ranges::begin(d_node->desc->outputs);
 
-        graph->add_link(s_sock, d_sock);
+        d_node->inputs.at(d_idx) = zeno::dop::Input_Link{.node = s_node, .sockid = (int)s_idx};
     }
 
     return graph;
