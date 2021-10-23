4713fbbb72c25142495df51b7c8993149a75a8e1
archibate <1931127624@qq.com>
2021-10-24 06:38:33 +0800

    set fusion style

--- editor/main.cpp
+++ editor/main.cpp
@@ -4,7 +4,24 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //a.setAttribute(Qt::AA_EnableHighDpiScaling);
+    //a.setStyle("Fusion");
+
+    QPalette palette;
+    palette.setColor(QPalette::Window, QColor(53, 53, 53));
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Base, QColor(25, 25, 25));
+    palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    palette.setColor(QPalette::ToolTipBase, Qt::black);
+    palette.setColor(QPalette::ToolTipText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::Button, QColor(53, 53, 53));
+    palette.setColor(QPalette::ButtonText, Qt::white);
+    palette.setColor(QPalette::BrightText, Qt::red);
+    palette.setColor(QPalette::Link, QColor(42, 130, 218));
+    palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    palette.setColor(QPalette::HighlightedText, Qt::black);
+    a.setPalette(palette);
+
     MainWindow w;
     w.show();
     return a.exec();
