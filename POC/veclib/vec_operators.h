d9e1da439716c9f31e22bb1b0e18bf53aa1331df
archibate <1931127624@qq.com>
2021-10-25 16:44:34 +0800

    use constexpr

--- POC/veclib/vec_operators.h
+++ POC/veclib/vec_operators.h
@@ -11,12 +11,12 @@ inline namespace math {
 
 
 template <is_not_vec T1, is_not_vec T2, class Func>
-auto vec_wise(T1 const &t1, T2 const &t2, Func func) {
+constexpr auto vec_wise(T1 const &t1, T2 const &t2, Func func) {
     return func(t1, t2);
 }
 
 template <size_t N, is_not_vec T1, is_not_vec T2, class Func>
-auto vec_wise(vec<N, T1> const &t1, T2 const &t2, Func func) {
+constexpr auto vec_wise(vec<N, T1> const &t1, T2 const &t2, Func func) {
     using T0 = std::remove_cvref_t<std::invoke_result_t<Func, T1, T2>>;
     vec<N, T0> t0;
     for (size_t i = 0; i < N; i++) {
@@ -26,7 +26,7 @@ auto vec_wise(vec<N, T1> const &t1, T2 const &t2, Func func) {
 }
 
 template <size_t N, is_not_vec T1, is_not_vec T2, class Func>
-auto vec_wise(T1 const &t1, vec<N, T2> const &t2, Func func) {
+constexpr auto vec_wise(T1 const &t1, vec<N, T2> const &t2, Func func) {
     using T0 = std::remove_cvref_t<std::invoke_result_t<Func, T1, T2>>;
     vec<N, T0> t0;
     for (size_t i = 0; i < N; i++) {
@@ -36,7 +36,7 @@ auto vec_wise(T1 const &t1, vec<N, T2> const &t2, Func func) {
 }
 
 template <size_t N, is_not_vec T1, is_not_vec T2, class Func>
-auto vec_wise(vec<N, T1> const &t1, vec<N, T2> const &t2, Func func) {
+constexpr auto vec_wise(vec<N, T1> const &t1, vec<N, T2> const &t2, Func func) {
     using T0 = std::remove_cvref_t<std::invoke_result_t<Func, T1, T2>>;
     vec<N, T0> t0;
     for (size_t i = 0; i < N; i++) {
@@ -47,31 +47,31 @@ auto vec_wise(vec<N, T1> const &t1, vec<N, T2> const &t2, Func func) {
 
 template <class T1, class T2>
     requires (concepts::has_plus<remove_vec_t<T1>, remove_vec_t<T2>>)
-decltype(auto) operator+(T1 const &t1, T2 const &t2) {
+constexpr decltype(auto) operator+(T1 const &t1, T2 const &t2) {
     return vec_wise(t1, t2, [] (auto &&t1, auto &&t2) { return t1 + t2; });
 }
 
 template <class T1, class T2>
     requires (concepts::has_minus<remove_vec_t<T1>, remove_vec_t<T2>>)
-decltype(auto) operator-(T1 const &t1, T2 const &t2) {
+constexpr decltype(auto) operator-(T1 const &t1, T2 const &t2) {
     return vec_wise(t1, t2, [] (auto &&t1, auto &&t2) { return t1 - t2; });
 }
 
 template <class T1, class T2>
     requires (concepts::has_times<remove_vec_t<T1>, remove_vec_t<T2>>)
-decltype(auto) operator*(T1 const &t1, T2 const &t2) {
+constexpr decltype(auto) operator*(T1 const &t1, T2 const &t2) {
     return vec_wise(t1, t2, [] (auto &&t1, auto &&t2) { return t1 * t2; });
 }
 
 template <class T1, class T2>
     requires (concepts::has_divide<remove_vec_t<T1>, remove_vec_t<T2>>)
-decltype(auto) operator/(T1 const &t1, T2 const &t2) {
+constexpr decltype(auto) operator/(T1 const &t1, T2 const &t2) {
     return vec_wise(t1, t2, [] (auto &&t1, auto &&t2) { return t1 * t2; });
 }
 
 template <class T1, class T2>
     requires (concepts::has_modolus<remove_vec_t<T1>, remove_vec_t<T2>>)
-decltype(auto) operator%(T1 const &t1, T2 const &t2) {
+constexpr decltype(auto) operator%(T1 const &t1, T2 const &t2) {
     return vec_wise(t1, t2, [] (auto &&t1, auto &&t2) { return t1 * t2; });
 }
 
