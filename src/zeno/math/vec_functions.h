bca4f51d9a0b1e489c0be04246b86d6ea70ff911
archibate <1931127624@qq.com>
2021-10-31 16:28:29 +0800

    hope it worked

--- src/zeno/math/vec_functions.h
+++ src/zeno/math/vec_functions.h
@@ -8,6 +8,14 @@
 ZENO_NAMESPACE_BEGIN
 namespace math {
 
+#define _OP3(fname) \
+template <class T1, class T2, class T3, \
+          class T0 = decltype(std::declval<remove_vec_t<T1>>() \
+                            + std::declval<remove_vec_t<T2>>() + std::declval<remove_vec_t<T3>>()> \
+    requires (requires (T0 t0) { std::fname(t0, t0, t0); }) \
+constexpr decltype(auto) fname(T1 const &t1, T2 const &t2, T3 const &t3) { \
+    return vec_wise(t1, t2, t3, [] (auto &&t1, auto &&t2, auto &&t3) { return (T0)std::fname((T0)t1, (T0)t2, (T0)t3); }); \
+}
 
 #define _OP2(fname) \
 template <class T1, class T2, \
@@ -15,6 +23,13 @@ template <class T1, class T2, \
     requires (requires (T0 t0) { std::fname(t0, t0); }) \
 constexpr decltype(auto) fname(T1 const &t1, T2 const &t2) { \
     return vec_wise(t1, t2, [] (auto &&t1, auto &&t2) { return (T0)std::fname((T0)t1, (T0)t2); }); \
+}
+
+#define _OP1(fname) \
+template <class T1> \
+    requires (requires (T1 t1) { std::fname(t1); }) \
+constexpr decltype(auto) fname(T1 const &t1) { \
+    return vec_wise(t1, [] (auto &&t1) { return (T1)std::fname(t1); }); \
 }
 
 _OP3(fma)
