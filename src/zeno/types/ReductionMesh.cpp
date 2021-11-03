38ca144d87cf0e3aa83f8d5ee9017a019cbdaec2
archibate <1931127624@qq.com>
2021-11-03 15:00:46 +0800

    rework

--- src/zeno/types/ReductionMesh.cpp
+++ src/zeno/types/ReductionMesh.cpp
@@ -1,6 +1,7 @@
 #include <zeno/dop/dop.h>
 #include <zeno/types/Mesh.h>
 #include <zeno/math/helpers.h>
+#include <zeno/ztd/assert.h>
 #include <zeno/ztd/error.h>
 #include <variant>
 
@@ -10,8 +11,8 @@ namespace types {
 namespace {
 
 
-template <class Variant, std::size_t I = 0>
-Variant variant_from_index(std::size_t index) {
+template <class Variant, size_t I = 0>
+Variant variant_from_index(size_t index) {
     if constexpr (I >= std::variant_size_v<Variant>)
         throw std::bad_variant_access{};
     else
@@ -24,11 +25,11 @@ struct boundbox {
     math::vec3f min;
     math::vec3f max;
 
-    constexpr void operator=(math::vec3f const &vert) {
+    constexpr void init(math::vec3f const &vert) {
         min = max = vert;
     }
 
-    constexpr void operator+=(boundbox const &that) {
+    constexpr void combine(boundbox const &that) {
         min = std::min(min, that.min);
         max = std::max(max, that.max);
     }
@@ -37,17 +38,17 @@ struct boundbox {
 struct centroid {
     math::vec3f pos;
 
-    constexpr void operator=(math::vec3f const &vert) {
+    constexpr void init(math::vec3f const &vert) {
         pos = vert;
     }
 
-    constexpr void operator+=(centroid const &that) {
+    constexpr void combine(centroid const &that) {
         pos += that.pos;
     }
 };
 
 
-auto operator_variant(std::string const &name) {
+auto reducer_from_name(std::string const &name) {
     static const std::array table = 
         { "centroid"
         , "boundbox"
@@ -63,41 +64,58 @@ auto operator_variant(std::string const &name) {
 }
 
 
+template <size_t BlkSize = 256, class Reducer>
+Reducer reduction(auto &&cgh, Reducer reducer, auto axr_vert) {
+    ZENO_ZTD_ASSERT(axr_vert.size());
+
+    size_t psumsize = math::divup(axr_vert.size(), BlkSize);
+    zycl::vector<Reducer> psum(psumsize);
+
+    {
+        auto axr_psum = zycl::make_access<zycl::access::mode::discard_write>(cgh, psum);
+        auto lxr_temp = zycl::local_access<zycl::access::mode::read_write, Reducer, 1>(cgh, zycl::range<1>(BlkSize));
+
+        cgh.parallel_for(
+            zycl::nd_range<1>(axr_vert.size(), BlkSize),
+            [=] (zycl::nd_item<1> it) {
+                auto gid = it.get_group(0);
+                auto lid = it.get_local_id(0);
+                auto id = it.get_global_id(0);
+                lxr_temp[lid].init(axr_vert[gid]);
+                it.barrier(sycl::access::fence_space::local_space);
+                for (int stride = BlkSize >> 1; stride; stride >>= 1) {
+                    if (lid < stride)
+                        lxr_temp[lid].combine(lxr_temp[lid + stride]);
+                    it.barrier(sycl::access::fence_space::local_space);
+                }
+                axr_psum[gid] = lxr_temp[0];
+            }
+        );
+    }
+
+    auto axr_psum = zycl::make_access<zycl::access::mode::read>(zycl::host_handler{}, psum);
+    Reducer sum = axr_psum[0];
+    for (size_t i = 0; i < psumsize; i++) {
+        sum.combine(axr_psum[i]);
+    }
+    return sum;
+}
+
+
 static void ReductionMesh(dop::FuncContext *ctx) {
     auto mesh = pointer_cast<Mesh>(ctx->inputs.at(0));
     auto opname = value_cast<std::string>(ctx->inputs.at(1));
-    constexpr size_t blksize = 256;
 
-    auto reducer = operator_variant(opname);
+    auto reducer = reducer_from_name(opname);
 
-    zycl::default_queue().submit([=] (zycl::handler &cgh) {
-        std::visit([&] <class Reducer> (Reducer reducer) {
+    math::vec3f out1, out2;
 
-            zycl::vector<Reducer> psum(math::divup(mesh->vert.size(), blksize));
-            {
-                auto axr_psum = zycl::make_access<zycl::access::mode::discard_write>(cgh, psum);
-                auto axr_vert = zycl::make_access<zycl::access::mode::read>(cgh, mesh->vert);
-                auto lxr_temp = zycl::local_access<zycl::access::mode::read_write, Reducer, 1>(cgh, zycl::range<1>(blksize));
-
-                cgh.parallel_for(
-                    zycl::nd_range<1>(mesh->vert.size(), blksize),
-                    [=] (zycl::nd_item<1> it) {
-                        auto gid = it.get_group(0);
-                        auto lid = it.get_local_id(0);
-                        auto id = it.get_global_id(0);
-                        lxr_temp[lid] = axr_vert[gid];
-                        it.barrier(sycl::access::fence_space::local_space);
-                        for (int stride = blksize >> 1; stride; stride >>= 1) {
-                            if (lid < stride)
-                                lxr_temp[lid] += lxr_temp[lid + stride];
-                            it.barrier(sycl::access::fence_space::local_space);
-                        }
-                        axr_psum[gid] = lxr_temp[0];
-                    }
-                );
-            }
+    zycl::default_queue().submit([&] (zycl::handler &cgh) {
+        std::visit([&] (auto reducer) {
+            auto axr_vert = zycl::make_access<zycl::access::mode::read>(cgh, mesh->vert);
+            auto res = reduction(cgh, reducer, axr_vert);
         }, reducer);
-    });
+    }).wait();
 
     ctx->outputs.at(0) = std::move(mesh);
 }
--- src/zeno/zycl/helper.h
+++ src/zeno/zycl/helper.h
@@ -11,27 +11,51 @@ namespace zycl {
 
 struct host_handler {};
 
-template <access::mode mode>
-auto make_access(auto &&cgh, auto &&buf) {
-    return buf.template get_access<mode>(std::forward<decltype(cgh)>(cgh));
-}
-
-template <access::mode mode, class T, size_t N>
-auto local_access(auto &&cgh, range<N> size) {
-    return accessor<T, N, mode, access::target::local>(size, std::forward<decltype(cgh)>(cgh));
-}
 
 template <class F>
 struct functor_accessor {
-    F f;
+    F _M_f;
 
-    constexpr functor_accessor(F &&f) : f(std::move(f)) {
+    constexpr functor_accessor(F &&f) : _M_f(std::move(f)) {
     }
 
     constexpr decltype(auto) operator[](auto &&t) const {
-        return f(std::forward<decltype(t)>(t));
+        return _M_f(std::forward<decltype(t)>(t));
     }
 };
+
+template <class Base, class Range>
+struct span {
+    Base _M_base;
+    Range _M_size;
+
+    constexpr span(Base &&base, Range const &size)
+        : _M_base(std::move(base)), _M_size(std::move(size)) {
+    }
+
+    constexpr Range size() const {
+        return _M_size;
+    }
+
+    constexpr decltype(auto) operator[](auto &&t) const {
+        return _M_base[std::forward<decltype(t)>(t)];
+    }
+};
+
+
+template <class Base, class Range>
+span(Base &&base, Range &&size) -> span<std::remove_cvref_t<Base>, std::remove_cvref_t<Range>>;
+
+
+template <access::mode mode>
+auto make_access(auto &&cgh, auto &&buf) {
+    return span{buf.template get_access<mode>(std::forward<decltype(cgh)>(cgh)), buf.size()};
+}
+
+template <access::mode mode, class T, size_t N>
+auto local_access(auto &&cgh, range<N> const &size) {
+    return span{accessor<T, N, mode, access::target::local>(size, std::forward<decltype(cgh)>(cgh)), size};
+}
 
 }
 ZENO_NAMESPACE_END
