9ef50fd3d98c2e58000904541ade1d7ed5ca14ab
archibate <1931127624@qq.com>
2021-11-03 13:12:10 +0800

    zpc :)

--- POC/zycltest/usm.cpp
+++ POC/zycltest/usm.cpp
@@ -1,6 +1,23 @@
 #include <CL/sycl.hpp>
 
 
+namespace zpc {
+
+
+using f32 = float;
+using f64 = double;
+using isize = std::intptr_t;
+using usize = std::uintptr_t;
+using i8 = std::int8_t;
+using u8 = std::uint8_t;
+using i16 = std::int16_t;
+using u16 = std::uint16_t;
+using i32 = std::int32_t;
+using u32 = std::uint32_t;
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+
+
 auto &default_queue() {
     static sycl::queue q;
     return q;
@@ -13,6 +30,7 @@ struct allocator : sycl::usm_allocator<T, alloc, align> {
 
     allocator() : allocator(default_queue()) {}
 };
+
 
 template <class T>
 concept has_span = requires (T t) {
@@ -51,19 +69,47 @@ struct span {
 span(has_span auto &&t) -> span<decltype(t.data()), decltype(t.size())>;
 
 
-int main() {
-    std::vector<int, allocator<int>> arr(32);
-    std::vector<int, allocator<int>> sum(1);
+auto parallel_for(auto &&...args) {
+    return default_queue().parallel_for(std::forward<decltype(args)>(args)...);
+}
 
-    span v_arr = arr;
-    default_queue().parallel_for(
-        sycl::nd_range<1>(arr.size(), 4),
-        sycl::reduction(sum.data(), 0, std::plus{}),
+auto single_task(auto &&...args) {
+    return default_queue().single_task(std::forward<decltype(args)>(args)...);
+}
+
+auto submit(auto &&...args) {
+    return default_queue().submit(std::forward<decltype(args)>(args)...);
+}
+
+auto copy(auto &&...args) {
+    return default_queue().copy(std::forward<decltype(args)>(args)...);
+}
+
+
+template <class T>
+using vector = std::vector<T, allocator<T>>;
+
+
+}
+
+
+int main() {
+    zpc::vector<float> arr(4096);
+    zpc::vector<float> sum(1);
+
+    for (auto &a: arr) {
+        a = drand48();
+    }
+
+    zpc::span v_arr = arr;
+    zpc::parallel_for(
+        sycl::nd_range<1>(arr.size(), 256),
+        sycl::reduction(sum.data(), 0.0f, std::plus{}),
         [=] (sycl::nd_item<1> it, auto &sum) {
             sum += v_arr[it.get_global_id(0)];
         }
     ).wait();
 
-    printf("%d\n", sum[0]);
+    printf("%f\n", sum[0]);
     return 0;
 }
