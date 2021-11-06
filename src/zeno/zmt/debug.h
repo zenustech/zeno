e18902a5c6215c1ebe374460bb0c36baa7bf8ba7
archibate <1931127624@qq.com>
2021-11-07 00:25:28 +0800

    zmt::debug() << "your msg"

--- src/zeno/zmt/debug.h
+++ src/zeno/zmt/debug.h
@@ -19,23 +19,31 @@ struct Debug {
     struct _M_had {
         Os &os;
 
-        _M_had(Os &os) : os(os) {}
+        inline _M_had(Os &os) : os(os) {}
 
-        _M_had &operator<<(auto &&t) const {
+        inline _M_had &operator<<(auto &&t) const {
             os << ' ';
             os << std::forward<decltype(t)>(t);
         }
 
-        ~_M_had() {
+        inline ~_M_had() {
             os << std::endl;
         }
     };
 
-    _M_had operator<<(auto &&t) const {
+    inline _M_had operator<<(auto &&t) const {
         os << std::forward<decltype(t)>(t);
         return {os};
     }
 };
+
+inline auto debug() {
+    return Debug(std::cout) << "zDebug:";
+}
+
+inline auto error() {
+    return Debug(std::cerr) << "zError:";
+}
 
 }
 ZENO_NAMESPACE_END
