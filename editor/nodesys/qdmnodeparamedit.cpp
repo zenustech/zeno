846f305865d7b813c2cb7cc9280d51c797ea4cdc
archibate <1931127624@qq.com>
2021-11-07 15:46:15 +0800

    cihou switch-case

--- editor/nodesys/qdmnodeparamedit.cpp
+++ editor/nodesys/qdmnodeparamedit.cpp
@@ -3,6 +3,8 @@
 #include <zeno/dop/Descriptor.h>
 #include <QFormLayout>
 #include <QLineEdit>
+#include <QIntValidator>
+#include <QDoubleValidator>
 
 ZENO_NAMESPACE_BEGIN
 
@@ -13,6 +15,31 @@ QDMNodeParamEdit::QDMNodeParamEdit(QWidget *parent)
 
 static QWidget *make_edit_for_type(std::string const &type)
 {
+    const std::array tab = {
+        "string",
+        "int",
+        "float",
+    };
+    switch (std::find(begin(tab), end(tab), type) - begin(tab)) {
+    case 0: {
+        auto edit = new QLineEdit;
+        return edit;
+    } break;
+    case 1: {
+        auto edit = new QLineEdit;
+        edit->setValidator(new QIntValidator);
+        return edit;
+    } break;
+    case 2: {
+        auto edit = new QLineEdit;
+        edit->setValidator(new QDoubleValidator);
+        return edit;
+    } break;
+    default: {
+        auto edit = new QLineEdit;
+        return edit;
+    } break;
+    }
 }
 
 void QDMNodeParamEdit::setCurrentNode(QDMGraphicsNode *node)
