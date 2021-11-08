67a7ce85ad51dfc6f39f2f4abc0f45a4784f9027
archibate <1931127624@qq.com>
2021-11-08 15:52:04 +0800

    scan depends on blksize

--- src/zeno/zycl/parallel_scan.h
+++ src/zeno/zycl/parallel_scan.h
@@ -11,8 +11,8 @@ ZENO_NAMESPACE_BEGIN
 namespace zycl {
 inline namespace ns_parallel_scan {
 
-template <class T>
-auto make_scanner(sycl::handler &cgh, size_t blksize) {
+template <size_t blksize, class T>
+auto make_scanner(sycl::handler &cgh) {
     sycl::accessor<T, 1, sycl::access::mode::read_write, sycl::access::target::local>
         lxr_blk(sycl::range<1>(blksize), cgh);
 
@@ -33,7 +33,7 @@ auto make_scanner(sycl::handler &cgh, size_t blksize) {
         if (tid == 0) {
             size_t di = blksize - 1;
             partial = lxr_blk[di];
-            lxr_blk[di] = 0;
+            lxr_blk[di] = T{};
         }
 
         for (size_t offset = blksize >> 1, stride = 1; stride < blksize; offset >>= 1, stride <<= 1) {
@@ -53,8 +53,8 @@ auto make_scanner(sycl::handler &cgh, size_t blksize) {
     };
 }
 
-template <class T>
-void prefix_scan(sycl::queue &q, sycl::buffer<T, 1> &buf, size_t bufsize, size_t blksize) {
+template <size_t blksize, class T>
+void parallel_scan(sycl::queue &q, sycl::buffer<T, 1> &buf, size_t bufsize) {
     if (!bufsize)
         return;
 
@@ -62,14 +62,14 @@ void prefix_scan(sycl::queue &q, sycl::buffer<T, 1> &buf, size_t bufsize, size_t
     sycl::buffer<T, 1> part(partsize);
 
     q.submit([&] (sycl::handler &cgh) {
-        auto axr_buf = buf.template get_access<sycl::access::mode::discard_read_write>(cgh);
+        auto axr_buf = buf.template get_access<sycl::access::mode::discard_read_write>(cgh, range<1>(bufsize));
         auto axr_part = part.template get_access<sycl::access::mode::discard_write>(cgh);
-        auto scanner = make_scanner<float>(cgh, blksize);
+        auto scanner = make_scanner<blksize, float>(cgh, blksize);
 
         cgh.parallel_for(sycl::nd_range<1>(partsize * blksize, blksize), [=] (sycl::nd_item<1> it) {
             size_t id = it.get_global_linear_id();
             size_t gid = it.get_group_linear_id();
-            T val{};
+            T val = T{};
             [[likely]] if (id < bufsize)
                 val = axr_buf[id];
             scanner(it, val, axr_part[gid]);
@@ -79,10 +79,10 @@ void prefix_scan(sycl::queue &q, sycl::buffer<T, 1> &buf, size_t bufsize, size_t
     });
 
     if (bufsize > blksize) {
-        prefix_scan<T>(q, part, partsize, blksize);
+        parallel_scan<blksize, T>(q, part, partsize);
 
         q.submit([&] (sycl::handler &cgh) {
-            auto axr_buf = buf.template get_access<sycl::access::mode::discard_read_write>(cgh);
+            auto axr_buf = buf.template get_access<sycl::access::mode::discard_read_write>(cgh), range<1>(bufsize);
             auto axr_part = part.template get_access<sycl::access::mode::read>(cgh);
 
             cgh.parallel_for(sycl::nd_range<1>(partsize * blksize, blksize), [=] (sycl::nd_item<1> it) {
