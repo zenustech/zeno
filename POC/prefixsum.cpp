1733dead9e47c85aa7db4ae8863f6eab2461916f
archibate <1931127624@qq.com>
2021-11-08 14:09:35 +0800

    its 2016!

--- POC/prefixsum.cpp
+++ POC/prefixsum.cpp
@@ -6,7 +6,7 @@ int main() {
     sycl::buffer<float, 1> out(64);
 
     constexpr size_t bufsize = 64;
-    constexpr size_t blksize = 8;
+    constexpr size_t blksize = 64;
 
     q.submit([&] (sycl::handler &cgh) {
         auto axr_buf = buf.get_access<sycl::access::mode::discard_write>(cgh);
@@ -19,30 +19,42 @@ int main() {
         auto axr_buf = buf.get_access<sycl::access::mode::read>(cgh);
         auto axr_out = buf.get_access<sycl::access::mode::discard_write>(cgh);
         sycl::accessor<float, 1, sycl::access::mode::read_write, sycl::access::target::local>
-            lxr_tmp(sycl::range<1>(blksize), cgh);
+            lxr_tmp(sycl::range<1>(blksize + (blksize >> 3)), cgh);
 
         cgh.parallel_for(sycl::nd_range<1>(bufsize, blksize), [=] (sycl::nd_item<1> it) {
             size_t tid = it.get_local_linear_id();
             size_t id = it.get_global_linear_id();
-            lxr_tmp[tid] = axr_buf[id];
 
-            for (size_t offset = 1, stride = blksize >> 1; stride > 0; offset <<= 1, stride >>= 1) {
+            {
+                size_t di = tid;
+                di += di >> 4;
+                lxr_tmp[di] = axr_buf[id];
+            }
+
+            for (size_t offset = 1, stride = blksize >> 1; stride > 1; offset <<= 1, stride >>= 1) {
                 it.barrier(sycl::access::fence_space::local_space);
                 if (tid < stride) {
                     size_t si = offset * (2 * tid + 1) - 1;
-                    size_t di = offset * (2 * tid + 2) - 1;
+                    size_t di = si + offset;
+                    si += si >> 4;
+                    di += di >> 4;
                     lxr_tmp[di] += lxr_tmp[si];
                 }
             }
 
-            if (tid == 0)
-                lxr_tmp[blksize - 1] = 0;
+            if (tid == 0) {
+                size_t di = blksize - 1;
+                di += di >> 4;
+                lxr_tmp[di] = 0;
+            }
 
             for (size_t offset = blksize >> 1, stride = 1; stride < blksize; offset >>= 1, stride <<= 1) {
                 it.barrier(sycl::access::fence_space::local_space);
                 if (tid < stride) {
                     size_t si = offset * (2 * tid + 1) - 1;
-                    size_t di = offset * (2 * tid + 2) - 1;
+                    size_t di = si + offset;
+                    si += si >> 4;
+                    di += di >> 4;
                     auto tmp = lxr_tmp[si];
                     lxr_tmp[si] = lxr_tmp[di];
                     lxr_tmp[di] += tmp;
@@ -50,7 +62,12 @@ int main() {
             }
 
             it.barrier(sycl::access::fence_space::local_space);
-            axr_out[id] = lxr_tmp[tid];
+
+            {
+                size_t si = tid;
+                si += si >> 4;
+                axr_out[id] = lxr_tmp[si];
+            }
         });
     });
 
