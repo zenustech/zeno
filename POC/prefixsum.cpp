ddf8c6a6f27fd46ab100b85a6ce2538fbbe35dfd
archibate <1931127624@qq.com>
2021-11-08 12:08:22 +0800

    fix prefixsum

--- POC/prefixsum.cpp
+++ POC/prefixsum.cpp
@@ -5,15 +5,23 @@ int main() {
     sycl::buffer<float, 1> buf(64);
     sycl::buffer<float, 1> out(64);
 
+    constexpr size_t bufsize = 64;
     constexpr size_t blksize = 8;
 
     q.submit([&] (sycl::handler &cgh) {
-        sycl::accessor<float, 1, sycl::access::mode::read_write, sycl::access::target::local>
-            lxr_tmp(sycl::range<1>(blksize), cgh);
+        auto axr_buf = buf.get_access<sycl::access::mode::discard_write>(cgh);
+        cgh.parallel_for(sycl::range<1>(bufsize), [=] (sycl::item<1> it) {
+            axr_buf[it] = it[0] + 1;
+        });
+    });
+
+    q.submit([&] (sycl::handler &cgh) {
         auto axr_buf = buf.get_access<sycl::access::mode::read>(cgh);
         auto axr_out = buf.get_access<sycl::access::mode::discard_write>(cgh);
+        sycl::accessor<float, 1, sycl::access::mode::read_write, sycl::access::target::local>
+            lxr_tmp(sycl::range<1>(blksize), cgh);
 
-        cgh.parallel_for(sycl::nd_range<1>(buf.size(), blksize), [=] (sycl::nd_item<1> it) {
+        cgh.parallel_for(sycl::nd_range<1>(bufsize, blksize), [=] (sycl::nd_item<1> it) {
             size_t tid = it.get_local_linear_id();
             size_t id = it.get_global_linear_id();
             lxr_tmp[tid] = axr_buf[id];
@@ -25,8 +33,6 @@ int main() {
                     size_t di = offset * (2 * tid + 2) - 1;
                     lxr_tmp[di] += lxr_tmp[si];
                 }
-                stride >>= 1;
-                offset <<= 1;
             }
 
             if (tid == 0)
@@ -47,4 +53,11 @@ int main() {
             axr_out[id] = lxr_tmp[tid];
         });
     });
+
+    auto axr_buf = buf.get_access<sycl::access::mode::discard_write>();
+    for (size_t i = 0; i < bufsize; i++) {
+        std::cout << axr_buf[i] << std::endl;
+    }
+
+    return 0;
 }
