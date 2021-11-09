800beb9b189f253b17f45a4c88e00dace471a7b2
legobadman <luzh16371@gmail.com>
2021-11-09 22:52:11 +0800

    disable xerces in editor

--- editor/main.cpp
+++ editor/main.cpp
@@ -3,19 +3,11 @@
 #include <kddockwidgets/DockWidget.h>
 #include <kddockwidgets/MainWindow.h>
 #include <kddockwidgets/Config.h>
-#include <xercesc/dom/DOM.hpp>
-#include <xercesc/util/PlatformUtils.hpp>
-#include <xercesc/util/XMLException.hpp>
-#include <xercesc/util/XMLString.hpp>
-#include <xercesc/util/XMLUniDefs.hpp>
-#include <xercesc/parsers/XercesDOMParser.hpp>
 #include "tmpwidgets/zmainwindow.h"
 #include "style/zenostyle.h"
 
-using namespace XERCES_CPP_NAMESPACE;
-
 //#define TEST_WEBENGINE
-#define TEST_XERCES
+//#define TEST_XERCES
 #define USE_KKDOCK
 
 ZENO_NAMESPACE_BEGIN
@@ -64,9 +56,18 @@ ZENO_NAMESPACE_END
 int main(int argc, char *argv[]) {
 
 #ifdef TEST_XERCES
+
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/util/XMLException.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/util/XMLUniDefs.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+
+using namespace XERCES_CPP_NAMESPACE;
+
     XMLPlatformUtils::Initialize();
     XercesDOMParser* parser = new XercesDOMParser;
-
 
 #elif defined(TEST_WEBENGINE)
     #include <QWebEngineView>
