f67b242ed9e5d0f8fc20667bce63d80cfcff38d9
archibate <1931127624@qq.com>
2021-11-09 09:49:17 +0800

    match visit

--- src/zeno/ztd/variant.h
+++ src/zeno/ztd/variant.h
@@ -13,15 +13,15 @@ inline namespace _H_variant {
 
 template <class T>
 std::optional<T> try_get(auto const &var) {
-    std::visit(match
-    ( [] (T const &t) { return std::make_optional(t); }
-    , [] (auto const &) {}
+    return std::visit(match
+    ( [] (T const &t) -> std::optional<T> { return std::make_optional(t); }
+    , [] (auto const &) -> std::optional<T> { return std::nullopt; }
     ), var);
 }
 
 
 template <class T>
-decltype(auto) visit(auto const &var, auto &&fs) {
+decltype(auto) match_visit(auto const &var, auto &&...fs) {
     return std::visit(match(std::forward<decltype(fs)>(fs)...), var);
 }
 
