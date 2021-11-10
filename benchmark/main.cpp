91ffeda5a7a07cf5877e7423bc4213b83a791faa
archibate <1931127624@qq.com>
2021-11-10 10:20:17 +0800

    rollback

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -9,7 +9,7 @@ USING_ZENO_NAMESPACE
 
 static types::Mesh getTestMesh() {
     types::Mesh mesh;
-    std::ifstream fin("models/cube.obj");
+    std::ifstream fin("models/Pig_Head.obj");
     types::readMeshFromOBJ(fin, mesh);
     return mesh;
 }
@@ -17,9 +17,18 @@ static types::Mesh getTestMesh() {
 static void BM_meshToTriangles(benchmark::State &state) {
     types::Mesh mesh = getTestMesh();
 
+    zycl::vector<math::vec3f> tris;
     for (auto _: state) {
-        auto tris = types::meshToTriangles(mesh);
-        zycl::host_get(tris);
+        tris = types::meshToTriangles(mesh);
+        benchmark::DoNotOptimize(tris);
     }
+    zycl::host_get(tris);
 }
-BENCHMARK(BM_meshToTriangles);
+BENCHMARK(BM_meshToTriangles)
+    ->ComputeStatistics("max", [] (std::vector<double> const &v) -> double {
+        return *(std::max_element(std::begin(v), std::end(v)));
+    })
+    ->ComputeStatistics("min", [] (std::vector<double> const &v) -> double {
+        return *(std::min_element(std::begin(v), std::end(v)));
+    })
+;
