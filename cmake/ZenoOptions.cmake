14f2d35170de0d7079134c1bf8c335b79b525ca5
archibate <1931127624@qq.com>
2021-11-22 16:20:35 +0800

    add install target

--- cmake/ZenoOptions.cmake
+++ cmake/ZenoOptions.cmake
@@ -1,6 +1,5 @@
 # https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards/
 set(ZENO_TARGET "Editor" CACHE STRING "Specify the Zeno target desired to build (Editor, Headless, Benchmark, Tests)")
-option(ZENO_WITH_ZPM "Use ZPM to manage Zeno dependencies" ON)
 option(ZENO_WITH_SYCL "Enable SYCL support for Zeno" OFF)
 option(ZENO_WITH_LEGACY "Build Zeno With Legacy Nodes" OFF)
 option(ZENO_WITH_BACKWARD "Enable stack backtrace for Zeno" OFF)
@@ -9,10 +8,10 @@ option(ZENO_WITH_BACKWARD "Enable stack backtrace for Zeno" OFF)
 if (UNIX)  # these are only used by archibate and zhxx1987
 
     if ($ENV{HOME} STREQUAL "/home/bate")
-        message("-- BATE detected, making him happy")
+        message(STATUS "BATE detected, making him happy")
         include(cmake/BATE.cmake)
     elseif ($ENV{HOME} STREQUAL "/home/dilei")
-        message("-- ZHXX detected, making him happy")
+        message(STATUS "ZHXX detected, making him happy")
         include(cmake/ZHXX.cmake)
     endif()
 
@@ -22,7 +21,7 @@ endif()  # normal users won't be affected
 if (UNIX)
     find_program(CCACHE_PROGRAM ccache)
     if (CCACHE_PROGRAM)
-        message("-- Found CCache: ${CCACHE_PROGRAM}")
+        message(STATUS "Found CCache: ${CCACHE_PROGRAM}")
         set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CCACHE_PROGRAM}")
         set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK "${CCACHE_PROGRAM}")
     endif()
@@ -31,11 +30,7 @@ endif()
 if (NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE Release)
 endif()
-message("-- Build type: ${CMAKE_BUILD_TYPE}")
-
-if (ZENO_WITH_ZPM)
-    include(cmake/ZenoRequires.cmake)
-endif()
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
 
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_EXTENSIONS OFF)
