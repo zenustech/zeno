1d7f938050b10c49739da6635c4886f0d8439062
archibate <1931127624@qq.com>
2021-11-22 15:51:27 +0800

    add range-v3 as pkg

--- cmake/ZenoSource.cmake
+++ cmake/ZenoSource.cmake
@@ -7,13 +7,17 @@ find_package(TBB COMPONENTS tbb REQUIRED)
 message(STATUS "Found TBB: ${TBB_DIR}")
 target_link_libraries(zeno PUBLIC TBB::tbb)
 
-find_package(fmt COMPONENTS fmt REQUIRED)
+find_package(fmt REQUIRED)
 message(STATUS "Found fmt: ${fmt_DIR}")
 target_link_libraries(zeno PUBLIC fmt::fmt)
 
-find_package(spdlog COMPONENTS spdlog REQUIRED)
+find_package(spdlog REQUIRED)
 message(STATUS "Found spdlog: ${spdlog_DIR}")
 target_link_libraries(zeno PUBLIC spdlog::spdlog)
+
+find_package(range-v3 REQUIRED)
+message(STATUS "Found range-v3: ${range-v3_DIR}")
+target_link_libraries(zeno PUBLIC range-v3::range-v3)
 
 if (ZENO_WITH_SYCL)
     message(STATUS "Building Zeno with hipSYCL targets: [${HIPSYCL_TARGETS}]")
