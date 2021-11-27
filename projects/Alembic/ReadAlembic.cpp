bfa329a8b85a116dcbee1eaef2b5d2fddc1d13b7
archibate <1931127624@qq.com>
2021-11-27 23:57:23 +0800

    looks reading!

--- projects/Alembic/ReadAlembic.cpp
+++ projects/Alembic/ReadAlembic.cpp
@@ -1,3 +1,5 @@
+// https://github.com/alembic/alembic/blob/master/lib/Alembic/AbcGeom/Tests/PolyMeshTest.cpp
+// SO WHY THE FKING ALEMBIC OFFICIAL GIVES NO DOC BUT ONLY "TESTS" FOR ME TO LEARN THEIR FKING LIB
 #include <zeno/zeno.h>
 #include <zeno/utils/logger.h>
 #include <zeno/StringObject.h>
@@ -27,10 +29,39 @@ static void traverseABC(Alembic::AbcGeom::IObject &obj)
         auto const &md = child.getMetaData();
         log_info("[alembic] meta data: [{}]", md.serialize());
 
-        if ( Alembic::AbcGeom::IPolyMeshSchema::matches(md)
-          || Alembic::AbcGeom::ISubDSchema::matches(md)
-           ) {
+        if (Alembic::AbcGeom::IGeomBase::matches(md)) {
             log_info("[alembic] found a mesh [{}]", child.getName());
+
+            Alembic::AbcGeom::IPolyMesh meshy(child);
+            Alembic::AbcGeom::IPolyMeshSchema &mesh = meshy.getSchema();
+
+            {
+                Alembic::AbcGeom::IPolyMeshSchema::Sample mesamp;
+                mesh.get(mesamp);
+                auto &possp = *mesamp.getPositions();
+                for (size_t i = 0; i < possp.size(); i++) {
+                    auto posi = possp[i];
+                    log_info("{}-th vertex: {} {} {}", i, posi[0], posi[1], posi[2]);
+                }
+            }
+
+            {
+                auto nrmpa = mesh.getNormalsParam();
+                auto &nrmsp = *nrmpa.getExpandedValue().getVals();
+                for (size_t i = 0; i < nrmsp.size(); i++) {
+                    auto nrmi = nrmsp[i];
+                    log_info("{}-th normal: {} {} {}", i, nrmi[0], nrmi[1], nrmi[2]);
+                }
+            }
+
+            {
+                auto uvpa = mesh.getUVsParam();
+                auto &uvsp = *uvpa.getExpandedValue().getVals();
+                for (size_t i = 0; i < uvsp.size(); i++) {
+                    auto uvi = uvsp[i];
+                    log_info("{}-th UV: {} {}", i, uvi[0], uvi[1]);
+                }
+            }
         }
 
         traverseABC(child);
