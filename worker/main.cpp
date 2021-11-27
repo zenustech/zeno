35a874281e01078ef56fbbf3f78b9fbda953ef07
archibate <1931127624@qq.com>
2021-11-28 07:03:24 +0800

    work

--- worker/main.cpp
+++ worker/main.cpp
@@ -1,7 +1,11 @@
 #include <cstdio>
+#include <zeno/zbb/parallel_for.h>
 
 int main()
 {
+    zbb::parallel_for(0, 100, [] (int i) {
+        printf("%d\n", i);
+    });
     printf("Hello, world!\n");
     return 0;
 }
