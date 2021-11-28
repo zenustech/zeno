82762d42d80b6ee5a227c86d48ed766aba198e7f
archibate <1931127624@qq.com>
2021-11-28 17:42:35 +0800

    zbb slow

--- worker/main.cpp
+++ worker/main.cpp
@@ -5,31 +5,33 @@
 int main()
 {
     std::vector<int> arr;
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 4096*768; i++) {
         arr.push_back(i + 1);
     }
 
-    {
-        zbb::auto_profiler _("omp");
-        int res = 0;
+    for (int _ = 0; _ < 32; _++) {
+        {
+            zbb::auto_profiler _("omp");
+            int res = 0;
 #pragma omp parallel for reduction(+: res)
-        for (std::size_t i = 0; i < arr.size(); i++) {
-            res += arr[i];
-        }
-        printf("result: %d\n", res);
-    }
-
-    {
-        zbb::auto_profiler _("zbb");
-        int res = zbb::parallel_reduce
-        ( zbb::make_blocked_range(arr.begin(), arr.end())
-        , int{0}, [] (int x, int y) { return x + y; }
-        , [&] (int &res, auto const &r) {
-            for (auto const &i: r) {
-                res += i;
+            for (std::size_t i = 0; i < arr.size(); i++) {
+                res += arr[i];
             }
-        });
-        printf("result: %d\n", res);
+            printf("result: %d\n", res);
+        }
+
+        {
+            zbb::auto_profiler _("zbb");
+            int res = zbb::parallel_reduce
+            ( zbb::make_blocked_range(arr.begin(), arr.end())
+            , int{0}, [] (int x, int y) { return x + y; }
+            , [&] (int &res, auto const &r) {
+                for (auto const &i: r) {
+                    res += i;
+                }
+            });
+            printf("result: %d\n", res);
+        }
     }
 
     return 0;
