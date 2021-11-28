ad2fc855355d46b064a2f119856c834912b149f6
archibate <1931127624@qq.com>
2021-11-28 16:42:40 +0800

    fail speed

--- core/src/zeno/zbb/blocked_range.h
+++ core/src/zeno/zbb/blocked_range.h
@@ -55,8 +55,8 @@ struct blocked_range {
 template <class T>
 inline constexpr blocked_range<T> make_blocked_range(T const &begin, T const &end) {
     std::size_t procs = get_num_procs();
-    std::size_t kprocs = 2 * procs;
-    return {begin, end, 0, procs, (static_cast<std::size_t>(end - begin) + kprocs) / kprocs};
+    std::size_t kprocs = 4 * procs;
+    return {begin, end, 0, procs, (static_cast<std::size_t>(end - begin) + kprocs - 1) / kprocs};
 }
 
 template <class T>
--- worker/main.cpp
+++ worker/main.cpp
@@ -1,24 +1,55 @@
 #include <cstdio>
 #include <zeno/zbb/parallel_for.h>
 #include <zeno/zbb/parallel_reduce.h>
+#include <iostream>
+#include <chrono>
+
+class AutoProfiler {
+private:
+    std::string m_name;
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_beg;
+public:
+    AutoProfiler(std::string name)
+        : m_name(std::move(name))
+        , m_beg(std::chrono::high_resolution_clock::now())
+    {}
+
+    ~AutoProfiler() {
+        auto end = std::chrono::high_resolution_clock::now();
+        auto dur = std::chrono::duration_cast<std::chrono::microseconds>(end - m_beg);
+        std::cout << m_name << " : " << dur.count() << " musec\n";
+    }
+};
 
 int main()
 {
-    std::vector<int> val;
-    for (int i = 0; i < 100; i++) {
-        val.push_back(i + 1);
+    std::vector<int> arr;
+    for (int i = 0; i < 1024*768; i++) {
+        arr.push_back(i + 1);
     }
 
-    int res = zbb::parallel_reduce
-    ( zbb::make_blocked_range(val.begin(), val.end())
-    , 0, [] (int x, int y) { return x + y; }
-    , [] (int &res, auto const &r) {
-        for (auto const &i: r) {
-            res += i;
+    {
+        AutoProfiler _("omp");
+        int res = 0;
+#pragma omp parallel for reduction(+: res)
+        for (std::size_t i = 0; i < arr.size(); i++) {
+            res += arr[i];
         }
-    });
+        printf("result: %d\n", res);
+    }
 
-    printf("result: %d\n", res);
+    {
+        AutoProfiler _("zbb");
+        int res = zbb::parallel_reduce
+        ( zbb::make_blocked_range(arr.begin(), arr.end())
+        , int{0}, [] (int x, int y) { return x + y; }
+        , [&] (int &res, auto const &r) {
+            for (auto const &i: r) {
+                res += i;
+            }
+        });
+        printf("result: %d\n", res);
+    }
 
     return 0;
 }
