8e15737e813a82a069227476d4789dd0d903e136
archibate <1931127624@qq.com>
2021-11-29 13:50:34 +0800

    work

--- core/src/zeno/zbb/concurrent_map.h
+++ core/src/zeno/zbb/concurrent_map.h
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <vector>
 #include <atomic>
+#include <mutex>
 
 
 ZENO_NAMESPACE_BEGIN
@@ -21,24 +22,48 @@ struct concurrent_u32_map {
 
     static constexpr std::uint32_t _kEmpty = 0;
 
-    std::vector<_Node> _table;
-    std::size_t _tabmask{};
+    std::vector<_Node> _table{4};
+    std::size_t _tabmask{3};
+    std::mutex _mtx;
 
-    static inline constexpr std::size_t _hash(std::uint32_t key) {
-        return static_cast<std::size_t>(key * 1.618033988749895f);
+    constexpr std::size_t _capacity() const noexcept {
+        return _tabmask + 1;
     }
 
-    T &put(std::uint32_t key) {
+    void _recapacity(std::size_t size) noexcept {
+        if (size == 0)
+            size = 1;
+        _table.resize(size);
+        _tabmask = size - 1;
+    }
+
+    static inline constexpr std::size_t _hash(std::uint32_t key) noexcept {
+        return std::size_t{static_cast<std::int32_t>(key * 1.618033988749895f)};
+    }
+
+    T &operator[](std::uint32_t key) noexcept {
         std::size_t base = _tabmask & _hash(key), end = base + _tabmask + 1;
         for (std::size_t slot = base; slot != end; slot++) {
             std::size_t slt = _tabmask & slot;
             std::uint32_t prev = _kEmpty;
-            if (_table[slt].key.compare_exchange_strong(prev, key)) {
-                return _table[slt].value;
-            } else if (prev == key) {
+            if (_table[slt].key.compare_exchange_strong(prev, key) || prev == key) {
                 return _table[slt].value;
             }
         }
+        std::lock_guard _(_mtx);
+        _table.resize((_tabmask + 1) << 1);
+        return *reinterpret_cast<T *>(0);
+    }
+
+    T &at(std::uint32_t key) noexcept {
+        std::size_t base = _tabmask & _hash(key), end = base + _tabmask + 1;
+        for (std::size_t slot = base; slot != end; slot++) {
+            std::size_t slt = _tabmask & slot;
+            if (_table[slt].key.load() == key) {
+                return _table[slt].value;
+            }
+        }
+        return *reinterpret_cast<T *>(0);
     }
 };
 
--- core/src/zeno/zbb/concurrent_vector.h
+++ core/src/zeno/zbb/concurrent_vector.h
@@ -4,14 +4,46 @@
 
 #include <zeno/common.h>
 #include <vector>
+#include <array>
+#include <mutex>
+#include <new>
 
 
 ZENO_NAMESPACE_BEGIN
 namespace zbb {
 
 
-template <class T, class Alloc = std::allocator<T>>
+template <class T>
 struct concurrent_vector {
+    using value_type = T;
+    inline static constexpr std::uint8_t _kBase = 8;
+    inline static constexpr std::uint8_t _kMax = 32;
+
+    std::array<std::vector<T>, _kMax> _bins;
+    std::uint8_t _binid{};
+    std::mutex _mtx;
+
+    concurrent_vector() = default;
+    concurrent_vector(concurrent_vector const &other) = default;
+    concurrent_vector(concurrent_vector &&) = default;
+    concurrent_vector &operator=(concurrent_vector const &) = default;
+    concurrent_vector &operator=(concurrent_vector &&) = default;
+    ~concurrent_vector() = default;
+
+    T *grow_twice() noexcept {
+        std::lock_guard _(_mtx);
+        std::uint8_t nbin = _binid++;
+        auto &bin = _bins[nbin];
+        bin.resize(1 << nbin);
+        return bin.data();
+    }
+
+    [[nodiscard]] T &at(std::size_t off) noexcept {
+        std::uint8_t pot = 0;
+        for (std::size_t i = off >> _kBase; i; i >>= 1)
+            pot++;
+        return _bins[pot][off & (1 << pot + _kBase) - 1];
+    }
 };
 
 
