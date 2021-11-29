db31bb09f859df960cba43ca1847743d1d378f40
archibate <1931127624@qq.com>
2021-11-29 15:09:52 +0800

    foreach

--- worker/main.cpp
+++ worker/main.cpp
@@ -1,15 +1,25 @@
-#include <zeno/zbb/parallel_for.h>
+#include <tbb/parallel_for.h>
+#include <tbb/parallel_for_each.h>
+#include <tbb/concurrent_vector.h>
+#include <tbb/concurrent_unordered_map.h>
 
 int main()
 {
-    std::vector<int> a;
+    std::vector<int> pos;
+    tbb::concurrent_unordered_multimap<int, size_t> lut;
 
-#pragma omp parallel
-    {
-        std::map<int, int> tls;
-        for (int i = 0; i < a.size(); i++) {
-        }
-    }
+    tbb::parallel_for
+    ( size_t{0}, pos.size(), size_t{1}
+    , [&] (size_t i) {
+        int p = pos[i];
+        lut.emplace(p, i);
+    });
+
+    tbb::parallel_for_each
+    ( lut.begin(), lut.end()
+    , [&] (decltype(lut)::value_type const &ent) {
+        printf("%d %ld\n", ent.first, ent.second);
+    });
 
     return 0;
 }
