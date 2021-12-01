444aeb87b510b3d63440d9e064bd1cf6b8f7ec67
archibate <1931127624@qq.com>
2021-12-01 11:46:53 +0800

    cihou mcut

--- worker/main.cpp
+++ worker/main.cpp
@@ -7,32 +7,30 @@ int main()
 {
     zty::Mesh mesh1;
     {
-        std::ifstream ifs("models/monkey.obj");
-        if (!ifs) throw std::runtime_error("src mesh file");
+        std::ifstream ifs("models/Pig_Head.obj");
+        if (!ifs) throw std::runtime_error("src mesh file read fail");
         readMeshFromOBJ(ifs, mesh1);
-        meshTriangulate(mesh1);
     }
 
     zty::Mesh mesh2;
     {
-        std::ifstream ifs("models/plane.obj");
-        if (!ifs) throw std::runtime_error("cut mesh file");
+        std::ifstream ifs("models/cube.obj");
+        if (!ifs) throw std::runtime_error("cut mesh file read fail");
         readMeshFromOBJ(ifs, mesh2);
-        meshTriangulate(mesh2);
     }
 
     zty::Mesh mesh3;
     {
         zty::MeshCutter mcut;
         mcut.dispatch(mesh1, mesh2);
-        mcut.selectComponents(zty::MeshCutter::CompType::all);
+        mcut.selectComponents(zty::MeshCutter::CompType::fragment);
         mcut.getComponent(0, mesh3);
     }
 
 
     {
         std::ofstream ofs("/tmp/a.obj");
-        if (!ofs) throw std::runtime_error("cut mesh file");
+        if (!ofs) throw std::runtime_error("cut mesh file write fail");
         writeMeshToOBJ(ofs, mesh3);
     }
 
