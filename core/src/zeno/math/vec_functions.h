45639aed7e697e012259f5b149d057c164f7eecd
archibate <1931127624@qq.com>
2021-12-07 11:20:51 +0800

    real fix array op

--- core/src/zeno/math/vec_functions.h
+++ core/src/zeno/math/vec_functions.h
@@ -76,5 +76,24 @@ _OP1(atanh)
 #undef _OP1
 
 
+template <class T1, class T0 = remove_vec_t<T1>>
+    requires (std::is_integral_v<T0> && std::is_unsigned_v<T0>)
+constexpr decltype(auto) abs(T1 const &t1) {
+    return vec_wise(t1, [] (auto &&t1) {
+        return (T0)t1; });
+}
+
+template <class T1, class T2, class T3,
+          class T0 = decltype(std::declval<remove_vec_t<T1>>()
+                            + std::declval<remove_vec_t<T2>>()
+                            + std::declval<remove_vec_t<T3>>())>
+    requires (vec_promotable<T1, T2, T3> && !requires (T0 t0) { std::lerp(t0, t0, t0); }
+              && std::is_integral_v<T0>)
+constexpr decltype(auto) lerp(T1 const &t1, T2 const &t2, T3 const &t3) {
+    return vec_wise(t1, t2, t3, [] (auto &&t1, auto &&t2, auto &&t3) {
+        return (T0)t1 + ((T0)t2 - (T0)t1) * (T0)t3; });
+}
+
+
 }
 ZENO_NAMESPACE_END
