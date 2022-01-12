7bbb9b89551067bebabf1e15490d72110fcc99d1
zhouhang95 <765229842@qq.com>
2022-01-12 17:24:57 +0800

    code2

--- projects/oldzenbase/mesh/zelloWorld.cpp
+++ projects/oldzenbase/mesh/zelloWorld.cpp
@@ -26,21 +26,18 @@ struct StringToMesh : zeno::INode {
         auto spacing = get_input("spacing")->as<zeno::NumericObject>()->get<float>();
         auto list = std::make_shared<zeno::ListObject>();
         auto list2 = std::make_shared<zeno::ListObject>();
-        auto AZ = get_input("AZmesh")->as<zeno::ListObject>();
         auto zello = get_input("string")->as<zeno::StringObject>()->get();
         std::vector<char> chars(zello.c_str(), zello.c_str() + zello.size() + 1u);
         for(int i=0;i<chars.size();i++)
             std::cout<<chars[i]<<std::endl;
         int count = 0;
         for (auto c:chars) {
-            if((c>='a'&&c<='z') ||(c>='A'&&c<='Z')){
-                char cu = toupper(c);
-                int idx = cu - 'A';
-                std::cout<<cu<<idx<<std::endl;
+            if (33 <= c && c <= 126) {
+                int idx = c - 33;
                 auto vec = zeno::IObject::make<zeno::NumericObject>();
                 vec->set<zeno::vec3f>(zeno::vec3f((float)count * spacing, 0.0f,0.0f));
                 //auto p = zeno::IObject::make<PrimitiveObject>();
-                auto const &obj = smart_any_cast<std::shared_ptr<IObject>>(AZ->arr[idx]);
+                auto const &obj = smart_any_cast<std::shared_ptr<IObject>>(alphaset->arr[idx]);
                 auto p = obj->clone();
                 //p->copy(dynamic_cast<PrimitiveObject *>(obj.get()));
                 list->arr.push_back(std::move(p));
