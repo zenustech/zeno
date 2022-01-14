7725a5ef52b03db2eefae9a65a0407284441ff62
archibate <1931127624@qq.com>
2022-01-14 17:02:16 +0800

    ma

--- .POC/ruipin/bound/main.cpp
+++ .POC/ruipin/bound/main.cpp
@@ -5,147 +5,39 @@
 #include <cstdlib>
 #include <benchmark/benchmark.h>
 #include <tbb/parallel_for.h>
-#include <tbb/blocked_range3d.h>
+#include <tbb/blocked_range2d.h>
 #include <x86intrin.h>
 #include "mtprint.h"
 #include <array>
 #include <atomic>
 
-constexpr size_t n = 512;
+template <size_t kSize, size_t kOffset, class Value>
+struct Grid {
+    std::array<Value, (kSize + kOffset * 2) * (kSize + kOffset * 2)> mArr;
 
-std::vector<float> a(n * n * n);
-std::vector<float> b(n * n * n);
-
-static void copy() {
-#if 1
-    tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, 0, n, 0, n),
-    [&] (tbb::blocked_range3d<size_t> const &r) {
-        for (size_t z = r.pages().begin(); z < r.pages().end(); z++) {
-            for (size_t y = r.cols().begin(); y < r.cols().end(); y++) {
-                for (size_t x = r.rows().begin(); x < r.rows().end(); x++) {
-                    b[(z * n + y) * n + x] = a[(z * n + y) * n + x];
-                }
-            }
-        }
-    });
-#else
-    std::memcpy(a.data(), b.data(), n * n * n * sizeof(float));
-#endif
-}
-
-static void BM_copy(benchmark::State &bm) {
-    for (auto _: bm) {
-        copy();
+    constexpr Value &operator()(size_t i, size_t j) {
+        return mArr[(j + kOffset) * (kSize + kOffset * 2) + (i + kOffset)];
     }
-}
-BENCHMARK(BM_copy);
+};
 
-static void jacobi() {
-    tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, 0, n, 0, n),
-    [&] (tbb::blocked_range3d<size_t> const &r) {
-        for (size_t z = std::max<size_t>(1, r.pages().begin()); z < std::min(n - 2, r.pages().end()); z++) {
-            for (size_t y = std::max<size_t>(1, r.cols().begin()); y < std::min(n - 2, r.cols().end()); y++) {
-                for (size_t x = std::max<size_t>(1, r.rows().begin()); x < std::min(n - 2, r.rows().end()); x++) {
-                    b[(z * n + y) * n + x] = -6 * a[(z * n + y) * n + x]
-                        + a[(z * n + y) * n + x + 1]
-                        + a[(z * n + y) * n + x - 1]
-                        + a[(z * n + y) * n + x + n]
-                        + a[(z * n + y) * n + x - n]
-                        + a[(z * n + y) * n + x + n * n]
-                        + a[(z * n + y) * n + x - n * n]
-                        ;
-                }
+template <size_t kBlock, size_t kSize, size_t kOffset, class Value>
+static void jacobi(Grid<kSize, kOffset, Value> &ogrid, Grid<kSize, kOffset, Value> &igrid) {
+    tbb::parallel_for(tbb::blocked_range2d<size_t>(0, kSize, kBlock, 0, kSize, kBlock),
+    [&] (tbb::blocked_range2d<size_t> const &r) {
+        for (size_t y = r.cols().begin(); y < r.cols().end(); y++) {
+            for (size_t x = r.rows().begin(); x < r.rows().end(); x++) {
+                ogrid(x, y) = igrid(x + 1, y) + igrid(x - 1, y) + igrid(x, y + 1) + igrid(x, y - 1);
             }
         }
     });
 }
 
 static void BM_jacobi(benchmark::State &bm) {
+    static Grid<4096, 0, float> g1, g2;
     for (auto _: bm) {
-        jacobi();
+        jacobi<32>(g1, g2);
     }
 }
 BENCHMARK(BM_jacobi);
-
-static void jacobi_grained() {
-    constexpr size_t gs = 128;
-    tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, gs, 0, n, gs, 0, n, gs),
-    [&] (tbb::blocked_range3d<size_t> const &r) {
-        for (size_t z = std::max<size_t>(1, r.pages().begin()); z < std::min(n - 2, r.pages().end()); z++) {
-            for (size_t y = std::max<size_t>(1, r.cols().begin()); y < std::min(n - 2, r.cols().end()); y++) {
-                for (size_t x = std::max<size_t>(1, r.rows().begin()); x < std::min(n - 2, r.rows().end()); x++) {
-                    b[(z * n + y) * n + x] = -6 * a[(z * n + y) * n + x]
-                        + a[(z * n + y) * n + x + 1]
-                        + a[(z * n + y) * n + x - 1]
-                        + a[(z * n + y) * n + x + n]
-                        + a[(z * n + y) * n + x - n]
-                        + a[(z * n + y) * n + x + n * n]
-                        + a[(z * n + y) * n + x - n * n]
-                        ;
-                }
-            }
-        }
-    }, tbb::simple_partitioner{});
-}
-
-static void BM_jacobi_grained(benchmark::State &bm) {
-    for (auto _: bm) {
-        jacobi_grained();
-    }
-}
-BENCHMARK(BM_jacobi_grained);
-
-static uint64_t mortondec0(uint64_t x) {
-    x = x & 0x5555555555555555ul;
-    x = (x | (x >> 1)) & 0x3333333333333333ull;
-    x = (x | (x >> 2)) & 0x0F0F0F0F0F0F0F0Full;
-    x = (x | (x >> 4)) & 0x00FF00FF00FF00FFull;
-    x = (x | (x >> 8)) & 0x0000FFFF0000FFFFull;
-    x = (x | (x >> 16)) & 0xFFFFFFFFFFFFFFFFull;
-    return x;
-}
-
-static uint32_t mortondec1(uint32_t x) {
-    x &= 0x09249249;
-    x = (x ^ (x >>  2)) & 0x030c30c3;
-    x = (x ^ (x >>  4)) & 0x0300f00f;
-    x = (x ^ (x >>  8)) & 0xff0000ff;
-    x = (x ^ (x >> 16)) & 0x000003ff;
-    return x;
-}
-
-static void jacobi_morton() {
-    constexpr size_t bs = 128;
-    tbb::parallel_for(tbb::blocked_range<size_t>(0, n / bs * n / bs * n / bs),
-    [&] (tbb::blocked_range<size_t> const &ro) {
-        for (size_t t = ro.begin(); t < ro.end(); t++) {
-            size_t x0 = mortondec1(t) * bs;
-            size_t y0 = mortondec1(t >> 1) * bs;
-            size_t z0 = mortondec1(t >> 2) * bs;
-            tbb::blocked_range3d<size_t> r(x0, x0 + bs, y0, y0 + bs, z0, z0 + bs);
-            for (size_t z = std::max<size_t>(1, r.pages().begin()); z < std::min(n - 2, r.pages().end()); z++) {
-                for (size_t y = std::max<size_t>(1, r.cols().begin()); y < std::min(n - 2, r.cols().end()); y++) {
-                    for (size_t x = std::max<size_t>(1, r.rows().begin()); x < std::min(n - 2, r.rows().end()); x++) {
-                        b[(z * n + y) * n + x] = -6 * a[(z * n + y) * n + x]
-                            + a[(z * n + y) * n + x + 1]
-                            + a[(z * n + y) * n + x - 1]
-                            + a[(z * n + y) * n + x + n]
-                            + a[(z * n + y) * n + x - n]
-                            + a[(z * n + y) * n + x + n * n]
-                            + a[(z * n + y) * n + x - n * n]
-                            ;
-                    }
-                }
-            }
-        }
-    });
-}
-
-static void BM_jacobi_morton(benchmark::State &bm) {
-    for (auto _: bm) {
-        jacobi_morton();
-    }
-}
-BENCHMARK(BM_jacobi_morton);
 
 BENCHMARK_MAIN();
---
+++ .POC/ruipin/bound/old.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <benchmark/benchmark.h>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range3d.h>
+#include <x86intrin.h>
+#include "mtprint.h"
+#include <array>
+#include <atomic>
+
+constexpr size_t n = 512;
+
+std::vector<float> a(n * n * n);
+std::vector<float> b(n * n * n);
+
+static void copy() {
+#if 1
+    tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, 0, n, 0, n),
+    [&] (tbb::blocked_range3d<size_t> const &r) {
+        for (size_t z = r.pages().begin(); z < r.pages().end(); z++) {
+            for (size_t y = r.cols().begin(); y < r.cols().end(); y++) {
+                for (size_t x = r.rows().begin(); x < r.rows().end(); x++) {
+                    b[(z * n + y) * n + x] = a[(z * n + y) * n + x];
+                }
+            }
+        }
+    });
+#else
+    std::memcpy(a.data(), b.data(), n * n * n * sizeof(float));
+#endif
+}
+
+static void BM_copy(benchmark::State &bm) {
+    for (auto _: bm) {
+        copy();
+    }
+}
+//BENCHMARK(BM_copy);
+
+static void jacobi() {
+    tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, 0, n, 0, n),
+    [&] (tbb::blocked_range3d<size_t> const &r) {
+        for (size_t z = std::max<size_t>(1, r.pages().begin()); z < std::min(n - 2, r.pages().end()); z++) {
+            for (size_t y = std::max<size_t>(1, r.cols().begin()); y < std::min(n - 2, r.cols().end()); y++) {
+                for (size_t x = std::max<size_t>(1, r.rows().begin()); x < std::min(n - 2, r.rows().end()); x++) {
+                    b[(z * n + y) * n + x] = -6 * a[(z * n + y) * n + x]
+                        + a[(z * n + y) * n + x + 1]
+                        + a[(z * n + y) * n + x - 1]
+                        + a[(z * n + y) * n + x + n]
+                        + a[(z * n + y) * n + x - n]
+                        + a[(z * n + y) * n + x + n * n]
+                        + a[(z * n + y) * n + x - n * n]
+                        ;
+                }
+            }
+        }
+    });
+}
+
+static void BM_jacobi(benchmark::State &bm) {
+    for (auto _: bm) {
+        jacobi();
+    }
+}
+//BENCHMARK(BM_jacobi);
+
+static void jacobi_grained() {
+    constexpr size_t gs = 128;
+    tbb::parallel_for(tbb::blocked_range3d<size_t>(0, n, gs, 0, n, gs, 0, n, gs),
+    [&] (tbb::blocked_range3d<size_t> const &r) {
+        for (size_t z = std::max<size_t>(1, r.pages().begin()); z < std::min(n - 2, r.pages().end()); z++) {
+            for (size_t y = std::max<size_t>(1, r.cols().begin()); y < std::min(n - 2, r.cols().end()); y++) {
+                for (size_t x = std::max<size_t>(1, r.rows().begin()); x < std::min(n - 2, r.rows().end()); x++) {
+                    b[(z * n + y) * n + x] = -6 * a[(z * n + y) * n + x]
+                        + a[(z * n + y) * n + x + 1]
+                        + a[(z * n + y) * n + x - 1]
+                        + a[(z * n + y) * n + x + n]
+                        + a[(z * n + y) * n + x - n]
+                        + a[(z * n + y) * n + x + n * n]
+                        + a[(z * n + y) * n + x - n * n]
+                        ;
+                }
+            }
+        }
+    }, tbb::simple_partitioner{});
+}
+
+static void BM_jacobi_grained(benchmark::State &bm) {
+    for (auto _: bm) {
+        jacobi_grained();
+    }
+}
+//BENCHMARK(BM_jacobi_grained);
+
+static uint64_t mortondec0(uint64_t x) {
+    x = x & 0x5555555555555555ul;
+    x = (x | (x >> 1)) & 0x3333333333333333ull;
+    x = (x | (x >> 2)) & 0x0F0F0F0F0F0F0F0Full;
+    x = (x | (x >> 4)) & 0x00FF00FF00FF00FFull;
+    x = (x | (x >> 8)) & 0x0000FFFF0000FFFFull;
+    x = (x | (x >> 16)) & 0xFFFFFFFFFFFFFFFFull;
+    return x;
+}
+
+// https://fgiesen.wordpress.com/2009/12/13/decoding-morton-codes/
+static uint32_t mortondec1(uint32_t x) {
+    x &= 0x09249249;
+    x = (x ^ (x >>  2)) & 0x030c30c3;
+    x = (x ^ (x >>  4)) & 0x0300f00f;
+    x = (x ^ (x >>  8)) & 0xff0000ff;
+    x = (x ^ (x >> 16)) & 0x000003ff;
+    return x;
+}
+
+template <class T>
+class Streamed {
+    T &m_t;
+
+public:
+    constexpr Streamed(T &t) : m_t(t) {}
+
+    constexpr Streamed &operator=(T const &t) {
+        _mm_stream_si32(&reinterpret_cast<int &>(m_t), reinterpret_cast<int const &>(t));
+        return *this;
+    }
+};
+
+template <class T>
+static Streamed<T> streamed(T &t) { return t; }
+
+template <size_t Step, class It, class Body, class Rest>
+static void tiled_range_for(It a, It b, Body &&body, Rest &&rest) {
+    It i = a, end = b - (Step - 1);
+    for (i = a; i < end; i += Step) {
+        body(i);
+    }
+    for (; i < b; ++i) {
+        rest(i);
+    }
+}
+
+static void jacobi_morton() {
+    constexpr size_t bs = 64;
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, n / bs * n / bs * n / bs),
+    [&] (tbb::blocked_range<size_t> const &ro) {
+        for (size_t t = ro.begin(); t < ro.end(); t++) {
+            size_t x0 = mortondec1(t) * bs;
+            size_t y0 = mortondec1(t >> 1) * bs;
+            size_t z0 = mortondec1(t >> 2) * bs;
+            tbb::blocked_range3d<size_t> r(x0, x0 + bs, y0, y0 + bs, z0, z0 + bs);
+            for (size_t z = std::max<size_t>(1, r.pages().begin()); z < std::min(n - 2, r.pages().end()); z++) {
+                for (size_t y = std::max<size_t>(1, r.cols().begin()); y < std::min(n - 2, r.cols().end()); y++) {
+                    for (size_t x = std::max<size_t>(1, r.rows().begin()); x < std::min(n - 2, r.rows().end()); x++) {
+                        streamed(b[(z * n + y) * n + x]) = -6 * a[(z * n + y) * n + x]
+                            + a[(z * n + y) * n + x + 1]
+                            + a[(z * n + y) * n + x - 1]
+                            + a[(z * n + y) * n + x + n]
+                            + a[(z * n + y) * n + x - n]
+                            //+ a[(z * n + y) * n + x + n * n]
+                            //+ a[(z * n + y) * n + x - n * n]
+                            ;
+                    }
+                }
+            }
+        }
+    });
+}
+
+static void BM_jacobi_morton(benchmark::State &bm) {
+    for (auto _: bm) {
+        jacobi_morton();
+    }
+}
+BENCHMARK(BM_jacobi_morton);
+
+BENCHMARK_MAIN();
