5cedc10a2a7774df940ea92744c22539be0cfe79
archibate <1931127624@qq.com>
2022-02-17 15:30:10 +0800

    fx

--- zeno/zeno/nodes/prim/PrimitiveBent.cpp
+++ zeno/zeno/nodes/prim/PrimitiveBent.cpp
@@ -20,13 +20,13 @@ struct PrimitiveBent : zeno::INode {
         auto limitMin = get_input<zeno::NumericObject>("limitMin")->get<float>();
         auto limitMax = get_input<zeno::NumericObject>("limitMax")->get<float>();
         auto midPoint = get_input<zeno::NumericObject>("midPoint")->get<float>();
+        auto biasDir = get_input<zeno::NumericObject>("biasDir")->get<float>();
         limitMin = std::min(1.f, std::max(0.f, limitMin));
         limitMax = std::min(1.f, std::max(0.f, limitMax));
         if (limitMin > limitMax)
             std::swap(limitMin, limitMax);
         midPoint = std::min(limitMax, std::max(limitMin, midPoint));
-        limitMin -= midPoint;
-        limitMax -= midPoint;
+        biasDir = std::min(1.f, std::max(0.f, biasDir));
 
         auto origin = has_input("origin") ? get_input<zeno::NumericObject>("origin")->get<vec3f>() : vec3f(0, 0, 0);
         auto tangent = has_input("tangent") ? get_input<zeno::NumericObject>("tangent")->get<vec3f>() : vec3f(0, 1, 0);
@@ -37,23 +37,35 @@ struct PrimitiveBent : zeno::INode {
         tangent = orb.tangent;
 
         if (std::abs(angle) > 0.005f && limitMax - limitMin > 0.001f) {
-            angle *= M_PI / 180;
+            angle *= -M_PI / 180;
             angle /= limitMax - limitMin;
 
-            auto acc = parallel_reduce_array(prim->size(), vec2f(prim->size() ? dot(tangent, prim->verts[0] + origin) : 0.f), [&] (size_t i) {
-                return vec2f(dot(tangent, prim->verts[i] + origin));
+            auto acc = parallel_reduce_array(prim->size(), vec2f(prim->size() ? dot(tangent, prim->verts[0]) : 0.f), [&] (size_t i) {
+                return vec2f(dot(tangent, prim->verts[i]));
             }, [&] (auto a, auto b) { return vec2f(std::min(a[0], b[0]), std::max(a[1], b[1])); });
+            auto accDir = parallel_reduce_array(prim->size(), vec2f(prim->size() ? dot(direction, prim->verts[0]) : 0.f), [&] (size_t i) {
+                return vec2f(dot(direction, prim->verts[i]));
+            }, [&] (auto a, auto b) { return vec2f(std::min(a[0], b[0]), std::max(a[1], b[1])); });
+
+            if (get_param<bool>("useOrigin")) {
+                biasDir = (dot(direction, origin) - accDir[0]) / (accDir[1] - accDir[0]);
+                midPoint = (dot(tangent, origin) - acc[0]) / (acc[1] - acc[0]);
+            }
+
             auto height = acc[1] - acc[0];
             auto average = (acc[1] + acc[0]) * 0.5f;
             auto middle = acc[1] * midPoint + acc[0] * (1 - midPoint);
             auto truemid = height * (0.5f - midPoint);
             auto radius = height / angle;
             auto inv_height = 1 / height;
-            auto delta = dot(direction, origin) * direction - dot(orb.bitangent, origin) * orb.bitangent + tangent * average;
+
+            biasDir = (accDir[1] + accDir[0]) * 0.5f + (biasDir - 0.5f) * (accDir[1] - accDir[0]);
+            limitMin -= midPoint;
+            limitMax -= midPoint;
 
 #pragma omp parallel for
             for (int i = 0; i < prim->verts.size(); i++) {
-                auto pos = prim->verts[i] + origin;
+                auto pos = prim->verts[i] + biasDir * direction;
                 auto tanpos = dot(tangent, pos);
                 auto dirpos = dot(direction, pos);
                 auto fac = (tanpos - middle) * inv_height;
@@ -73,7 +85,7 @@ struct PrimitiveBent : zeno::INode {
                 newdirpos -= radius;
                 pos += (newtanpos - tanpos) * tangent + (newdirpos - dirpos) * direction;
 
-                prim->verts[i] = pos + delta;
+                prim->verts[i] = pos + biasDir * direction + tangent * average;
             }
 
         }
@@ -92,11 +104,13 @@ ZENDEFNODE(PrimitiveBent, {
     {"float", "limitMin", "0"},
     {"float", "limitMax", "1"},
     {"float", "midPoint", "0.5"},
+    {"float", "biasDir", "0.5"},
     },
     {
     {"PrimitiveObject", "prim"},
     },
     {
+    {"bool", "useOrigin", "0"},
     },
     {"primitive"},
 });
