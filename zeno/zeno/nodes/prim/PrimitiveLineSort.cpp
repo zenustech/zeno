fc40ac45edcf6e0632742f8a7d50b0f397c1dc94
archibate <1931127624@qq.com>
2022-02-17 23:50:11 +0800

    work

--- zeno/zeno/nodes/prim/PrimitiveLineSort.cpp
+++ zeno/zeno/nodes/prim/PrimitiveLineSort.cpp
@@ -2,7 +2,7 @@
 #include <zeno/types/PrimitiveObject.h>
 #include <zeno/types/StringObject.h>
 #include <zeno/types/NumericObject.h>
-#include <zeno/utils/variantswitch.h>
+#include <zeno/utils/logger.h>
 #include <unordered_map>
 #include <cassert>
 
@@ -14,37 +14,45 @@ void primLineSort(PrimitiveObject *prim) {
         std::unordered_multimap<int, int> v2l;
         for (int i = 0; i < prim->lines.size(); i++) {
             auto line = prim->lines[i];
-            v2l.emplace(line[0], i);
-        }
-
-        std::vector<int> tovisit;
-        tovisit.reserve(prim->lines.size());
-        for (int i = 0; i < prim->lines.size(); i++) {
-            auto line = prim->lines[i];
-            tovisit.push_back(line[0]);
+            //log_debug("line {} {}", line[0], line[1]);
+            v2l.emplace(line[1], i);
         }
 
         int nsorted = 0;
         visited.resize(prim->verts.size(), -1);
-        while (!tovisit.empty()) {
-            int vert = tovisit.back();
-            tovisit.pop_back();
-            if (visited.at(vert) != -1)
-                continue;
-            visited[vert] = nsorted++;
+
+        auto visit = [&] (auto &visit, int vert) -> void {
+            if (visited[vert] != -1)
+                return;
+            visited[vert] = -2;
             auto [it0, it1] = v2l.equal_range(vert);
             for (auto it = it0; it != it1; ++it) {
                 auto line = prim->lines[it->second];
-                auto next = line[1];
-                tovisit.push_back(next);
+                //assert(line[1] == vert);
+                auto next = line[0];
+                visit(visit, next);
             }
+            visited[vert] = nsorted++;
+        };
+
+        for (int i = 0; i < prim->lines.size(); i++) {
+            auto line = prim->lines[i];
+            visit(visit, line[1]);
         }
-        for (int i = 0; i < visited.size(); i++) {
-            if (visited[i] == -1) {
-                visited[i] = nsorted++;
+
+        log_debug("sorted {} of {}", nsorted, visited.size());
+        if (nsorted != visited.size()) {
+            for (int i = 0; i < visited.size(); i++) {
+                if (visited[i] == -1) {
+                    visited[i] = nsorted++;
+                }
             }
+            assert(nsorted == visited.size());
         }
-        assert(nsorted == visited.size());
+
+        //for (int i = 0; i < visited.size(); i++) {
+            //log_debug("{} -> {} = {} {} {}", i, visited[i], prim->verts[i][0], prim->verts[i][1], prim->verts[i][2]);
+        //}
     }
 
     {
@@ -78,12 +86,21 @@ void primLineSort(PrimitiveObject *prim) {
                 int j = visited[i];
                 newArr[j] = arr[i];
             }
+            /*if constexpr (std::is_same_v<vec3f, std::decay_t<decltype(arr[0])>>) {
+                for (int i = 0; i < newArr.size(); i++) {
+                    log_info("{} = {} {} {}", i, newArr[i][0], newArr[i][1], newArr[i][2]);
+                }
+            }*/
             std::swap(arr, newArr);
         };
         revampvec(prim->verts.values);
-        prim->foreach_attr([&] (auto const &key, auto &attr) {
+        prim->verts.foreach_attr([&] (auto const &key, auto &attr) {
             revampvec(attr);
         });
+
+        //for (int i = 0; i < prim->verts.size(); i++) {
+            //log_info("{} = {} {} {}", i, prim->verts[i][0], prim->verts[i][1], prim->verts[i][2]);
+        //}
     }
 }
 
