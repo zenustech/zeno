ffb2f77aeedc7aedf92708defc047574fba3f08d
archibate <1931127624@qq.com>
2022-02-20 11:20:15 +0800

    axisnodes

---
+++ src/zeno/nodes/AxisNodes.cpp
@@ -0,0 +1,67 @@
+#include <zeno/zeno.h>
+#include <zeno/types/AxisObject.h>
+#include <zeno/utils/logger.h>
+
+namespace zeno {
+namespace {
+
+struct ExtractAxis : zeno::INode {
+    virtual void apply() override {
+        auto p = get_input<AxisObject>("axis");
+        set_output2("origin", p->origin);
+        set_output2("axisX", p->axisX);
+        set_output2("axisY", p->axisY);
+        set_output2("axisZ", p->axisZ);
+    }
+};
+
+ZENDEFNODE(ExtractAxis, {
+    {
+    {"AxisObject", "axis"},
+    },
+    {
+    {"vec3f", "origin"},
+    {"vec3f", "axisX"},
+    {"vec3f", "axisY"},
+    {"vec3f", "axisZ"},
+    },
+    {},
+    {"math"},
+});
+
+struct MakeAxis : zeno::INode {
+    virtual void apply() override {
+        auto origin = get_input2<vec3f>("origin");
+        auto axisX = get_input2<vec3f>("axisX");
+        auto axisY = get_input2<vec3f>("axisY");
+        auto axisZ = get_input2<vec3f>("axisZ");
+        auto p = std::make_shared<AxisObject>(AxisObject{origin, axisX, axisY, axisZ});
+        auto by = get_param<std::string>("normalize");
+        if (by == "X")
+            p->renormalizeByX();
+        else if (by == "Y")
+            p->renormalizeByY();
+        else if (by == "Z")
+            p->renormalizeByZ();
+        set_output("axis", std::move(p));
+    }
+};
+
+ZENDEFNODE(MakeAxis, {
+    {
+    {"vec3f", "origin"},
+    {"vec3f", "axisX"},
+    {"vec3f", "axisY"},
+    {"vec3f", "axisZ"},
+    },
+    {
+    {"AxisObject", "axis"},
+    },
+    {
+    {"enum off X Y Z", "normalize", "off"},
+    },
+    {"math"},
+});
+
+}
+}
---
+++ src/zeno/types/AxisObject.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <zeno/core/IObject.h>
+#include <zeno/utils/vec.h>
+#include <zeno/utils/orthonormal.h>
+
+namespace zeno {
+
+struct AxisObject : IObjectClone<AxisObject> {
+    vec3f origin, axisX, axisY, axisZ;
+
+    AxisObject(vec3f const &origin, vec3f const &axisX, vec3f const &axisY, vec3f const &axisZ)
+        : origin(origin), axisX(axisX), axisY(axisY), axisZ(axisZ) {}
+
+    void renormalizeByX() {
+        orthonormal orb(axisX, axisY);
+        axisX = orb.normal;
+        axisY = orb.tangent;
+        axisZ = orb.bitangent;
+    }
+
+    void renormalizeByY() {
+        orthonormal orb(axisY, axisZ);
+        axisY = orb.normal;
+        axisZ = orb.tangent;
+        axisX = orb.bitangent;
+    }
+
+    void renormalizeByZ() {
+        orthonormal orb(axisZ, axisX);
+        axisZ = orb.normal;
+        axisX = orb.tangent;
+        axisY = orb.bitangent;
+    }
+
+    vec3f transformPoint(vec3f const &v) {
+        return origin + axisX * v[0] + axisY * v[1] + axisZ + v[2];
+    }
+
+    vec3f transformDirection(vec3f const &v) {
+        return axisX * v[0] + axisY * v[1] + axisZ + v[2];
+    }
+};
+
+} // namespace zeno
