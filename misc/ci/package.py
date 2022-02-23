efc6779dcba18dc6e763e4c34628cf14576d6c23
archibate <1931127624@qq.com>
2022-02-23 12:42:11 +0800

    cihoulinuxdep

--- misc/ci/package.py
+++ misc/ci/package.py
@@ -4,14 +4,14 @@ import subprocess
 import shutil
 
 binpath = os.path.join('build', 'bin')
-targets = ['zenoedit', 'zenorunner']
 
 if sys.platform == 'win32':
-    for target in targets:
+    for target in ['zenoedit', 'zenorunner']:
         subprocess.check_call([
             '..\\Qt\\5.15.2\\msvc2019_64\\bin\\windeployqt.exe',
             os.path.join(binpath, target + '.exe'),
         ])
+    shutil.copyfile(os.path.join('misc', 'ci', 'launch', '000_start.bat'), binpath)
 elif sys.platform == 'linux':
     subprocess.check_call([
         'wget',
@@ -24,10 +24,21 @@ elif sys.platform == 'linux':
         '+x',
         '../linuxdeployqt',
     ])
-    for target in targets:
+    os.mkdir(os.path.join(binpath, 'usr'))
+    os.mkdir(os.path.join(binpath, 'usr', 'lib'))
+    os.mkdir(os.path.join(binpath, 'usr', 'bin'))
+    shutil.copyfile(os.path.join('misc', 'ci', 'launch', '000_start.sh'), binpath)
+    shutil.copytree(os.path.join('misc', 'ci', 'share'), os.path.join(binpath, 'usr', 'share'))
+    for target in ['zenoedit', 'zenorunner']:
+        shutil.move(os.path.join(binpath, target), os.path.join(binpath, 'usr', 'bin', target))
+    for target in os.listdir(binpath):
+        if 'so' in target.split('.'):
+            shutil.move(os.path.join(binpath, target), os.path.join(binpath, 'usr', 'lib', target))
         subprocess.check_call([
             '../linuxdeployqt',
-            os.path.join(binpath, target),
+            os.path.join(binpath, 'usr', 'share', ', 'applications', 'zeno.desktop'),
+            '-executable=' + os.path.join(binpath, 'usr', 'bin', 'zenorunner')
+            '-bundle-non-qt-libs',
         ])
 else:
     assert False, sys.platform
