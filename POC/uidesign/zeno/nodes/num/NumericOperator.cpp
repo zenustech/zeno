a865811ec80bf20c438296020d5a5d5730e48816
zhxx1987 <zhangshinshin@gmail.com>
2022-03-01 15:00:23 +0800

    function refix

--- POC/uidesign/zeno/nodes/num/NumericOperator.cpp
+++ POC/uidesign/zeno/nodes/num/NumericOperator.cpp
@@ -151,7 +151,8 @@ struct NumericOperator : zeno::INode {
         // todo: no ternary ops..
         std::visit([op, &ret](auto const &lhs, auto const &rhs) {
 
-            if (op == "copy" || op == "copyr") ret->value = remove_bool(lhs);
+            if (op == "copy") ret->value = remove_bool(lhs);
+            if (op == "copyr") lhs->value = remove_bool(rhs->value);
 #define _PER_OP(name) else if (op == #name) ret->value = remove_bool(op_##name(lhs));
 _PER_OP(pos)
 _PER_OP(neg)
