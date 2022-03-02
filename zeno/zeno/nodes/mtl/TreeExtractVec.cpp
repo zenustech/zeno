d39ed50fb77766985d77acaa9c97b2d12f51c71d
archibate <1931127624@qq.com>
2022-03-02 11:24:00 +0800

    extract

---
+++ zeno/zeno/nodes/mtl/TreeExtractVec.cpp
@@ -0,0 +1,54 @@
+#include <zeno/zeno.h>
+#include <zeno/extra/TreeNode.h>
+#include <zeno/types/TreeObject.h>
+#include <zeno/utils/string.h>
+
+namespace zeno {
+
+
+namespace {
+struct ImplTreeExtractVec : TreeNode {
+    int comp{};
+
+    virtual int determineType(EmissionPass *em) override {
+        auto in1 = em->determineType(get_input("in").get());
+        return 1;
+    }
+
+    virtual void emitCode(EmissionPass *em) override {
+        auto in = em->determineExpr(get_input("in").get());
+
+        em->emitCode(in + "." + "xyzw"[comp]);
+    }
+};
+}
+
+struct TreeExtractVec : INode {
+    virtual void apply() override {
+        for (int i = 0; i < 4; i++) {
+            auto node = std::make_shared<ImplTreeExtractVec>();
+            node->comp = i;
+            auto tree = std::make_shared<TreeObject>(node.get());
+            tree->extra_data = std::move(node);
+            set_output(std::string("xyzw"[i], 1), std::move(tree));
+        }
+    }
+};
+
+
+ZENDEFNODE(TreeExtractVec, {
+    {
+        {"vec3f", "in"},
+    },
+    {
+        {"float", "x", "0"},
+        {"float", "y", "0"},
+        {"float", "z", "0"},
+        {"float", "w", "0"},
+    },
+    {},
+    {"tree"},
+});
+
+
+}
