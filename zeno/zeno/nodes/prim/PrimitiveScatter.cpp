8fe9189ff69e002f33912be30cbc498c583747ae
archibate <1931127624@qq.com>
2022-03-03 16:07:23 +0800

    ci

---
+++ zeno/zeno/nodes/prim/PrimitiveScatter.cpp
@@ -0,0 +1,125 @@
+#include <zeno/zeno.h>
+#include <zeno/types/PrimitiveObject.h>
+#include <zeno/types/StringObject.h>
+#include <zeno/types/NumericObject.h>
+#include <zeno/utils/orthonormal.h>
+#include <zeno/utils/parallel.h>
+#include <sstream>
+#include <iostream>
+#include <random>
+#include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+namespace zeno {
+
+struct PrimitiveScatter : INode {
+    virtual void apply() override {
+        auto prim = get_input<PrimitiveObject>("prim");
+        auto npoints = get_input<NumericObject>("npoints")->get<int>();
+        auto seed = get_input<NumericObject>("seed")->get<int>();
+        auto type = get_param<std::string>("type");
+        auto retprim = std::make_shared<PrimitiveObject>();
+
+        if (type == "tris") {
+            float total = 0;
+            std::vector<float> cdf(prim->tris.size());
+            for (size_t i = 0; i < prim->tris.size(); i++) {
+                auto const &ind = prim->tris[i];
+                std::cout << '?' << i << ' ' << ind[0] << std::endl;
+                auto a = prim->verts[ind[0]];
+                auto b = prim->verts[ind[1]];
+                auto c = prim->verts[ind[2]];
+                auto area = length(cross(c - a, c - b));
+                total += area;
+                cdf[i] = total;
+            }
+            auto inv_total = 1 / total;
+            for (size_t i = 0; i < cdf.size(); i++) {
+                cdf[i] *= inv_total;
+            }
+
+            std::mt19937 gen(seed);
+            std::uniform_real_distribution<float> unif;
+
+            retprim->verts.reserve(npoints);
+            for (size_t i = 0; i < npoints; i++) {
+                auto val = unif(gen);
+                auto it = std::lower_bound(cdf.begin(), cdf.end(), val);
+                size_t index = it - cdf.begin();
+                index = std::min(index, prim->tris.size() - 1);
+                auto const &ind = prim->tris[index];
+                std::cout << '!' << index << ' ' << ind[0] << std::endl;
+                auto a = prim->verts[ind[0]];
+                auto b = prim->verts[ind[1]];
+                auto c = prim->verts[ind[2]];
+                auto r1 = std::sqrt(unif(gen));
+                auto r2 = unif(gen);
+                auto p = (1 - r1) * a + (r1 * (1 - r2)) * b + (r1 * r2) * c;
+                retprim->verts.push_back(p);
+            }
+
+        } else if (type == "lines") {
+            float total = 0;
+            std::vector<float> cdf(prim->tris.size());
+            for (size_t i = 0; i < prim->tris.size(); i++) {
+                auto const &ind = prim->tris[i];
+                std::cout << '?' << i << ' ' << ind[0] << std::endl;
+                auto a = prim->verts[ind[0]];
+                auto b = prim->verts[ind[1]];
+                auto c = prim->verts[ind[2]];
+                auto area = length(cross(c - a, c - b));
+                total += area;
+                cdf[i] = total;
+            }
+            auto inv_total = 1 / total;
+            for (size_t i = 0; i < cdf.size(); i++) {
+                cdf[i] *= inv_total;
+            }
+
+            std::mt19937 gen(seed);
+            std::uniform_real_distribution<float> unif;
+
+            retprim->verts.reserve(npoints);
+            for (size_t i = 0; i < npoints; i++) {
+                auto val = unif(gen);
+                auto it = std::lower_bound(cdf.begin(), cdf.end(), val);
+                size_t index = it - cdf.begin();
+                index = std::min(index, prim->tris.size() - 1);
+                auto const &ind = prim->tris[index];
+                std::cout << '!' << index << ' ' << ind[0] << std::endl;
+                auto a = prim->verts[ind[0]];
+                auto b = prim->verts[ind[1]];
+                auto c = prim->verts[ind[2]];
+                auto r1 = std::sqrt(unif(gen));
+                auto r2 = unif(gen);
+                auto p = (1 - r1) * a + (r1 * (1 - r2)) * b + (r1 * r2) * c;
+                retprim->verts.push_back(p);
+            }
+
+        } else {
+            throw;
+        }
+
+        set_output("points", std::move(retprim));
+    }
+};
+
+
+ZENDEFNODE(PrimitiveScatter, {
+    {
+    {"PrimitiveObject", "prim"},
+    {"int", "npoints", "100"},
+    {"int", "seed", "0"},
+    },
+    {
+    {"PrimitiveObject", "points"},
+    },
+    {
+    {"enum tris lines", "type", "tris"},
+    },
+    {"primitive"},
+});
+
+}
