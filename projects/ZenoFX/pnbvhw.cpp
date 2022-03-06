19633c0a918172977d726122993970ac858b3cfa
archibate <1931127624@qq.com>
2022-03-06 11:25:33 +0800

    cihou wxl

--- projects/ZenoFX/pnbvhw.cpp
+++ projects/ZenoFX/pnbvhw.cpp
@@ -3,6 +3,7 @@
 #include <zeno/types/PrimitiveObject.h>
 #include <zeno/types/NumericObject.h>
 #include <zeno/types/DictObject.h>
+#include "LinearBvh.h"
 #include <zfx/zfx.h>
 #include <zfx/x64.h>
 #include <cassert>
@@ -13,6 +14,7 @@
 #if defined(_OPENMP)
 #include <omp.h>
 #endif
+
 namespace {
 
 static zfx::Compiler compiler;
@@ -25,121 +27,12 @@ struct Buffer {
     int which = 0;
 };
 
-struct HashGrid : zeno::IObject {
-    float inv_dx;
-    float radius;
-    float radius_sqr;
-    float radius_sqr_min;
-    std::vector<zeno::vec3f> const &refpos;
-
-    std::vector<std::vector<int>> table;
-
-//#define DILEI
-#define XUBEN
-
-    int hash(int x, int y, int z) {
-#ifdef XUBEN
-        return (x%gridRes[0]+gridRes[0])%gridRes[0] + ((y%gridRes[1]+gridRes[1])%gridRes[1]) * gridRes[0] + ((z%gridRes[2]+gridRes[2])%gridRes[2]) * gridRes[0] * gridRes[1];
-#else
-        return ((73856093 * x) ^ (19349663 * y) ^ (83492791 * z)) % table.size();
-#endif
-    }
-
-#ifdef XUBEN
-    zeno::vec3f pMin, pMax;
-    zeno::vec3i gridRes;
-#endif
-
-    HashGrid(std::vector<zeno::vec3f> const &refpos_,
-            float radius_, float radius_min)
-        : refpos(refpos_) {
-
-        radius = radius_;
-        radius_sqr = radius * radius;
-        radius_sqr_min = radius_min < 0.f ? -1.f : radius_min * radius_min;
-#ifdef DILEI
-        inv_dx = 0.5f / radius;
-#else
-        inv_dx = 1.0f / radius;
-#endif
-
-#ifdef XUBEN
-        pMin = refpos[0];
-        pMax = refpos[0];
-        for (int i = 1; i < refpos.size(); i++) {
-            auto coor = refpos[i];
-            pMin = zeno::min(pMin, coor);
-            pMax = zeno::max(pMax, coor);
-        }
-        pMin -= radius;
-        pMax += radius;
-        gridRes = zeno::toint(zeno::floor((pMax - pMin) * inv_dx)) + 1;
-
-        dbg_printf("grid res: %dx%dx%d\n", gridRes[0], gridRes[1], gridRes[2]);
-        table.clear();
-        table.resize(gridRes[0] * gridRes[1] * gridRes[2]);
-#else
-        int table_size = refpos.size() / 8;
-        dbg_printf("table size: %d\n", table_size);
-        table.clear();
-        table.resize(table_size);
-#endif
-
-        for (int i = 0; i < refpos.size(); i++) {
-#ifdef XUBEN
-            auto coor = zeno::toint(zeno::floor((refpos[i] - pMin) * inv_dx));
-#else
-            auto coor = zeno::toint(zeno::floor(refpos[i] * inv_dx));
-#endif
-            auto key = hash(coor[0], coor[1], coor[2]);
-            table[key].push_back(i);
-        }
-    }
-
-    template <class F>
-    void iter_neighbors(zeno::vec3f const &pos, F const &f) {
-#ifdef XUBEN
-#ifdef DILEI
-        auto coor = zeno::toint(zeno::floor((pos - pMin) * inv_dx - 0.5f));
-#else
-        auto coor = zeno::toint(zeno::floor((pos - pMin) * inv_dx));
-#endif
-#else
-#ifdef DILEI
-        auto coor = zeno::toint(zeno::floor(pos * inv_dx - 0.5f));
-#else
-        auto coor = zeno::toint(zeno::floor(pos * inv_dx));
-#endif
-#endif
-#ifdef DILEI
-        for (int dz = 0; dz < 2; dz++) {
-            for (int dy = 0; dy < 2; dy++) {
-                for (int dx = 0; dx < 2; dx++) {
-#else
-        for (int dz = -1; dz < 2; dz++) {
-            for (int dy = -1; dy < 2; dy++) {
-                for (int dx = -1; dx < 2; dx++) {
-#endif
-                    int key = hash(coor[0] + dx, coor[1] + dy, coor[2] + dz);
-                    for (int pid: table[key]) {
-                        //auto dist = refpos[pid] - pos;
-                        //auto dis2 = zeno::dot(dist, dist);
-                        //if (dis2 <= radius_sqr && dis2 > radius_sqr_min) {
-                            f(pid);
-                        //}
-                    }
-                }
-            }
-        }
-    }
-};
-
-static void vectors_wrangle
+static void bvh_vectors_wrangle
     ( zfx::x64::Executable *exec
     , std::vector<Buffer> const &chs
     , std::vector<Buffer> const &chs2
     , std::vector<zeno::vec3f> const &pos
-    , HashGrid *hashgrid
+    , zeno::LBvh *lbvh
     ) {
     if (chs.size() == 0)
         return;
@@ -151,7 +44,7 @@ static void vectors_wrangle
             if (!chs[k].which)
                 ctx.channel(k)[0] = chs[k].base[chs[k].stride * i];
         }
-        hashgrid->iter_neighbors(pos[i], [&] (int pid) {
+        lbvh->iter_neighbors(pos[i], [&] (int pid) {
             for (int k = 0; k < chs.size(); k++) {
                 if (chs[k].which)
                     ctx.channel(k)[0] = chs2[k].base[chs2[k].stride * pid];
@@ -165,30 +58,29 @@ static void vectors_wrangle
     }
 }
 
-struct ParticlesBuildHashGrid : zeno::INode {
+struct ParticlesBuildBvh : zeno::INode {
     virtual void apply() override {
         auto primNei = get_input<zeno::PrimitiveObject>("primNei");
         float radius = get_input<zeno::NumericObject>("radius")->get<float>();
         float radiusMin = has_input("radiusMin") ?
             get_input<zeno::NumericObject>("radiusMin")->get<float>() : -1.f;
-        auto hashgrid = std::make_shared<HashGrid>(
-                primNei->attr<zeno::vec3f>("pos"), radius, radiusMin);
-        set_output("hashGrid", std::move(hashgrid));
+        auto lbvh = std::make_shared<zeno::LBvh>(primNei, radius);
+        set_output("lbvh", std::move(lbvh));
     }
 };
 
-ZENDEFNODE(ParticlesBuildHashGrid, {
+ZENDEFNODE(ParticlesBuildBvh, {
     {{"PrimitiveObject", "primNei"}, {"numeric:float", "radius"}, {"numeric:float", "radiusMin"}},
-    {{"hashgrid", "hashGrid"}},
+    {{"LBvh", "lbvh"}},
     {},
     {"zenofx"},
 });
 
-struct ParticlesNeighborWrangle : zeno::INode {
+struct ParticlesNeighborBvhWrangle : zeno::INode {
     virtual void apply() override {
         auto prim = get_input<zeno::PrimitiveObject>("prim");
         auto primNei = get_input<zeno::PrimitiveObject>("primNei");
-        auto hashgrid = get_input<HashGrid>("hashGrid");
+        auto lbvh = get_input<zeno::LBvh>("lbvh");
         auto code = get_input<zeno::StringObject>("zfxCode")->get();
 
         zfx::Options opts(zfx::Options::for_x64);
@@ -329,15 +221,15 @@ struct ParticlesNeighborWrangle : zeno::INode {
             chs2[i] = iob;
         }
 
-        vectors_wrangle(exec, chs, chs2, prim->attr<zeno::vec3f>("pos"),
-                hashgrid.get());
+        bvh_vectors_wrangle(exec, chs, chs2, prim->attr<zeno::vec3f>("pos"),
+                lbvh.get());
 
         set_output("prim", std::move(prim));
     }
 };
 
-ZENDEFNODE(ParticlesNeighborWrangle, {
-    {{"PrimitiveObject", "prim"}, {"PrimitiveObject", "primNei"}, {"HashGrid", "hashGrid"},
+ZENDEFNODE(ParticlesNeighborBvhWrangle, {
+    {{"PrimitiveObject", "prim"}, {"PrimitiveObject", "primNei"}, {"LBvh", "lbvh"},
      {"string", "zfxCode"}, {"DictObject:NumericObject", "params"}},
     {{"PrimitiveObject", "prim"}},
     {},
