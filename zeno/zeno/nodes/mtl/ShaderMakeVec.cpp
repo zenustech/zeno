9dab1bc438e97f98f1d1d685ecf2fe422d6a0333
archibate <1931127624@qq.com>
2022-03-06 14:46:25 +0800

    re

--- zeno/zeno/nodes/mtl/ShaderMakeVec.cpp
+++ zeno/zeno/nodes/mtl/ShaderMakeVec.cpp
@@ -16,7 +16,7 @@ struct ShaderPackVec : ShaderNode {
         auto t4 = has_input("w") ? em->determineType(get_input("w").get()) : 0;
         ty = t1 + t2 + t3 + t4;
         if (ty > 4)
-            throw zeno::Exception("ShaderMakeVec expect sum of dimension to no more than 4");
+            throw zeno::Exception("ShaderPackVec expect sum of dimension to no more than 4");
         return ty;
     }
 
@@ -59,43 +59,8 @@ ZENDEFNODE(ShaderPackVec, {
 });
 
 
-struct ShaderMakeVec : ShaderNode {
-    int ty{};
-
-    virtual int determineType(EmissionPass *em) override {
-        auto t1 = has_input("x") ? em->determineType(get_input("x").get()) : 0;
-        auto t2 = has_input("y") ? em->determineType(get_input("y").get()) : 0;
-        auto t3 = has_input("z") ? em->determineType(get_input("z").get()) : 0;
-        auto t4 = has_input("w") ? em->determineType(get_input("w").get()) : 0;
-        ty = t1 + t2 + t3 + t4;
-        if (ty > 4)
-            throw zeno::Exception("ShaderMakeVec expect sum of dimension to no more than 4");
-        return ty;
-    }
-
-    virtual void emitCode(EmissionPass *em) override {
-        std::string exp;
-        if (has_input("x")) {
-            if (!exp.empty()) exp += ", ";
-            exp += em->determineExpr(get_input("x").get());
-        }
-        if (has_input("y")) {
-            if (!exp.empty()) exp += ", ";
-            exp += em->determineExpr(get_input("y").get());
-        }
-        if (has_input("z")) {
-            if (!exp.empty()) exp += ", ";
-            exp += em->determineExpr(get_input("z").get());
-        }
-        if (has_input("w")) {
-            if (!exp.empty()) exp += ", ";
-            exp += em->determineExpr(get_input("w").get());
-        }
-
-        em->emitCode(em->typeNameOf(ty) + "(" + exp + ")");
-    }
+struct ShaderMakeVec : ShaderPackVec {
 };
-
 
 ZENDEFNODE(ShaderMakeVec, {
     {
