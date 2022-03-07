b50d23448f5622906087651fcf674d258b3ab12f
archibate <1931127624@qq.com>
2022-03-07 17:12:23 +0800

    vecv

--- ui/zenotest/main.cpp
+++ ui/zenotest/main.cpp
@@ -16,6 +16,72 @@ struct fvector_view {
     fvector_view(fvector &arr) : m_arr(arr) {
     }
 
+    float &operator[](std::size_t idx) const {
+        return m_arr[idx];
+    }
+
+    float &at(std::size_t idx) const {
+        return m_arr.at(idx);
+    }
+
+    auto begin() const {
+        return m_arr.begin();
+    }
+
+    auto end() const {
+        return m_arr.end();
+    }
+
+    float &front() const {
+        return m_arr.front();
+    }
+
+    float &back() const {
+        return m_arr.back();
+    }
+
+    std::size_t size() const {
+        return m_arr.size();
+    }
+
+    float *data() const {
+        return m_arr.data();
+    }
+
+    void clear() const {
+        m_arr.clear();
+    }
+
+    void resize(std::size_t n) const {
+        m_arr.resize(n);
+    }
+
+    std::size_t capacity() const {
+        return m_arr.capacity();
+    }
+
+    void reserve(std::size_t n) const {
+        m_arr.reserve(n);
+    }
+
+    void shrink_to_fit() const {
+        m_arr.shrink_to_fit();
+    }
+
+    void push_back(float val) const {
+        m_arr.push_back(val);
+    }
+
+    auto grow_by(std::size_t n) const {
+        auto ret = m_arr.end();
+        m_arr.resize(m_arr.size() + n);
+        return ret;
+    }
+
+    void pop_back() const {
+        m_arr.pop_back();
+    }
+
     ~fvector_view() {
     }
 };
@@ -24,6 +90,38 @@ struct fvector_const_view {
     fvector const &m_arr;
 
     fvector_const_view(fvector const &arr) : m_arr(arr) {
+    }
+
+    float const &operator[](std::size_t idx) const {
+        return m_arr[idx];
+    }
+
+    float const &at(std::size_t idx) const {
+        return m_arr.at(idx);
+    }
+
+    auto begin() const {
+        return m_arr.begin();
+    }
+
+    auto end() const {
+        return m_arr.end();
+    }
+
+    float const &front() const {
+        return m_arr.front();
+    }
+
+    float const &back() const {
+        return m_arr.back();
+    }
+
+    std::size_t size() const {
+        return m_arr.size();
+    }
+
+    float const *data() const {
+        return m_arr.data();
     }
 };
 
