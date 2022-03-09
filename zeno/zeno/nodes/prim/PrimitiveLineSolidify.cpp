938d4d00e50a2465ae66edc1783e53401f6f97ed
jiayaozhang <jiayao.zhang@kaust.edu.sa>
2022-03-09 13:39:29 +0800

    cihou pyb

--- zeno/zeno/nodes/prim/PrimitiveLineSolidify.cpp
+++ zeno/zeno/nodes/prim/PrimitiveLineSolidify.cpp
@@ -51,7 +51,7 @@ struct PrimitiveLineSolidify : zeno::INode {
         auto radiusAttr = get_input<zeno::StringObject>("radiusAttr")->get();
         bool isTri = get_input2<bool>("isTri");
         bool sealEnd = get_input2<bool>("sealEnd");
-
+        
         if (get_input2<bool>("lineSort"))
             primLineSort(prim.get());
 
@@ -85,8 +85,8 @@ struct PrimitiveLineSolidify : zeno::INode {
 
             for (int a = 0; a < count; a++) {
                 float ang = a * (float{M_PI} * 2 / count);
-                sinang[a] = std::sin(ang) * radius;
-                cosang[a] = std::cos(ang) * radius;
+                sinang[a] = std::sin(ang) * 1;
+                cosang[a] = std::cos(ang) * 1;
             }
 
             std::vector<vec3f> bidirections(n);
@@ -118,8 +118,9 @@ struct PrimitiveLineSolidify : zeno::INode {
                 auto bitangent = bidirections[i];
                 for (int a = 0; a < count; a++) {
                     auto offs = tangent * sinang[a] + bitangent * cosang[a];
-                    if constexpr (has_radius_attr.value)
-                        offs *= radattr[i];
+                    if (prim->has_attr(radiusAttr))
+                        offs *= prim->attr<float>(radiusAttr)[i];
+                    else offs *= radius;
                     prim->verts[i + n * a] = currpos + offs;
                 }
             }
