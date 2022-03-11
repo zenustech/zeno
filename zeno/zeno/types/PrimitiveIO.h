079f135554d10a488c30ed9d2955bf2deae28570
archibate <1931127624@qq.com>
2022-03-11 16:35:52 +0800

    fuck vwjjjjjj

--- zeno/zeno/types/PrimitiveIO.h
+++ zeno/zeno/types/PrimitiveIO.h
@@ -11,123 +11,6 @@
 
 namespace zeno {
 
-struct AttrVectorHeader {
-    size_t size;
-    size_t nattrs;
-    char buff[];
-};
-
-enum class AttributeType {
-    Vec3f,
-    Float,
-};
-
-struct AttributeHeader {
-    AttributeType type;
-    size_t size;
-    size_t namelen;
-    char name[128];
-    char buff[];
-};
-
-template <typename T>
-static std::size_t serializeSize(const zeno::AttrVector<T> &arr)
-{
-    std::size_t size{0};
-
-    size += sizeof(AttrVectorHeader);
-    size += sizeof(T) * arr.size();
-
-    arr.foreach_attr(
-        [&size](const auto &key, const auto &attr)
-        {
-            using U = std::decay_t<decltype(attr[0])>;
-
-            size += sizeof(AttributeHeader);
-            size += sizeof(U) * attr.size();
-        });
-
-    return size;
-}
-
-template <typename T>
-static std::vector<char> serialize(const AttrVector<T> &arr)
-{
-    std::vector<char> str;
-    str.resize(serializeSize(arr));
-
-    auto buff{str.data()};
-    auto attrVectorHeader{reinterpret_cast<AttrVectorHeader *>(buff)};
-
-    attrVectorHeader->size = arr.size();
-    attrVectorHeader->nattrs = arr.num_attrs();
-    memcpy(attrVectorHeader->buff, arr.data(), sizeof(T) * arr.size());
-    buff = attrVectorHeader->buff + sizeof(T) * arr.size();
-
-    arr.foreach_attr(
-        [&buff](const auto &key, const auto &attr)
-        {
-            using U = std::decay_t<decltype(attr[0])>;
-
-            auto attributeHeader{reinterpret_cast<AttributeHeader *>(buff)};
-
-            if constexpr (std::is_same_v<U, float>)
-            {
-                attributeHeader->type = AttributeType::Float;
-            }
-            else if constexpr (std::is_same_v<U, vec3f>)
-            {
-                attributeHeader->type = AttributeType::Vec3f;
-            }
-            else
-            {
-                static_assert(std::is_void_v<std::is_void<U>>);
-            }
-            attributeHeader->size = attr.size();
-            attributeHeader->namelen = key.size();
-            memcpy(attributeHeader->name, key.c_str(), sizeof(attributeHeader->name));
-            memcpy(attributeHeader->buff, attr.data(), sizeof(U) * attr.size());
-            buff = attributeHeader->buff + sizeof(U) * attr.size();
-        });
-
-    return str;
-}
-
-template <typename T>
-static void deserialize(const std::vector<char> &str, AttrVector<T> &arr)
-{
-    auto buff{str.data()};
-    auto attrVectorHeader{reinterpret_cast<const AttrVectorHeader *>(buff)};
-
-    arr.values.reserve(attrVectorHeader->size);
-    std::copy_n((T *)attrVectorHeader->buff, attrVectorHeader->size, std::back_inserter(arr.values));
-
-    buff = attrVectorHeader->buff + sizeof(T) * attrVectorHeader->size;
-
-    for (std::size_t i{0}; i < attrVectorHeader->nattrs; ++i)
-    {
-        auto attributeHeader{reinterpret_cast<const AttributeHeader *>(buff)};
-        
-        std::string key{attributeHeader->name, attributeHeader->namelen};
-
-        if (attributeHeader->type == AttributeType::Vec3f)
-        {
-            auto &attr = arr.template add_attr<vec3f>(key);
-            attr.clear();
-            attr.reserve(attributeHeader->size);
-            std::copy_n((vec3f *)attributeHeader->buff, attributeHeader->size, std::back_inserter(attr));
-            buff = attributeHeader->buff + sizeof(vec3f) * attributeHeader->size;
-        }
-        else if (attributeHeader->type == AttributeType::Float)
-        {
-            auto &attr = arr.template add_attr<float>(key);
-            attr.clear();
-            attr.reserve(attributeHeader->size);
-            std::copy_n((float *)attributeHeader->buff, attributeHeader->size, std::back_inserter(attr));
-            buff = attributeHeader->buff + sizeof(float) * attributeHeader->size;
-        }
-    }
-}
 
 static void writezpm(PrimitiveObject const *prim, const char *path) {
     FILE *fp = fopen(path, "wb");
@@ -175,19 +58,13 @@ static void writezpm(PrimitiveObject const *prim, const char *path) {
     fwrite(&size, sizeof(size_t), 1, fp);
     fwrite(prim->lines.data(), sizeof(prim->lines[0]), prim->lines.size(), fp);
 
-    const auto trisStr{serialize(prim->tris)};
-    size = trisStr.size();
+    size = prim->tris.size();
     fwrite(&size, sizeof(size_t), 1, fp);
-    if (size != 0) {
-      fwrite(trisStr.data(), sizeof(trisStr[0]), trisStr.size(), fp);
-    }
+    fwrite(prim->tris.data(), sizeof(prim->tris[0]), prim->tris.size(), fp);
 
-    const auto quadsStr{serialize(prim->quads)};
-    size = quadsStr.size();
+    size = prim->quads.size();
     fwrite(&size, sizeof(size_t), 1, fp);
-    if (size != 0) {
-      fwrite(quadsStr.data(), sizeof(quadsStr[0]), quadsStr.size(), fp);
-    }
+    fwrite(prim->quads.data(), sizeof(prim->quads[0]), prim->quads.size(), fp);
 
     if (prim->mtl != nullptr)
     {
@@ -272,20 +149,12 @@ static void readzpm(PrimitiveObject *prim, const char *path) {
     fread(prim->lines.data(), sizeof(prim->lines[0]), prim->lines.size(), fp);
 
     fread(&size, sizeof(size_t), 1, fp);
-    if (size != 0) {
-      std::vector<char> trisStr;
-      trisStr.resize(size);
-      fread(trisStr.data(), sizeof(trisStr[0]), trisStr.size(), fp);
-      deserialize(trisStr, prim->tris);
-    }
+    prim->tris.resize(size);
+    fread(prim->tris.data(), sizeof(prim->tris[0]), prim->tris.size(), fp);
 
     fread(&size, sizeof(size_t), 1, fp);
-    if (size != 0) {
-      std::vector<char> quadsStr;
-      quadsStr.resize(size);
-      fread(quadsStr.data(), sizeof(quadsStr[0]), quadsStr.size(), fp);
-      deserialize(quadsStr, prim->quads);
-    }
+    prim->quads.resize(size);
+    fread(prim->quads.data(), sizeof(prim->quads[0]), prim->quads.size(), fp);
 
     fread(&size, sizeof(size_t), 1, fp);
     if (size != 0)
