4948f7d477217fbc36ee285e928e2664c3eef502
archibate <1931127624@qq.com>
2022-03-12 09:11:10 +0800

    djtesla!

--- zeno/zeno/nodes/prim/PrimitiveLineDistance.cpp
+++ zeno/zeno/nodes/prim/PrimitiveLineDistance.cpp
@@ -12,7 +12,7 @@
 namespace zeno {
 
 /* AWAK, NIKOLA TESLA'S JOB IS DJ, I.E. DJTESLA */
-ZENO_API void primLineDistance(PrimitiveObject *prim, int start) {
+ZENO_API void primLineDistance(PrimitiveObject *prim, std::string resAttr, int start) {
     if (!prim->lines.size()) return;
 
     const float kFloatMax = std::numeric_limits<float>::max();
@@ -25,7 +25,8 @@ ZENO_API void primLineDistance(PrimitiveObject *prim, int start) {
         neigh.emplace(line[1], std::pair{line[0], dist});
     }
 
-    std::vector<float> result(prim->verts.size(), kFloatMax);
+    auto &result = prim->add_attr<float>(resAttr);
+    std::fill(result.begin(), result.end(), kFloatMax);
     result[start] = 0;
 
     std::vector<float> table(prim->verts.size(), kFloatMax);
@@ -67,7 +68,9 @@ ZENO_API void primLineDistance(PrimitiveObject *prim, int start) {
 struct PrimitiveLineDistance : zeno::INode {
     virtual void apply() override {
         auto prim = get_input<zeno::PrimitiveObject>("prim");
-        primLineDistance(prim.get(), get_param<int>("start"));
+        primLineDistance(prim.get(),
+                         get_input2<std::string>("resAttr"),
+                         get_input2<int>("start"));
         set_output("prim", std::move(prim));
     }
 };
@@ -75,12 +78,13 @@ struct PrimitiveLineDistance : zeno::INode {
 ZENDEFNODE(PrimitiveLineDistance, {
     {
     {"PrimitiveObject", "prim"},
+    {"string", "resAttr", "len"},
+    {"int", "start", "0"},
     },
     {
     {"PrimitiveObject", "prim"},
     },
     {
-    {"int", "start", "0"},
     },
     {"primitive"},
 });
--- zeno/zeno/types/PrimitiveUtils.h
+++ zeno/zeno/types/PrimitiveUtils.h
@@ -2,6 +2,7 @@
 
 #include <zeno/utils/api.h>
 #include <zeno/types/PrimitiveObject.h>
+#include <string>
 
 namespace zeno {
 
@@ -10,6 +11,6 @@ ZENO_API void prim_polys_to_tris(PrimitiveObject *prim);
 ZENO_API void prim_polys_to_tris_with_uv(PrimitiveObject *prim);
 ZENO_API void primCalcNormal(PrimitiveObject *prim);
 ZENO_API void primLineSort(PrimitiveObject *prim, bool reversed = false);
-ZENO_API void primLineDistance(PrimitiveObject *prim, int start = 0);
+ZENO_API void primLineDistance(PrimitiveObject *prim, std::string resAttr, int start = 0);
 
 }
