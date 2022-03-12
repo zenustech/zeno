be800c060d962e5d867a4231eccc303ed670cb57
archibate <1931127624@qq.com>
2022-03-12 10:22:16 +0800

    add zeno2 utils

--- zeno/zeno/utils/tuple_hash.h
+++ zeno/zeno/utils/tuple_hash.h
@@ -9,6 +9,11 @@ struct tuple_hash {
         auto const &[x, y] = key;
         return (x * 2718281828) ^ (y * 3141592653);
     }
+
+    std::size_t operator()(std::tuple<int, int, int> const &key) const {
+        auto const &[x, y, z] = key;
+        return (x * 2718281828) ^ (y * 3141592653) ^ (z * 1618033989);
+    }
 };
 
 }
