c2b10ea0c3926056a517be8353492e42e284e665
archibate <1931127624@qq.com>
2022-03-14 15:20:57 +0800

    tmp

---
+++ zeno/zeno/utils/bit_operations.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace zeno {
+
+int ceil_log2(unsigned long long x)
+{
+  static const unsigned long long t[6] = {
+    0xFFFFFFFF00000000ull,
+    0x00000000FFFF0000ull,
+    0x000000000000FF00ull,
+    0x00000000000000F0ull,
+    0x000000000000000Cull,
+    0x0000000000000002ull
+  };
+
+  int y = (((x & (x - 1)) == 0) ? 0 : 1);
+  int j = 32;
+  int i;
+
+  for (i = 0; i < 6; i++) {
+    int k = (((x & t[i]) == 0) ? 0 : j);
+    y += k;
+    x >>= k;
+    j >>= 1;
+  }
+
+  return y;
+}
+
+}
