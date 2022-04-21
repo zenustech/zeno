428308acce3f6931c529c859689c54cce1a6d713
archibate <1931127624@qq.com>
2022-04-21 11:25:45 +0800

    add camnodes

--- projects/SharedExtension/dummy_export.cpp
+++ projects/SharedExtension/dummy_export.cpp
@@ -5,6 +5,9 @@ namespace zeno {
 __declspec(dllexport)
 #endif
 int _zenosharedext_link_what_you_use();
+#ifdef _MSC_VER
+__declspec(dllexport)
+#endif
 int _zenosharedext_link_what_you_use() { return 1; }
 
 }
--- zeno/include/zeno/types/LightObject.h
+++ zeno/include/zeno/types/LightObject.h
@@ -1,12 +1,17 @@
+#pragma once
+
 #include <zeno/core/IObject.h>
 #include <zeno/utils/vec.h>
 
 namespace zeno {
 
-struct LightObject : IObjectClone<LightObject> {
+struct LightData {
     vec3f pos{1, 1, 0};
     vec3f dir{-1, -1, 0};
     float intensity{100};
+};
+
+struct LightObject : IObjectClone<LightObject>, LightData {
 };
 
 }
