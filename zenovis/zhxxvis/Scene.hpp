a8f5973cb6b3997865a6911a24acda54329e8be9
archibate <1931127624@qq.com>
2022-05-03 16:15:23 +0800

    import zhxxvis

---
+++ zenovis/zhxxvis/Scene.hpp
@@ -0,0 +1,56 @@
+#ifndef __SCENE_HPP__
+#define __SCENE_HPP__
+
+#include <Light.hpp>
+#include <vector>
+#include <memory>
+
+namespace zenvis
+{
+    struct Scene
+    {
+        Scene(const Scene &) = delete;
+        Scene(Scene &&) = delete;
+        Scene &operator=(const Scene &) = delete;
+        Scene &operator=(Scene &) = delete;
+        static Scene &getInstance()
+        {
+            static Scene scene;
+            return scene;
+        }
+
+    private:
+        Scene() = default;
+        ~Scene() = default;
+
+    public:
+        static constexpr std::size_t maxLightsNum = 16;
+        std::vector<std::unique_ptr<Light>> lights;
+    
+        Light *addLight()
+        {
+            if (lights.size() >= maxLightsNum)
+            {
+                return nullptr;
+            }
+            auto light = std::make_unique<Light>();
+            auto pLight = light.get();
+            lights.push_back(std::move(light));
+            return pLight;
+        }
+
+        bool removeLight(std::size_t index)
+        {
+            if (index < 0 || index >= lights.size())
+            {
+                return false;
+            }
+            lights.erase(lights.begin() + index);
+            return true;
+        }
+
+    }; // struct Scene
+
+}; // namespace zenvis
+
+#endif // #include __SCENE_HPP__
---
+++ zenovis/zhxxvis/main.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "stdafx.hpp"
+
+namespace zenvis {
+
+void set_program_uniforms(Program *pro);
+
+extern int curr_frameid;
+extern bool render_wireframe;
+
+}
