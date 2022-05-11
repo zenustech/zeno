e9044b7319613f38f60fc28ab192615003c54899
archibate <1931127624@qq.com>
2022-05-11 10:56:06 +0800

    fix light

--- zenovis/zhxxvis/update.sh
+++ zenovis/zhxxvis/update.sh
@@ -3,4 +3,4 @@
 cp `ls ~/Codes/zeno-blender/external/zeno/zenvis/*.{cpp,h,hpp} | grep -v python.cpp | grep -v IGraphic.cpp | grep -v zenvisapi.hpp` "$(dirname "$0")"
 sed -i 's/void new_frame() {/void new_frame(int _new_frame_fbo) {/' main.cpp
 sed -i 's/  paint_graphics();/  paint_graphics(_new_frame_fbo);/' main.cpp
-
+#sed -i 's/\<GL_FRAMEBUFFER\>/GL_DRAW_FRAMEBUFFER/g' Light.hpp
