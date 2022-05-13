e02200aa5dbcb130a5de06b996963228fb9bc03b
littlemine <wxlwxl1993@zju.edu.cn>
2022-05-13 16:54:43 +0800

    split impls

--- projects/gmpm/fem/Generation.cpp
+++ projects/gmpm/fem/Generation.cpp
@@ -3,6 +3,7 @@
 #include "zensim/geometry/PoissonDisk.hpp"
 #include "zensim/geometry/VdbLevelSet.h"
 #include "zensim/geometry/VdbSampler.h"
+#include "zensim/io/MeshIO.hpp"
 #include "zensim/omp/execution/ExecutionPolicy.hpp"
 #include "zensim/types/Property.h"
 #include <atomic>
@@ -16,7 +17,7 @@
 
 namespace zeno {
 
-struct ReadTetPrim : INode {
+struct ReadVtkMesh : INode {
   void apply() override {
     auto path = get_input<StringObject>("path")->get();
     auto prim = std::make_shared<PrimitiveObject>();
@@ -24,56 +25,76 @@ struct ReadTetPrim : INode {
     auto &quads = prim->quads;
     auto ompExec = zs::omp_exec();
 
-    mshio::MshSpec spec = mshio::load_msh(path);
-    const auto numVerts = spec.nodes.num_nodes;
-    prim->resize(spec.nodes.num_nodes);
-    const auto numEles = spec.elements.num_elements;
+    zs::Mesh<float, 3, int, 4> tet;
+    read_tet_mesh_vtk(path, tet);
+    const auto numVerts = tet.nodes.size();
+    const auto numEles = tet.elems.size();
+    prim->resize(numVerts);
     quads.resize(numEles);
+    ompExec(zs::range(numVerts), [&](int i) { pos[i] = tet.nodes[i]; });
+    ompExec(zs::range(numEles), [&](int i) { quads[i] = tet.elems[i]; });
 
-    /// load vert positions
-    ompExec(spec.nodes.entity_blocks, [&](const auto &nodeBlock) {
-      const std::size_t stride = 3 + nodeBlock.parametric;
-      for (auto tag : nodeBlock.tags) {
-        std::size_t j = tag - 1; // [1, N]
-        auto x =
-            vec3f{nodeBlock.data[j * stride], nodeBlock.data[j * stride + 1],
-                  nodeBlock.data[j * stride + 2]};
-        pos[j] = x;
-      }
-    });
-    ompExec(spec.elements.entity_blocks, [&](const auto &eleBlock) {
-      const std::size_t n = mshio::nodes_per_element(eleBlock.element_type);
-      const std::size_t stride = n + 1;
-      if (n != 4)
-        fmt::print("damn, it\'s not a tet.\n");
-      if (eleBlock.element_type != 4)
-        fmt::print("not the expected tet.\n");
-      for (std::size_t i = 0; i != eleBlock.num_elements_in_block; ++i) {
-        std::size_t j = eleBlock.data[i * stride] - 1;
-        for (int k = 0; k != n; ++k)
-          quads[j][k] = eleBlock.data[j * stride + k + 1];
-      }
-    });
+    set_output("prim", std::move(prim));
+  }
+};
 
-    if (get_param<bool>("surface_extraction")) {
+ZENDEFNODE(ReadVtkMesh, {/* inputs: */ {
+                             {"readpath", "path"},
+                         },
+                         /* outputs: */
+                         {
+                             {"primitive", "prim"},
+                         },
+                         /* params: */
+                         {},
+                         /* category: */
+                         {
+                             "primitive",
+                         }});
+
+struct ExtractMeshSurface : INode {
+  void apply() override {
+    auto prim = get_input<PrimitiveObject>("prim");
+    auto &pos = prim->attr<vec3f>("pos");
+    auto &quads = prim->quads;
+    auto ompExec = zs::omp_exec();
+    const auto numVerts = pos.size();
+    const auto numEles = quads.size();
+
+    auto op = get_param<std::string>("op");
+    bool includePoints = false;
+    bool includeLines = false;
+    bool includeTris = false;
+    if (op == "all") {
+      includePoints = true;
+      includeLines = true;
+      includeTris = true;
+    } else if (op == "point")
+      includePoints = true;
+    else if (op == "edge")
+      includeLines = true;
+    else if (op == "surface")
+      includeTris = true;
+
+    std::vector<int> points;
+    std::vector<vec2i> lines;
+    std::vector<vec3i> tris;
+    {
       using namespace zs;
       zs::HashTable<int, 3, int> surfTable{0};
       constexpr auto space = zs::execspace_e::openmp;
-      auto &tris = prim->tris;     // surfaces
-      auto &lines = prim->lines;   // surface edges
-      auto &points = prim->points; // surface points
 
       surfTable.resize(ompExec, 4 * numEles);
       surfTable.reset(ompExec, true);
       // compute getsurface
-      std::vector<int> tri2tet(4 * numEles);
-      ompExec(range(numEles), [table = proxy<space>(surfTable), &quads,
-                               &tri2tet](int ei) mutable {
+      // std::vector<int> tri2tet(4 * numEles);
+      ompExec(range(numEles), [table = proxy<space>(surfTable),
+                               &quads](int ei) mutable {
         using table_t = RM_CVREF_T(table);
         using vec3i = zs::vec<int, 3>;
-        auto record = [&table, &tri2tet, ei](const vec3i &triInds) mutable {
+        auto record = [&table, ei](const vec3i &triInds) mutable {
           if (auto sno = table.insert(triInds); sno != table_t::sentinel_v)
-            tri2tet[sno] = ei;
+            ; // tri2tet[sno] = ei;
           else
             printf("ridiculous, more than one tet share the same surface!");
         };
@@ -87,7 +108,6 @@ struct ReadTetPrim : INode {
       tris.resize(numEles * 4);
       Vector<int> surfCnt{1, memsrc_e::host};
       surfCnt.setVal(0);
-#if 1
       ompExec(range(surfTable.size()),
               [table = proxy<space>(surfTable), surfCnt = surfCnt.data(),
                &tris](int i) mutable {
@@ -105,21 +125,10 @@ struct ReadTetPrim : INode {
               });
       auto scnt = surfCnt.getVal();
       tris.resize(scnt);
-#else
-      ompExec(range(numEles), [&](int ei) {
-        auto inds = quads[ei];
-        tris[ei * 4 + 0] = vec3i{inds[0], inds[2], inds[1]};
-        tris[ei * 4 + 1] = vec3i{inds[0], inds[3], inds[2]};
-        tris[ei * 4 + 2] = vec3i{inds[0], inds[1], inds[3]};
-        tris[ei * 4 + 3] = vec3i{inds[0], inds[2], inds[3]};
-      });
-      auto scnt = numEles * 4;
-      tris.resize(scnt);
-#endif
       fmt::print("{} surfaces\n", scnt);
 
       // surface points
-      HashTable<int, 1, int> vertTable{pos.size()};
+      HashTable<int, 1, int> vertTable{numVerts};
       HashTable<int, 2, int> edgeTable{3 * numEles};
       vertTable.reset(ompExec, true);
       edgeTable.reset(ompExec, true);
@@ -161,24 +170,19 @@ struct ReadTetPrim : INode {
       lines.resize(secnt);
       fmt::print("{} surface edges\n", secnt);
     }
+    if (includeTris)
+      prim->tris.values = tris; // surfaces
+    if (includeLines)
+      prim->lines.values = lines; // surfaces edges
+    if (includePoints)
+      prim->points.values = points; // surfaces points
     set_output("prim", std::move(prim));
   }
 };
 
-ZENDEFNODE(ReadTetPrim, {/* inputs: */ {
-                             {"readpath", "path"},
-                         },
-                         /* outputs: */
-                         {
-                             {"primitive", "prim"},
-                         },
-                         /* params: */
-                         {
-                             {"bool", "surface_extraction", "1"},
-                         },
-                         /* category: */
-                         {
-                             "primitive",
-                         }});
+ZENDEFNODE(ExtractMeshSurface, {{{"quad (tet) mesh", "prim"}},
+                                {{"mesh with surface topos", "prim"}},
+                                {{"enum all point edge surface", "op", "all"}},
+                                {"primitive"}});
 
 } // namespace zeno
