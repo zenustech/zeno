bbd749441c021c9b735504b1a90e543b4485b3b4
archibate <1931127624@qq.com>
2022-05-18 11:06:44 +0800

    move mat

--- zenovis/xinxinoptix/optixPathTracer.h
+++ zenovis/xinxinoptix/optixPathTracer.h
@@ -85,3 +85,19 @@ struct HitGroupData
     float3  diffuse_color;
     float4* vertices;
 };
+
+
+static __forceinline__ __device__ void* unpackPointer( unsigned int i0, unsigned int i1 )
+{
+    const unsigned long long uptr = static_cast<unsigned long long>( i0 ) << 32 | i1;
+    void*           ptr = reinterpret_cast<void*>( uptr );
+    return ptr;
+}
+
+
+static __forceinline__ __device__ void  packPointer( void* ptr, unsigned int& i0, unsigned int& i1 )
+{
+    const unsigned long long uptr = reinterpret_cast<unsigned long long>( ptr );
+    i0 = uptr >> 32;
+    i1 = uptr & 0x00000000ffffffff;
+}
