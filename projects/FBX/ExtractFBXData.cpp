f0c87dfad880382666084547e38cbee61e700c93
Aaron <910854285@qq.com>
2022-05-19 14:49:32 +0800

    FBX Extension # Material Step 1

--- projects/FBX/ExtractFBXData.cpp
+++ projects/FBX/ExtractFBXData.cpp
@@ -2,7 +2,9 @@
 #include <zeno/utils/logger.h>
 #include <zeno/extra/GlobalState.h>
 #include <zeno/types/NumericObject.h>
+#include <zeno/types/StringObject.h>
 #include <zeno/types/PrimitiveObject.h>
+#include <zeno/types/ListObject.h>
 
 #include "assimp/scene.h"
 
@@ -43,3 +45,37 @@ ZENDEFNODE(ExtractFBXData,
                }
            });
 
+struct ExtractMatTexList : zeno::INode {
+
+    virtual void apply() override {
+        auto mat = get_input<IMaterial>("material");
+        auto key = get_input<zeno::StringObject>("key")->get();
+
+        zeno::log_info(">>>>> Get Key {}", key);
+        zeno::log_info(">>>>> Get Mat Name {}", mat->value.at(key).matName);
+        auto lo = std::make_shared<zeno::ListObject>();
+        lo->arr = mat->value.at(key).getTexList();
+
+        for(auto&l: lo->arr){
+            //zeno::log_info("Tex: {}", std::any_cast<std::string>(l));
+        }
+        zeno::log_info(">>>>> Get TexLen {}", lo->arr.size());
+
+        set_output("texLists", std::move(lo));
+    }
+};
+ZENDEFNODE(ExtractMatTexList,
+           {       /* inputs: */
+               {
+                   {"IMaterial", "material"}, {"string", "key"}
+               },  /* outputs: */
+               {
+                    "texLists"
+               },  /* params: */
+               {
+
+               },  /* category: */
+               {
+                   "primitive",
+               }
+           });
