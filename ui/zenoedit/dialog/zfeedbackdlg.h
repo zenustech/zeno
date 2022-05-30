89f2b1eddaf7bfca5cfb7bd5eb175a23f86b43bb
luzh <luzh16371@gmail.com>
2022-05-30 20:50:56 +0800

    [dev] feedback by sending email. (but now not supporting adding attachment automatically).

---
+++ ui/zenoedit/dialog/zfeedbackdlg.h
@@ -0,0 +1,24 @@
+#ifndef __ZFEEDBACK_DLG_H__
+#define __ZFEEDBACK_DLG_H__
+
+#include <QtWidgets>
+#include <QTcpSocket>
+
+namespace Ui {
+    class FeedBackDlg;
+}
+
+class ZFeedBackDlg : public QDialog
+{
+    Q_OBJECT
+public:
+    ZFeedBackDlg(QWidget *parent = nullptr);
+    QString content() const;
+    bool isSendFile() const;
+    void sendEmail(const QString& subject, const QString& content, const QString& zsgContent);
+
+private:
+    Ui::FeedBackDlg *m_ui;
+};
+
+#endif
