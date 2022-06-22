4613abb06960ef539c5eafe1644158a48b262a08
archibate <1931127624@qq.com>
2022-06-22 16:37:57 +0800

    nookeep

--- zeno/include/zeno/funcs/PrimitiveUtils.h
+++ zeno/include/zeno/funcs/PrimitiveUtils.h
@@ -10,7 +10,7 @@ ZENO_API void primTriangulateQuads(PrimitiveObject *prim);
 ZENO_API void primTriangulate(PrimitiveObject *prim, bool with_uv = true);
 ZENO_API void primPolygonate(PrimitiveObject *prim, bool with_uv = true);
 
-ZENO_API void primSepTriangles(PrimitiveObject *prim, bool smoothNormal = false, bool keepTriFaces = true);
+ZENO_API void primSepTriangles(PrimitiveObject *prim, bool smoothNormal = true, bool keepTriFaces = true);
 //ZENO_API void primSmoothNormal(PrimitiveObject *prim, bool isFlipped = false);
 
 ZENO_API void primCalcNormal(PrimitiveObject *prim, float flip = 1.0f);
