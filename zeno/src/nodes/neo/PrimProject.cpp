03e287649dbdbf2c6ae31e41486f8a12378b8124
archibate <1931127624@qq.com>
2022-06-27 14:51:01 +0800

    prjclrok

--- zeno/src/nodes/neo/PrimProject.cpp
+++ zeno/src/nodes/neo/PrimProject.cpp
@@ -3,22 +3,27 @@
 #include <zeno/types/PrimitiveUtils.h>
 #include <zeno/types/StringObject.h>
 #include <zeno/types/NumericObject.h>
+#include <zeno/utils/variantswitch.h>
+#include <zeno/utils/arrayindex.h>
 #include <unordered_map>
+#include <functional>
 
 namespace zeno {
 namespace {
 
-static float tri_intersect(vec3f const &ro, vec3f const &rd, vec3f const &v0, vec3f const &v1, vec3f const &v2) {
+template <class Cond>
+static float tri_intersect(Cond cond, vec3f const &ro, vec3f const &rd,
+                           vec3f const &v0, vec3f const &v1, vec3f const &v2) {
         vec3f u = v1 - v0;
         vec3f v = v2 - v0;
-        vec3f norm = cross(u, v);
+        vec3f n = cross(u, v);
 
-        float b = dot(norm, rd);
-        if (std::abs(b) >= eps) {
-            float w0 = ro - v0;
-            float a = -dot(norm, w0);
+        float b = dot(n, rd);
+        if (std::abs(b) > 0) {
+            vec3f w0 = ro - v0;
+            float a = -dot(n, w0);
             float r = a / b;
-            if (r > 0) {
+            if (cond(r)) {  // r > 0
                 vec3f ip = ro + r * rd;
                 float uu = dot(u, u);
                 float uv = dot(u, v);
@@ -26,8 +31,7 @@ static float tri_intersect(vec3f const &ro, vec3f const &rd, vec3f const &v0, ve
                 vec3f w = ip - v0;
                 float wu = dot(w, u);
                 float wv = dot(w, v);
-                float d = uv * uv - uu * vv;
-                d = 1.0f / d;
+                float d = 1.0f / (uv * uv - uu * vv);
                 float s = (uv * wv - vv * wu) * d;
                 float t = (uv * wu - uu * wv) * d;
                 if (0 <= s && s <= 1 && 0 <= t && s + t <= 1)
@@ -44,24 +48,49 @@ struct BVH {
         this->prim = prim;
     }
 
-    void intersect(vec3f ro, vec3f rd) {
+    template <class Cond>
+    float intersect(Cond cond, vec3f const &ro, vec3f const &rd) {
         for (size_t i = 0; i < prim->tris.size(); i++) {
             auto ind = prim->tris[i];
             auto a = prim->verts[ind[0]];
             auto b = prim->verts[ind[1]];
             auto c = prim->verts[ind[2]];
+            float d = tri_intersect(cond, ro, rd, a, b, c);
+            if (d != 0)
+                return d;
         }
+        return 0;
     }
 };
 
 struct PrimProject : INode {
     virtual void apply() override {
         auto prim = get_input<PrimitiveObject>("prim");
+        auto targetPrim = get_input<PrimitiveObject>("targetPrim");
         auto offset = get_input2<float>("offset");
         auto limit = get_input2<float>("limit");
         auto nrmAttr = get_input2<std::string>("nrmAttr");
-        auto positive = get_input2<bool>("positive");
-        auto negative = get_input2<bool>("negative");
+        auto allowDir = get_input2<std::string>("allowDir");
+
+        BVH bvh;
+        bvh.build(targetPrim.get());
+        auto const &nrm = prim->verts.attr<vec3f>(nrmAttr);
+        auto comp = enum_variant<std::variant<
+            std::greater<float>, std::less<float>, std::not_equal_to<float>
+            >>(array_index_safe({"front", "back", "both"},
+                                allowDir, "allowDir"));
+        std::visit([&] (auto comp) {
+            auto cond = [=] (float r) { return comp(r, 0.f); };
+            for (size_t i = 0; i < prim->verts.size(); i++) {
+                auto ro = prim->verts[i];
+                auto rd = normalizeSafe(nrm[i], 1e-6f);
+                float t = bvh.intersect(cond, ro, rd);
+                if (limit > 0 && std::abs(t) > limit)
+                    t = 0;
+                t -= offset;
+                prim->verts[i] = ro + t * rd;
+            }
+        }, comp);
 
         set_output("prim", std::move(prim));
     }
@@ -74,8 +103,7 @@ ZENDEFNODE(PrimProject, {
     {"string", "nrmAttr", "nrm"},
     {"float", "offset", "0"},
     {"float", "limit", "0"},
-    {"bool", "positive", "1"},
-    {"bool", "negitive", "1"},
+    {"enum front back both", "allowDir", "both"},
     //{"bool", "targetPositive", "1"},
     //{"bool", "targetNegitive", "1"},
     },
