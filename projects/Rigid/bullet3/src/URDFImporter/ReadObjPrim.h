56726957286e156e0831824481acbb41a3e33b9b
archibate <1931127624@qq.com>
2022-06-29 21:37:14 +0800

    bullet3 as submod

--- projects/Rigid/bullet3/src/URDFImporter/ReadObjPrim.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZENO_READOBJPRIM_H
-#define ZENO_READOBJPRIM_H
-#include <zeno/utils/vec.h>
-#include <zeno/types/PrimitiveObject.h>
-
-std::shared_ptr<zeno::PrimitiveObject> wenqiang_parse_obj(std::vector<char> &&bin);
-
-
-#endif //ZENO_READOBJPRIM_H
