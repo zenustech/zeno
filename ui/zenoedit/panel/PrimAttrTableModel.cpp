c7da38de2eda37b9f4b146f232b46de699397898
zhouhang95 <765229842@qq.com>
2022-07-15 14:49:04 +0800

    userdata-spreadsheet

--- ui/zenoedit/panel/PrimAttrTableModel.cpp
+++ ui/zenoedit/panel/PrimAttrTableModel.cpp
@@ -4,7 +4,8 @@
 
 #include "PrimAttrTableModel.h"
 #include <zeno/types/PrimitiveObject.h>
-#include "zeno/utils/logger.h"
+#include "zeno/types/UserData.h"
+#include <zeno/funcs/LiterialConverter.h>
 
 PrimAttrTableModel::PrimAttrTableModel(QObject* parent)
     : QAbstractTableModel(parent)
@@ -13,7 +14,12 @@ PrimAttrTableModel::PrimAttrTableModel(QObject* parent)
 
 int PrimAttrTableModel::rowCount(const QModelIndex &parent) const {
     if (m_prim) {
-        return (int)(m_prim->verts.size());
+        if (sel_attr == "Vertex") {
+            return (int)(m_prim->verts.size());
+        }
+        else {
+            return 1;
+        }
     }
     else {
         return 0;
@@ -22,7 +28,12 @@ int PrimAttrTableModel::rowCount(const QModelIndex &parent) const {
 
 int PrimAttrTableModel::columnCount(const QModelIndex &parent) const {
     if (m_prim) {
-        return (int)m_prim->num_attrs();
+        if (sel_attr == "Vertex") {
+            return (int)m_prim->num_attrs();
+        }
+        else {
+            return m_prim->userData().size();
+        }
     }
     else {
         return 0;
@@ -40,20 +51,46 @@ QVariant PrimAttrTableModel::data(const QModelIndex& index, int role) const
     }
     else if (Qt::DisplayRole == role)
     {
-        std::string attr_name = m_prim->attr_keys()[index.column()];
-        if (m_prim->attr_is<float>(attr_name)) {
-            return m_prim->attr<float>(attr_name)[index.row()];
+        if (sel_attr == "Vertex") {
+            std::string attr_name = m_prim->attr_keys()[index.column()];
+            if (m_prim->attr_is<float>(attr_name)) {
+                return m_prim->attr<float>(attr_name)[index.row()];
+            }
+            else if (m_prim->attr_is<zeno::vec3f>(attr_name)) {
+                auto v = m_prim->attr<zeno::vec3f>(attr_name)[index.row()];
+                return QString("%1, %2, %3").arg(v[0]).arg(v[1]).arg(v[2]);
+            }
+            else if (m_prim->attr_is<int>(attr_name)) {
+                return m_prim->attr<int>(attr_name)[index.row()];
+            }
+            else if (m_prim->attr_is<zeno::vec3i>(attr_name)) {
+                auto v = m_prim->attr<zeno::vec3i>(attr_name)[index.row()];
+                return QString("%1, %2, %3").arg(v[0]).arg(v[1]).arg(v[2]);
+            }
         }
-        else if (m_prim->attr_is<zeno::vec3f>(attr_name)) {
-            auto v = m_prim->attr<zeno::vec3f>(attr_name)[index.row()];
-            return QString("%1, %2, %3").arg(v[0]).arg(v[1]).arg(v[2]);
-        }
-        else if (m_prim->attr_is<int>(attr_name)) {
-            return m_prim->attr<int>(attr_name)[index.row()];
-        }
-        else if (m_prim->attr_is<zeno::vec3i>(attr_name)) {
-            auto v = m_prim->attr<zeno::vec3i>(attr_name)[index.row()];
-            return QString("%1, %2, %3").arg(v[0]).arg(v[1]).arg(v[2]);
+        else {
+            auto it = m_prim->userData().begin();
+            auto i = index.column();
+            while (i) {
+                it++;
+                i--;
+            }
+            if (zeno::objectIsLiterial<float>(it->second)) {
+                auto v = zeno::objectToLiterial<float>(it->second);
+                return v;
+            }
+            else if (zeno::objectIsLiterial<int>(it->second)) {
+                auto v = zeno::objectToLiterial<int>(it->second);
+                return v;
+            }
+            else if (zeno::objectIsLiterial<zeno::vec3f>(it->second)) {
+                auto v = zeno::objectToLiterial<zeno::vec3f>(it->second);
+                return QString("%1, %2, %3").arg(v[0]).arg(v[1]).arg(v[2]);
+            }
+            else if (zeno::objectIsLiterial<zeno::vec3i>(it->second)) {
+                auto v = zeno::objectToLiterial<zeno::vec3i>(it->second);
+                return QString("%1, %2, %3").arg(v[0]).arg(v[1]).arg(v[2]);
+            }
         }
         return "-";
     }
@@ -67,7 +104,18 @@ QVariant PrimAttrTableModel::headerData(int section, Qt::Orientation orientation
 
     if (orientation == Qt::Horizontal)
     {
-        return QString(m_prim->attr_keys()[section].c_str());
+        if (sel_attr == "Vertex") {
+            return QString(m_prim->attr_keys()[section].c_str());
+        }
+        else {
+            auto it = m_prim->userData().begin();
+            auto i = section;
+            while (i) {
+                it++;
+                i--;
+            }
+            return QString(it->first.c_str());
+        }
     }
     else if (orientation == Qt::Vertical)
     {
@@ -83,5 +131,11 @@ void PrimAttrTableModel::setModelData(zeno::PrimitiveObject *prim) {
         m_prim = std::make_shared<zeno::PrimitiveObject>(*prim);
     else
         m_prim = nullptr;
+    endResetModel();
+}
+
+void PrimAttrTableModel::setSelAttr(std::string sel_attr_) {
+    beginResetModel();
+    sel_attr = sel_attr_;
     endResetModel();
 }
