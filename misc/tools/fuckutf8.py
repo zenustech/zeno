ae655448acf7edbb74d5d023cc4891374d030dbf
archibate <1931127624@qq.com>
2022-07-22 15:57:05 +0800

    fuckutf

---
+++ misc/tools/fuckutf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+
+
+def process(path):
+    out = ''
+    changed = False
+    with open(path, 'rb') as f:
+        for n, bs in enumerate(f.readlines()):
+            for b in bs:
+                if not 0 <= b <= 0x7f:
+                    # print('{}:{}: 0x{:02X}'.format(path, n + 1, b))
+                    out += '\\x{:02X}'.format(b)
+                    changed = True
+                else:
+                    out += chr(b)
+    if changed:
+        with open(path, 'w') as f:
+            f.write(out)
+
+if len(sys.argv) > 1:
+    process(sys.argv[1])
+else:
+    exit(os.system(r"find ui zeno zenovis -type f -regex '.*\.\(c\|h\)\(pp\)?' | xargs -n1 python -O misc/tools/fuckutf8.py"))
--- misc/tools/scanutf8.py
+++ misc/tools/scanutf8.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
-# find ui zeno zenovis -type f -regex '.*\.\(c\|h\)\(pp\)?' | xargs -n1 python misc/tools/scanutf8.py
 import sys
+import locale
 import os
 
 
 def process(path):
-    with open(path, 'r') as f:
-        for n, bs in enumerate(f.readlines()):
-            for b in map(ord, bs):
-                if not 0 <= b <= 0x7f:
-                    print('{}:{}: U+{:04x}'.format(path, n + 1, b))
-                    exit(1)
+    try:
+        with open(path, 'r') as f:
+            for n, bs in enumerate(f.readlines()):
+                for b in map(ord, bs):
+                    if not 0 <= b <= 0x7f:
+                        print('{}:{}: U+{:04X}'.format(path, n + 1, b))
+                        exit(1)
+    except UnicodeDecodeError:
+        print('{}: failed to decode as {}'.format(path, locale.getdefaultlocale()))
+        exit(2)
 
-# process('zeno/src/nodes/prim/WBPrimBend.cpp')
 if len(sys.argv) > 1:
     process(sys.argv[1])
 else:
-    exit(os.system("find ui zeno zenovis -type f -regex '.*\.\(c\|h\)\(pp\)?' | xargs -n1 python -O misc/tools/scanutf8.py"))
+    exit(os.system(r"find ui zeno zenovis -type f -regex '.*\.\(c\|h\)\(pp\)?' | xargs -n1 python -O misc/tools/scanutf8.py"))
