07b4ea4e24c695ff13bf7a24c213794660869908
archibate <1931127624@qq.com>
2022-07-26 00:04:47 +0800

    tmp

--- projects/Pass/main.cpp
+++ projects/Pass/main.cpp
@@ -1,10 +1,24 @@
 #include <zeno/zeno.h>
+#include <zeno/types/PrimitiveObject.h>
+#include <zeno/types/ListObject.h>
+#include <zeno/types/LightObject.h>
+#include <zeno/types/CameraObject.h>
 
 namespace zeno {
 namespace {
 
 struct ForwardPass : INode {
     virtual void apply() override {
+        auto objects = has_input("objects")
+            ? get_input<ListObject>("objects")
+            : std::make_shared<ListObject>();
+        auto lights = has_input("lights")
+            ? get_input<ListObject>("lights")
+            : std::make_shared<ListObject>();
+        auto materials = has_input("materials")
+            ? get_input<ListObject>("materials")
+            : std::make_shared<ListObject>();
+        auto camera = get_input<CameraObject>();
     }
 };
 
