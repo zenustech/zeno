66822128eafc42c02b7bbc83379187c16dc256a8
luzh <luzh16371@gmail.com>
2022-08-04 19:26:45 +0800

    [ui] tabbar: 20%

--- ui/zenoui/comctrl/ztabbar.h
+++ ui/zenoui/comctrl/ztabbar.h
@@ -3,11 +3,15 @@
 
 #include <QtWidgets>
 
+class ZIconButton;
+
 struct Tab {
     QIcon icon;
     QString text;
     QRect rect;
-    QToolButton* pCloseButton;
+    ZIconButton* pCloseButton;
+
+    Tab(const QIcon& ico, const QString& txt) : icon(ico), text(txt), pCloseButton(nullptr) {}
 };
 
 class ZTabBar : public QWidget
@@ -17,10 +21,34 @@ public:
     explicit ZTabBar(QWidget* parent = nullptr);
     ~ZTabBar();
 
-    Tab* at(int index);
+    Tab at(int index);
+    int addTab(const QString& text);
+    int addTab(const QIcon& icon, const QString& text);
+    int insertTab(int index, const QString& text);
+    int insertTab(int index, const QIcon& icon, const QString& text);
     void moveTab(int srcIndex, int dstIndex);
+    void removeTab(int index);
 
-    QList<Tab> tabList;
+    QString tabText(int index) const;
+    void setTabText(int index, const QString& text);
+
+public slots:
+    void setCurrentIndex(int index);
+
+signals:
+    void currentChanged(int index);
+    void tabCloseBtnClicked(int index);
+    void tabMoved(int from, int to);
+    void tabBarClicked(int index);
+
+private:
+    inline bool validIndex(int index) const { return index >= 0 && index < m_tabList.count(); }
+    void refresh();
+    void _closeTab();
+
+    QList<Tab> m_tabList;
+    int m_currentIndex;
+    bool m_bCloseBtn;
 };
 
 
