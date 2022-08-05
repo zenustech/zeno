b6bcb4e6ac5978fc5d9e16b63f202f8420e8d4a2
chunleili <li_cl@foxmail.com>
2022-08-05 15:42:19 +0800

    test apply() and ZENDEFNODE

--- projects/PBD/PBD.cpp
+++ projects/PBD/PBD.cpp
@@ -1,22 +1,33 @@
 #include <zeno\zeno.h>
+#include <zeno\types\PrimitiveObject.h>
 #include "PBD.h"
 
 namespace zeno {
     struct PBD : zeno::INode {
-        virtual void apply() override {
-            
-        }
+        virtual void apply() override 
+        {
+            if (has_input("prim")) 
+            {
+                auto prim = get_input<PrimitiveObject>("prim");
+                auto &pos = prim->attr<vec3f>("pos");
+                for(int i = 0; i < pos.size(); i++)
+                {
+                    pos[i] += 1.0;
+                }
+                set_output("prim", std::move(prim));
+            }
+        };
     };
+
     ZENDEFNODE(PBD, {
-         {
-            {}
-         },
-         {
-         },
-         {},
-         {
-             "PBD"
-         },
+        // inputs:
+        {"prim"},
+        // outputs:
+        {"prim"},
+        // params:
+        {{"vec3f","external_force","0, -9.8, 0"}},
+        //category
+        {"PBD"},
     });
 
 
