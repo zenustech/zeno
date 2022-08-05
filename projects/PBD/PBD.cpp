4170564035afedefc1282e30b0863eca1d3c2eb9
chunleili <li_cl@foxmail.com>
2022-08-05 23:21:31 +0800

    add subSteps

--- projects/PBD/PBD.cpp
+++ projects/PBD/PBD.cpp
@@ -14,39 +14,56 @@ struct PBD : zeno::INode
         std::vector<zeno::vec3f> vel;
         int numParticles = pos.size();
         vel.resize(numParticles);
+        
+        //physical param
+        zeno::vec3f g{0, -9.8, 0};
+        int numSubsteps = 10;
+        float dt = 1.0/60.0/numSubsteps;
+        float edgeCompliance = 100.0;
+        float volumeCompliance = 0.0;
+        int numEdges = prim->lines.size();   
+        int numTets = prim->quads.size();
+        std::cout<<"numTets"<<numTets<<std::endl;
+        std::cout<<"numEdges"<<numEdges<<std::endl;
         /* -------------------------------------------------------------------------- */
         /*                                  preSolve                                  */
         /* -------------------------------------------------------------------------- */
         // std::cout<<"presolve"<<std::endl;
-        zeno::vec3f g{0, -9.8, 0};
-        float dt = 1.0/30.0;
-        for(int i = 0; i < pos.size(); i++)
+        for(int steps = 0; steps < numSubsteps; steps++)
         {
-            prevPos[i] = pos[i];
-            vel[i] += g * dt;
-            pos[i] += vel[i] * dt;
-            if(pos[i][1]<0.0)
+            for(int i = 0; i < pos.size(); i++)
             {
-                pos[i] = prevPos[i];
-                pos[i][1] = 0.0;
+                prevPos[i] = pos[i];
+                vel[i] += g * dt;
+                pos[i] += vel[i] * dt;
+                if(pos[i][1]<0.0)
+                {
+                    pos[i] = prevPos[i];
+                    pos[i][1] = 0.0;
+                }
+            }
+            /* -------------------------------------------------------------------------- */
+            /*                                    solve                                   */
+            /* -------------------------------------------------------------------------- */
+            /* ------------------------------- solve edge ------------------------------- */
+            float alpha = edgeCompliance / dt / dt;
+            zeno::vec3f grads{0,0,0};
+            for(int i = 0; i < numEdges; i++)
+            {
+
+            }
+            /* ------------------------------ solve volume ------------------------------ */
+
+            /* -------------------------------------------------------------------------- */
+            /*                                  postSolve                                 */
+            /* -------------------------------------------------------------------------- */
+            for(int i = 0; i < pos.size(); i++)
+            {
+                vel[i] = (pos[i] - prevPos[i]) / dt;
             }
         }
-        /* -------------------------------------------------------------------------- */
-        /*                                    solve                                   */
-        /* -------------------------------------------------------------------------- */
-
-
-        /* -------------------------------------------------------------------------- */
-        /*                                  postSolve                                 */
-        /* -------------------------------------------------------------------------- */
-        for(int i = 0; i < pos.size(); i++)
-        {
-            vel[i] = (pos[i] - prevPos[i]) / dt;
-        }
-
 
         set_output("prim", std::move(prim));
-        
     };
 };
 
