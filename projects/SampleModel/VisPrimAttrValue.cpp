809c6060cc2998b1e36c71baaf11d53e1161a2c3
archibate <1931127624@qq.com>
2022-08-14 12:38:17 +0800

    add stars vis

--- projects/SampleModel/VisPrimAttrValue.cpp
+++ projects/SampleModel/VisPrimAttrValue.cpp
@@ -31,54 +31,61 @@ struct VisPrimAttrValue : INode {
         auto precision = get_input2<int>("precision");
         auto includeSelf = get_input2<bool>("includeSelf");
         auto dotDecoration = get_input2<bool>("dotDecoration");
+        bool textDecoration = !attrName.empty();
 
         std::vector<PrimitiveObject *> outprim2;
         std::vector<std::shared_ptr<PrimitiveObject>> outprim;
-        prim->verts.attr_visit<AttrAcceptAll>(attrName, [&] (auto const &attarr) {
-            outprim.resize(attarr.size());
-            outprim2.resize(attarr.size());
-#pragma omp parallel for
-            for (int i = 0; i < attarr.size(); i++) {
-                auto value = attarr[i];
-                auto pos = prim->verts[i];
-                std::ostringstream ss;
-                ss << std::setprecision(precision);
-                number_printer<std::decay_t<decltype(value)>>{}(ss, value);
-                auto str = ss.str();
-
-                auto numprim = std::static_pointer_cast<PrimitiveObject>(
-                    getThisGraph()->callTempNode("LoadStringPrim", {
-                        {"triangulate", std::make_shared<NumericObject>((bool)0)},
-                        {"decodeUVs", std::make_shared<NumericObject>((bool)0)},
-                        {"str", objectFromLiterial(str)},
-                    }).at("prim"));
-                //auto numprim = std::make_shared<PrimitiveObject>();
-                for (int i = 0; i < numprim->verts.size(); i++) {
-                    auto &v = numprim->verts[i];
-                    v = v * scale + pos;
-                }
-                outprim2[i] = numprim.get();
-                outprim[i] = std::move(numprim);
-            }
-            if (dotDecoration) {
-                auto numprim = std::static_pointer_cast<PrimitiveObject>(
-                    getThisGraph()->callTempNode("LoadSampleModel", {
-                        {"triangulate", std::make_shared<NumericObject>((bool)0)},
-                        {"decodeUVs", std::make_shared<NumericObject>((bool)0)},
-                        {"name", objectFromLiterial("star")},
-                    }).at("prim"));
+        if (textDecoration) {
+            prim->verts.attr_visit<AttrAcceptAll>(attrName, [&] (auto const &attarr) {
+                outprim.resize(attarr.size());
+                outprim2.resize(attarr.size());
 #pragma omp parallel for
                 for (int i = 0; i < attarr.size(); i++) {
+                    auto value = attarr[i];
                     auto pos = prim->verts[i];
-                    auto offprim = std::make_shared<PrimitiveObject>(*numprim);
-                    for (int i = 0; i < offprim->verts.size(); i++) {
-                        offprim->verts[i] += pos;
+                    std::ostringstream ss;
+                    ss << std::setprecision(precision);
+                    number_printer<std::decay_t<decltype(value)>>{}(ss, value);
+                    auto str = ss.str();
+
+                    auto numprim = std::static_pointer_cast<PrimitiveObject>(
+                        getThisGraph()->callTempNode("LoadStringPrim", {
+                            {"triangulate", std::make_shared<NumericObject>((bool)0)},
+                            {"decodeUVs", std::make_shared<NumericObject>((bool)0)},
+                            {"str", objectFromLiterial(str)},
+                        }).at("prim"));
+                    //auto numprim = std::make_shared<PrimitiveObject>();
+                    for (int j = 0; j < numprim->verts.size(); j++) {
+                        auto &v = numprim->verts[j];
+                        v = (v + vec3f(dotDecoration ? 0.5f : 0.3f, -0.3f, 0.0f)) * scale + pos;
                     }
-                    outprim2[i + attarr.size()] = offprim.get();
-                    outprim[i + attarr.size()] = std::move(offprim);
+                    outprim2[i] = numprim.get();
+                    outprim[i] = std::move(numprim);
                 }
+            });
+        }
+        if (dotDecoration) {
+            int attarrsize = textDecoration ? outprim.size() : prim->verts.size();
+            outprim.resize(attarrsize * (1 + (int)textDecoration));
+            outprim2.resize(attarrsize * (1 + (int)textDecoration));
+            auto numprim = std::static_pointer_cast<PrimitiveObject>(
+                getThisGraph()->callTempNode("LoadSampleModel", {
+                    {"triangulate", std::make_shared<NumericObject>((bool)0)},
+                    {"decodeUVs", std::make_shared<NumericObject>((bool)0)},
+                    {"name", objectFromLiterial("star")},
+                }).at("prim"));
+#pragma omp parallel for
+            for (int i = 0; i < attarrsize; i++) {
+                auto pos = prim->verts[i];
+                auto offprim = std::make_shared<PrimitiveObject>(*numprim);
+                for (int j = 0; j < offprim->verts.size(); j++) {
+                    auto &v = offprim->verts[j];
+                    v = v * (scale * 0.25f) + pos;
+                }
+                outprim2[i + attarrsize * (int)textDecoration] = offprim.get();
+                outprim[i + attarrsize * (int)textDecoration] = std::move(offprim);
             }
-        });
+        }
         if (includeSelf) {
             outprim2.push_back(prim.get());
         }
