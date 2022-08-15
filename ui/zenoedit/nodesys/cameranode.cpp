f08a6d37fac895391e79bfec00be539541cc5e3c
Aaron <910854285@qq.com>
2022-08-15 21:41:04 +0800

    camera prop

--- ui/zenoedit/nodesys/cameranode.cpp
+++ ui/zenoedit/nodesys/cameranode.cpp
@@ -4,6 +4,14 @@
 #include "zenoapplication.h"
 #include "graphsmanagment.h"
 
+#include <viewport/zenovis.h>
+#include "zenovis/Session.h"
+#include <zeno/core/Session.h>
+#include <zeno/extra/GlobalState.h>
+
+// FIXME  fatal error C1189: #error:  OpenGL header already included,
+//  remove this include, glad already provides it
+//#include "zenovis/Camera.h"
 
 CameraNode::CameraNode(const NodeUtilParam& params, QGraphicsItem* parent)
     : ZenoNode(params, parent)
@@ -42,6 +50,13 @@ void CameraNode::onEditClicked()
     IGraphsModel* pModel = zenoApp->graphsManagment()->currentModel();
     ZASSERT_EXIT(pModel);
 
+    auto &inst = Zenovis::GetInstance();
+    auto sess = inst.getSession();
+    ZASSERT_EXIT(sess);
+
+    auto scene = sess->get_scene();
+    ZASSERT_EXIT(scene);
+
     UI_VECTYPE vec({ 0., 0., 0. });
 
     PARAM_UPDATE_INFO info;
@@ -49,33 +64,34 @@ void CameraNode::onEditClicked()
     pModel->beginTransaction("update camera info");
 
     INPUT_SOCKET pos = inputs["pos"];
-    //TODO: get pos from viewport camera.
-    //vec = ...
+    //vec = {scene->camera->m_lodcenter.x, scene->camera->m_lodcenter.y, scene->camera->m_lodcenter.z};
+    std::vector<float> camProp = scene->getCameraProp();
+    vec = {camProp[0], camProp[1], camProp[2]};
     info.name = "pos";
     info.oldValue = pos.info.defaultValue;
     info.newValue = QVariant::fromValue(vec);
     pModel->updateSocketDefl(nodeid, info, this->subgIndex(), true);
 
     INPUT_SOCKET up = inputs["up"];
-    //TODO: get pos from viewport camera.
-    //vec = ...
+    vec = {camProp[6], camProp[7], camProp[8]};
     info.name = "up";
     info.oldValue = up.info.defaultValue;
     info.newValue = QVariant::fromValue(vec);
     pModel->updateSocketDefl(nodeid, info, this->subgIndex(), true);
 
     INPUT_SOCKET view = inputs["view"];
-    //TODO: get view from viewport camera.
-    //vec = ...
+    vec = {camProp[3], camProp[4], camProp[5]};
     info.name = "view";
     info.oldValue = view.info.defaultValue;
     info.newValue = QVariant::fromValue(vec);
     pModel->updateSocketDefl(nodeid, info, this->subgIndex(), true);
 
     INPUT_SOCKET frame = inputs["frame"];
-    //TODO: get frame from viewport camera.
-    //frame = ...
+    // FIXME Not work
+    int frameId = sess->get_curr_frameid();
+    frameId = zeno::getSession().globalState->frameid;
+
     info.name = "frame";
     info.oldValue = frame.info.defaultValue;
-    frame.info.defaultValue = QVariant::fromValue(vec);
+    frame.info.defaultValue = QVariant::fromValue(frameId);
 }
