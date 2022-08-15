ea4f91bf1750b7b15b573d9750dfd34e06e27033
archibate <1931127624@qq.com>
2022-08-15 18:06:51 +0800

    [release] fuckwendous

--- projects/SampleModel/LoadSampleModel.cpp
+++ projects/SampleModel/LoadSampleModel.cpp
@@ -61,8 +61,8 @@ struct LoadStringPrim : INode {
                     continue;
                 }
                 prim = std::static_pointer_cast<PrimitiveObject>(getThisGraph()->callTempNode("ReadObjPrim", {
-                    {"triangulate:", objectFromLiterial(bool(0))},
-                    {"decodeUVs:", objectFromLiterial(bool(0))},
+                    {"triangulate:", objectFromLiterial(int(0))},
+                    {"decodeUVs:", objectFromLiterial(int(0))},
                     {"path", objectFromLiterial(path)},
                 }).at("prim"));
                 cache[c] = std::make_shared<PrimitiveObject>(*prim);
