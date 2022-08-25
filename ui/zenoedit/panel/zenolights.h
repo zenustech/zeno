2355cbafa7163ffdf60c506eab407b13fcc5c8e4
Zhou Hang <765229842@qq.com>
2022-08-25 17:18:35 +0800

    Update zenolights.h

--- ui/zenoedit/panel/zenolights.h
+++ ui/zenoedit/panel/zenolights.h
@@ -39,7 +39,7 @@ public:
 public:
     ZenoLights(QWidget* parent = nullptr);
     void updateLights();
-    std::vector<zeno::vec3f> ZenoLights::computeLightPrim(zeno::vec3f position, zeno::vec3f rotate, zeno::vec3f scale);
+    std::vector<zeno::vec3f> computeLightPrim(zeno::vec3f position, zeno::vec3f rotate, zeno::vec3f scale);
 };
 
 
