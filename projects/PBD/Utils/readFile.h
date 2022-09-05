723334b62a0b11e765de50eecdd657e39748098d
chunleili <li_cl@foxmail.com>
2022-09-05 17:33:16 +0800

    add readScalarField

--- projects/PBD/Utils/readFile.h
+++ projects/PBD/Utils/readFile.h
@@ -5,14 +5,14 @@
 #include <string>
 
 /**
- * @brief 读取txt文件到场中（如pos lines等）
+ * @brief 读取txt文件到向量场中（如pos lines等）
  * 
  * @tparam T 场类型，如std::vector<vec3f>
- * @param absPath txt文件的绝对路径，注意windows路径用\\
+ * @param absPath txt文件的绝对路径，注意windows路径用双反斜杠
  * @param saveTo 要保存到的场如pos, lines, tets等
  */
 template<typename T>
-void readFile(const std::string &absPath, T &saveTo)
+void readVectorField(const std::string &absPath, T &saveTo)
 {
     std::ifstream in(absPath);
     std::string line;
@@ -26,5 +26,25 @@ void readFile(const std::string &absPath, T &saveTo)
             ss >> temp[i];
         saveTo.emplace_back(temp);
     }
+    in.close();
+}
+
+
+/**
+ * @brief 读取txt文件到标量场中（如temperature等）
+ * 
+ * @tparam T 场类型，如std::vector<vec3f>
+ * @param absPath txt文件的绝对路径，注意windows路径用双反斜杠
+ * @param saveTo 要保存到的场
+ */
+template<typename T>
+void readScalarField(const std::string &absPath, T &saveTo)
+{
+    std::ifstream in(absPath);
+    std::string line;
+
+    while (std::getline(in, line)) 
+        saveTo.emplace_back(std::stoi(line));
+
     in.close();
 }
