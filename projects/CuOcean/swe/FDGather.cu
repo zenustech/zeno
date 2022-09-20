fdfc5945e40e73492f21202db59ee82606fa5a96
littlemine <wxlwxl1993@zju.edu.cn>
2022-09-20 22:24:27 +0800

    supplement grid2dsample

--- projects/CuOcean/swe/FDGather.cu
+++ projects/CuOcean/swe/FDGather.cu
@@ -103,4 +103,78 @@ ZENDEFNODE(ZSGather2DFiniteDifference, {
                                            {"zenofx"},
                                        });
 
+template <class T> static constexpr T lerp(T a, T b, float c) {
+    return (1 - c) * a + c * b;
+}
+template <auto nchn>
+void sample2D(typename ZenoParticles::particles_t &prim, const zs::SmallString &coordTag,
+              const zs::SmallString &fieldTag, int nx, int ny, float h, zs::vec<float, 3> bmin) {
+    using vec3f = zs::vec<float, 3>;
+    using namespace zs;
+    constexpr auto space = execspace_e::cuda;
+    auto pol = cuda_exec().device(0);
+    using T = conditional_t<nchn == 1, zs::vec<float, 1>, vec3f>;
+
+    Vector<T> temp(prim.get_allocator(), prim.size());
+
+    pol(range(prim.size()), [temp = proxy<space>(temp), prim = proxy<space>({}, prim), coordTag, fieldTag, bmin, nx, ny,
+                             h] ZS_LAMBDA(int tidx) mutable {
+        auto uv = prim.template pack<3>(coordTag, tidx);
+        auto uv2 = (uv - bmin) / h;
+        uv2[1] = 0;
+        uv2[0] = zs::min(zs::max(uv2[0], 0.01f), nx - 1.01f);
+        uv2[2] = zs::min(zs::max(uv2[2], 0.01f), ny - 1.01f);
+        // uv2 = zeno::min(zeno::max(uv2, vec3f{0.01, 0.0, 0.01}), vec3f{nx - 1.01, 0.0, ny - 1.01});
+        int i = uv2[0];
+        int j = uv2[2];
+        float cx = uv2[0] - i, cy = uv2[2] - j;
+        size_t idx00 = j * nx + i, idx01 = j * nx + i + 1, idx10 = (j + 1) * nx + i, idx11 = (j + 1) * nx + i + 1;
+        T f00 = prim.template pack<nchn>(fieldTag, idx00);
+        T f01 = prim.template pack<nchn>(fieldTag, idx01);
+        T f10 = prim.template pack<nchn>(fieldTag, idx10);
+        T f11 = prim.template pack<nchn>(fieldTag, idx11);
+        temp[tidx] = lerp<T>(lerp<T>(f00, f01, cx), lerp<T>(f10, f11, cx), cy);
+    });
+
+    pol(range(prim.size()), [temp = proxy<space>(temp), prim = proxy<space>({}, prim), fieldTag] ZS_LAMBDA(
+                                int tidx) mutable { prim.template tuple<nchn>(fieldTag, tidx) = temp[tidx]; });
+}
+struct ZSGrid2DSample : zeno::INode {
+    virtual void apply() override {
+        using vec3f = zs::vec<float, 3>;
+        auto nx = get_input2<int>("nx");
+        auto ny = get_input2<int>("ny");
+        auto bmin = get_input2<zeno::vec3f>("bmin");
+        auto grid = get_input<ZenoParticles>("grid");
+        auto attrT = get_input2<std::string>("attrT");
+        auto channel = get_input2<std::string>("channel");
+        auto sampleby = get_input2<std::string>("sampleBy");
+        auto h = get_input2<float>("h");
+
+        auto &pars = grid->getParticles();
+        if (pars.hasProperty(channel) && pars.hasProperty(sampleby)) {
+            if (attrT == "float") {
+                sample2D<1>(pars, sampleby, channel, nx, ny, h, vec3f{bmin[0], bmin[1], bmin[2]});
+            } else if (attrT == "vec3f") {
+                sample2D<3>(pars, sampleby, channel, nx, ny, h, vec3f{bmin[0], bmin[1], bmin[2]});
+            }
+        }
+
+        set_output("prim", std::move(grid));
+    }
+};
+ZENDEFNODE(ZSGrid2DSample, {
+                               {{"ZenoParticles", "grid"},
+                                {"int", "nx", "1"},
+                                {"int", "ny", "1"},
+                                {"float", "h", "1"},
+                                {"vec3f", "bmin", "0,0,0"},
+                                {"string", "channel", "pos"},
+                                {"string", "sampleBy", "pos"},
+                                {"enum vec3 float", "attrT", "float"}},
+                               {{"ZenoParticles", "prim"}},
+                               {},
+                               {"zenofx"},
+                           });
+
 } // namespace zeno
