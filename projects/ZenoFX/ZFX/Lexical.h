b4cfb3b7bfec8655b24dffe7c290402099518089
nm243012@163.com <nm243012@163.com>
2022-09-28 10:34:22 +0800

    add []

--- projects/ZenoFX/ZFX/Lexical.h
+++ projects/ZenoFX/ZFX/Lexical.h
@@ -4,13 +4,14 @@
 
 namespace zfx {
 
-inline char opchars[] = "+-*/%=(),.;<>!&|^?:";
+inline char opchars[] = "+-*/%=(),.;<>!&|^?:[]";
 inline std::set<std::string> opstrs = {
     "(", ")", ",", ".", ";",
     "+", "-", "*", "/", "%", "=",
     "+=", "-=", "*=", "/=", "%=",
     "==", "!=", "<", "<=", ">", ">=",
     "&", "&!", "|", "^", "!", "?", ":",
+    ,"[","]"
     };
 
 inline bool is_literial_atom(std::string const &s) {
