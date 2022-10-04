f042794b83545bc652d874181aea954223479fbe
chunleili <li_cl@foxmail.com>
2022-10-04 17:08:27 +0800

    cloth init & build edge

--- projects/PBD/PBDCloth/PBDClothInit.cpp
+++ projects/PBD/PBDCloth/PBDClothInit.cpp
@@ -1,17 +1,18 @@
 #include <zeno/types/PrimitiveObject.h>
 #include <zeno/zeno.h>
-#include <Eigen/Eigen>
-
-using Matrix4r = Eigen::Matrix<float, 4, 4>;
 namespace zeno {
 struct PBDClothInit : zeno::INode {
-private:
-    std::vector<float> restLen;
-    std::vector<float> invMass;
-    std::vector<float> restAng;
 
-    int numParticles;
 
+    /**
+     * @brief 计算两个面夹角的辅助函数。
+     * 
+     * @param p0 顶点0位置
+     * @param p1 顶点1位置
+     * @param p2 顶点2位置
+     * @param p3 顶点3位置
+     * @return float 两个面夹角
+     */
     float computeAng(
         const vec3f &p0,
         const vec3f &p1, 
@@ -21,38 +22,69 @@ private:
         auto n1 = cross((p1 - p0), (p2 - p0));
         n1 = n1 / length(n1);
         auto n2 = cross((p1 - p0), (p3 - p0));
-        n2 = n2 / length(n2) ;
+        n2 = n2 / length(n2);
         auto res = dot(n1, n2);
         if(res<-1.0) res = -1.0;
         if(res>1.0)  res = 1.0;
-        return res;
+        return acos(res);
     }
 
-    void initRestLen(PrimitiveObject *prim) 
+    /**
+     * @brief 计算所有原角度
+     * 
+     * @param pos 顶点
+     * @param quads 四个顶点连成一块布片
+     * @param restAng 原角度
+     */
+    void initRestAng(
+        const AttrVector<vec3f> &pos,
+        const AttrVector<vec4i> &quads,
+        std::vector<float> &restAng
+    ) 
     {
-        auto &pos = prim->verts;
-        auto &edge = prim->lines;
-        for(int i = 0; i < edge.size(); i++)
-            restLen[i] = length((pos[edge[i][0]] - pos[edge[i][1]]));
-    }
-
-    void initRestAng(PrimitiveObject *prim) 
-    {
-        auto &pos = prim->verts;
-        auto &quads = prim->quads;
         for(int i = 0; i < quads.size(); i++)
         {
-            vec3f &p1 = pos[quads[i][0]];
-            vec3f &p2 = pos[quads[i][1]];
-            vec3f &p3 = pos[quads[i][2]];
-            vec3f &p4 = pos[quads[i][3]];
+            const vec3f &p1 = pos[quads[i][0]];
+            const vec3f &p2 = pos[quads[i][1]];
+            const vec3f &p3 = pos[quads[i][2]];
+            const vec3f &p4 = pos[quads[i][3]];
             restAng[i] = computeAng(p1,p2,p3,p4);
         }
     }
 
-    void initInvMass(PrimitiveObject *prim, float areaDensity) {
-        auto &pos = prim->verts;
-        auto &quads = prim->quads;
+
+    /**
+     * @brief 计算所有原长
+     * 
+     * @param pos 顶点
+     * @param edge 边连接关系
+     * @param restLen 原长
+     */
+    void initRestLen(
+        AttrVector<vec3f> &pos,
+        AttrVector<vec2i> &edge,
+        std::vector<float> &restLen
+    ) 
+    {
+        for(int i = 0; i < edge.size(); i++)
+            restLen[i] = length((pos[edge[i][0]] - pos[edge[i][1]]));
+    }
+
+
+    /**
+     * @brief 计算所有质量倒数
+     * 
+     * @param pos 顶点
+     * @param quads 四个点连接关系
+     * @param invMass 质量倒数
+     * @param areaDensity 面密度
+     */
+    void initInvMass(        
+        const AttrVector<vec3f> &pos,
+        const AttrVector<vec4i> &quads,
+        std::vector<float> &invMass,
+        const float areaDensity) 
+    {
         for(int i = 0; i < quads.size(); i++)
         {
             float quad_area = dot(pos[quads[i][1]] - pos[quads[i][0]],  pos[quads[i][3]] - pos[quads[i][2]]);
@@ -63,28 +95,43 @@ private:
         }
     }
 
-    void init(PrimitiveObject *prim, float areaDensity)
+    void buildEdge(const AttrVector<vec4i> &quads, AttrVector<vec2i> &edge)
     {
-        invMass.resize(prim->verts.size());
-        restLen.resize(prim->lines.size());
-        restAng.resize(prim->quads.size());
-
-        initRestLen(prim);
-        initRestAng(prim);
-        initInvMass(prim, areaDensity);
-
-        numParticles = prim->verts.size();
+        for(int i= 0; i<quads.size(); i++)
+        {
+            int q0 = quads[i][0];
+            int q1 = quads[i][1];
+            int q2 = quads[i][2];
+            int q3 = quads[i][3];
+            edge[i + 0] = vec2i(q0,q1);
+            edge[i + 1] = vec2i(q1,q3);            
+            edge[i + 2] = vec2i(q3,q0);
+            edge[i + 3] = vec2i(q1,q2);           
+            edge[i + 4] = vec2i(q3,q2);
+        }
     }
-
 
 
 public:
     virtual void apply() override {
         auto prim = get_input<PrimitiveObject>("prim");
 
+        auto &pos = prim->verts;
+        auto &quads = prim->quads;
+        auto &edge = prim->lines;
+
         auto areaDensity = get_input<zeno::NumericObject>("areaDensity")->get<int>();
 
-        init(prim.get(), areaDensity);
+        auto &invMass = prim->verts.add_attr<float>("invMass");
+        auto &restLen = prim->lines.add_attr<float>("restLen");
+        auto &restAng = prim->quads.add_attr<float>("restAng");
+        invMass.resize(prim->verts.size());
+        restLen.resize(prim->lines.size());
+        invMass.resize(prim->quads.size());
+
+        initInvMass(pos,quads,invMass,areaDensity);
+        initRestLen(pos,edge,restLen);
+        initRestAng(pos,quads,restAng);
 
         set_output("outPrim", std::move(prim));
 
