06b10b1008ccb7d2cb37e3bbbe3d2172c4b9b025
chunleili <li_cl@foxmail.com>
2022-10-23 11:54:44 +0800

    test kernels poyly6 and spiky

--- projects/PBD/test/testKernels.cpp
+++ projects/PBD/test/testKernels.cpp
@@ -1,6 +1,8 @@
 #include "../PBF/SPHKernels.h"
 #include "../Utils/myPrint.h"
 #include <vector>
+#include <zeno/utils/vec.h>
+
 float CubicKernel::m_radius;
 float CubicKernel::m_k;
 float CubicKernel::m_l;
@@ -13,10 +15,27 @@ int main()
     CubicKernel::set(h);
 
     std::vector<float> cubicW(num);
+    std::vector<zeno::vec3f> cubic_gradW(num);
+    std::vector<float> poly6W(num);
+    std::vector<zeno::vec3f> Spiky_gradW(num);
     for (size_t i = 0; i < num; i++)
     {
+        //cubic W
         float dist = h/num * i;
         cubicW[i] = CubicKernel::W(dist);
+        
+        //cubic gradW
+        zeno::vec3f distVec = zeno::vec3f{h/num*i,h/num*i,h/num*i};
+        cubic_gradW[i] = CubicKernel::gradW(distVec);
+
+        //poly6 W
+        poly6W[i] = Poly6Kernel::W(dist);
+        //spiky gradW
+        Spiky_gradW[i] = SpikyKernel::gradW(distVec);
     }
     printScalarField("cubicW.csv",cubicW);
+    printVectorField("cubic_gradW.csv",cubic_gradW);
+    printScalarField("poly6W.csv",poly6W);
+    printVectorField("Spiky_gradW.csv",Spiky_gradW);
+
 }
