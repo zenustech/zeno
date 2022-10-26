d62b0ef22d3add8761402780b35a350db816efb5
kaiyang-1 <yangkai759@foxmail.com>
2022-10-26 13:05:02 +0800

    Node for renormalizing SDF on sparse grid

--- projects/CuOcean/levelset/Grid_creator.cu
+++ projects/CuOcean/levelset/Grid_creator.cu
@@ -12,7 +12,7 @@
 
 namespace zeno {
 
-struct MakeSparseGrid : INode {
+struct ZSMakeSparseGrid : INode {
     void apply() override {
         float dx = get_param<float>("dx");
         if (has_input("Dx")) {
@@ -26,7 +26,7 @@ struct MakeSparseGrid : INode {
         int nby = float(ny + 7) / 8.f;
         int nbz = float(nz + 7) / 8.f;
 
-        int numExpectedBlocks = nbx * nby * nbz;
+        size_t numExpectedBlocks = nbx * nby * nbz;
 
         auto zsSPG = zeno::IObject::make<ZenoSparseGrid>();
         auto &spg = zsSPG->spg;
@@ -47,25 +47,26 @@ struct MakeSparseGrid : INode {
                 table.insert(ivec3{i * 8, j * 8, k * 8});
             });
 
-		ivec3 sphere_c{nx/2, ny/2, nz/2};
-		float sphere_r = 10.f;
+        ivec3 sphere_c{nx / 2, ny / 2, nz / 2};
+        float sphere_r = 10.f;
 
-		auto bcnt = spg.numBlocks();
+        auto bcnt = spg.numBlocks();
         pol(zs::range(bcnt * 512), [spgv = zs::proxy<space>(spg), sphere_c, sphere_r] __device__(int cellno) mutable {
-            int bno = cellno / 512;
-			int cno = cellno & 511;
-			auto bcoord = spgv._table._activeKeys[bno];
-			auto cellid = RM_CVREF_T(spgv)::local_offset_to_coord(cno);
-			auto ccoord = bcoord + cellid;
+#if 0            
+			int bno = cellno / 512;
+            int cno = cellno & 511;
+            auto bcoord = spgv._table._activeKeys[bno];
+            auto cellid = RM_CVREF_T(spgv)::local_offset_to_coord(cno);
+            auto ccoord = bcoord + cellid;
+#endif
+			auto icoord = spgv.iCoord(cellno);
+            auto dx = spgv.voxelSize()[0]; // spgv._transform(0, 0);
 
-			auto dx = spgv._transform(0, 0);
+            float dist2c = zs::sqrt(float(zs::sqr(icoord[0] - sphere_c[0]) + zs::sqr(icoord[1] - sphere_c[1]) +
+                                          zs::sqr(icoord[2] - sphere_c[2])));
 
-			float dist2c = zs::sqrt(float(zs::sqr(ccoord[0] - sphere_c[0])
-										+ zs::sqr(ccoord[1] - sphere_c[1])
-										+ zs::sqr(ccoord[2] - sphere_c[2])));
-			
-			//spgv("sdf", bno, cno) = ;
-			spgv("sdf", ccoord) = dist2c > sphere_r ? dx : -dx;
+            //spgv("sdf", bno, cno) = ;
+            spgv("sdf", icoord) = dist2c > sphere_r ? dx : -dx;
         });
 
         // spg.resize(numExpectedBlocks);
@@ -76,15 +77,15 @@ struct MakeSparseGrid : INode {
     }
 };
 
-ZENDEFNODE(MakeSparseGrid, {
-                               /* inputs: */
-                               {"Dx", {"int", "nx", "128"}, {"int", "ny", "128"}, {"int", "nz", "128"}},
-                               /* outputs: */
-                               {"Grid"},
-                               /* params: */
-                               {{"float", "dx", "1.0"}},
-                               /* category: */
-                               {"Volume"},
-                           });
+ZENDEFNODE(ZSMakeSparseGrid, {
+                                 /* inputs: */
+                                 {"Dx", {"int", "nx", "128"}, {"int", "ny", "128"}, {"int", "nz", "128"}},
+                                 /* outputs: */
+                                 {"Grid"},
+                                 /* params: */
+                                 {{"float", "dx", "1.0"}},
+                                 /* category: */
+                                 {"Volume"}
+                             });
 
 } // namespace zeno
--- projects/CuOcean/levelset/Reinitialization.cu
+++ projects/CuOcean/levelset/Reinitialization.cu
@@ -1,8 +1,8 @@
 #include "Structures.hpp"
-#include "zensim/geometry/LevelSetUtils.tpp"
 #include "zensim/cuda/execution/ExecutionPolicy.cuh"
-#include "zensim/omp/execution/ExecutionPolicy.hpp"
+#include "zensim/geometry/LevelSetUtils.tpp"
 #include "zensim/geometry/SparseGrid.hpp"
+#include "zensim/omp/execution/ExecutionPolicy.hpp"
 #include "zensim/zpc_tpls/fmt/color.h"
 #include "zensim/zpc_tpls/fmt/format.h"
 
@@ -11,8 +11,36 @@
 #include <zeno/types/PrimitiveObject.h>
 
 namespace zeno {
+struct ZSRenormalizeSDF : INode {
+    void apply() override {
+        int nIter = get_param<int>("iterations");
+        auto sdfGrid = get_input<ZenoSparseGrid>("SDF");
 
-struct : INode {};
+        auto &sdf = sdfGrid->spg;
+        auto block_cnt = sdf.numBlocks();
 
+        ZenoSparseGrid::spg_t sdf_tmp{{{"sdf", 1}}, block_cnt, zs::memsrc_e::device, 0};
+        sdf_tmp._table = sdf._table;
+        sdf_tmp._transform = sdf._transform;
+        sdf_tmp._background = sdf._background;
+        
+        auto pol = zs::cuda_exec();
+        constexpr auto space = zs::execspace_e::cuda;
+        pol(zs::range(block_cnt * 512), [spgv = zs::proxy<space>(sdf)] __device__(int cellno) mutable {
+            
+        });
 
-}
+        set_output("SDF", sdfGrid);
+    }
+};
+
+ZENDEFNODE(ZSRenormalizeSDF, {/* inputs: */
+                              {"SDF"},
+                              /* outputs: */
+                              {"SDF"},
+                              /* params: */
+                              {{"int", "iterations", "5"}},
+                              /* category: */
+                              {"Volume"}});
+
+} // namespace zeno
