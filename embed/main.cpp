aff501e6ce3ae4e1ce499cf63031056339045087
archibate <1931127624@qq.com>
2022-10-28 10:02:18 +0800

    t

--- embed/main.cpp
+++ embed/main.cpp
@@ -1,11 +1,11 @@
-#include <zeno/core/Graph.h>
 #include <zeno/core/Session.h>
+#include <zeno/core/Graph.h>
 
 int main() {
     const char *json = "json here";
-    auto g = zeno::getSession().createGraph();
-    g->addSubnetNode("CustomMain")->loadGraph(json);
+    zeno::Graph *g = zeno::getSession().createGraph();
+    g->addSubnetNode("custom")->loadGraph(json);
     std::map<std::string, std::shared_ptr<zeno::IObject>> inputs, outputs;
-    outputs = g->callTempNode("CustomMain", std::move(inputs));
+    outputs = g->callTempNode("custom", std::move(inputs));
     return 0;
 }
