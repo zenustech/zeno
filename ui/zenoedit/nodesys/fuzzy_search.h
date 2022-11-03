ca240e85df319da05f493c5cbf14fcf8c2e6734e
luzh <luzh16371@gmail.com>
2022-11-03 12:06:53 +0800

    [revert] #751 add highlight for fuzzy search result.

--- ui/zenoedit/nodesys/fuzzy_search.h
+++ ui/zenoedit/nodesys/fuzzy_search.h
@@ -4,103 +4,76 @@
 #include <QList>
 #include <QSet>
 
-struct FuzzyMatchKey
-{
-    QString name;
-    QVector<int> matchIndices;
-    FuzzyMatchKey() = default;
-    FuzzyMatchKey(const QString& name, const QVector<int>& matchIndices)
-        : name(name)
-        , matchIndices(matchIndices)
-    {
-    }
-};
-
-static QVector<int> key_appear_by_order(const QString& pattern, const QString& key) {
-    QVector<int> matchIndices;
-    int from = 0;
+static bool key_appear_by_order(const QString& pattern, QString key) {
     for (auto i = 0; i < pattern.size(); i++) {
         QChar c = pattern.at(i);
-        auto res = key.indexOf(c, from, Qt::CaseInsensitive);
+        auto res = key.indexOf(c, 0, Qt::CaseInsensitive);
         if (res == -1) {
-            return {};
+            return false;
         }
-        matchIndices.push_back(res);
-        from = res + 1;
+        key = key.mid(res + 1);
     }
-    return matchIndices;
+    return true;
 }
 
-static QVector<int> capital_match(const QString& pattern, const QString& key) {
-    QVector<int> upperIndices;
+static bool capital_match(const QString& pattern, const QString& key) {
     QString only_upper;
     for (auto i = 0; i < key.size(); i++) {
         if (key.at(i).isUpper()) {
             only_upper += key.at(i);
-            upperIndices.push_back(i);
         }
     }
-    int from = only_upper.indexOf(pattern, 0, Qt::CaseInsensitive);
-    if (from == -1) {
-        return {};
-    }
-    QVector<int> matchIndices;
-    for (int i = 0; i < pattern.size(); ++i) {
-        matchIndices.push_back(upperIndices[from + i]);
-    }
-    return matchIndices;
+    return only_upper.contains(pattern, Qt::CaseInsensitive);
 }
 
 static void merge_condidates(
-    QList<FuzzyMatchKey>& ret_list,
+    QList<QString>& ret_list,
     QSet<QString>& ret_set,
-    const QList<FuzzyMatchKey>& lst
+    const QList<QString>& lst
 ) {
     const int MAX_COUNT = 30;
     for (auto i = 0; i < lst.size(); i++) {
         if (ret_list.size() > MAX_COUNT) {
             break;
         }
-        auto& s = lst[i].name;
+        auto s = lst[i];
         if (!ret_set.contains(s)) {
-            ret_list.push_back(lst[i]);
+            ret_list.push_back(s);
             ret_set.insert(s);
         }
     }
 }
 
-static QList<FuzzyMatchKey> fuzzy_search(const QString& pattern, const QList<QString>& keys) {
-    QList<FuzzyMatchKey> key_appear_by_order_conds;
+static QList<QString> fuzzy_search(const QString& pattern, const QList<QString>& keys) {
+    QList<QString> key_appear_by_order_conds;
     for (auto i = 0; i < keys.size(); i++) {
-        auto& k = keys[i];
-        auto indices = key_appear_by_order(pattern, k);
-        if (!indices.empty()) {
-            key_appear_by_order_conds.push_back({ k, indices });
+        auto k = keys[i];
+        if (key_appear_by_order(pattern, k)) {
+            key_appear_by_order_conds.push_back(k);
         }
     }
-    QList<FuzzyMatchKey> direct_match_conds;
+    QList<QString> direct_match_conds;
     for (auto i = 0; i < key_appear_by_order_conds.size(); i++) {
-        auto& k = key_appear_by_order_conds[i].name;
+        auto k = key_appear_by_order_conds[i];
         if (k.contains(pattern, Qt::CaseInsensitive)) {
-            direct_match_conds.push_back(key_appear_by_order_conds[i]);
+            direct_match_conds.push_back(k);
         }
     }
-    QList<FuzzyMatchKey> prefix_match_conds;
+    QList<QString> prefix_match_conds;
     for (auto i = 0; i < direct_match_conds.size(); i++) {
-        auto& k = direct_match_conds[i].name;
+        auto k = direct_match_conds[i];
         if (k.indexOf(pattern, 0, Qt::CaseInsensitive) == 0) {
-            prefix_match_conds.push_back(direct_match_conds[i]);
+            prefix_match_conds.push_back(k);
         }
     }
-    QList<FuzzyMatchKey> capital_match_conds;
+    QList<QString> capital_match_conds;
     for (auto i = 0; i < key_appear_by_order_conds.size(); i++) {
-        auto& k = key_appear_by_order_conds[i].name;
-        auto indices = capital_match(pattern, k);
-        if (!indices.empty()) {
-            capital_match_conds.push_back(key_appear_by_order_conds[i]);
+        auto k = key_appear_by_order_conds[i];
+        if (capital_match(pattern, k)) {
+            capital_match_conds.push_back(k);
         }
     }
-    QList<FuzzyMatchKey> ret_list;
+    QList<QString> ret_list;
     QSet<QString> ret_set;
     merge_condidates(ret_list, ret_set, prefix_match_conds);
     merge_condidates(ret_list, ret_set, capital_match_conds);
