f1ed1e7a1fc039a61d3ecf026509d2059ceecc38
ShuliangLu <graphicslsl@gmail.com>
2022-11-06 20:34:24 +0800

    fix kernel mark

--- projects/CuLagrange/geometry/kernel/calculate_area.hpp
+++ projects/CuLagrange/geometry/kernel/calculate_area.hpp
@@ -27,13 +27,14 @@ namespace zeno {
         TILEVEC_OPS::fill(pol,points,pointAreaTag,(T)0.0);
 
         pol(zs::range(tris.size()),
-            [tris = proxy<space>({},tris),points = proxy<space>({},points),triAreaTag,pointAreaTag] ZS_LAMBDA(int ti) mutable {
+            [tris = proxy<space>({},tris),points = proxy<space>({},points),triAreaTag,pointAreaTag] __device__(int ti) mutable {
                 auto subarea = tris(triAreaTag,ti) / (T)3.0;
                 for(int i = 0;i != 3;++i) {
                     auto pidx = reinterpret_bits<int>(tris("fp_inds",i,ti));
                     atomic_add(exec_cuda,&points(pointAreaTag,pidx),subarea);
                 }
         });
+        return true;
         
     }
 
@@ -51,13 +52,14 @@ namespace zeno {
         TILEVEC_OPS::fill(pol,lines,lineAreaTag,(T)0.0);
 
         pol(zs::range(lines.size()),
-            [verts = proxy<space>({},verts),lines = proxy<space>({},lines),tris = proxy<space>({},tris),triAreaTag,lineAreaTag] ZS_LAMBDA(int li) mutable {
+            [verts = proxy<space>({},verts),lines = proxy<space>({},lines),tris = proxy<space>({},tris),triAreaTag,lineAreaTag] __device__(int li) mutable {
                 for(int i = 0;i != 2;++i){
                     auto tidx = reinterpret_bits<int>(lines("fe_inds",i,li));
                     auto subArea = tris(triAreaTag,tidx) / (T)2.0;
                     lines(lineAreaTag,li) += subArea;
                 }
         });
+        return true;
     }
 
     template<typename Pol,typename VTileVec,typename TTileVec>
@@ -70,14 +72,18 @@ namespace zeno {
         if(!tris.hasProperty(areaTag))
             return false;
 
+        // std::cout << "try calculate tris area" << std::endl;
         pol(zs::range(tris.size()),
-            [verts = proxy<space>({},verts),tris = proxy<space>({},tris),xTag,areaTag] ZS_LAMBDA(int ti) mutable {
+            [verts = proxy<space>({},verts),tris = proxy<space>({},tris),xTag,areaTag] __device__(int ti) mutable {
                 auto inds = tris.template pack<3>("inds",ti).reinterpret_bits(int_c);
                 auto a0 = verts.template pack<3>(xTag,inds[0]);
                 auto a1 = verts.template pack<3>(xTag,inds[1]);
                 auto a2 = verts.template pack<3>(xTag,inds[2]);
                 tris(areaTag,ti) = LSL_GEO::area(a0,a1,a2);
+                // tris(areaTag,ti) = (T)1.0;
         });
+        // std::cout << "try calculate tris area" << std::endl;
+        return true;
     }
 
 };
