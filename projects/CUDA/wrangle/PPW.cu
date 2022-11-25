ec356d2a964d4b07f54c701711e292da717b59a8
littlemine <wxlwxl1993@zju.edu.cn>
2022-11-25 20:01:15 +0800

    fix "x" "v" zswrangler issues

--- projects/CUDA/wrangle/PPW.cu
+++ projects/CUDA/wrangle/PPW.cu
@@ -131,14 +131,10 @@ struct ZSParticleParticleWrangler : INode {
 
         /// symbols
         auto def_sym = [&opts](const std::string &prefix, std::string key, int dim) {
-            if (key == "x" || key == "v")
-                zeno::log_warn("please use property/attribute name [pos] and [vel] instead of [x] and [v] from "
-                               "now on to keep "
-                               "consistent with cpu-side wrangles");
-            if (key == "pos")
-                key = "x";
-            else if (key == "vel")
-                key = "v";
+            if (key == "x")
+                opts.define_symbol(prefix + "pos", dim);
+            else if (key == "v")
+                opts.define_symbol(prefix + "vel", dim);
             opts.define_symbol(prefix + key, dim);
         };
 
@@ -205,6 +201,14 @@ struct ZSParticleParticleWrangler : INode {
             cuLinkDestroy((CUlinkState)state);
         }
 
+        auto transTag = [](std::string str) {
+            if (str == "pos")
+                str = "x";
+            else if (str == "vel")
+                str = "v";
+            return str;
+        };
+
         /// symbols
         zs::Vector<AccessorAoSoA> haccessors{prog->symbols.size()};
         auto unitBytes = sizeof(RM_CVREF_T(pars)::value_type);
@@ -227,7 +231,7 @@ struct ZSParticleParticleWrangler : INode {
                                               (unsigned short)unitBytes,
                                               (unsigned short)tileSize,
                                               (unsigned short)targetParPtr->numChannels(),
-                                              (unsigned short)(targetParPtr->getPropertyOffset(name) + dimid),
+                                              (unsigned short)(targetParPtr->getPropertyOffset(transTag(name)) + dimid),
                                               (unsigned short)isNeighborProperty};
         }
         auto daccessors = haccessors.clone({zs::memsrc_e::device, 0});
