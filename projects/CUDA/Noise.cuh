ff66c635280b9b23b3c11b365574c045127d37be
kaiyang-1 <yangkai759@foxmail.com>
2022-11-30 13:29:32 +0800

    organize the perlin noise declaration

---
+++ projects/CUDA/Noise.cuh
@@ -0,0 +1,15 @@
+#pragma once
+#include "zensim/math/Vec.h"
+
+namespace zeno {
+
+struct PerlinNoise {
+    using vec3f = zs::vec<float, 3>;
+    static __device__ __host__ vec3f perlin_hash22(vec3f p);
+
+    static __device__ __host__ float perlin_lev1(vec3f p);
+
+    static __device__ __host__ float perlin(vec3f a, float power, float depth);
+};
+
+}
