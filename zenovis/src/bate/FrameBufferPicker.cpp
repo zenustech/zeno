5dee5f16d6b8943e0a19d1b456984714ed2f125a
ryan <ryannas@163.com>
2022-12-24 23:11:28 +0800

    fix framebuffer picker crash when using camera node

--- zenovis/src/bate/FrameBufferPicker.cpp
+++ zenovis/src/bate/FrameBufferPicker.cpp
@@ -292,12 +292,12 @@ struct FrameBufferPicker : IPicker {
                     }
                 }
             }
-            prims.emplace_back(std::make_pair(prim_name, prim));
+            if (prim) prims.emplace_back(std::make_pair(prim_name, prim));
         }
         if (prims.empty()) {
             for (const auto& [prim_name, prim] : prims_shared) {
                 auto p = dynamic_cast<PrimitiveObject*>(prim.get());
-                prims.emplace_back(std::make_pair(prim_name, p));
+                if (p) prims.emplace_back(std::make_pair(prim_name, p));
             }
         }
 
