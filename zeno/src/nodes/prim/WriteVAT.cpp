ae3acd63f2fd8e521b39695d8fb79ad51018d88c
zhouhang95 <765229842@qq.com>
2023-02-05 00:00:46 +0800

    vat bbox

--- zeno/src/nodes/prim/WriteVAT.cpp
+++ zeno/src/nodes/prim/WriteVAT.cpp
@@ -3,6 +3,7 @@
 #include <zeno/types/NumericObject.h>
 #include <zeno/extra/GlobalState.h>
 #include <zeno/utils/log.h>
+#include "zeno/para/parallel_reduce.h"
 
 #include <vector>
 #include <iostream>
@@ -11,6 +12,14 @@ using std::vector;
 using zeno::vec3f;
 
 namespace zeno {
+
+static int16_t f32_to_i16(float v) {
+    return v * std::numeric_limits<int16_t>::max();
+}
+
+static float i16_to_f32(int16_t v) {
+    return (float)v / std::numeric_limits<int16_t>::max();
+}
 
 static void write_vec3f(std::ofstream &file, zeno::vec3f vec) {
     file.write((char*)&vec[0], sizeof(float));
@@ -26,8 +35,48 @@ static zeno::vec3f read_vec3f(std::ifstream &file) {
     return vec;
 }
 
+static void write_normalized_vec3f(std::ofstream &file, vec3f vec, vec3f _min, vec3f _max) {
+    vec = (vec - _min) / (_max - _min);
+
+    int16_t _0 = f32_to_i16(vec[0]);
+    int16_t _1 = f32_to_i16(vec[1]);
+    int16_t _2 = f32_to_i16(vec[2]);
+
+    file.write((char*)&_0, sizeof(int16_t));
+    file.write((char*)&_1, sizeof(int16_t));
+    file.write((char*)&_2, sizeof(int16_t));
+}
+
+static zeno::vec3f read_normalized_vec3f(std::ifstream &file, vec3f _min, vec3f _max) {
+    int16_t _0;
+    int16_t _1;
+    int16_t _2;
+    file.read((char*)&_0, sizeof (int16_t));
+    file.read((char*)&_1, sizeof (int16_t));
+    file.read((char*)&_2, sizeof (int16_t));
+
+    zeno::vec3f vec = {
+            i16_to_f32(_0),
+            i16_to_f32(_1),
+            i16_to_f32(_2)
+    };
+    vec = vec * (_max - _min) + _min;
+    return vec;
+}
+
 static void write_vat(vector<vector<vec3f>> &v, const std::string &path) {
     std::ofstream file(path, std::ios::out | std::ios::binary);
+    vector<vec3f> temp_bboxs;
+    for (const auto& i: v) {
+        auto bbox = parallel_reduce_minmax(i.begin(), i.end());
+        temp_bboxs.push_back(bbox.first);
+        temp_bboxs.push_back(bbox.second);
+    }
+    auto bbox = parallel_reduce_minmax(temp_bboxs.begin(), temp_bboxs.end());
+    zeno::log_info("{} {}", bbox.first, bbox.second);
+    write_vec3f(file, bbox.first);
+    write_vec3f(file, bbox.second);
+
     int height = v.size();
     file.write((char*)&height, sizeof(int));
 
@@ -35,7 +84,7 @@ static void write_vat(vector<vector<vec3f>> &v, const std::string &path) {
         int width = v[i].size();
         file.write((char*)&width, sizeof(int));
         for (auto j = 0; j < width; j++) {
-            write_vec3f(file, v[i][j]);
+            write_normalized_vec3f(file, v[i][j], bbox.first, bbox.second);
         }
         zeno::log_info("VAT: write frame {} done ({} face vec)!", i, width);
     }
@@ -44,6 +93,8 @@ static void write_vat(vector<vector<vec3f>> &v, const std::string &path) {
 static vector<vector<vec3f>> read_vat(const std::string &path) {
     vector<vector<vec3f>> v;
     std::ifstream file(path, std::ios::in | std::ios::binary);
+    auto _min = read_vec3f(file);
+    auto _max = read_vec3f(file);
 
     int height = 0;
     file.read((char*)&height, sizeof (int));
@@ -53,11 +104,10 @@ static vector<vector<vec3f>> read_vat(const std::string &path) {
     for (auto i = 0; i < height; i++) {
         int width = 0;
         file.read((char*)&width, sizeof (int));
-        zeno::log_info("VAT: read frame {} ({} face vec)!", i, width);
 
         v[i].resize(width);
         for (auto j = 0; j < width; j++) {
-            v[i][j] = read_vec3f(file);
+            v[i][j] = read_normalized_vec3f(file, _min, _max);
         }
         zeno::log_info("VAT: read frame {} done ({} face vec)!", i, width);
     }
