db60052981b78fe8e0d39ed95e008477983c228e
XinXin <xinxin@zenus.com>
2023-03-17 08:35:47 +0800

    ray tracing bug fix

--- zenovis/xinxinoptix/DisneyBSDF.h
+++ zenovis/xinxinoptix/DisneyBSDF.h
@@ -528,8 +528,9 @@ namespace DisneyBSDF{
         float NoV = abs(wo.z);
 
         //float d = BRDFBasics::GTR1(abs(NoH),lerp(0.1f, 0.001f, clearcoatGloss));
-        float d = BRDFBasics::GTR1(abs(NoH),(0.1f + clearcoatGloss * (0.001f-0.1f) ));
-        float f = BRDFBasics::fresnelSchlick(LoH,0.04f);
+        float d = BRDFBasics::GTR1(abs(NoH),mix(0.1f, 0.001f, clearcoatGloss));
+        //previous: float f = BRDFBasics::fresnelSchlick(fLoH, 0.04); wrong
+        float f = BRDFBasics::fresnelSchlick(0.04f, LoH);
         float g = BRDFBasics::SeparableSmithGGXG1(wi,  wm, 0.25f, 0.25f);
 
         fPdf = d / (4.0f * dot(wo,wm));
