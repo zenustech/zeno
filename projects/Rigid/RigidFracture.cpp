ee778560b5b342a5e195f84cc991de87c29cf066
littlemine <wxlwxl1993@zju.edu.cn>
2023-04-06 23:32:11 +0800

    WIP: build compounds by manual links

--- projects/Rigid/RigidFracture.cpp
+++ projects/Rigid/RigidFracture.cpp
@@ -1,3 +1,25 @@
+#include <memory>
+#include <vector>
+
+// zeno basics
+#include <zeno/DictObject.h>
+#include <zeno/ListObject.h>
+#include <zeno/NumericObject.h>
+#include <zeno/PrimitiveObject.h>
+#include <zeno/logger.h>
+#include <zeno/utils/UserData.h>
+#include <zeno/utils/fileio.h>
+#include <zeno/zeno.h>
+
+// zpc
+#include "zensim/container/Bht.hpp"
+#include "zensim/math/matrix/SparseMatrix.hpp"
+#include "zensim/math/matrix/SparseMatrixOperations.hpp"
+#if 1
+#include "zensim/graph/ConnectedComponents.hpp"
+#endif
+#include "zensim/omp/execution/ExecutionPolicy.hpp"
+#include "zensim/zpc_tpls/fmt/color.h"
 
 #include "RigidTest.h"
 
@@ -7,4 +29,71 @@
 #include <hacdICHull.h>
 #include <hacdVector.h>
 
-namespace zeno {}
+namespace zeno {
+
+struct BulletGlueRigidBodies : zeno::INode {
+    virtual void apply() override {
+        using namespace zs;
+        constexpr auto space = execspace_e::openmp;
+        auto rbs = get_input<ListObject>("rbList")->get<BulletObject>();
+        const auto nrbs = rbs.size();
+
+        auto compounds = std::make_shared<ListObject>();
+
+        float mass = 0.f;
+        for (auto rb : rbs) {
+            float m = rb->body->getMass();
+            mass += m;
+            // fmt::print("rb[{}] mass: {} (accum: {})\n", (std::uintptr_t)rb->body.get(), m, mass);
+        }
+
+        auto glueList = get_input<ListObject>("glueListVec2i")->getLiterial<vec2i>();
+        auto ncons = glueList.size();
+
+        auto pol = omp_exec();
+        std::vector<int> is(ncons), js(ncons);
+        pol(zip(is, js, glueList), [](auto &i, auto &j, const auto &ij) {
+            i = ij[0];
+            j = ij[1];
+        });
+
+        SparseMatrix<int, true> spmat{(int)nrbs, (int)nrbs};
+        spmat.build(pol, (int)nrbs, (int)nrbs, range(is), range(js), true_c);
+
+        std::vector<int> fas(nrbs);
+        union_find(pol, spmat, range(fas));
+
+        bht<int, 1, int> tab{spmat.get_allocator(), nrbs};
+        tab.reset(pol, true);
+        pol(range(nrbs), [&fas, tab = proxy<space>(tab)](int vi) mutable {
+            auto fa = fas[vi];
+            while (fa != fas[fa])
+                fa = fas[fa];
+            fas[vi] = fa;
+            tab.insert(fa);
+        });
+
+        auto ncompounds = tab.size();
+        fmt::print("{} rigid bodies, {} compounds.\n", nrbs, ncompounds);
+
+        //auto comShape = static_cast<btCompoundShape *>(shape.get());
+        //comShape->addChildShape(trans, child->shape.get());
+        //children.push_back(std::move(child));
+
+        // set_output("object", std::make_shared<PrimitiveObject>());
+    }
+};
+
+ZENDEFNODE(BulletGlueRigidBodies, {
+                                      {
+                                          "rbList",
+                                          "glueListVec2i",
+                                      },
+                                      {
+                                          // "object",
+                                      },
+                                      {},
+                                      {"Bullet"},
+                                  });
+
+} // namespace zeno
