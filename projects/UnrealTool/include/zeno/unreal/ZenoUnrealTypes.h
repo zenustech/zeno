14c9ae66f0a7db15696ddfe5906659bbc8aeaeb2
DarcJC <me@darc.pro>
2023-04-22 03:13:53 +0800

    feat: remove extra definition from ZenoUnrealTypes.h

--- projects/UnrealTool/include/zeno/unreal/ZenoUnrealTypes.h
+++ projects/UnrealTool/include/zeno/unreal/ZenoUnrealTypes.h
@@ -8,7 +8,6 @@
 #include <rapidjson/document.h>
 #include <rapidjson/stringbuffer.h>
 #include <vector>
-#include <cassert>
 
 namespace zeno::unreal {
 
@@ -82,85 +81,6 @@ struct NodeParamInput {
     template <class T>
     void pack(T& pack) {
         pack(data);
-    }
-};
-
-struct Diff {
-    std::set<std::string> data;
-
-    template <class T>
-    void pack(T& pack) {
-        pack(data);
-    }
-};
-
-enum class ESubjectType : int16_t {
-    Invalid = -1,
-    Mesh = 0,
-    HeightField,
-    Num,
-};
-
-struct SubjectContainer {
-    std::string Name;
-    int16_t Type;
-    std::vector<uint8_t> Data;
-
-    ESubjectType GetType() const {
-        return static_cast<ESubjectType>(Type);
-    }
-
-    template <class T>
-    void pack(T& pack) {
-        pack(Name, Type, Data);
-    }
-};
-
-struct SubjectContainerList {
-    std::vector<SubjectContainer> Data;
-
-    template <class T>
-    void pack(T& pack) {
-        pack(Data);
-    }
-};
-
-struct HeightField {
-    int32_t Nx = 0, Ny = 0;
-    std::vector<std::vector<uint16_t>> Data;
-
-    HeightField() = default;
-
-    HeightField(int32_t InNx, int32_t InNy, const std::vector<uint16_t>& InData)
-        : Nx(InNx)
-        , Ny(InNy)
-    {
-        assert(Nx * Ny == InData.size());
-        Data.resize(Ny);
-        for (std::vector<uint16_t>& Vy : Data) { Vy.resize(Nx); }
-        for (size_t Y = 0; Y < Ny; ++Y) {
-            for (size_t X = 0; X < Nx; ++X) {
-                const size_t Idx = Y * Ny + X;
-                Data[Y][X] = InData[Idx];
-            }
-        }
-    }
-
-    std::vector<uint16_t> ToFlat() const {
-        std::vector<uint16_t> Result;
-        Result.reserve(Nx * Ny);
-        for (const std::vector<uint16_t>& Ry : Data) {
-            for (uint16_t Rx : Ry) {
-                Result.push_back(Rx);
-            }
-        }
-
-        return Result;
-    }
-
-    template <class T>
-    void pack(T& pack) {
-        pack(Nx, Ny, Data);
     }
 };
 
