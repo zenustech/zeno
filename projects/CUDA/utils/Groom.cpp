069b6c563e4947f8739a59bba7ba076182979239
littlemine <wxlwxl1993@zju.edu.cn>
2023-05-08 02:36:49 +0800

    WIP: vdb collision resolve

--- projects/CUDA/utils/Groom.cpp
+++ projects/CUDA/utils/Groom.cpp
@@ -8,15 +8,21 @@
 #include "zensim/graph/ConnectedComponents.hpp"
 #include "zensim/math/matrix/SparseMatrixOperations.hpp"
 #include "zensim/omp/execution/ExecutionPolicy.hpp"
+#include "zensim/zpc_tpls/fmt/format.h"
 #include <cassert>
 #include <cstdlib>
 #include <cstring>
+#include <openvdb/tree/LeafManager.h>
+#include <openvdb/openvdb.h>
+#include <openvdb/tools/GridOperators.h>
+#include <openvdb/tools/VolumeToSpheres.h>
 #include <zeno/ListObject.h>
 #include <zeno/funcs/PrimitiveUtils.h>
 #include <zeno/types/PrimitiveObject.h>
 #include <zeno/types/UserData.h>
 #include <zeno/utils/log.h>
 #include <zeno/zeno.h>
+#include <zeno/VDBGrid.h>
 
 namespace zeno {
 
@@ -108,10 +114,9 @@ ZENDEFNODE(GenerateHairs, {
                               {
                                   {"PrimitiveObject", "guide_lines"},
                                   {"float", "thickness", "0.1"},
-                                  {"float", "radius", "0.3"},
                                   {"string", "denAttr", ""},
                                   {"float", "density", "100"},
-                                  {"float", "minSeperateRadius", "0"},
+                                  {"float", "minRadius", "0"},
                                   {"bool", "interpAttrs", "1"},
                                   {"int", "relax_iterations", "3"},
                               },
@@ -123,3 +128,55 @@ ZENDEFNODE(GenerateHairs, {
                           });
 
 } // namespace zeno
+
+struct RepelPoints : zeno::INode {
+    virtual void apply() override {
+        auto points = get_input<zeno::PrimitiveObject>("points");
+        auto collider = get_input2<zeno::VDBFloatGrid>("vdb_collider");
+        auto sep_dist = get_input2<float>("sep_dist");
+        auto maxIters = get_input2<int>("max_iter");
+        auto grid = collider->m_grid;
+        grid->tree().voxelizeActiveTiles();
+        auto dx = collider->getVoxelSize()[0];
+        openvdb::Vec3fGrid::Ptr gridGrad = openvdb::tools::gradient(*grid);
+
+        using namespace zs;
+        auto pol = omp_exec();
+        constexpr auto space = execspace_e::openmp;
+        auto &pos = points->attr<zeno::vec3f>("pos");
+        pol(range(pos.size()), [&grid, &gridGrad, &pos, dx, sep_dist](int vi) {
+            // auto p = vec_to_other<openvdb::Vec3R>(pos[vi]);
+            auto p = zeno::vec_to_other<openvdb::Vec3R>(pos[vi]);
+            for (auto sdf = openvdb::tools::BoxSampler::sample(grid->getConstUnsafeAccessor(), grid->worldToIndex(p));
+                 sdf < sep_dist && maxIters-- > 0;
+                 sdf = openvdb::tools::BoxSampler::sample(grid->getConstUnsafeAccessor(), grid->worldToIndex(p))) {
+
+                auto ddd =
+                    openvdb::tools::BoxSampler::sample(gridGrad->getConstUnsafeAccessor(), gridGrad->worldToIndex(p));
+
+                // fmt::print("pt[{}] current at <{}, {}, {}>, sdf: {}, direction <{}, {}, {}>\n", vi, p[0], p[1], p[2], sdf, ddd[0], ddd[1], ddd[2]);
+
+                // p += ddd.normalize() * dx;
+                // p += ddd.normalize() * -sdf;
+                p += ddd;
+            }
+            pos[vi] = zeno::other_to_vec<3>(p);
+        });
+        set_output("prim", points);
+    }
+};
+
+ZENDEFNODE(RepelPoints, {
+                            {
+                                {"PrimitiveObject", "points"},
+                                {"vdb_collider"},
+                                {"float", "sep_dist", "0"},
+                                {"int", "max_iter", "100"},
+                            },
+                            {
+                                {"PrimitiveObject", "prim"},
+                            },
+                            {},
+                            {"zs_hair"},
+                        });
+
