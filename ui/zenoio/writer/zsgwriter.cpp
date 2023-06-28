57b1e2df58778512237ab02f5581a10cbd9aa35a
zhuohy <1445643474@qq.com>
2023-06-28 09:46:34 +0800

    fix bug about writing log

--- ui/zenoio/writer/zsgwriter.cpp
+++ ui/zenoio/writer/zsgwriter.cpp
@@ -208,8 +208,7 @@ void ZsgWriter::dumpSocket(SOCKET_INFO socket, bool bInput, RAPIDJSON_WRITER& wr
         bool bOK = AddVariant(deflVal, sockType, writer);
         if (!bOK)
         {
-            zeno::log_error(QString("write default-value error. nodeId : %1, socket : %2").arg(socket.nodeid, socket.name));
-            ZASSERT_EXIT(bOK);
+            zeno::log_error("write default-value error. nodeId : {}, socket : {}", socket.nodeid.toStdString(), socket.name.toStdString());
         }
 
         writer.Key("control");
