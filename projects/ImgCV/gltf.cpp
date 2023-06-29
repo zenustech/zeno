41b08237f26657230c4dd02ab1d409a521684335
Endlessdaydream <endlessdaydream@qq.com>
2023-06-29 18:45:58 +0800

    gltf verts normal tris

--- projects/ImgCV/gltf.cpp
+++ projects/ImgCV/gltf.cpp
@@ -7,34 +7,19 @@
 #include <zeno/StringObject.h>
 #include <cstring>
 #include <zeno/types/PrimitiveObject.h>
-#include <zeno/types/DictObject.h>
 #include <zeno/types/StringObject.h>
-#include <zeno/types/PrimitiveTools.h>
-#include <zeno/utils/string.h>
 #include <zeno/utils/logger.h>
 #include <zeno/utils/vec.h>
-#include <cstdlib>
 #include <cassert>
-#include <cstdio>
-#include <iostream>
 #include <fstream>
 #include <rapidjson\document.h>
-//#include <json/json.h>
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/document.h"
-#include <zeno/types/PrimitiveObject.h>
-#include <zeno/types/NumericObject.h>
 #include <string>
 #include <vector>
 
 namespace zeno {
-
-struct Vertex1 {
-    float position[3];
-    float normal[3];
-    float texCoord[2];
-};
 
 struct ReadGLTF : zeno::INode {
     virtual void apply() override {
@@ -43,7 +28,7 @@ struct ReadGLTF : zeno::INode {
         auto &pos = prim->verts;
         std::ifstream gltfFile(path.c_str());
         if (!gltfFile.is_open()) {
-            zeno::log_info( "Failed to open .gltf file:{}" , path.c_str());
+            zeno::log_info("Failed to open .gltf file:{}", path.c_str());
         }
         std::string gltfContent((std::istreambuf_iterator<char>(gltfFile)),
                                 std::istreambuf_iterator<char>());
@@ -52,42 +37,107 @@ struct ReadGLTF : zeno::INode {
         root.Parse(gltfContent.c_str());
 
         if (root.HasParseError()) {
-            zeno::log_info("Failed to parse .gltf file: {}",path.c_str());
+            zeno::log_info("Failed to parse .gltf file: {}", path.c_str());
         }
-        const rapidjson::Value& accessors = root["accessors"];
-        const rapidjson::Value& bufferViews = root["bufferViews"];
-        const rapidjson::Value& buffers = root["buffers"];
+//        int PrimitiveIdx = 0;
+        int PrimitiveCount = root["meshes"].Size();
+        zeno::log_info("PrimitiveCount:{}",PrimitiveCount);
+        for(size_t PrimitiveIdx = 0; PrimitiveIdx < PrimitiveCount; PrimitiveIdx++){
+// verts
+            const rapidjson::Value &primitives = root["meshes"][PrimitiveIdx]["primitives"];
+            const rapidjson::Value &attributes = primitives[PrimitiveIdx]["attributes"];
+            int BufferIdxOfPosition = attributes["POSITION"].GetInt();
+            const rapidjson::Value &accessor = root["accessors"][BufferIdxOfPosition];
+            const rapidjson::Value &bufferViews = root["bufferViews"][BufferIdxOfPosition];
+            int VertByteOffset = bufferViews["byteOffset"].GetInt();
+            int VertByteLength = bufferViews["byteLength"].GetInt();
+            int VertBufferIndex = bufferViews["buffer"].GetInt();
+            int VertexCount = accessor["count"].GetInt();
+    /////////////////////
 
-        const int verticesAccessorIndex = 0;
-        const int verticesBufferViewIndex = accessors[verticesAccessorIndex]["bufferView"].GetInt();
-        const int verticesBufferIndex = bufferViews[verticesBufferViewIndex]["buffer"].GetInt();
-        const int byteOffset = bufferViews[verticesBufferViewIndex]["byteOffset"].GetInt();
-        const int vertexCount = accessors[verticesAccessorIndex]["count"].GetInt();
-        const int bufferLength = buffers[verticesBufferIndex]["byteLength"].GetInt();
-        const std::string& uri = buffers[verticesBufferIndex]["uri"].GetString();
+            const rapidjson::Value &buffers = root["buffers"][VertBufferIndex];
+            std::string uri = buffers["uri"].GetString();
 
-        size_t lastBackslashPos = path.find_last_of("/\\");
-        if (lastBackslashPos != std::string::npos) {
-            path.erase(lastBackslashPos + 1);
-        }
-//        std::string bpath = path.substr(0, path.length() - 5);
-//        std::string str = ".bin";
-        path.append(uri);
-        std::ifstream bufferFile(path.c_str(), std::ios::binary);
-        if (!bufferFile.is_open()) {
-            zeno::log_info("Failed to open buffer file");
-        }
-        bufferFile.seekg(byteOffset, std::ios::beg);
-        std::vector<Vertex1> vertices(vertexCount);
-        bufferFile.read(reinterpret_cast<char*>(vertices.data()), vertexCount * sizeof(Vertex1));
-        bufferFile.close();
-        prim->verts.resize(vertexCount);
-        auto &nrm = prim->verts.add_attr<vec3f>("nrm");
-        for(size_t i = 0;i < vertexCount;i++){
-            zeno::vec3f v = {vertices[i].position[0],vertices[i].position[1],vertices[i].position[2]};
-            pos[i] = v;
-            zeno::vec3f n = {vertices[i].normal[0],vertices[i].normal[1],vertices[i].normal[2]};
-            nrm[i] = n;
+            size_t lastBackslashPos = path.find_last_of("/\\");
+            if (lastBackslashPos != std::string::npos) {
+                path.erase(lastBackslashPos + 1);
+            }
+            auto vpath = path;
+            vpath.append(uri);
+            std::ifstream bufferFile(vpath.c_str(), std::ios::binary);
+            if (!bufferFile.is_open()) {
+                zeno::log_info("Failed to open buffer file");
+            }
+            bufferFile.seekg(VertByteOffset, std::ios::beg);
+            std::vector<vec3f> vertices1(VertexCount);
+            bufferFile.read(reinterpret_cast<char *>(vertices1.data()), VertByteLength);
+
+            prim->verts.resize(VertexCount);
+            prim->uvs.resize(VertexCount);
+            auto &uvs = prim->uvs;
+            auto &nrm = prim->verts.add_attr<vec3f>("nrm");
+            for (size_t i = 0; i < VertexCount; i++) {
+                zeno::vec3f v = {static_cast<float>(vertices1[i][0]),
+                                 static_cast<float>(vertices1[i][1]),
+                                 static_cast<float>(vertices1[i][2])};
+                pos[i] = v;
+            }
+    //normal
+            int BufferIdxOfNormal = attributes["NORMAL"].GetInt();
+            const rapidjson::Value &NormalAccessor = root["accessors"][BufferIdxOfNormal];
+            const rapidjson::Value &NormalBufferViews = root["bufferViews"][BufferIdxOfNormal];
+            int NormalByteOffset = NormalBufferViews["byteOffset"].GetInt();
+            int NormalByteLength = NormalBufferViews["byteLength"].GetInt();
+            int NormalBufferIndex = NormalBufferViews["buffer"].GetInt();
+            int NormalCount = NormalAccessor["count"].GetInt();
+    /////////////////////
+            const rapidjson::Value &NormalBuffers = root["buffers"][NormalBufferIndex];
+            bufferFile.seekg(NormalByteOffset, std::ios::beg);
+            std::vector<vec3f> normalo(NormalCount);
+            bufferFile.read(reinterpret_cast<char *>(normalo.data()), NormalByteLength);
+            auto &n = prim->verts.add_attr<vec3f>("nrm");
+            for (size_t i = 0; i < NormalCount; i++) {
+                zeno::vec3f no = {static_cast<float>(normalo[i][0]),
+                                  static_cast<float>(normalo[i][1]),
+                                  static_cast<float>(normalo[i][2])};
+                n[i] = no;
+            }
+    //tris
+            int BufferIdxOfIndices = primitives[PrimitiveIdx]["indices"].GetInt();
+            const rapidjson::Value &IndexBufferViews = root["bufferViews"][BufferIdxOfIndices];
+            int IndexByteOffset = IndexBufferViews["byteOffset"].GetInt();
+            int IndexByteLength = IndexBufferViews["byteLength"].GetInt();
+            int IndexBufferIndex = IndexBufferViews["buffer"].GetInt();
+            const rapidjson::Value &indexBuffers = root["buffers"][IndexBufferIndex];
+    ////////////////////////
+            int indexSizeInBytes = 2;
+            int indexCount = IndexByteLength / indexSizeInBytes / 3;
+            bufferFile.seekg(IndexByteOffset, std::ios::beg);
+            std::vector<unsigned short> indiceso;
+//            indiceso.resize(indexCount);
+//            bufferFile.read(reinterpret_cast<char *>(indiceso.data()), IndexByteLength);
+//            bufferFile.close();
+//            auto &tri = prim->tris;
+//            prim->tris.resize(indexCount+1);
+//            for (size_t i = 0; i < indexCount * 3; i += 3) {
+//                zeno::vec3i tr = {indiceso[i], indiceso[i + 1], indiceso[i + 2]};
+//                tri[(int)(i/3)] = tr;
+//            }
+            if (bufferFile) {
+                bufferFile.seekg(0, std::ios::end);
+                std::streampos fileSize = bufferFile.tellg();
+                bufferFile.seekg(0, std::ios::beg);
+                indiceso.resize(fileSize / sizeof(unsigned short));
+                bufferFile.read(reinterpret_cast<char*>(indiceso.data()), fileSize);
+            }
+            auto &tri = prim->tris;
+            prim->tris.resize(indexCount);
+            for(size_t i = 0;i < indexCount*3;i+=3){
+                zeno::vec3i tr = {indiceso[i],indiceso[i+1],indiceso[i+2]};
+                tri[(int)(i/3)] = tr;
+            }
+            bufferFile.close();
+
         }
         set_output("prim", std::move(prim));
     }
@@ -103,4 +153,6 @@ ZENDEFNODE(ReadGLTF, {
     {},
     { "primitive" },
 });
+
+
 }
