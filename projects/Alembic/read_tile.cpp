fa8b346e8ff981896263051254e79c5864941db3
zhouhang95 <765229842@qq.com>
2023-07-04 16:31:52 +0800

    temp

--- projects/Alembic/read_tile.cpp
+++ projects/Alembic/read_tile.cpp
@@ -16,40 +16,6 @@
 #include "draco/compression/decode.h"
 
 namespace zeno {
-struct ReadTile : INode {
-    virtual void apply() override {
-        auto path = get_input2<std::string>("path");
-        auto json = zeno::file_get_content(path);
-        rapidjson::Document doc;
-        doc.Parse(json.c_str());
-        const auto& root = doc["root"];
-        const auto& children = root["children"];
-
-        zeno::log_info("count {}", children.Size());
-
-        for (auto i = 0; i < children.Size(); i++) {
-            const auto &c = children[i];
-            std::string uri = c["content"]["uri"].GetString();
-            const auto &box = c["boundingVolume"]["box"];
-            vec3f center = {
-                    box[0].GetFloat(),
-                    box[1].GetFloat(),
-                    box[2].GetFloat(),
-            };
-            zeno::log_info("{}: {} {}", i, uri, center);
-        }
-    }
-};
-
-ZENDEFNODE(ReadTile, {
-    {
-        {"readpath", "path"},
-    },
-    {},
-    {},
-    {"alembic"},
-});
-
 namespace zeno_gltf {
     enum class ComponentType {
         GL_BYTE = 0x1400,
@@ -81,164 +47,166 @@ namespace zeno_gltf {
     };
 namespace fs = std::filesystem;
 
-struct LoadGLTFModel : INode {
-    virtual void apply() override {
-        auto path = get_input2<std::string>("path");
-        rapidjson::Document doc;
-        std::vector<std::vector<char>> buffers;
-        if (zeno::ends_with(path, ".glb", false)) {
-            auto data = zeno::file_get_binary(path);
-            auto reader = BinaryReader(data);
-            reader.seek_from_begin(8);
-            auto total_len = reader.read_LE<int>();
-            auto json_len = reader.read_LE<int>();
+static std::shared_ptr<PrimitiveObject> read_gltf_model(std::string path) {
+    rapidjson::Document doc;
+    std::vector<std::vector<char>> buffers;
+    if (zeno::ends_with(path, ".glb", false)) {
+        auto data = zeno::file_get_binary(path);
+        auto reader = BinaryReader(data);
+        reader.seek_from_begin(8);
+        auto total_len = reader.read_LE<int>();
+        auto json_len = reader.read_LE<int>();
+        reader.skip(4);
+        std::string json;
+        for (auto i = 0; i < json_len; i++) {
+            json.push_back(reader.read_LE<char>());
+        }
+        zeno::file_put_content(path + ".json", json);
+        doc.Parse(json.c_str());
+        while (!reader.is_eof()) {
+            auto len = reader.read_LE<int>();
             reader.skip(4);
-            std::string json;
-            for (auto i = 0; i < json_len; i++) {
-                json.push_back(reader.read_LE<char>());
+            std::vector<char> buffer;
+            for (auto i = 0; i < len; i++) {
+                buffer.push_back(reader.read_LE<char>());
             }
-            zeno::file_put_content("fuck3.json", json);
-            doc.Parse(json.c_str());
-            while (!reader.is_eof()) {
-                auto len = reader.read_LE<int>();
-                reader.skip(4);
-                std::vector<char> buffer;
-                for (auto i = 0; i < len; i++) {
-                    buffer.push_back(reader.read_LE<char>());
-                }
-                buffers.push_back(buffer);
+            buffers.push_back(buffer);
+        }
+    }
+    else {
+        auto json = zeno::file_get_content(path);
+        doc.Parse(json.c_str());
+
+        zeno::log_info("buffers {}", doc["buffers"].Size());
+        for (auto i = 0; i < doc["buffers"].Size(); i++) {
+            fs::path p = path;
+            auto parent = p.parent_path().string();
+            std::string bin_path = parent + '/' + doc["buffers"][i]["uri"].GetString();
+            auto buffer = zeno::file_get_binary(bin_path);
+            zeno::log_info("{}", bin_path);
+            buffers.push_back(buffer);
+        }
+    }
+    std::vector<Accessor> accessors;
+    {
+        for (auto i = 0; i < doc["accessors"].Size(); i++) {
+            const auto & a = doc["accessors"][i];
+            Accessor accessor;
+            accessor.bufferView = std::nullopt;
+            if (a.HasMember("bufferView")) {
+                accessor.bufferView = a["bufferView"].GetInt();
+            }
+            accessor.count = a["count"].GetInt();
+            accessor.componentType = ComponentType(a["componentType"].GetInt());
+            std::string str_type = a["type"].GetString();
+            if (str_type == "SCALAR") {
+                accessor.type = Type::SCALAR;
+            }
+            else if (str_type == "VEC2") {
+                accessor.type = Type::VEC2;
+            }
+            else if (str_type == "VEC3") {
+                accessor.type = Type::VEC3;
+            }
+            else if (str_type == "VEC4") {
+                accessor.type = Type::VEC4;
+            }
+            accessors.push_back(accessor);
+        }
+    }
+
+    std::vector<BufferView> bufferViews;
+    {
+        for (auto i = 0; i < doc["bufferViews"].Size(); i++) {
+            const auto & v = doc["bufferViews"][i];
+            BufferView bufferView;
+            bufferView.buffer = v["buffer"].GetInt();
+            bufferView.byteOffset = v.HasMember("byteOffset")? v["byteOffset"].GetInt() : 0;
+            bufferView.byteStride = v.HasMember("byteStride")? v["byteStride"].GetInt() : 0;
+            bufferView.byteLength = v["byteLength"].GetInt();
+            bufferViews.push_back(bufferView);
+        }
+    }
+
+    auto prim = std::make_shared<zeno::PrimitiveObject>();
+    {
+        const auto &mesh = doc["meshes"][0];
+        const auto &primitive = mesh["primitives"][0];
+        if (primitive.HasMember("extensions") && primitive["extensions"].HasMember("KHR_draco_mesh_compression")) {
+            const auto &draco_info = primitive["extensions"]["KHR_draco_mesh_compression"];
+            auto bufferView = draco_info["bufferView"].GetInt();
+            const auto &bv = bufferViews[bufferView];
+            draco::Decoder dracoDecoder;
+            draco::DecoderBuffer dracoDecoderBuffer;
+            auto pData = &buffers[bv.buffer][bv.byteOffset];
+            dracoDecoderBuffer.Init(reinterpret_cast<char *>(pData), bv.byteLength);
+            auto type_statusor = draco::Decoder::GetEncodedGeometryType(&dracoDecoderBuffer);
+            const draco::EncodedGeometryType geom_type = type_statusor.value();
+            if (geom_type != draco::TRIANGULAR_MESH) {
+                zeno::log_error("not draco::TRIANGULAR_MESH");
+                throw std::runtime_error("not draco::TRIANGULAR_MESH");
+            }
+
+            draco::StatusOr<std::unique_ptr<draco::Mesh>> decoderStatus = dracoDecoder.DecodeMeshFromBuffer(&dracoDecoderBuffer);
+            auto mesh = std::move(decoderStatus).value();
+            auto vertexCount = mesh->num_points();
+            prim->resize(vertexCount);
+            auto faceCount = mesh->num_faces();
+            prim->tris.resize(faceCount);
+
+            // from https://github.com/google/draco/blob/master/src/draco/io/obj_encoder.cc
+            const draco::PointAttribute *const att = mesh->GetNamedAttribute(draco::GeometryAttribute::POSITION);
+            for (draco::AttributeValueIndex i(0); i < static_cast<uint32_t>(att->size()); ++i) {
+                att->ConvertValue<float, 3>(i, prim->verts[i.value()].data());
+            }
+            for (draco::FaceIndex i(0); i < faceCount; ++i) {
+                int _0 = att->mapped_index(mesh->face(i)[0]).value();
+                int _1 = att->mapped_index(mesh->face(i)[1]).value();
+                int _2 = att->mapped_index(mesh->face(i)[2]).value();
+                prim->tris[i.value()] = {_0, _1, _2};
             }
         }
         else {
-            auto json = zeno::file_get_content(path);
-            doc.Parse(json.c_str());
-
-            zeno::log_info("buffers {}", doc["buffers"].Size());
-            for (auto i = 0; i < doc["buffers"].Size(); i++) {
-                fs::path p = path;
-                auto parent = p.parent_path().string();
-                std::string bin_path = parent + '/' + doc["buffers"][i]["uri"].GetString();
-                auto buffer = zeno::file_get_binary(bin_path);
-                zeno::log_info("{}", bin_path);
-                buffers.push_back(buffer);
-            }
-        }
-        std::vector<Accessor> accessors;
-        {
-            for (auto i = 0; i < doc["accessors"].Size(); i++) {
-                const auto & a = doc["accessors"][i];
-                Accessor accessor;
-                accessor.bufferView = std::nullopt;
-                if (a.HasMember("bufferView")) {
-                    accessor.bufferView = a["bufferView"].GetInt();
-                }
-                accessor.count = a["count"].GetInt();
-                accessor.componentType = ComponentType(a["componentType"].GetInt());
-                std::string str_type = a["type"].GetString();
-                if (str_type == "SCALAR") {
-                    accessor.type = Type::SCALAR;
-                }
-                else if (str_type == "VEC2") {
-                    accessor.type = Type::VEC2;
-                }
-                else if (str_type == "VEC3") {
-                    accessor.type = Type::VEC3;
-                }
-                else if (str_type == "VEC4") {
-                    accessor.type = Type::VEC4;
-                }
-                accessors.push_back(accessor);
-            }
-        }
-
-        std::vector<BufferView> bufferViews;
-        {
-            for (auto i = 0; i < doc["bufferViews"].Size(); i++) {
-                const auto & v = doc["bufferViews"][i];
-                BufferView bufferView;
-                bufferView.buffer = v["buffer"].GetInt();
-                bufferView.byteOffset = v.HasMember("byteOffset")? v["byteOffset"].GetInt() : 0;
-                bufferView.byteStride = v.HasMember("byteStride")? v["byteStride"].GetInt() : 0;
-                bufferView.byteLength = v["byteLength"].GetInt();
-                bufferViews.push_back(bufferView);
-            }
-        }
-
-        auto prim = std::make_shared<zeno::PrimitiveObject>();
-        {
-            const auto &mesh = doc["meshes"][0];
-            const auto &primitive = mesh["primitives"][0];
-            if (primitive.HasMember("extensions") && primitive["extensions"].HasMember("KHR_draco_mesh_compression")) {
-                const auto &draco_info = primitive["extensions"]["KHR_draco_mesh_compression"];
-                auto bufferView = draco_info["bufferView"].GetInt();
-                zeno::log_info("KHR_draco_mesh_compression bufferView: {}", bufferView);
-                const auto &bv = bufferViews[bufferView];
-                draco::Decoder dracoDecoder;
-                draco::DecoderBuffer dracoDecoderBuffer;
-                auto pData = &buffers[bv.buffer][bv.byteOffset];
-                dracoDecoderBuffer.Init(reinterpret_cast<char *>(pData), bv.byteLength);
-                auto type_statusor = draco::Decoder::GetEncodedGeometryType(&dracoDecoderBuffer);
-                const draco::EncodedGeometryType geom_type = type_statusor.value();
-                if (geom_type != draco::TRIANGULAR_MESH) {
-                    zeno::log_error("not draco::TRIANGULAR_MESH");
-                    throw std::runtime_error("not draco::TRIANGULAR_MESH");
-                }
-
-                draco::StatusOr<std::unique_ptr<draco::Mesh>> decoderStatus = dracoDecoder.DecodeMeshFromBuffer(&dracoDecoderBuffer);
-                auto mesh = std::move(decoderStatus).value();
-                auto vertexCount = mesh->num_points();
-                prim->resize(vertexCount);
-                auto faceCount = mesh->num_faces();
-                prim->tris.resize(faceCount);
-
-                zeno::log_info("vertexCount: {}, faceCount: {}", vertexCount, faceCount);
-
-                // from https://github.com/google/draco/blob/master/src/draco/io/obj_encoder.cc
-                const draco::PointAttribute *const att = mesh->GetNamedAttribute(draco::GeometryAttribute::POSITION);
-                for (draco::AttributeValueIndex i(0); i < static_cast<uint32_t>(att->size()); ++i) {
-                    att->ConvertValue<float, 3>(i, prim->verts[i.value()].data());
-                }
-                for (draco::FaceIndex i(0); i < faceCount; ++i) {
-                    int _0 = att->mapped_index(mesh->face(i)[0]).value();
-                    int _1 = att->mapped_index(mesh->face(i)[1]).value();
-                    int _2 = att->mapped_index(mesh->face(i)[2]).value();
-                    prim->tris[i.value()] = {_0, _1, _2};
+            {
+                const auto &position = primitive["attributes"]["POSITION"].GetInt();
+                const auto &acc = accessors[position];
+                const auto &bv = bufferViews[acc.bufferView.value()];
+                auto reader = BinaryReader(buffers[bv.buffer]);
+                reader.seek_from_begin(bv.byteOffset);
+                prim->resize(acc.count);
+                for (auto i = 0; i < acc.count; i++) {
+                    prim->verts[i] = reader.read_LE<vec3f>();
                 }
             }
-            else {
-                {
-                    const auto &position = primitive["attributes"]["POSITION"].GetInt();
-                    const auto &acc = accessors[position];
-                    const auto &bv = bufferViews[acc.bufferView.value()];
-                    auto reader = BinaryReader(buffers[bv.buffer]);
-                    reader.seek_from_begin(bv.byteOffset);
-                    prim->resize(acc.count);
-                    for (auto i = 0; i < acc.count; i++) {
-                        prim->verts[i] = reader.read_LE<vec3f>();
+            {
+                auto index = primitive["indices"].GetInt();
+                const auto &acc = accessors[index];
+                const auto &bv = bufferViews[acc.bufferView.value()];
+                auto reader = BinaryReader(buffers[bv.buffer]);
+                reader.seek_from_begin(bv.byteOffset);
+                auto count = acc.count / 3;
+                prim->tris.resize(count);
+                for (auto i = 0; i < count; i++) {
+                    if (acc.componentType == ComponentType::GL_UNSIGNED_SHORT) {
+                        auto f0 = reader.read_LE<uint16_t>();
+                        auto f1 = reader.read_LE<uint16_t>();
+                        auto f2 = reader.read_LE<uint16_t>();
+                        prim->tris[i] = {f0, f1, f2};
                     }
-                }
-                {
-                    auto index = primitive["indices"].GetInt();
-                    const auto &acc = accessors[index];
-                    const auto &bv = bufferViews[acc.bufferView.value()];
-                    auto reader = BinaryReader(buffers[bv.buffer]);
-                    reader.seek_from_begin(bv.byteOffset);
-                    auto count = acc.count / 3;
-                    prim->tris.resize(count);
-                    for (auto i = 0; i < count; i++) {
-                        if (acc.componentType == ComponentType::GL_UNSIGNED_SHORT) {
-                            auto f0 = reader.read_LE<uint16_t>();
-                            auto f1 = reader.read_LE<uint16_t>();
-                            auto f2 = reader.read_LE<uint16_t>();
-                            prim->tris[i] = {f0, f1, f2};
-                        }
-                        else {
-                            zeno::log_info("not support componentType: {}", int(acc.componentType));
-                        }
+                    else {
+                        zeno::log_info("not support componentType: {}", int(acc.componentType));
                     }
                 }
             }
         }
+    }
+    return prim;
+}
+
+struct LoadGLTFModel : INode {
+    virtual void apply() override {
+        auto path = get_input2<std::string>("path");
+        auto prim = read_gltf_model(path);
 
         set_output("prim", std::move(prim));
     }
@@ -250,6 +218,56 @@ ZENDEFNODE(LoadGLTFModel, {
     },
     {
         "prim"
+    },
+    {},
+    {"alembic"},
+});
+
+struct ReadTile : INode {
+    virtual void apply() override {
+        auto path = get_input2<std::string>("path");
+
+        fs::path p = path;
+        auto parent = p.parent_path().string();
+        auto json = zeno::file_get_content(path);
+        rapidjson::Document doc;
+        doc.Parse(json.c_str());
+        const auto& root = doc["root"];
+        const auto& children = root["children"];
+
+        zeno::log_info("count {}", children.Size());
+        auto list = std::make_shared<ListObject>();
+
+        for (auto i = 0; i < children.Size(); i++) {
+//        for (auto i = 0; i < 10; i++) {
+            const auto &c = children[i];
+            std::string uri = c["content"]["uri"].GetString();
+            uri = parent + "/" + uri.substr(0, uri.size() - 4) + "glb";
+            const auto &box = c["boundingVolume"]["box"];
+            vec3f ct = {
+                    box[0].GetFloat(),
+                    box[1].GetFloat(),
+                    box[2].GetFloat(),
+            };
+            auto prim = read_gltf_model(uri);
+            for (auto i = 0; i < prim->verts.size(); i++) {
+                prim->verts[i] += vec3f(ct[0], ct[2], -ct[1]);
+            }
+            list->arr.push_back(prim);
+        }
+        auto pPrims = list->getRaw<PrimitiveObject>();
+        auto output = primMerge(pPrims);
+        set_output("prim", std::move(list));
+    }
+};
+
+ZENDEFNODE(ReadTile, {
+    {
+        {"readpath", "path"},
+        {"frame"},
+    },
+    {
+        "prim",
     },
     {},
     {"alembic"},
