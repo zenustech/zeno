8e457801338f39f2f566e45b1887400faa38fad3
littlemine <wxlwxl1993@zju.edu.cn>
2023-07-11 13:47:38 +0800

    iterate environment binary path

--- projects/CUDA/pyzfx/pyzfx.cpp
+++ projects/CUDA/pyzfx/pyzfx.cpp
@@ -6,6 +6,7 @@
 
 #include "zensim/zpc_tpls/fmt/color.h"
 #include "zensim/zpc_tpls/fmt/format.h"
+#include "zensim/ZpcBuiltin.hpp"
 #include <cstdlib>
 #include <zeno/utils/log.h>
 
@@ -15,7 +16,27 @@ struct PyZpcLite : INode {
     void apply() override {
         char *p;
         p = getenv("PATH");
-        fmt::print("path: {}\n", p);
+        std::set<std::string> pathLocations;
+        auto processTags = [](std::string tags, std::set<std::string> &res, std::string sep) {
+            using Ti = RM_CVREF_T(std::string::npos);
+            Ti st = tags.find_first_not_of(sep, 0);
+            for (auto ed = tags.find_first_of(sep, st + 1); ed != std::string::npos; ed = tags.find_first_of(sep, st + 1)) {
+                res.insert(tags.substr(st, ed - st));
+                st = tags.find_first_not_of(sep, ed);
+                if (st == std::string::npos)
+                    break;
+            }
+            if (st != std::string::npos && st < tags.size()) {
+                res.insert(tags.substr(st));
+            }
+        };
+#ifdef ZS_PLATFORM_WINDOWS
+        processTags(p, pathLocations, ";");
+#else
+        processTags(p, pathLocations, ":");
+#endif
+        for (const auto &path : pathLocations)
+            fmt::print("iterate path: {}\n", path);
     }
 };
 
