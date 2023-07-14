3bb24da8ce33b84ab88f0a7af9985bdd2072b1a8
littlemine <wxlwxl1993@zju.edu.cn>
2023-07-15 02:59:10 +0800

    initial pyfunc test

--- projects/PyZpc/pyzfx.cpp
+++ projects/PyZpc/pyzfx.cpp
@@ -17,19 +17,61 @@ namespace zeno {
 struct PyZfx : INode {
     void apply() override {
         Py_Initialize();
+        //
+        // ref: https://www.cnblogs.com/panliu/p/4485183.html
+        //
+        PyRun_SimpleString("import sys");
+        PyRun_SimpleString(fmt::format("sys.path.append(\'{}\')", zs::abs_exe_directory() + "/" + "resource/").data());
         // PyRun_SimpleString("print(\'Hello World\')");
 
         //
         // ref: https://docs.python.org/3/extending/embedding.html#pure-embedding
         //
-        PyObject *pName, *pModule, *pFunc;
-#if RESOURCE_AT_RELATIVE_PATH
-        auto scriptPath = zs::abs_exe_directory() + "/" + "resource/HelloWorld.py";
-#else
-        auto scriptPath = std::string{AssetDirPath} + "/" + "Scripts/HelloWorld.py";
-#endif
-        pName = PyUnicode_DecodeFSDefault(scriptPath.c_str());
+        PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
+        pName = PyUnicode_DecodeFSDefault("HelloWorld");
         pModule = PyImport_Import(pName);
+        Py_DECREF(pName);
+
+	fmt::print("done import\n");
+
+        long args[2] = {33, 2};
+        if (pModule != NULL) {
+            pFunc = PyObject_GetAttrString(pModule, "multiply");
+            /* pFunc is a new reference */
+
+            if (pFunc && PyCallable_Check(pFunc)) {
+                pArgs = PyTuple_New(2);
+                for (int i = 0; i < 2; ++i) {
+                    pValue = PyLong_FromLong(args[i]);
+                    if (!pValue) {
+                        Py_DECREF(pArgs);
+                        Py_DECREF(pModule);
+                        fprintf(stderr, "Cannot convert argument\n");
+                        exit(1);
+                    }
+                    /* pValue reference stolen here: */
+                    PyTuple_SetItem(pArgs, i, pValue);
+                }
+                pValue = PyObject_CallObject(pFunc, pArgs);
+                Py_DECREF(pArgs);
+                if (pValue != NULL) {
+                    printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                    Py_DECREF(pValue);
+                } else {
+                    Py_DECREF(pFunc);
+                    Py_DECREF(pModule);
+                    PyErr_Print();
+                    fprintf(stderr, "Call failed\n");
+                    exit(1);
+                }
+            } else {
+                if (PyErr_Occurred())
+                    PyErr_Print();
+                fprintf(stderr, "Cannot find function \"%s\"\n", "multiply");
+            }
+            Py_XDECREF(pFunc);
+            Py_DECREF(pModule);
+        }
 
         Py_Finalize();
         fmt::print(fg(fmt::color::blue), "done pyzfx node test.\n");
