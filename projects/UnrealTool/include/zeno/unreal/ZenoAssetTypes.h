764985faf694202b576901a78c73755c6e88d66d
DarcJC <me@darc.pro>
2023-07-19 19:19:01 +0800

    feat: add types

--- projects/UnrealTool/include/zeno/unreal/ZenoAssetTypes.h
+++ projects/UnrealTool/include/zeno/unreal/ZenoAssetTypes.h
@@ -5,14 +5,30 @@
 #include <string>
 #include <variant>
 #include <vector>
+#include <type_traits>
+#include <tuple>
 
 namespace zeno::unreal {
 
     template<size_t T>
+#if 1
     using TVectorFloat = std::array<float, T>;
+#else
+    struct TVectorFloat {
+        float Data[T];
+
+        float& operator[](size_t Index) {
+            return Data[Index];
+        }
+
+        NOP_STRUCTURE(TVectorFloat, Data);
+    };
+#endif
 
     using Vector2f = TVectorFloat<2>;
     using Vector3f = TVectorFloat<3>;
+
+    using ByteArray = std::vector<uint8_t>;
 
     using Any =
         std::variant<
@@ -22,13 +38,31 @@ namespace zeno::unreal {
             std::make_signed<size_t>::type,
             Vector2f,
             Vector3f,
-            struct LandscapeData
+            ByteArray,
+            struct LandscapeData,
+            struct AssetBundle
         >
     ;
 
-    struct LandscapeData {
+    ////////////////////////////////////////////////////////////
+    /// Component Types
+    ////////////////////////////////////////////////////////////
+
+    /**
+     * Contains the transform of an entity
+     */
+    struct TransformComponent {
+        Vector3f Position;
+        Vector3f Rotation;
         Vector3f Scale;
+    };
+
+    ////////////////////////////////////////////////////////////
+    /// Asset Data Types
+    ////////////////////////////////////////////////////////////
+    struct LandscapeData {
         std::vector<uint16_t> HeightField;
+        TransformComponent Transform;
     };
 
     struct AssetBundle {
