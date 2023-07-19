c6194cde24d01f96c2bc381becd682b0d144a6a6
littlemine <wxlwxl1993@zju.edu.cn>
2023-07-20 03:25:32 +0800

    gltf to obj

--- projects/CUDA/utils/Conversion.cpp
+++ projects/CUDA/utils/Conversion.cpp
@@ -1,14 +1,101 @@
 #include "Structures.hpp"
 #include "zensim/io/MeshIO.hpp"
 #include "zensim/omp/execution/ExecutionPolicy.hpp"
-#include <zeno/types/NumericObject.h>
-#include <zeno/types/PrimitiveObject.h>
 #include <zeno/funcs/PrimitiveUtils.h>
 #include <zeno/para/parallel_for.h>
 #include <zeno/para/parallel_scan.h>
+#include <zeno/types/NumericObject.h>
+#include <zeno/types/PrimitiveObject.h>
 #include <zeno/utils/variantswitch.h>
 
+#include "zensim/resource/Filesystem.hpp"
+#include "zensim/zpc_tpls/fmt/color.h"
+#include "zensim/zpc_tpls/fmt/format.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 namespace zeno {
+
+struct GltfToObj : INode {
+    void apply() override {
+        auto pathStr = get_input2<std::string>("path");
+        char *p = nullptr;
+        if (pathStr.empty())
+            p = getenv("PATH");
+        else
+            p = pathStr.data();
+        std::set<std::string> pathLocations;
+        auto processTags = [](std::string tags, std::set<std::string> &res, std::string sep) {
+            using Ti = RM_CVREF_T(std::string::npos);
+            Ti st = tags.find_first_not_of(sep, 0);
+            for (auto ed = tags.find_first_of(sep, st + 1); ed != std::string::npos;
+                 ed = tags.find_first_of(sep, st + 1)) {
+                res.insert(tags.substr(st, ed - st));
+                st = tags.find_first_not_of(sep, ed);
+                if (st == std::string::npos)
+                    break;
+            }
+            if (st != std::string::npos && st < tags.size()) {
+                res.insert(tags.substr(st));
+            }
+        };
+#ifdef ZS_PLATFORM_WINDOWS
+        processTags(p, pathLocations, ";");
+        const std::string target = "blender.exe";
+#else
+        processTags(p, pathLocations, ":");
+        const std::string target = "blender";
+#endif
+
+        auto inputFile = get_input2<std::string>("input_model_file");
+        fs::path inputPath = inputFile, outputPath;
+        if (auto str = get_input2<std::string>("output_model_file"); str.empty())
+            outputPath = inputPath.parent_path().string() + "/" + inputPath.stem().string() + ".obj";
+        else
+            outputPath = str;
+
+        for (const auto &path : pathLocations) {
+            fmt::print("iterate path: {}\n", path);
+            fs::path loc = path + "/" + target;
+            bool ifExist = false;
+            try {
+                ifExist = fs::exists(loc);
+            } catch (const std::exception &e) {
+                fmt::print("\tskipping path {} due to exception (e.g. inaccessibility).\n", path);
+                continue;
+            }
+            if (ifExist) {
+                /// write script to a file
+                auto scriptPath = zs::abs_exe_directory() + "/" + "resource/GltfToObj.py";
+                std::ofstream os;
+                os.open(scriptPath);
+                std::string script = "import bpy\n";
+                script += fmt::format("bpy.ops.import_scene.gltf(filepath=\"{}\")\n", inputFile);
+                script +=
+                    fmt::format("bpy.ops.export_scene.obj(filepath=\"{}\", use_selection=True)\n", outputPath.string());
+                os << script;
+                os.close();
+
+                /// execute this blender script
+                auto cmdStr = fmt::format("{} -b -P {}", loc.string(), scriptPath);
+                fmt::print("\tfound {} at \"{}\". executing \"{}\"\n", target, path, cmdStr);
+                std::system(cmdStr.c_str());
+            }
+        }
+    }
+};
+ZENDEFNODE(GltfToObj,
+           {/* inputs: */ {
+                {"string", "input_model_file", ""}, {"string", "path", ""}, {"string", "output_model_file", ""}},
+            /* outputs: */
+            {},
+            /* params: */
+            {},
+            /* category: */
+            {
+                "blender",
+            }});
 
 // treat poly as lines (strands)
 void primLineify(PrimitiveObject *prim, bool with_uv) {
@@ -78,7 +165,6 @@ ZENDEFNODE(PrimitiveLineify, {/* inputs: */
                                   "primitive",
                               }});
 
-
 struct PointsToZSParticles : INode {
     void apply() override {
         using namespace zs;
@@ -105,31 +191,31 @@ struct PointsToZSParticles : INode {
         outParticles->sprayedOffset = obj.size();
 
         // attributes
-        std::vector<zs::PropertyTag> tags{{"x", 3},   {"v", 3}};
+        std::vector<zs::PropertyTag> tags{{"x", 3}, {"v", 3}};
 
         // prim attrib tags
         std::vector<zs::PropertyTag> auxVertAttribs{};
         for (auto &&[key, arr] : inParticles->verts.attrs) {
-                const auto checkDuplication = [&tags](const std::string &name) {
-                    for (std::size_t i = 0; i != tags.size(); ++i)
-                        if (tags[i].name == name.data())
-                            return true;
-                    return false;
-                };
-                if (checkDuplication(key) || key == "pos" || key == "vel")
-                    continue;
-                const auto &k{key};
-                match(
-                    [&k, &auxVertAttribs](const std::vector<vec3f> &vals) {
-                        auxVertAttribs.push_back(PropertyTag{k, 3});
-                    },
-                    [&k, &auxVertAttribs](const std::vector<float> &vals) {
-                        auxVertAttribs.push_back(PropertyTag{k, 1});
-                    },
-                    [&k, &auxVertAttribs](const std::vector<vec3i> &vals) {},
-                    [&k, &auxVertAttribs](const std::vector<int> &vals) {},
-                    [](...) { throw std::runtime_error("what the heck is this type of attribute!"); })(arr);
-            }
+            const auto checkDuplication = [&tags](const std::string &name) {
+                for (std::size_t i = 0; i != tags.size(); ++i)
+                    if (tags[i].name == name.data())
+                        return true;
+                return false;
+            };
+            if (checkDuplication(key) || key == "pos" || key == "vel")
+                continue;
+            const auto &k{key};
+            match(
+                [&k, &auxVertAttribs](const std::vector<vec3f> &vals) {
+                    auxVertAttribs.push_back(PropertyTag{k, 3});
+                },
+                [&k, &auxVertAttribs](const std::vector<float> &vals) {
+                    auxVertAttribs.push_back(PropertyTag{k, 1});
+                },
+                [&k, &auxVertAttribs](const std::vector<vec3i> &vals) {},
+                [&k, &auxVertAttribs](const std::vector<int> &vals) {},
+                [](...) { throw std::runtime_error("what the heck is this type of attribute!"); })(arr);
+        }
         tags.insert(std::end(tags), std::begin(auxVertAttribs), std::end(auxVertAttribs));
 
         fmt::print("[PointsToZSParticles] to be converted vert properties:\n");
@@ -139,26 +225,25 @@ struct PointsToZSParticles : INode {
         outParticles->particles = std::make_shared<typename ZenoParticles::particles_t>(tags, size, memsrc_e::host);
         auto &pars = outParticles->getParticles(); // tilevector
         {
-            ompExec(zs::range(size),
-                    [pars = proxy<execspace_e::openmp>({}, pars), &obj,
-                     &inParticles, &auxVertAttribs](size_t pi) mutable {
-                        // pos
-                        pars.tuple(dim_c<3>, "x", pi) = obj[pi];
+            ompExec(zs::range(size), [pars = proxy<execspace_e::openmp>({}, pars), &obj, &inParticles,
+                                      &auxVertAttribs](size_t pi) mutable {
+                // pos
+                pars.tuple(dim_c<3>, "x", pi) = obj[pi];
 
-                        // vel
-                        if (inParticles->has_attr("vel"))
-                            pars.tuple(dim_c<3>, "v", pi) = inParticles->attr<vec3f>("vel")[pi];
-                        else
-                            pars.tuple(dim_c<3>, "v", pi) = zs::vec<float, 3>::zeros();
+                // vel
+                if (inParticles->has_attr("vel"))
+                    pars.tuple(dim_c<3>, "v", pi) = inParticles->attr<vec3f>("vel")[pi];
+                else
+                    pars.tuple(dim_c<3>, "v", pi) = zs::vec<float, 3>::zeros();
 
-                        // additional attributes
-                        for (auto &prop : auxVertAttribs) {
-                            if (prop.numChannels == 3)
-                                pars.tuple(dim_c<3>, prop.name, pi) = inParticles->attr<vec3f>(std::string{prop.name})[pi];
-                            else
-                                pars(prop.name, pi) = inParticles->attr<float>(std::string{prop.name})[pi];
-                        }
-                    });
+                // additional attributes
+                for (auto &prop : auxVertAttribs) {
+                    if (prop.numChannels == 3)
+                        pars.tuple(dim_c<3>, prop.name, pi) = inParticles->attr<vec3f>(std::string{prop.name})[pi];
+                    else
+                        pars(prop.name, pi) = inParticles->attr<float>(std::string{prop.name})[pi];
+                }
+            });
 
             pars = pars.clone({memsrc_e::device, 0});
         }
@@ -168,11 +253,11 @@ struct PointsToZSParticles : INode {
 };
 
 ZENDEFNODE(PointsToZSParticles, {
-                              {"prim", {"bool", "add_customed_attr", "1"}},
-                              {"ZSParticles"},
-                              {},
-                              {"conversion"},
-                          });
+                                    {"prim", {"bool", "add_customed_attr", "1"}},
+                                    {"ZSParticles"},
+                                    {},
+                                    {"conversion"},
+                                });
 
 struct PointsToZSParticles2 : INode {
     void apply() override {
@@ -219,7 +304,7 @@ struct PointsToZSParticles2 : INode {
         outParticles->sprayedOffset = obj.size();
 
         // attributes
-        std::vector<zs::PropertyTag> tags{{"x", 3},   {"v", 3}};
+        std::vector<zs::PropertyTag> tags{{"x", 3}, {"v", 3}};
 
         // prim attrib tags
         std::vector<zs::PropertyTag> auxVertAttribs{};
@@ -257,26 +342,25 @@ struct PointsToZSParticles2 : INode {
         outParticles->particles = std::make_shared<typename ZenoParticles::particles_t>(tags, size, memsrc_e::host);
         auto &pars = outParticles->getParticles(); // tilevector
         {
-            ompExec(zs::range(size),
-                    [pars = proxy<execspace_e::openmp>({}, pars), &obj,
-                     &inParticles, &auxVertAttribs](size_t pi) mutable {
-                        // pos
-                        pars.tuple(dim_c<3>, "x", pi) = obj[pi];
+            ompExec(zs::range(size), [pars = proxy<execspace_e::openmp>({}, pars), &obj, &inParticles,
+                                      &auxVertAttribs](size_t pi) mutable {
+                // pos
+                pars.tuple(dim_c<3>, "x", pi) = obj[pi];
 
-                        // vel
-                        if (inParticles->has_attr("vel"))
-                            pars.tuple(dim_c<3>, "v", pi) = inParticles->attr<vec3f>("vel")[pi];
-                        else
-                            pars.tuple(dim_c<3>, "v", pi) = zs::vec<float, 3>::zeros();
+                // vel
+                if (inParticles->has_attr("vel"))
+                    pars.tuple(dim_c<3>, "v", pi) = inParticles->attr<vec3f>("vel")[pi];
+                else
+                    pars.tuple(dim_c<3>, "v", pi) = zs::vec<float, 3>::zeros();
 
-                        // additional attributes
-                        for (auto &prop : auxVertAttribs) {
-                            if (prop.numChannels == 3)
-                                pars.tuple(dim_c<3>, prop.name, pi) = inParticles->attr<vec3f>(std::string{prop.name})[pi];
-                            else
-                                pars(prop.name, pi) = inParticles->attr<float>(std::string{prop.name})[pi];
-                        }
-                    });
+                // additional attributes
+                for (auto &prop : auxVertAttribs) {
+                    if (prop.numChannels == 3)
+                        pars.tuple(dim_c<3>, prop.name, pi) = inParticles->attr<vec3f>(std::string{prop.name})[pi];
+                    else
+                        pars(prop.name, pi) = inParticles->attr<float>(std::string{prop.name})[pi];
+                }
+            });
 
             pars = pars.clone({memsrc_e::device, 0});
         }
@@ -286,12 +370,14 @@ struct PointsToZSParticles2 : INode {
 };
 
 ZENDEFNODE(PointsToZSParticles2, {
-                              {"prim", {"bool", "add_customed_attr", "1"}, 
-                                {"string", "selected_custom_attr_tags", ""},
-                              },
-                              {"ZSParticles"},
-                              {},
-                              {"conversion"},
-                          });
+                                     {
+                                         "prim",
+                                         {"bool", "add_customed_attr", "1"},
+                                         {"string", "selected_custom_attr_tags", ""},
+                                     },
+                                     {"ZSParticles"},
+                                     {},
+                                     {"conversion"},
+                                 });
 
-}
+} // namespace zeno
