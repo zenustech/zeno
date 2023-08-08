ad762505cf27a3370c8ac152c128a1e285589ea0
DarcJC <me@darc.pro>
2023-08-08 16:31:53 +0800

    feat: add example

--- projects/Roads/nodes/src/cost.cpp
+++ projects/Roads/nodes/src/cost.cpp
@@ -1,10 +1,48 @@
-
 #include "zeno/zeno.h"
+#include "zeno/utils/logger.h"
+#include "zeno/PrimitiveObject.h"
+#include "zeno/types/UserData.h"
+#include "zeno/utils/PropertyVisitor.h"
+#include "roads/roads.h"
+
+template <typename ...Args>
+inline void RoadsAssert(const bool Expr, const std::string& InMsg = "[Roads] Assert Failed", Args... args) {
+    if (!Expr) {
+        zeno::log_error(InMsg, args...);
+    }
+}
+
+template <typename GridType = roads::Point>
+roads::DynamicGrid<GridType> BuildGridFromPrimitive(zeno::PrimitiveObject* InPrimitive) {
+    RoadsAssert(nullptr != InPrimitive, "[Roads] InPrimitive shouldn't be nullptr !");
+
+    const auto Nx = InPrimitive->userData().get2<int>("nx");
+    const auto Ny = InPrimitive->userData().get2<int>("ny");
+
+    RoadsAssert(Nx * Ny <= InPrimitive->verts.size());
+
+    roads::DynamicGrid<GridType> Grid(Nx, Ny);
+    for (size_t i = 0; i < Nx * Ny; ++i) {
+        Grid[i] = InPrimitive->verts[i];
+    }
+}
 
 namespace {
     using namespace zeno;
 
-    struct CalcPathCost_Simple : public INode {
+    struct CalcPathCostParameter : public zeno::reflect::NodeParameterBase {
+        explicit CalcPathCostParameter(INode* Node) : zeno::reflect::NodeParameterBase(Node) {
+            RunInputHooks();
+        }
+
+        std::shared_ptr<zeno::PrimitiveObject> Primitive;
+        zeno::reflect::InputField<CalcPathCostParameter, decltype(Primitive)> PrimitiveField {*this, Primitive, "prim", false};
+
+        std::string OutputChannel;
+        zeno::reflect::InputField<CalcPathCostParameter, decltype(OutputChannel)> OutputChannelField { *this, OutputChannel, "output_channel" };
+    };
+
+    struct CalcPathCost_Simple : public zeno::reflect::IAutoNode<CalcPathCostParameter> {
         void apply() override;
     };
 
@@ -24,5 +62,6 @@ namespace {
     )
 
     void CalcPathCost_Simple::apply() {
+        zeno::log_info("aaaaaaaaaaa: {}", AutoParameter->OutputChannel);
     }
 }
--- projects/Roads/utilities/include/roads/data.h
+++ projects/Roads/utilities/include/roads/data.h
@@ -5,8 +5,10 @@
 #include <cassert>
 #include <cmath>
 #include <vector>
+#include <functional>
 
 namespace roads {
+
     enum class TriangleType {
         ACUTE = 0,
         RIGHT = 1,
@@ -33,5 +35,16 @@ namespace roads {
 
     template <size_t X, size_t Y, typename GridPointType = Point>
     struct Grid : public std::array<GridPointType, X * Y> {};
+
+    template <typename GridPointType = Point>
+    struct DynamicGrid : public std::vector<GridPointType> {
+        size_t Nx, Ny;
+
+        DynamicGrid(const size_t InNx, const size_t InNy) : std::vector<GridPointType>(InNx * InNy), Nx(InNx), Ny(InNy) {}
+        DynamicGrid(DynamicGrid&& OtherGridToMove)  noexcept : std::vector<GridPointType>(std::forward(OtherGridToMove)) {
+            Nx = OtherGridToMove.Nx;
+            Ny = OtherGridToMove.Ny;
+        }
+    };
 
 }// namespace roads
