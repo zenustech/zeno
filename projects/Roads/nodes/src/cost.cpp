0121bdb9f0097768af2dab4f20301777b54c7e95
DarcJC <me@darc.pro>
2023-08-16 18:22:20 +0800

    feat: add simple flow path

--- projects/Roads/nodes/src/cost.cpp
+++ projects/Roads/nodes/src/cost.cpp
@@ -1,12 +1,15 @@
+#include "boost/graph/dijkstra_shortest_paths.hpp"
 #include "roads/roads.h"
-#include <limits>
 #include "zeno/PrimitiveObject.h"
 #include "zeno/types/UserData.h"
 #include "zeno/utils/PropertyVisitor.h"
 #include "zeno/utils/logger.h"
 #include "zeno/zeno.h"
-#include "boost/graph/dijkstra_shortest_paths.hpp"
 #include <iostream>
+#include <limits>
+#include <numeric>
+#include <queue>
+#include <stack>
 
 template<typename... Args>
 inline void RoadsAssert(const bool Expr, const std::string &InMsg = "[Roads] Assert Failed", Args... args) {
@@ -33,7 +36,7 @@ namespace {
     using namespace zeno;
     using namespace roads;
 
-    struct PrimCalcSlope : public zeno::reflect::IParameterAutoNode<PrimCalcSlope> {
+    struct ZENO_CRTP(PrimCalcSlope, zeno::reflect::IParameterAutoNode) {
         ZENO_GENERATE_NODE_BODY(PrimCalcSlope);
 
         std::shared_ptr<zeno::PrimitiveObject> Primitive;
@@ -154,6 +157,104 @@ namespace {
             for (size_t i = 0; i < path.size() - 1; ++i) {
                 AutoParameter->Primitive->lines[i] = zeno::vec2i(path[i], path[i+1]);
             }
+        }
+    };
+
+    struct ZENO_CRTP(HeightFieldFlowPath_Simple, zeno::reflect::IParameterAutoNode) {
+        ZENO_GENERATE_NODE_BODY(HeightFieldFlowPath_Simple);
+
+        std::shared_ptr<zeno::PrimitiveObject> Primitive;
+        ZENO_DECLARE_INPUT_FIELD(Primitive, "Prim");
+        ZENO_DECLARE_OUTPUT_FIELD(Primitive, "Prim");
+
+        bool bShouldSmooth;
+        ZENO_DECLARE_INPUT_FIELD(bShouldSmooth, "Enable Smooth", false, "", "false");
+
+        float DeltaAltitudeThreshold;
+        ZENO_DECLARE_INPUT_FIELD(DeltaAltitudeThreshold, "Delta Altitude Threshold", false, "", "1e-06");
+
+        float HeuristicRatio;
+        ZENO_DECLARE_INPUT_FIELD(HeuristicRatio, "Heuristic Ratio (0 - 1)", false, "", "0.3");
+
+        std::string SizeXChannel;
+        ZENO_DECLARE_INPUT_FIELD(SizeXChannel, "Nx Channel (UserData)", false, "", "nx");
+
+        std::string SizeYChannel;
+        ZENO_DECLARE_INPUT_FIELD(SizeYChannel, "Ny Channel (UserData)", false, "", "ny");
+
+        int Nx = 0;
+        ZENO_BINDING_PRIMITIVE_USERDATA(Primitive, Nx, SizeXChannel, false);
+
+        int Ny = 0;
+        ZENO_BINDING_PRIMITIVE_USERDATA(Primitive, Ny, SizeYChannel, false);
+
+        std::string RiverChannel;
+        ZENO_DECLARE_INPUT_FIELD(RiverChannel, "Output River Channel (Vertex Attr)", false, "", "is_river");
+
+        std::string LakeChannel;
+        ZENO_DECLARE_INPUT_FIELD(LakeChannel, "Output Lake Channel (Vertex Attr)", false, "", "is_lake");
+
+        std::string HeightChannel;
+        ZENO_DECLARE_INPUT_FIELD(HeightChannel, "Height Channel (Vertex Attr)", false, "", "height");
+
+        std::string WaterChannel;
+        ZENO_DECLARE_INPUT_FIELD(WaterChannel, "Water Channel (Vertex Attr)", false, "", "water");
+
+        zeno::AttrVector<float> Heightmap{};
+        ZENO_BINDING_PRIMITIVE_ATTRIBUTE(Primitive, Heightmap, HeightChannel, zeno::reflect::EZenoPrimitiveAttr::VERT);
+
+        zeno::AttrVector<float> WaterMask{};
+        ZENO_BINDING_PRIMITIVE_ATTRIBUTE(Primitive, WaterMask, WaterChannel, zeno::reflect::EZenoPrimitiveAttr::VERT);
+
+        void apply() override {
+            auto& Prim = AutoParameter->Primitive;
+            auto& HeightField = AutoParameter->Heightmap;
+            auto& Water = AutoParameter->WaterMask;
+            const auto SizeX = AutoParameter->Nx;
+            const auto SizeY = AutoParameter->Ny;
+            const size_t NumVert = SizeX * SizeY;
+
+            auto& River = Prim->add_attr<float>(AutoParameter->RiverChannel);
+
+            static const std::array<IntPoint2D, 8> SDirection {
+                IntPoint2D { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 },
+                { -1, -1 }, { 1, -1 }, { -1, 1 }, { 1, 1 }
+            };
+
+            std::stack<size_t> Stack;
+
+            const float MaxHeight = *std::max_element(std::begin(HeightField), std::end(HeightField));
+            const float MinHeight = *std::min_element(std::begin(HeightField), std::end(HeightField));
+            const float RiverHeightMax = (MaxHeight - MinHeight) * AutoParameter->HeuristicRatio;
+
+            std::set<size_t> Visited;
+            for (size_t i = 0; i < NumVert; ++i) {
+                if (Water[i] < 1e-7) {
+                    continue;
+                }
+
+                Stack.push(i);
+                while (!Stack.empty()) {
+                    long idx = long(Stack.top());
+                    Visited.insert(idx);
+                    Stack.pop();
+                    if (HeightField[idx] > RiverHeightMax) continue;
+                    River[idx] = 1;
+                    long y = idx / SizeX;
+                    long x = idx % SizeX;
+                    for (const IntPoint2D& Dir : SDirection) {
+                        long ix = x + Dir[0];
+                        long iy = y + Dir[1];
+                        if (ix > 0 && iy > 0 && ix < SizeX && iy < SizeX) {
+                            size_t nidx = iy * SizeX + ix;
+                            if (Visited.find(nidx) == std::end(Visited) && std::abs(HeightField[nidx] - HeightField[idx]) < AutoParameter->DeltaAltitudeThreshold) {
+                                Stack.push(nidx);
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     };
 }// namespace
--- projects/Roads/utilities/include/roads/grid.h
+++ projects/Roads/utilities/include/roads/grid.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "pch.h"
+#include <numeric>
 
 //#include "boost/graph/use_mpi.hpp"
 #include "boost/graph/graph_traits.hpp"
@@ -32,6 +33,31 @@ namespace roads {
     ROADS_API WeightedGridUndirectedGraph CreateWeightGraphFromCostGrid(const DynamicGrid<CostPoint >& InCostGrid, const ConnectiveType Type);
 
     ROADS_API ArrayList<ArrayList<double>> FloydWarshallShortestPath(WeightedGridUndirectedGraph &InGraph);
+
+    template <typename T>
+    ROADS_API void AverageSmooth(ArrayList<T>& InOutContainer, size_t Iteration = 1) {
+        auto num_vertices = InOutContainer.size();
+        ArrayList<float> NewHeights(num_vertices);
+
+        for (unsigned int iteration = 0; iteration < 4; ++iteration) {
+#pragma omp parallel for
+            for (size_t i = 0; i < num_vertices; ++i) {
+                // Include the vertex itself and its neighbors.
+                ArrayList<T*> Points = InOutContainer[i].Neighbors;
+                Points.push_back(&InOutContainer[i]);
+
+                // Calculate the average height.
+                NewHeights[i] = std::accumulate(begin(Points), end(Points), 0.0f,
+                                                [](float sum, const T* v) { return sum + v->Height; }) / Points.size();
+            }
+
+            // Update the heights for the next iteration or the final result.
+#pragma omp parallel for
+            for (size_t i = 0; i < num_vertices; ++i) {
+                InOutContainer[i].Height = NewHeights[i];
+            }
+        }
+    }
 
     namespace energy {
         ROADS_API double CalculateStepSize(const Point2D &Pt, const Point2D &P, const Point2D &PrevX, const Point2D &CurrX);
