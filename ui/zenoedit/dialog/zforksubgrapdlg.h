0616ff3203aaa0b7180ff5a296e6a449208069d1
zhuohy <1445643474@qq.com>
2023-12-13 19:17:16 +0800

    match matreial json

--- ui/zenoedit/dialog/zforksubgrapdlg.h
+++ ui/zenoedit/dialog/zforksubgrapdlg.h
@@ -4,6 +4,7 @@
 #include <QtWidgets>
 #include "zenoui/comctrl/dialog/zframelessdialog.h"
 #include <rapidjson/document.h>
+#include "zmathpresetsubgrapdlg.h"
 
 class ZForkSubgraphDlg : public ZFramelessDialog
 {
@@ -18,11 +19,11 @@ private:
     void initUi();
     QMap<QString, QMap<QString, QVariant>> readFile();
 private:
-    QString m_version;
     QTableWidget* m_pTableWidget;
     QMap<QString, QString> m_subgsMap; // <mtlid, preset mat>
     QString m_importPath;
     QPushButton* m_pImportBtn;
+    QMap<QString, STMatchMatInfo> m_matchInfo;
 };
 
 #endif
