6e533da188a10c255e51bd03935329a6d29b20e8
seeeagull <yuzhujiang0324@gmail.com>
2024-01-04 17:43:35 +0800

    input&output with prim

--- projects/Geometry/stitchmesh.cpp
+++ projects/Geometry/stitchmesh.cpp
@@ -14,34 +14,18 @@ namespace zeno {
 
 struct StitchMeshing : INode {
     virtual void apply() override {
+        // TODO(@seeegull):
+        // 1. change quad mesh extraction part to instant meshes
+        // 2. render arrow texture in zeno
+
         auto prim = get_input<PrimitiveObject>("prim");
-        auto input_dir = get_input2<std::string>("input_dir");
-        input_dir.erase(input_dir.find_last_not_of(" ")+1);
-        auto output_dir = get_input2<std::string>("output_dir");
         float scale = get_input2<float>("scale");
+        bool flip = get_input2<bool>("flip_course_wale");
 
         int argc = 1;
         char* argv[10];
         argv[0] = (char*)malloc(sizeof("./stitch-meshing\0"));
         strcpy(argv[0], "./stitch-meshing\0");
-        if (input_dir.size() > 0) {
-            argv[argc] = (char*)malloc(sizeof("-i\0"));
-            strcpy(argv[argc], "-i\0");
-            argv[argc+1] = (char*)malloc((input_dir.size()+1)*sizeof(char));
-            for (int i = 0; i < input_dir.size(); ++i)
-                argv[argc+1][i] = input_dir[i];
-            argv[argc+1][input_dir.size()] = '\0';
-            argc += 2;
-        }
-        if (output_dir.size() > 0) {
-            argv[argc] = (char*)malloc(sizeof("-o\0"));
-            strcpy(argv[argc], "-o\0");
-            argv[argc+1] = (char*)malloc((output_dir.size()+1)*sizeof(char));
-            for (int i = 0; i < output_dir.size(); ++i)
-                argv[argc+1][i] = output_dir[i];
-            argv[argc+1][output_dir.size()] = '\0';
-            argc += 2;
-        }
         if (scale > 0) {
             argv[argc] = (char*)malloc(sizeof("-s\0"));
             strcpy(argv[argc], "-s\0");
@@ -52,18 +36,41 @@ struct StitchMeshing : INode {
             argv[argc+1][scale_str.size()] = '\0';
             argc += 2;
         }
+        if (flip) {
+            argv[argc] = (char*)malloc(sizeof("--flip\0"));
+            strcpy(argv[argc], "--flip\0");
+            argc += 1;
+        }
 
         std::vector<std::vector<int>> faces(prim->tris->size(), std::vector<int>{});
         std::vector<std::vector<float>> verts(prim->verts->size(), std::vector<float>{});
+        auto &pos = prim->verts;
         for (int i = 0; i < prim->tris->size(); ++i)
             for (int j = 0; j < 3; ++j)
                 faces[i].push_back(prim->tris[i][j]);
         for (int i = 0; i < prim->verts->size(); ++i)
             for (int j = 0; j < 3; ++j)
-                verts[i].push_back(prim->verts[i][j]);
+                verts[i].push_back(pos[i][j]);
 
         runStitchMeshing(faces, verts, argc, argv);
 
+        pos.resize(verts.size());
+        for (int i = 0; i < pos.size(); ++i)
+            for (int j = 0; j < 3; ++j)
+                pos[i][j] = verts[i][j];
+        prim->verts.foreach_attr<zeno::AttrAcceptAll>([&] (auto const &key, auto &arr) {
+            arr.resize(verts.size());
+        });
+        prim->lines.clear();
+        prim->tris.clear();
+        prim->loops.clear();
+        prim->polys.clear();
+        for (int i = 0; i < faces.size(); ++i) {
+            int beg = prim->loops.size();
+            for (auto j : faces[i])
+                prim->loops.push_back(j);
+            prim->polys.emplace_back(beg, faces[i].size());
+        }
         set_output("prim", std::move(prim));
     }
 };
@@ -71,9 +78,8 @@ struct StitchMeshing : INode {
 ZENO_DEFNODE(StitchMeshing)
 ({
     {{"prim"},
-    {"readpath", "input_dir", " "},
-    {"string", "output_dir"},
-    {"float", "scale", "0"},},
+    {"float", "scale", "0"},
+    {"bool", "flip_course_wale", "0"}},
     {("prim")},
     {},
     {"primitive"},
