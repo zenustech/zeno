dd84a53f1bab83f830e47e37c318d4b5f5874b25
littlemine <wxlwxl1993@zju.edu.cn>
2024-01-19 14:24:39 +0800

    fix typo

--- zeno/src/nodes/neo/PrimMerge.cpp
+++ zeno/src/nodes/neo/PrimMerge.cpp
@@ -1,15 +1,16 @@
-#include <zeno/zeno.h>
-#include <zeno/utils/log.h>
-#include <zeno/types/PrimitiveObject.h>
 #include <zeno/funcs/PrimitiveUtils.h>
-#include <zeno/types/ListObject.h>
-#include <zeno/types/StringObject.h>
 #include <zeno/para/parallel_for.h>
+#include <zeno/types/ListObject.h>
+#include <zeno/types/PrimitiveObject.h>
+#include <zeno/types/StringObject.h>
 #include <zeno/types/UserData.h>
+#include <zeno/utils/log.h>
+#include <zeno/zeno.h>
 
 namespace zeno {
 
-ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::PrimitiveObject *> const &primList, std::string const &tagAttr) {
+ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::PrimitiveObject *> const &primList,
+                                                          std::string const &tagAttr) {
     //zeno::log_critical("asdfjhl {}", primList.size());
     //throw;
 
@@ -35,14 +36,13 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
         for (size_t primIdx = 0; primIdx < primList.size(); primIdx++) {
             auto prim = primList[primIdx];
             /// @note promote pure vert prim to point-based prim
-            if (!(prim->points.size() || prim->lines.size() || prim->tris.size() || prim->quads.size() || prim->polys.size())) {
+            if (!(prim->points.size() || prim->lines.size() || prim->tris.size() || prim->quads.size() ||
+                  prim->polys.size())) {
                 auto nverts = prim->verts.size();
                 prim->points.resize(nverts);
-                parallel_for(nverts, [&points = prim->points.values](size_t i) {
-                    points[i] = i;
-                });
+                parallel_for(nverts, [&points = prim->points.values](size_t i) { points[i] = i; });
             }
-            /// 
+            ///
             total += prim->verts.size();
             pointtotal += prim->points.size();
             linetotal += prim->lines.size();
@@ -81,44 +81,44 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
         }
         for (size_t primIdx = 0; primIdx < primList.size(); primIdx++) {
             auto const &prim = primList[primIdx];
-            prim->verts.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->verts.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->verts.add_attr<T>(key);
             });
-            prim->points.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->points.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->points.add_attr<T>(key);
             });
-            prim->lines.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->lines.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->lines.add_attr<T>(key);
             });
-            prim->tris.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->tris.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->tris.add_attr<T>(key);
             });
-            prim->quads.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->quads.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->quads.add_attr<T>(key);
             });
-            prim->loops.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->loops.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->loops.add_attr<T>(key);
             });
-            prim->uvs.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->uvs.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->uvs.add_attr<T>(key);
             });
-            prim->polys.foreach_attr<AttrAcceptAll>([&] (auto const &key, auto const &arr) {
+            prim->polys.foreach_attr<AttrAcceptAll>([&](auto const &key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 outprim->polys.add_attr<T>(key);
             });
         }
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto base = bases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -158,11 +158,11 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto vbase = bases[primIdx];
             auto base = pointbases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -206,11 +206,11 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto vbase = bases[primIdx];
             auto base = linebases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -254,11 +254,11 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto vbase = bases[primIdx];
             auto base = tribases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -302,11 +302,11 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto vbase = bases[primIdx];
             auto base = quadbases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -350,11 +350,11 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto vbase = bases[primIdx];
             auto base = loopbases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -379,19 +379,22 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
                     for (size_t i = 0; i < n; i++) {
                         outarr[base + i] = vbase + arr[i];
                     }
-                } else if (key == "uvs") {
-                    auto &outarr = outprim->loops.attr<int>("uvs");
-                    size_t n = std::min(arr.size(), prim->loops.size());
-                    size_t offset = uvbases[primIdx];
-                    for (size_t i = 0; i < n; i++) {
-                        outarr[base + i] = arr[i] + offset;
+                    return;
+                } else if constexpr (std::is_same_v<T, int>) {
+                    if (key == "uvs") {
+                        auto &outarr = outprim->loops.attr<int>("uvs");
+                        size_t n = std::min(arr.size(), prim->loops.size());
+                        size_t offset = uvbases[primIdx];
+                        for (size_t i = 0; i < n; i++) {
+                            outarr[base + i] = arr[i] + offset;
+                        }
+                        return;
                     }
-                } else {
-                    auto &outarr = outprim->loops.attr<T>(key);
-                    size_t n = std::min(arr.size(), prim->loops.size());
-                    for (size_t i = 0; i < n; i++) {
-                        outarr[base + i] = arr[i];
-                    }
+                }
+                auto &outarr = outprim->loops.attr<T>(key);
+                size_t n = std::min(arr.size(), prim->loops.size());
+                for (size_t i = 0; i < n; i++) {
+                    outarr[base + i] = arr[i];
                 }
 #endif
             };
@@ -405,10 +408,10 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto base = uvbases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
                 if constexpr (std::is_same_v<decltype(key), std::true_type>) {
                     auto &outarr = outprim->uvs.values;
@@ -434,11 +437,11 @@ ZENO_API std::shared_ptr<zeno::PrimitiveObject> primMerge(std::vector<zeno::Prim
             }
         });
 
-        parallel_for(primList.size(), [&] (size_t primIdx) {
+        parallel_for(primList.size(), [&](size_t primIdx) {
             auto prim = primList[primIdx];
             auto lbase = loopbases[primIdx];
             auto base = polybases[primIdx];
-            auto core = [&] (auto key, auto const &arr) {
+            auto core = [&](auto key, auto const &arr) {
                 using T = std::decay_t<decltype(arr[0])>;
 #if 0
                 auto &outarr = [&] () -> auto & {
@@ -495,57 +498,46 @@ struct PrimMerge : INode {
         //initialize
 
         std::vector<std::string> matNameList(0);
-        for(auto &p:primList)
-        {
+        for (auto &p : primList) {
             //if p has material
-            int matNum = p->userData().get2<int>("matNum",0);
-            if(matNum>0)
-            {
+            int matNum = p->userData().get2<int>("matNum", 0);
+            if (matNum > 0) {
                 //for p's tris, quads...
                 //    tris("matid")[i] += matNameList.size();
-                for(int i=0; i<p->tris.size();i++)
-                {
-                    if(p->tris.attr<int>("matid")[i] != -1)
-                    {
+                for (int i = 0; i < p->tris.size(); i++) {
+                    if (p->tris.attr<int>("matid")[i] != -1) {
                         p->tris.attr<int>("matid")[i] += matNameList.size();
                     }
                 }
-                for(int i=0; i<p->quads.size();i++)
-                {
-                    if(p->quads.attr<int>("matid")[i] != -1)
-                    {
+                for (int i = 0; i < p->quads.size(); i++) {
+                    if (p->quads.attr<int>("matid")[i] != -1) {
                         p->quads.attr<int>("matid")[i] += matNameList.size();
                     }
                 }
-                for(int i=0; i<p->polys.size();i++)
-                {
-                    if(p->polys.attr<int>("matid")[i]!=-1)
-                    {
+                for (int i = 0; i < p->polys.size(); i++) {
+                    if (p->polys.attr<int>("matid")[i] != -1) {
                         p->polys.attr<int>("matid")[i] += matNameList.size();
                     }
                 }
                 //for p's materials
                 //    add them to material list
-                for(int i=0;i<matNum;i++)
-                {
+                for (int i = 0; i < matNum; i++) {
                     auto matIdx = "Material_" + to_string(i);
                     auto matName = p->userData().get2<std::string>(matIdx, "Default");
                     matNameList.emplace_back(matName);
                 }
-            }
-            else
-            {
+            } else {
                 //for p's tris, quads...
                 //    tris("matid")[] = -1;
-                if(p->tris.size()>0) {
+                if (p->tris.size() > 0) {
                     p->tris.add_attr<int>("matid");
                     p->tris.attr<int>("matid").assign(p->tris.size(), -1);
                 }
-                if(p->quads.size()>0) {
+                if (p->quads.size() > 0) {
                     p->quads.add_attr<int>("matid");
                     p->quads.attr<int>("matid").assign(p->quads.size(), -1);
                 }
-                if(p->polys.size()>0) {
+                if (p->polys.size() > 0) {
                     p->polys.add_attr<int>("matid");
                     p->polys.attr<int>("matid").assign(p->polys.size(), -1);
                 }
@@ -554,16 +546,14 @@ struct PrimMerge : INode {
 
         auto outprim = primMerge(primList, tagAttr);
 
-        for(auto &p:primList){
+        for (auto &p : primList) {
             outprim->userData().merge(p->userData());
         }
 
-        if(matNameList.size()>0)
-        {
+        if (matNameList.size() > 0) {
             //add matNames to userData
-            int i=0;
-            for(auto name:matNameList)
-            {
+            int i = 0;
+            for (auto name : matNameList) {
                 auto matIdx = "Material_" + to_string(i);
                 outprim->userData().setLiterial(matIdx, name);
                 i++;
@@ -577,17 +567,16 @@ struct PrimMerge : INode {
 };
 
 ZENDEFNODE(PrimMerge, {
-    {
-        {"list", "listPrim"},
-        {"string", "tagAttr", ""},
-    },
-    {
-        {"primitive", "prim"},
-    },
-    {
-    },
-    {"primitive"},
-});
+                          {
+                              {"list", "listPrim"},
+                              {"string", "tagAttr", ""},
+                          },
+                          {
+                              {"primitive", "prim"},
+                          },
+                          {},
+                          {"primitive"},
+                      });
 
-}
-}
+} // namespace
+} // namespace zeno
