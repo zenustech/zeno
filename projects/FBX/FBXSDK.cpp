e0e266dc05e1e3fe8152e9d2dea220702aff3ddb
zhouhang95 <765229842@qq.com>
2024-03-08 12:23:06 +0800

    first new fbx

--- projects/FBX/FBXSDK.cpp
+++ projects/FBX/FBXSDK.cpp
@@ -10,6 +10,9 @@
 
 #ifdef ZENO_FBXSDK
 #include <fbxsdk.h>
+#include "zeno/utils/log.h"
+#include <zeno/types/UserData.h>
+#include "zeno/types/PrimitiveObject.h"
 
 namespace FBX{
     void GetChildNodePathRecursive(FbxNode* node, std::string& path) {
@@ -286,4 +289,327 @@ ZENDEFNODE(FBXSDKVisibility,
             }
            });
 
+namespace zeno {
+/**
+* Return a string-based representation based on the attribute type.
+*/
+FbxString GetAttributeTypeName(FbxNodeAttribute::EType type) {
+    switch(type) {
+        case FbxNodeAttribute::eUnknown: return "unidentified";
+        case FbxNodeAttribute::eNull: return "null";
+        case FbxNodeAttribute::eMarker: return "marker";
+        case FbxNodeAttribute::eSkeleton: return "skeleton";
+        case FbxNodeAttribute::eMesh: return "mesh";
+        case FbxNodeAttribute::eNurbs: return "nurbs";
+        case FbxNodeAttribute::ePatch: return "patch";
+        case FbxNodeAttribute::eCamera: return "camera";
+        case FbxNodeAttribute::eCameraStereo: return "stereo";
+        case FbxNodeAttribute::eCameraSwitcher: return "camera switcher";
+        case FbxNodeAttribute::eLight: return "light";
+        case FbxNodeAttribute::eOpticalReference: return "optical reference";
+        case FbxNodeAttribute::eOpticalMarker: return "marker";
+        case FbxNodeAttribute::eNurbsCurve: return "nurbs curve";
+        case FbxNodeAttribute::eTrimNurbsSurface: return "trim nurbs surface";
+        case FbxNodeAttribute::eBoundary: return "boundary";
+        case FbxNodeAttribute::eNurbsSurface: return "nurbs surface";
+        case FbxNodeAttribute::eShape: return "shape";
+        case FbxNodeAttribute::eLODGroup: return "lodgroup";
+        case FbxNodeAttribute::eSubDiv: return "subdiv";
+        default: return "unknown";
+    }
+}
+
+/**
+* Print an attribute.
+*/
+void PrintAttribute(FbxNodeAttribute* pAttribute) {
+    if(!pAttribute) return;
+
+    FbxString typeName = GetAttributeTypeName(pAttribute->GetAttributeType());
+    FbxString attrName = pAttribute->GetName();
+    // Note: to retrieve the character array of a FbxString, use its Buffer() method.
+    printf("<attribute type='%s' name='%s'/>\n", typeName.Buffer(), attrName.Buffer());
+}
+
+//void PrintNode(FbxNode* pNode) {
+//    const char* nodeName = pNode->GetName();
+//    FbxDouble3 translation = pNode->LclTranslation.Get();
+//    FbxDouble3 rotation = pNode->LclRotation.Get();
+//    FbxDouble3 scaling = pNode->LclScaling.Get();
+//
+//    // Print the contents of the node.
+//    printf("<node name='%s' translation='(%f, %f, %f)' rotation='(%f, %f, %f)' scaling='(%f, %f, %f)'>\n",
+//           nodeName,
+//           translation[0], translation[1], translation[2],
+//           rotation[0], rotation[1], rotation[2],
+//           scaling[0], scaling[1], scaling[2]
+//    );
+//
+//    // Print the node's attributes.
+//    for(int i = 0; i < pNode->GetNodeAttributeCount(); i++)
+//        PrintAttribute(pNode->GetNodeAttributeByIndex(i));
+//
+//    // Recursively print the children.
+//    for(int j = 0; j < pNode->GetChildCount(); j++)
+//        PrintNode(pNode->GetChild(j));
+//
+//    printf("</node>\n");
+//}
+bool GetMesh(FbxNode* pNode, std::shared_ptr<PrimitiveObject> prim) {
+    FbxMesh* pMesh = pNode->GetMesh();
+    if (!pMesh) return false;
+
+    FbxAMatrix bindMatrix = pNode->EvaluateGlobalTransform();
+    auto s = bindMatrix.GetS();
+    auto t = bindMatrix.GetT();
+    zeno::log_info("s {} {} {}", s[0], s[1], s[2]);
+    zeno::log_info("t {} {} {}", t[0], t[1], t[2]);
+
+    int numVertices = pMesh->GetControlPointsCount();
+    FbxVector4* vertices = pMesh->GetControlPoints();
+    prim->verts.resize(numVertices);
+
+    for (int i = 0; i < numVertices; ++i) {
+        auto pos = bindMatrix.MultT( FbxVector4(vertices[i][0], vertices[i][1], vertices[i][2], 1.0));
+        prim->verts[i] = vec3f(pos[0], pos[1], pos[2]);
+        zeno::log_info("ss {}", vertices[i][3]);
+    }
+    int numPolygons = pMesh->GetPolygonCount();
+    prim->polys.resize(numPolygons);
+    std::vector<int> loops;
+    loops.reserve(numPolygons * 4);
+    int count = 0;
+    for (int i = 0; i < numPolygons; ++i) {
+        int numVertices = pMesh->GetPolygonSize(i);
+        for (int j = 0; j < numVertices; ++j) {
+            int vertexIndex = pMesh->GetPolygonVertex(i, j);
+            loops.push_back(vertexIndex);
+        }
+        prim->polys[i] = {count, numVertices};
+        count += numVertices;
+    }
+    loops.shrink_to_fit();
+    prim->loops.values = loops;
+    zeno::log_info("pMesh->GetDeformerCount(FbxDeformer::eSkin) {}", pMesh->GetDeformerCount(FbxDeformer::eSkin));
+    auto &ud = prim->userData();
+    if (pMesh->GetDeformerCount(FbxDeformer::eSkin)) {
+        auto &bi = prim->verts.add_attr<vec4i>("boneName");
+        std::fill(bi.begin(), bi.end(), vec4i(-1, -1, -1, -1));
+        auto &bw = prim->verts.add_attr<vec4f>("boneWeight");
+        std::fill(bw.begin(), bw.end(), vec4f(-1.0, -1.0, -1.0, -1.0));
+
+        FbxSkin* pSkin = (FbxSkin*)pMesh->GetDeformer(0, FbxDeformer::eSkin);
+        std::vector<std::string> bone_names;
+
+        // Iterate over each cluster (bone)
+        for (int j = 0; j < pSkin->GetClusterCount(); ++j) {
+            FbxCluster* pCluster = pSkin->GetCluster(j);
+
+            // Get the link node (bone)
+            FbxNode* pBoneNode = pCluster->GetLink();
+            if (!pBoneNode) continue;
+
+            // Get the bone weights
+            int numIndices = pCluster->GetControlPointIndicesCount();
+            int* indices = pCluster->GetControlPointIndices();
+            double* weights = pCluster->GetControlPointWeights();
+
+            bone_names.emplace_back(pBoneNode->GetName());
+            for (int k = 0; k < numIndices; ++k) {
+                for (auto l = 0; l < 4; l++) {
+                    if (bi[indices[k]][l] == -1) {
+                        bi[indices[k]][l] = j;
+                        bw[indices[k]][l] = weights[k];
+                        break;
+                    }
+                }
+            }
+        }
+        ud.set2("boneName_count", int(bone_names.size()));
+        for (auto i = 0; i < bone_names.size(); i++) {
+            ud.set2(zeno::format("boneName_{}", i), bone_names[i]);
+        }
+    }
+    return true;
+}
+
+struct NewFBXImportSkin : INode {
+    virtual void apply() override {
+        // Change the following filename to a suitable filename value.
+        auto lFilename = get_input2<std::string>("path");
+
+        // Initialize the SDK manager. This object handles all our memory management.
+        FbxManager* lSdkManager = FbxManager::Create();
+
+        // Create the IO settings object.
+        FbxIOSettings *ios = FbxIOSettings::Create(lSdkManager, IOSROOT);
+        lSdkManager->SetIOSettings(ios);
+
+        // Create an importer using the SDK manager.
+        FbxImporter* lImporter = FbxImporter::Create(lSdkManager,"");
+
+        // Use the first argument as the filename for the importer.
+        if(!lImporter->Initialize(lFilename.c_str(), -1, lSdkManager->GetIOSettings())) {
+            printf("Call to FbxImporter::Initialize() failed.\n");
+            printf("Error returned: %s\n\n", lImporter->GetStatus().GetErrorString());
+            exit(-1);
+        }
+        int major, minor, revision;
+        lImporter->GetFileVersion(major, minor, revision);
+
+        // Create a new scene so that it can be populated by the imported file.
+        FbxScene* lScene = FbxScene::Create(lSdkManager,"myScene");
+
+        // Import the contents of the file into the scene.
+        lImporter->Import(lScene);
+
+        // The file is imported; so get rid of the importer.
+        lImporter->Destroy();
+
+        // Print the nodes of the scene and their attributes recursively.
+        // Note that we are not printing the root node because it should
+        // not contain any attributes.
+        auto prim = std::make_shared<PrimitiveObject>();
+        auto &ud = prim->userData();
+        ud.set2("version", vec3i(major, minor, revision));
+        FbxNode* lRootNode = lScene->GetRootNode();
+        if(lRootNode) {
+            for(int i = 0; i < lRootNode->GetChildCount(); i++) {
+                if (GetMesh(lRootNode->GetChild(i), prim)) {
+                    break;
+                }
+            }
+        }
+        if (get_input2<bool>("ConvertUnits")) {
+            for (auto & v: prim->verts) {
+                v = v * 0.01;
+            }
+        }
+        set_output("prim", prim);
+        // Destroy the SDK manager and all the other objects it was handling.
+        lSdkManager->Destroy();
+    }
+};
+
+ZENDEFNODE(NewFBXImportSkin, {
+    {
+        {"readpath", "path"},
+        {"bool", "ConvertUnits", "1"},
+    },
+    {
+        "prim",
+    },
+    {},
+    {"primitive"},
+});
+
+struct NewFBXImportSkeleton : INode {
+    virtual void apply() override {
+        // Change the following filename to a suitable filename value.
+        auto lFilename = get_input2<std::string>("path");
+
+        // Initialize the SDK manager. This object handles all our memory management.
+        FbxManager* lSdkManager = FbxManager::Create();
+
+        // Create the IO settings object.
+        FbxIOSettings *ios = FbxIOSettings::Create(lSdkManager, IOSROOT);
+        lSdkManager->SetIOSettings(ios);
+
+        // Create an importer using the SDK manager.
+        FbxImporter* lImporter = FbxImporter::Create(lSdkManager,"");
+
+        // Use the first argument as the filename for the importer.
+        if(!lImporter->Initialize(lFilename.c_str(), -1, lSdkManager->GetIOSettings())) {
+            printf("Call to FbxImporter::Initialize() failed.\n");
+            printf("Error returned: %s\n\n", lImporter->GetStatus().GetErrorString());
+            exit(-1);
+        }
+        int major, minor, revision;
+        lImporter->GetFileVersion(major, minor, revision);
+
+        // Create a new scene so that it can be populated by the imported file.
+        FbxScene* lScene = FbxScene::Create(lSdkManager,"myScene");
+
+        // Import the contents of the file into the scene.
+        lImporter->Import(lScene);
+
+        // The file is imported; so get rid of the importer.
+        lImporter->Destroy();
+
+        // Print the nodes of the scene and their attributes recursively.
+        // Note that we are not printing the root node because it should
+        // not contain any attributes.
+        auto prim = std::make_shared<PrimitiveObject>();
+        auto &ud = prim->userData();
+        ud.set2("version", vec3i(major, minor, revision));
+
+        auto pose_count = lScene->GetPoseCount();
+        bool found_bind_pose = false;
+        for (auto i = 0; i < pose_count; i++) {
+            auto pose = lScene->GetPose(i);
+            if (pose == nullptr || !pose->IsBindPose()) {
+                continue;
+            }
+            found_bind_pose = true;
+        }
+        if (found_bind_pose == false) {
+            lSdkManager->CreateMissingBindPoses(lScene);
+        }
+        pose_count = lScene->GetPoseCount();
+        for (auto i = 0; i < pose_count; i++) {
+            auto pose = lScene->GetPose(i);
+            if (pose == nullptr || !pose->IsBindPose()) {
+                continue;
+            }
+            std::string name = pose->GetName();
+            prim->verts.resize(pose->GetCount());
+            std::vector<std::string> bone_names;
+            auto &boneNames = prim->verts.add_attr<int>("boneName");
+            auto &transform_r0 = prim->verts.add_attr<vec3f>("transform_r0");
+            auto &transform_r1 = prim->verts.add_attr<vec3f>("transform_r1");
+            auto &transform_r2 = prim->verts.add_attr<vec3f>("transform_r2");
+            for (int j = 0; j < pose->GetCount(); ++j) {
+                FbxMatrix transformMatrix = pose->GetMatrix(j);
+                auto t = transformMatrix.GetRow(3);
+                prim->verts[j] = vec3f(t[0], t[1], t[2]);
+
+                auto r0 = transformMatrix.GetRow(0);
+                auto r1 = transformMatrix.GetRow(1);
+                auto r2 = transformMatrix.GetRow(2);
+                transform_r0[j] = vec3f(r0[0], r0[1], r0[2]);
+                transform_r1[j] = vec3f(r1[0], r1[1], r1[2]);
+                transform_r2[j] = vec3f(r2[0], r2[1], r2[2]);
+
+                bone_names.emplace_back(pose->GetNode(j)->GetName());
+                boneNames[j] = j;
+            }
+            ud.set2("boneName_count", int(bone_names.size()));
+            for (auto i = 0; i < bone_names.size(); i++) {
+                ud.set2(zeno::format("boneName_{}", i), bone_names[i]);
+            }
+        }
+
+        if (get_input2<bool>("ConvertUnits")) {
+            for (auto & v: prim->verts) {
+                v = v * 0.01;
+            }
+        }
+        set_output("prim", prim);
+        // Destroy the SDK manager and all the other objects it was handling.
+        lSdkManager->Destroy();
+    }
+};
+
+ZENDEFNODE(NewFBXImportSkeleton, {
+    {
+        {"readpath", "path"},
+        {"bool", "ConvertUnits", "1"},
+    },
+    {
+        "prim",
+    },
+    {},
+    {"primitive"},
+});
+}
 #endif
