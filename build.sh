802fc6e9bf80de3b11ba64f3e2c359ed0bdbce5a
iaomw <iaomw@live.com>
2024-03-18 16:51:21 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,28 @@
-# [ "$1" ] && [ $1 = Debug ] && flag="Debug" || flag="Release"
-# echo $flag
+[ "$1" ] && [ $1 = full ] && pack="full" || pack="mini"
 
-# cmake -B build -DCMAKE_BUILD_TYPE=$flag \
-#     -DZENO_MULTIPROCESS=ON \
-#     -DZENO_WITH_zenvdb:BOOL=ON \
-#     -DZENO_SYSTEM_OPENVDB=OFF \
-#     -DZENO_WITH_ZenoFX:BOOL=ON \
-#     -DZENO_ENABLE_OPTIX:BOOL=ON \
-#     -DZENO_WITH_FBX:BOOL=ON \
-#     -DZENO_WITH_Alembic:BOOL=ON \
-#     -DZENO_WITH_MeshSubdiv:BOOL=ON \
-#     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+[ "$2" ] && [ $2 = debug ] && flag="debug" || flag="release"
+echo $flag
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release \
+if [ $pack != "full" ]; then
+
+echo "making minimum build..."
+
+cmake -B build -DCMAKE_BUILD_TYPE=$flag \
+    -DZENO_MULTIPROCESS=ON \
+    -DZENO_WITH_zenvdb:BOOL=ON \
+    -DZENO_SYSTEM_OPENVDB=OFF \
+    -DZENO_WITH_ZenoFX:BOOL=ON \
+    -DZENO_ENABLE_OPTIX:BOOL=ON \
+    -DZENO_WITH_FBX:BOOL=ON \
+    -DZENO_WITH_Alembic:BOOL=ON \
+    -DZENO_WITH_MeshSubdiv:BOOL=ON \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+
+else
+
+echo "making full build..."
+
+cmake -B build -DCMAKE_BUILD_TYPE=$flag \
     -DZENO_WITH_ZenoFX:BOOL=ON \
     -DZENO_ENABLE_OPTIX:BOOL=ON \
     -DZENO_SYSTEM_OPENVDB=OFF \
@@ -41,10 +51,10 @@ cmake -B build -DCMAKE_BUILD_TYPE=Release \
     -DZENO_WITH_GUI:BOOL=ON \
     -DZENO_WITH_ImgCV:BOOL=ON \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+fi
 
 cmake --build build --parallel $(nproc) \
 
 # ln -s ./build/compile_commands.json ./
-# cp ./build/compile_commands.json ./
-
-# rm -rf /var/tmp/OptixCache_$USER/
+cp ./build/compile_commands.json ./
+rm -rf /var/tmp/OptixCache_$USER/
