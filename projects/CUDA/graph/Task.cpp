322dadda76fb4fb71046d5de94e2507113e83160
littlemine <wxlwxl1993@zju.edu.cn>
2024-04-02 13:00:06 +0800

    upd jit, WIP py parsing

--- projects/CUDA/graph/Task.cpp
+++ projects/CUDA/graph/Task.cpp
@@ -1,3 +1,6 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
 #include "zensim/ZpcTuple.hpp"
 #include "zensim/zpc_tpls/fmt/format.h"
 #include <cctype>
@@ -30,6 +33,47 @@ struct WorkNode : IObject {
   std::map<std::string, std::shared_ptr<WorkNode>> deps;
 };
 
+namespace detail {
+void python_evaluate(const std::string& fmtStr, const std::vector<const char*> args) {
+// #if defined(ZENO_WITH_PYTHON3) or ZS_PYTHON_FOUND
+    std::string evalStmt = "print(\'" + fmtStr + "\'";
+    if (args.size()) {
+      evalStmt += ").format("+std::string(args[0]);
+      for (int i = 1; i != args.size(); ++i)
+          evalStmt += ", " + std::string(args[i]);
+      evalStmt += ")";
+    }
+    evalStmt += ")";
+
+    PyRun_SimpleString(
+        "import sys\n"
+        "from io import StringIO\n"
+        "old_stdout = sys.stdout\n"  // Backup the original stdout
+        "sys.stdout = StringIO()\n"  // Replace stdout with a StringIO object to capture outputs
+    );
+    PyRun_SimpleString(evalStmt.data());
+    fmt::print("assembled cmd str: {}\n", evalStmt);
+
+    PyObject *sys = PyImport_ImportModule("sys");
+    PyObject *stdout = PyObject_GetAttrString(sys, "stdout");
+    PyObject *output = PyObject_GetAttrString(stdout, "getvalue");
+    PyObject *result = PyObject_CallObject(output, NULL);
+
+    // Convert the captured output to a C++ string
+    const char* result_cstr = PyUnicode_AsUTF8(result);
+    std::string result_str(result_cstr);
+
+    // Print the captured output in C++
+    std::cout << "Captured Python output: " << result_str << std::endl;
+
+    // Restore the original stdout
+    PyRun_SimpleString("sys.stdout = old_stdout");
+
+    // Finalize the Python interpreter
+    Py_Finalize();
+}
+}
+
 struct CommandGenerator : INode {
   void apply() override {
     auto tag = get_input2<std::string>("name_tag");
@@ -37,9 +81,16 @@ struct CommandGenerator : INode {
       throw std::runtime_error("work name must not be empty!");
 
     auto cmdFmtStr = get_input2<std::string>("cmd_fmt_string");
-    auto cmd = fmt::format(cmdFmtStr, 0);
 
+    auto args = get_input<ListObject>("arguments");
+    std::vector<const char*> as;
+    for (auto &&arg : args->get())
+      if (auto ptr = dynamic_cast<StringObject *>(arg.get()); ptr != nullptr)
+        as.push_back(ptr->get().c_str());
+    // auto cmd = fmt::format(cmdFmtStr, as);
     // fmt::print("parsed: [[{}]]\n", cmd);
+    detail::python_evaluate(cmdFmtStr, as);
+
     auto ret = std::make_shared<WorkNode>();
 
     set_output("job", ret);
