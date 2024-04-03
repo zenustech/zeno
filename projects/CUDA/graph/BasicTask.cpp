f1092fe5e21573ed8309a5e2d439027071fc21ee
littlemine <wxlwxl1993@zju.edu.cn>
2024-04-03 19:46:21 +0800

    isolate py impl portion

---
+++ projects/CUDA/graph/BasicTask.cpp
@@ -0,0 +1,162 @@
+#include "Task.hpp"
+#include <tinygltf/json.hpp>
+
+namespace zeno {
+
+struct WriteTaskDependencyGraph : INode {
+  using Json = nlohmann::json;
+  static void process_node(std::vector<Json> &jsons, WorkNode *node,
+                           std::set<WorkNode *> &records) {
+    if (records.find(node) != records.end())
+      return;
+    records.insert(node);
+
+    for (auto &&[tag, node] : node->deps)
+      process_node(jsons, node.get(), records);
+
+    Json json;
+    json["name"] = node->tag;
+    json["cmds"] = node->workItems;
+    std::vector<std::string> depWorkNames;
+    for (auto &&[tag, node] : node->deps)
+      depWorkNames.push_back(node->tag);
+    json["deps"] = depWorkNames;
+    Json j;
+    j[node->tag] = json;
+    jsons.push_back(j);
+  }
+  void apply() override {
+    std::vector<WorkNode *> nodes;
+    auto jobs = get_input("job");
+    if (auto ptr = std::dynamic_pointer_cast<WorkNode>(jobs); ptr)
+      nodes.push_back(ptr.get());
+    else if (auto list = std::dynamic_pointer_cast<ListObject>(jobs); list) {
+      for (auto &&arg : list->get())
+        if (auto ptr = std::dynamic_pointer_cast<WorkNode>(arg); ptr)
+          nodes.push_back(ptr.get());
+    }
+    auto filename = get_input2<std::string>("json_file_path");
+
+    std::vector<Json> jsons;
+
+    std::set<WorkNode *> records;
+    for (auto &node : nodes)
+      process_node(jsons, node, records);
+
+    Json json = Json(jsons);
+
+    std::ofstream file(filename);
+    if (file.is_open()) {
+      // dump(4) prints the JSON data with an indentation of 4 spaces
+      file << json.dump(4);
+      file.close();
+      // fmt::print("Task Dependency Graph [{}] written to {} in json\n",
+      //           node->tag, filename);
+    } else {
+      throw std::runtime_error(
+          fmt::format("Could not open file [{}] for writing.", filename));
+    }
+    set_output("job", jobs);
+  }
+};
+ZENO_DEFNODE(WriteTaskDependencyGraph)
+({/* inputs: */
+  {
+      {"list", "job"},
+      {"writepath", "json_file_path", ""},
+  },
+  /* outputs: */
+  {
+      {"job"},
+  },
+  /* params: */
+  {},
+  /* category: */
+  {
+      "task",
+  }});
+
+struct AssembleJob : INode {
+  void apply() override {
+    auto ret = std::make_shared<WorkNode>();
+
+    auto tag = get_input2<std::string>("name_tag");
+    if (tag.empty())
+      throw std::runtime_error("work name must not be empty!");
+    ret->tag = tag;
+
+    std::vector<std::string> workItems;
+    auto cmds = get_input("scripts");
+    if (auto ptr = std::dynamic_pointer_cast<StringObject>(cmds); ptr)
+      workItems.push_back(ptr->get());
+    else if (auto list = std::dynamic_pointer_cast<ListObject>(cmds); list) {
+      for (auto &&arg : list->get())
+        if (auto ptr = std::dynamic_pointer_cast<StringObject>(arg); ptr)
+          workItems.push_back(ptr->get());
+    }
+    ret->workItems = workItems;
+
+    auto deps = has_input("dependencies")
+                    ? get_input<ListObject>("dependencies")
+                    : std::make_shared<ListObject>();
+    for (auto &&arg : deps->get())
+      if (auto ptr = std::dynamic_pointer_cast<WorkNode>(arg); ptr)
+        ret->deps[ptr->tag] = ptr;
+
+    set_output("job", ret);
+  }
+};
+ZENO_DEFNODE(AssembleJob)
+({/* inputs: */
+  {
+      {"string", "name_tag"},
+      {"list", "scripts"},
+      {"list", "dependencies"},
+  },
+  /* outputs: */
+  {
+      {"WorkNode", "job"},
+  },
+  /* params: */
+  {},
+  /* category: */
+  {
+      "task",
+  }});
+
+struct SetWorkDependencies : INode {
+  void apply() override {
+    auto node = get_input<WorkNode>("job");
+    auto reset = get_input2<bool>("reset");
+    if (reset)
+      node->deps.clear();
+
+    auto deps = has_input("dependencies")
+                    ? get_input<ListObject>("dependencies")
+                    : std::make_shared<ListObject>();
+    for (auto &&arg : deps->get())
+      if (auto ptr = std::dynamic_pointer_cast<WorkNode>(arg); ptr)
+        node->deps[ptr->tag] = ptr;
+
+    set_output("job", node);
+  }
+};
+ZENO_DEFNODE(SetWorkDependencies)
+({/* inputs: */
+  {
+      {"WorkNode", "job"},
+      {"list", "dependencies"},
+      {"bool", "reset", "false"},
+  },
+  /* outputs: */
+  {
+      {"WorkNode", "job"},
+  },
+  /* params: */
+  {},
+  /* category: */
+  {
+      "task",
+  }});
+
+} // namespace zeno
--- projects/CUDA/graph/Task.cpp
+++ projects/CUDA/graph/Task.cpp
@@ -1,44 +1,9 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-#include "zensim/ZpcReflection.hpp"
-#include "zensim/ZpcTuple.hpp"
-#include "zensim/types/Polymorphism.h"
-#include "zensim/zpc_tpls/fmt/format.h"
-#include <cctype>
-#include <filesystem>
-#include <fstream>
-#include <random>
-#include <sstream>
-
-#include <tinygltf/json.hpp>
-
-#include <zeno/core/Graph.h>
-#include <zeno/extra/assetDir.h>
-#include <zeno/funcs/PrimitiveUtils.h>
-#include <zeno/types/DictObject.h>
-#include <zeno/types/ListObject.h>
-#include <zeno/types/NumericObject.h>
-#include <zeno/types/PrimitiveObject.h>
-#include <zeno/types/StringObject.h>
-#include <zeno/types/UserData.h>
-#include <zeno/utils/log.h>
-#include <zeno/utils/string.h>
-#include <zeno/zeno.h>
+#include "Task.hpp"
 
 namespace zeno {
-
-using GenericWorkAttribute =
-    std::variant<zs::tuple<int, int, int>, std::vector<int>>;
-
-struct WorkNode : IObject {
-  //
-  std::string tag;
-  std::vector<std::string> workItems;
-  std::map<std::string, std::shared_ptr<WorkNode>> deps;
-  // reserved
-  std::map<std::string, GenericWorkAttribute> attributes;
-};
 
 namespace detail {
 std::string python_evaluate(const std::string &fmtStr,
@@ -287,79 +252,6 @@ ZENO_DEFNODE(CommandGenerator)
       "task",
   }});
 
-struct WriteTaskDependencyGraph : INode {
-  using Json = nlohmann::json;
-  static void process_node(std::vector<Json> &jsons, WorkNode *node,
-                           std::set<WorkNode *> &records) {
-    if (records.find(node) != records.end())
-      return;
-    records.insert(node);
-
-    for (auto &&[tag, node] : node->deps)
-      process_node(jsons, node.get(), records);
-
-    Json json;
-    json["name"] = node->tag;
-    json["cmds"] = node->workItems;
-    std::vector<std::string> depWorkNames;
-    for (auto &&[tag, node] : node->deps)
-      depWorkNames.push_back(node->tag);
-    json["deps"] = depWorkNames;
-    Json j;
-    j[node->tag] = json;
-    jsons.push_back(j);
-  }
-  void apply() override {
-    std::vector<WorkNode *> nodes;
-    auto jobs = get_input("job");
-    if (auto ptr = std::dynamic_pointer_cast<WorkNode>(jobs); ptr)
-      nodes.push_back(ptr.get());
-    else if (auto list = std::dynamic_pointer_cast<ListObject>(jobs); list) {
-      for (auto &&arg : list->get())
-        if (auto ptr = std::dynamic_pointer_cast<WorkNode>(arg); ptr)
-          nodes.push_back(ptr.get());
-    }
-    auto filename = get_input2<std::string>("json_file_path");
-
-    std::vector<Json> jsons;
-
-    std::set<WorkNode *> records;
-    for (auto &node : nodes)
-      process_node(jsons, node, records);
-
-    Json json = Json(jsons);
-
-    std::ofstream file(filename);
-    if (file.is_open()) {
-      // dump(4) prints the JSON data with an indentation of 4 spaces
-      file << json.dump(4);
-      file.close();
-      // fmt::print("Task Dependency Graph [{}] written to {} in json\n",
-      //           node->tag, filename);
-    } else {
-      throw std::runtime_error(
-          fmt::format("Could not open file [{}] for writing.", filename));
-    }
-    set_output("job", jobs);
-  }
-};
-ZENO_DEFNODE(WriteTaskDependencyGraph)
-({/* inputs: */
-  {
-      {"list", "job"},
-      {"writepath", "json_file_path", ""},
-  },
-  /* outputs: */
-  {
-      {"job"},
-  },
-  /* params: */
-  {},
-  /* category: */
-  {
-      "task",
-  }});
-
 struct CapturePyScriptOutput : INode {
   void apply() override {
     auto script = get_input2<std::string>("script");
@@ -388,89 +280,6 @@ ZENO_DEFNODE(CapturePyScriptOutput)
   /* outputs: */
   {
       {"string", "output"},
-  },
-  /* params: */
-  {},
-  /* category: */
-  {
-      "task",
-  }});
-
-struct AssembleJob : INode {
-  void apply() override {
-    auto ret = std::make_shared<WorkNode>();
-
-    auto tag = get_input2<std::string>("name_tag");
-    if (tag.empty())
-      throw std::runtime_error("work name must not be empty!");
-    ret->tag = tag;
-
-    std::vector<std::string> workItems;
-    auto cmds = get_input("scripts");
-    if (auto ptr = std::dynamic_pointer_cast<StringObject>(cmds); ptr)
-      workItems.push_back(ptr->get());
-    else if (auto list = std::dynamic_pointer_cast<ListObject>(cmds); list) {
-      for (auto &&arg : list->get())
-        if (auto ptr = std::dynamic_pointer_cast<StringObject>(arg); ptr)
-          workItems.push_back(ptr->get());
-    }
-    ret->workItems = workItems;
-
-    auto deps = has_input("dependencies")
-                    ? get_input<ListObject>("dependencies")
-                    : std::make_shared<ListObject>();
-    for (auto &&arg : deps->get())
-      if (auto ptr = std::dynamic_pointer_cast<WorkNode>(arg); ptr)
-        ret->deps[ptr->tag] = ptr;
-
-    set_output("job", ret);
-  }
-};
-ZENO_DEFNODE(AssembleJob)
-({/* inputs: */
-  {
-      {"string", "name_tag"},
-      {"list", "scripts"},
-      {"list", "dependencies"},
-  },
-  /* outputs: */
-  {
-      {"WorkNode", "job"},
-  },
-  /* params: */
-  {},
-  /* category: */
-  {
-      "task",
-  }});
-
-struct SetWorkDependencies : INode {
-  void apply() override {
-    auto node = get_input<WorkNode>("job");
-    auto reset = get_input2<bool>("reset");
-    if (reset)
-      node->deps.clear();
-
-    auto deps = has_input("dependencies")
-                    ? get_input<ListObject>("dependencies")
-                    : std::make_shared<ListObject>();
-    for (auto &&arg : deps->get())
-      if (auto ptr = std::dynamic_pointer_cast<WorkNode>(arg); ptr)
-        node->deps[ptr->tag] = ptr;
-
-    set_output("job", node);
-  }
-};
-ZENO_DEFNODE(SetWorkDependencies)
-({/* inputs: */
-  {
-      {"WorkNode", "job"},
-      {"list", "dependencies"},
-      {"bool", "reset", "false"},
-  },
-  /* outputs: */
-  {
-      {"WorkNode", "job"},
   },
   /* params: */
   {},
