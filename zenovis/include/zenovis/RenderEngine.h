d5906ceb819e27e038e24c5dde870b31aa11e3f9
luzh <luzh16371@gmail.com>
2024-04-19 15:45:44 +0800

    add interface cleanupOptix into RenderEngine.

--- zenovis/include/zenovis/RenderEngine.h
+++ zenovis/include/zenovis/RenderEngine.h
@@ -15,6 +15,7 @@ struct Scene;
 struct RenderEngine {
     virtual void draw(bool record) = 0;
     virtual void update() = 0;
+    virtual void cleanupOptix() = 0;
 
     virtual ~RenderEngine() = default;
 };
