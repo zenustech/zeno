3e1ccc52cdb7a7875f00d8a1dec5a7e6d8f5407a
zhuohy <1445643474@qq.com>
2024-05-14 16:41:57 +0800

    add cache shortcut

--- ui/zenoedit/nodesys/zenosubgraphscene.h
+++ ui/zenoedit/nodesys/zenosubgraphscene.h
@@ -75,7 +75,7 @@ private:
     void onTempLinkClosed();
     ZenoNode* createNode(const QModelIndex& idx, const NodeUtilParam& params);
     void initLink(const QModelIndex& linkIdx);
-    void updateNodeStatus(bool &bOn, int option);
+    void updateNodeStatus(int option);
 
     NodeUtilParam m_nodeParams;
     QPersistentModelIndex m_subgIdx;      //index to the subgraphmodel or node in "graphsModel"
@@ -85,10 +85,6 @@ private:
     ZenoTempLink* m_tempLink;
 
     QVector<QPair<QString, bool>> m_selChanges;
-
-    bool m_bOnceOn;
-    bool m_bBypassOn;
-    bool m_bViewOn;
 };
 
 #endif
