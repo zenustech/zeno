1f6a6d72301c243aea40b3ed5d3544e01220f711
luzh <luzh16371@gmail.com>
2024-05-29 11:19:29 +0800

    miss commit coreparam.cpp

---
+++ zenocore/src/core/CoreParam.cpp
@@ -0,0 +1,35 @@
+#include <zeno/core/CoreParam.h>
+#include <zeno/core/INode.h>
+#include <zeno/utils/helper.h>
+
+namespace zeno {
+
+    ParamObject ObjectParam::exportParam() const {
+        ParamObject objparam;
+        objparam.name = name;
+        objparam.bInput = bInput;
+        objparam.type = type;
+        objparam.socketType = socketType;
+        for (auto spLink : links) {
+            objparam.links.push_back(getEdgeInfo(spLink));
+        }
+        return objparam;
+    }
+
+    ParamPrimitive PrimitiveParam::exportParam() const {
+        ParamPrimitive param;
+        param.name = name;
+        param.bInput = bInput;
+        param.type = type;
+        param.socketType = socketType;
+        param.defl = defl;
+        param.result = result;
+        param.control = control;
+        param.ctrlProps = optCtrlprops;
+        for (auto spLink : links) {
+            param.links.push_back(getEdgeInfo(spLink));
+        }
+        return param;
+    }
+
+}
