73cb68c7e9bbd76bc0956e30debc2eb84fbf0d5d
luzh <luzh16371@gmail.com>
2024-07-19 16:04:11 +0800

    meet some mistake, remove the ZenoReflect first

--- zenoreflect/cmake/FixWindows.cmake
+++
@@ -1,3 +0,0 @@
-
-add_compile_options("$<$<C_COMPILER_ID:MSVC>:/utf-8>")
-add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/utf-8>")
--- zenoreflect/src/args.cpp
+++
@@ -1,7 +0,0 @@
-#include "args.hpp"
-
-ControlFlags parse_args(int argc, char* argv[]) {
-    return argparse::parse<ControlFlags>(argc, argv);
-}
-
-ControlFlags* GLOBAL_CONTROL_FLAGS = nullptr;
