a38bed7c8f0de27cd5945a533e41e663e3ed9c6a
zhouhang95 <765229842@qq.com>
2024-09-26 20:29:49 +0800

    param

--- projects/Nemo/node.cpp
+++ projects/Nemo/node.cpp
@@ -7,6 +7,7 @@
 #include <zeno/extra/GlobalState.h>
 #include <zeno/utils/string.h>
 #include <zeno/utils/bit_operations.h>
+#include <zeno/types/PrimitiveUtils.h>
 #include <boost/algorithm/string.hpp>
 
 #include "Evaluate.h"
@@ -44,6 +45,51 @@ ZENDEFNODE(NemoEvaluator, {
     { "Nemo" },
 });
 
+static std::shared_ptr<ListObject> nemo_split_by_name(std::shared_ptr<PrimitiveObject> prim, bool add_when_none) {
+    auto list = std::make_shared<ListObject>();
+    if (prim->verts.size() == 0) {
+        return list;
+    }
+    int faceset_count = prim->userData().get2<int>("faceset_count");
+    if (add_when_none && faceset_count == 0) {
+        auto name = prim->userData().get2<std::string>("abcpath_0");
+        prim_set_faceset(prim.get(), name);
+        faceset_count = 1;
+    }
+    std::map<int, std::vector<int>> faceset_map;
+    for (auto f = 0; f < faceset_count; f++) {
+        faceset_map[f] = {};
+    }
+    if (prim->polys.size()) {
+        auto &faceset = prim->polys.add_attr<int>("faceset");
+        for (auto j = 0; j < faceset.size(); j++) {
+            auto f = faceset[j];
+            faceset_map[f].push_back(j);
+        }
+        for (auto f = 0; f < faceset_count; f++) {
+            auto name = prim->userData().get2<std::string>(zeno::format("faceset_{}", f));
+            auto new_prim = std::dynamic_pointer_cast<PrimitiveObject>(prim->clone());
+            new_prim->polys.resize(faceset_map[f].size());
+            for (auto i = 0; i < faceset_map[f].size(); i++) {
+                new_prim->polys[i] = prim->polys[faceset_map[f][i]];
+            }
+            new_prim->polys.foreach_attr<AttrAcceptAll>([&](auto const &key, auto &arr) {
+                using T = std::decay_t<decltype(arr[0])>;
+                auto &attr = prim->polys.attr<T>(key);
+                for (auto i = 0; i < arr.size(); i++) {
+                    arr[i] = attr[faceset_map[f][i]];
+                }
+            });
+            for (auto j = 0; j < faceset_count; j++) {
+                new_prim->userData().del(zeno::format("faceset_{}", j));
+            }
+            prim_set_faceset(new_prim.get(), name);
+            list->arr.push_back(new_prim);
+        }
+    }
+    return list;
+}
+
 struct NemoPlay : INode {
     virtual void apply() override {
         auto evaluator = get_input2<NemoObject>("Evaluator");
@@ -57,7 +103,7 @@ struct NemoPlay : INode {
         evaluator->evaluator->evaluate(frame);
         bool skipHiddenPrim = get_input2<bool>("skipHiddenPrim");
         std::map<int, std::vector<int>> mapping_mesh_to_faceset;
-        int test_count = evaluator->evaluator->facesets.size();
+
         auto readFaceset = get_input2<bool>("readFaceset");
         if (readFaceset) {
             for (auto i = 0; i < evaluator->evaluator->facesets.size(); i++) {
@@ -70,6 +116,10 @@ struct NemoPlay : INode {
                 }
             }
         }
+
+        auto splitByFaceset = get_input2<bool>("splitByFaceset");
+        auto killDeadVerts = get_input2<bool>("killDeadVerts");
+        auto triangulate = get_input2<bool>("triangulate");
 
         auto prims = std::make_shared<zeno::ListObject>();
         std::map<unsigned, std::string> meshes;
@@ -110,10 +160,10 @@ struct NemoPlay : INode {
                     sub_prim->uvs.emplace_back(uValues[i], vValues[i]);
                 }
             }
+            std::vector<std::string> faceSetNames;
             if (readFaceset) {
                 auto &faceset_attr = sub_prim->polys.add_attr<int>("faceset");
                 std::fill(faceset_attr.begin(), faceset_attr.end(), -1);
-                std::vector<std::string> faceSetNames;
                 if (mapping_mesh_to_faceset.count(plug_id)) {
                     for (auto fi: mapping_mesh_to_faceset[plug_id]) {
                         auto &faceset = evaluator->evaluator->facesets[fi];
@@ -132,7 +182,26 @@ struct NemoPlay : INode {
             }
             prim_set_abcpath(sub_prim.get(), "/ABC"+path);
             sub_prim->userData().set2("vis", int(evaluator->evaluator->isVisible(plug_id)));
-            prims->arr.emplace_back(sub_prim);
+            if (splitByFaceset && faceSetNames.size() >= 2) {
+                auto list = nemo_split_by_name(sub_prim, true);
+                for (auto p: list->arr) {
+                    auto np = std::dynamic_pointer_cast<PrimitiveObject>(p);
+                    if (killDeadVerts) {
+                        primKillDeadVerts(np.get());
+                    }
+                    prims->arr.emplace_back(p);
+                }
+            }
+            else {
+                prims->arr.emplace_back(sub_prim);
+            }
+        }
+
+        if (triangulate) {
+            for (auto &prim: prims->arr) {
+                auto _prim = std::dynamic_pointer_cast<PrimitiveObject>(prim);
+                zeno::primTriangulate(_prim.get());
+            }
         }
 
         set_output2("prims", prims);
@@ -146,6 +215,9 @@ ZENDEFNODE(NemoPlay, {
         { "frame" },
         { "bool", "skipHiddenPrim", "1" },
         { "bool", "readFaceset", "1" },
+        { "bool", "splitByFaceset", "0"},
+        { "bool", "triangulate", "0"},
+        { "bool", "killDeadVerts", "1"},
     },
     {
         "prims",
