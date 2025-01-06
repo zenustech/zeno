edab05b90ac9ffc24096daaeb48f00f5d32fcff9
iaomw <iaomw@live.com>
2025-01-06 20:04:19 +0800

    improve shader nodes

--- zeno/src/nodes/mtl/ShaderTypeCast.cpp
+++ zeno/src/nodes/mtl/ShaderTypeCast.cpp
@@ -2,6 +2,10 @@
 #include <zeno/extra/ShaderNode.h>
 #include <zeno/types/ShaderObject.h>
 
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+
 namespace zeno {
 
 static std::string dataTypeDefaultString() {
@@ -43,8 +47,55 @@ ZENDEFNODE(ShaderTypeCast, {
     },
     {
         {"enum bit_cast data_cast ", "op", "bit_cast"},
-        {"enum" + ShaderDataTypeNamesString, "type", "bool"},
+        {"enum " + ShaderDataTypeNamesString, "type", "bool"},
     },
+    {"shader"},
+});
+
+struct ShaderPrint : ShaderNodeClone<ShaderPrint> {
+    virtual int determineType(EmissionPass *em) override {
+        auto in = get_input("in").get();
+        auto in_type = em->determineType(in);
+        return in_type;
+    }
+
+    virtual void emitCode(EmissionPass *em) override {
+        
+        auto in = get_input("in").get();
+        auto in_type = em->determineType(in);
+        auto in_expr = em->determineExpr(in);
+
+        auto str = get_input2<std::string>("str");
+
+        static const std::map<std::string, std::string> typeTable {
+            {"float", "%f"}, {"bool", "%d"},
+            {"int", "%d"}, {"uint", "%u"},
+            {"int64", "%ll"}, {"uint64", "%llu"}
+        };
+
+        auto typeStr = TypeHintReverse.at(in_type);
+        auto typePri = typeTable.at(typeStr);
+        auto content = str + ": " + typePri + "\\n";
+
+        std::stringstream ss;
+        ss << "printf(";
+        ss << std::quoted(content, '"', '\n');
+        ss << "," << in_expr << ");";
+
+        em->lines.back() += ss.str();
+        em->emitCode(in_expr);
+    }
+};
+
+ZENDEFNODE(ShaderPrint, {
+    {
+        {"in"},
+        {"string", "str", ""}
+    },
+    {
+        {"shader", "out"},
+    },
+    {},
     {"shader"},
 });
 
